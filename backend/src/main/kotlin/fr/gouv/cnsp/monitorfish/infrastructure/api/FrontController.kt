d91cf08899ac308efc3e34605051ec10e4d55f69
Loup Theron <loup-rodney@hotmail.fr>
2020-09-25 15:40:55 +0200

    Add Makefile and static controller in backend

---
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/FrontController.kt
@@ -0,0 +1,15 @@
+package fr.gouv.cnsp.monitorfish.infrastructure.api
+
+import io.swagger.annotations.ApiOperation
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class FrontController {
+
+    @GetMapping("/auth-callback", "/home")
+    @ApiOperation("Get the Single Page Application index file")
+    fun redirectToSPA() : String {
+        return "index.html"
+    }
+}

171d82b69a8d52cdecf4de8b0086a3205466ea55
Loup Theron <loup-rodney@hotmail.fr>
2020-10-27 14:33:30 +0100

    Add another non null receiver

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/outputs/PositionsDataOutput.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/outputs/PositionsDataOutput.kt
@@ -7,23 +7,25 @@ data class PositionsDataOutput(val positions: List<PositionDataOutput>) {
         fun fromPositions(positions: List<Position>): PositionsDataOutput {
             return PositionsDataOutput(
                     positions = positions.map {
-                        PositionDataOutput(
-                                internalReferenceNumber = it.internalReferenceNumber,
-                                IRCS = it.IRCS,
-                                MMSI = it.MMSI,
-                                externalReferenceNumber = it.externalReferenceNumber,
-                                dateTime = it.dateTime,
-                                latitude = it.latitude,
-                                longitude = it.longitude,
-                                vesselName = it.vesselName,
-                                speed = it.speed,
-                                course = it.course,
-                                flagState = it.flagState,
-                                destination = it.destination,
-                                from = it.from,
-                                tripNumber = it.tripNumber,
-                                positionType = it.positionType
-                        )
+                        it?.let {
+                            PositionDataOutput(
+                                    internalReferenceNumber = it.internalReferenceNumber,
+                                    IRCS = it.IRCS,
+                                    MMSI = it.MMSI,
+                                    externalReferenceNumber = it.externalReferenceNumber,
+                                    dateTime = it.dateTime,
+                                    latitude = it.latitude,
+                                    longitude = it.longitude,
+                                    vesselName = it.vesselName,
+                                    speed = it.speed,
+                                    course = it.course,
+                                    flagState = it.flagState,
+                                    destination = it.destination,
+                                    from = it.from,
+                                    tripNumber = it.tripNumber,
+                                    positionType = it.positionType
+                            )
+                        }
                     }
             )
         }

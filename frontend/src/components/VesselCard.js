b9dc1723c82e195e012b342493a9daf0057d5a20
Loup Theron <loup-rodney@hotmail.fr>
2020-11-19 10:10:31 +0100

    Add vessel card on map

---
+++ frontend/src/components/VesselCard.js
@@ -0,0 +1,216 @@
+import React from "react";
+import styled from 'styled-components';
+import ReactCountryFlag from "react-country-flag";
+import {transform} from "ol/proj";
+import {BACKEND_PROJECTION, OPENLAYERS_PROJECTION} from "../domain/map";
+import {toStringHDMS} from "ol/coordinate";
+
+const VesselCard = props => {
+    const showDateTime = dateString => {
+        if (dateString) {
+            const date = new Date(dateString)
+            return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()} ${date.toLocaleTimeString('fr-FR')}`
+        }
+    }
+
+    const getCoordinates = coordinates => {
+        const transformedCoordinates = transform(coordinates, OPENLAYERS_PROJECTION, BACKEND_PROJECTION)
+        const hourCoordinates = toStringHDMS(transformedCoordinates)
+        let nSplit = hourCoordinates.split('N')
+        if (nSplit.length > 1) {
+            return [`${nSplit[0]} N`, nSplit[1]]
+        }
+
+        let sSplit = hourCoordinates.split('S')
+        if (sSplit.length > 1) {
+            return [`${nSplit[0]} S`, nSplit[1]]
+        }
+    }
+
+    return (
+        <>
+            <VesselCardHeader>
+                <ReactCountryFlag countryCode={props.vessel.getProperties().flagState} style={{fontSize: '1.5em', marginLeft: '5px'}}/>{' '}
+                <VesselCardTitle>{props.vessel.getProperties().vesselName ? props.vessel.getProperties().vesselName : 'NOM INCONNU'} {props.vessel.getProperties().flagState ? <>({props.vessel.getProperties().flagState})</> : ''}</VesselCardTitle>
+            </VesselCardHeader>
+            <VesselCardBody>
+                <LatLon>
+                    <FieldName>LATITUDE</FieldName>
+                    <FieldValue>{getCoordinates(props.vessel.getGeometry().getCoordinates())[0]}</FieldValue>
+                    <FieldName>LONGITUDE</FieldName>
+                    <FieldValue>{getCoordinates(props.vessel.getGeometry().getCoordinates())[1]}</FieldValue>
+                </LatLon>
+                <Course>
+                    <FieldName>ROUTE</FieldName>
+                    <FieldValue>{props.vessel.getProperties().course ? <>{props.vessel.getProperties().course}°</> : <NoValue>-</NoValue>}</FieldValue>
+                    <FieldName>VITESSE</FieldName>
+                    <FieldValue>{props.vessel.getProperties().speed ? <>{props.vessel.getProperties().speed} Nds</> : <NoValue>-</NoValue>}</FieldValue>
+                </Course>
+                <Position>
+                    <FieldName>TYPE DE SIGNAL</FieldName>
+                    <FieldValue>{props.vessel.getProperties().positionType ? props.vessel.getProperties().positionType : <NoValue>-</NoValue>}</FieldValue>
+                    <FieldName>DERNIER SIGNAL</FieldName>
+                    <FieldValue>
+                        {
+                            props.vessel.getProperties().dateTime ? <>
+                                    {showDateTime(props.vessel.getProperties().dateTime)}{' '}
+                                    <Gray>CET</Gray></>
+                                : <NoValue>-</NoValue>
+                        }
+
+                    </FieldValue>
+                </Position>
+            </VesselCardBody>
+            <VesselCardBottom>
+                <ColumnOne>
+                    <Row>
+                        <BottomFieldName>CFR</BottomFieldName>
+                        <BottomFieldValue>{props.vessel.getProperties().internalReferenceNumber ? props.vessel.getProperties().internalReferenceNumber : <NoValue>-</NoValue>}</BottomFieldValue>
+                    </Row>
+                    <Row>
+                        <BottomFieldName>XR</BottomFieldName>
+                        <BottomFieldValue>{props.vessel.getProperties().externalReferenceNumber ? props.vessel.getProperties().externalReferenceNumber : <NoValue>-</NoValue>}</BottomFieldValue>
+                    </Row>
+                </ColumnOne>
+                <ColumnTwo>
+                    <Row>
+                        <BottomFieldName>MMSI</BottomFieldName>
+                        <BottomFieldValue>{props.vessel.getProperties().MMSI ? props.vessel.getProperties().MMSI : <NoValue>-</NoValue>}</BottomFieldValue>
+                    </Row>
+                    <Row>
+                        <BottomFieldName>CALL SIGN</BottomFieldName>
+                        <BottomFieldValue>{props.vessel.getProperties().IRCS ? props.vessel.getProperties().IRCS : <NoValue>-</NoValue>}</BottomFieldValue>
+                    </Row>
+                </ColumnTwo>
+            </VesselCardBottom>
+            <TrianglePointer>
+                <TriangleShadow />
+                <Triangle />
+            </TrianglePointer>
+        </>
+    )
+}
+
+const TrianglePointer = styled.div`
+  margin-left:auto;
+    margin-right:auto;
+    height:auto; 
+    width:auto;
+`
+
+const TriangleShadow = styled.div`
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 17px 9px 0 9px;
+  border-color: rgba(5, 5, 94, 0.3) transparent transparent transparent;
+  margin-left: 43.7%;
+  margin-top: -1px;
+  clear: top;
+`
+
+const Triangle = styled.div`
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 15px 8.5px 0 8.5px;
+  border-color: white transparent transparent transparent;
+  margin-left: 44%;
+  margin-top: -1px;
+  clear: top;
+
+`
+
+const NoValue = styled.span`
+  color: grey;
+  font-weight: 300;
+`
+
+const Row = styled.div`
+  margin-top: -5px;
+`
+
+const BottomFieldName = styled.span`
+  color: gray;
+  font-size: 0.8em;
+`
+
+const BottomFieldValue = styled.span`
+  margin-left: 5px;
+  font-size: 0.8em;
+  font-weight: 500;
+`
+
+const ColumnOne = styled.div`
+  width: 50%;
+  order: 1;
+  padding: 10px 5px 5px 15px;
+`
+
+const ColumnTwo = styled.div`
+  width: 50%;
+  order: 2;
+  padding: 10px 5px 5px 15px;
+`
+
+const VesselCardBottom = styled.div`
+  border-top: 1px rgba(5, 5, 94, 0.1) solid;
+  display: flex
+`
+
+const Gray = styled.span`
+  color: gray;
+  font-weight: 300;
+`
+
+const FieldName = styled.div`
+  margin-top: 2px;
+  color: gray;
+  font-size: 0.8em;
+`
+
+const FieldValue = styled.div`
+  font-size: 0.8em;
+  font-weight: 500;
+`
+
+const LatLon = styled.div`
+  width: 120px;
+  order: 1;
+  border-right: 1px rgba(5, 5, 94, 0.1) solid;
+  padding: 5px 5px 5px 5px;
+`
+
+const Course = styled.div`
+  width: 100px;
+  order: 2;
+  border-right: 1px rgba(5, 5, 94, 0.1) solid;
+  padding: 5px 5px 5px 5px;
+`
+
+const Position = styled.div`
+  width: 210px;
+  order: 3;
+  padding: 5px 5px 5px 5px;
+`
+
+const VesselCardHeader = styled.div`
+  background: rgba(5, 5, 94, 0.1);
+  padding: 5px 5px 5px 5px;
+  border-bottom: 1px rgba(5, 5, 94, 0.1) solid;
+`
+
+const VesselCardTitle = styled.span`
+  margin-left: 5px;
+  display: inline-block;
+  vertical-align: middle;
+`
+
+const VesselCardBody = styled.div`
+  display: flex;
+  text-align: center;
+`
+
+export default VesselCard

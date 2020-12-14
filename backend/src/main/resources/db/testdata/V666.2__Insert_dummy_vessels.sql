366f8b1aeba6b887cb4b263c38de29febb0f5ec0
Loup Theron <loup-rodney@hotmail.fr>
2020-12-14 14:21:31 +0100

    Refactor to new vessel table data structure

--- backend/src/main/resources/db/testdata/V666.2__Insert_dummy_vessels.sql
+++ backend/src/main/resources/db/testdata/V666.2__Insert_dummy_vessels.sql
@@ -1,7 +1,7 @@
 INSERT INTO vessels (internal_reference_number, mmsi, ircs, vessel_name, flag_state, width, length, district, district_code, gauge,
                      registry_port, power, vessel_type, sailing_category, sailing_type,
-                     declared_fishing_gear_main, declared_fishing_gear_secondary, declared_fishing_gear_third,
+                     declared_fishing_gears,
                      weight_authorized_on_deck, pinger, navigation_licence_expiration_date, shipowner_name,
-                     shipowner_telephone_number, shipowner_email, fisher_name, fisher_telephone_number, fisher_email) VALUES
-('FR209143000','224103750',null,'LA BOETIE','FR',5.0,12.89, 'Auray', 'AY', 123, 'LORIENT', 127, 'Pêche côtière', '3', 'Pêche', 'Trémail', 'Filets maillants calés', null, 120, false, now() - random() * INTERVAL '5 hours', 'LE PELETIER', '+33 6 84 56 32 14', 'lepeletier@gmail.com', 'EL MALIK', '+33 6 45 25 14', 'elmalik@gmail.com'),
-('FR263418260','224103750',null,'LE b@TO','FR',6.0,14, 'Lorient', 'LT', 123, 'BREST', 237, 'Pêche côtière', '3', 'Pêche', 'Senne', 'Chalut de fond', 'Filets maillants calés', 135, true,  now(), 'DUPOND', '+33 6 84 56 32 14', 'dupond@gmail.com', 'DURAND', '+33 6 45 25 14', 'durand@gmail.com');
+                     shipowner_phones, shipowner_emails, fisher_name, fisher_phones, fisher_emails) VALUES
+('FR209143000','224103750',null,'LA BOETIE','FR',5.0,12.89, 'Auray', 'AY', 123, 'LORIENT', 127, 'Pêche côtière', '3', 'Pêche', ARRAY['Trémail', 'Filets maillants calés'],  120, false, now() - random() * INTERVAL '5 hours', 'LE PELETIER', ARRAY['+33 6 84 56 32 14'], ARRAY['lepeletier@gmail.com'], 'EL MALIK', ARRAY['+33 6 45 25 14'], ARRAY['elmalik@gmail.com']),
+('FR263418260','224103750',null,'LE b@TO','FR',6.0,14, 'Lorient', 'LT', 123, 'BREST', 237, 'Pêche côtière', '3', 'Pêche', ARRAY['Senne', 'Chalut de fond', 'Filets maillants calés'], 135, true,  now(), 'DUPOND', ARRAY['+33 6 84 56 32 14'], ARRAY['dupond@gmail.com'], 'DURAND', ARRAY['+33 6 45 25 14'], ARRAY['durand@gmail.com']);

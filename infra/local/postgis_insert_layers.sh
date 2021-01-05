b8afd444bf47f2be26d3f43f9f41846bbc609889
Loup Theron <loup-rodney@hotmail.fr>
2021-01-05 17:12:58 +0100

    Add layers PROD instructions

--- infra/local/postgis_insert_layers.sh
+++ infra/local/postgis_insert_layers.sh
@@ -3,6 +3,11 @@ set -e
 
 PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f backend/src/main/resources/db/migration/layers/V0.1__Create_fao_areas_table.sql
 PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f backend/src/main/resources/db/migration/layers/V0.2__Create_eez_areas_table.sql
+PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f backend/src/main/resources/db/migration/layers/V0.3__Create_3_miles_areas_table.sql
+PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f backend/src/main/resources/db/migration/layers/V0.4__Create_6_miles_areas_table.sql
+PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f backend/src/main/resources/db/migration/layers/V0.5__Create_12_miles_areas_table.sql
+PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f backend/src/main/resources/db/migration/layers/V0.6__Create_100_miles_areas_table.sql
+PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f backend/src/main/resources/db/migration/layers/V0.7__Create_coast_lines_table.sql
 
 PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f sig/layersdata/Insert_fao_areas.sql
 PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f sig/layersdata/Insert_eez_areas.sql
@@ -11,4 +16,3 @@ PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f sig/layersdata/Insert_6_miles_are
 PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f sig/layersdata/Insert_12_miles_areas.sql
 PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f sig/layersdata/Insert_100_miles_areas.sql
 PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f sig/layersdata/Insert_coast_lines.sql
-PGCLIENTENCODING=LATIN1 psql -d cnsp -U adl -f sig/layersdata/Insert_regulatory_areas.sql

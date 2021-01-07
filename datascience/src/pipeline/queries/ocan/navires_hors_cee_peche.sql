312661c6bd96331a1facde29f8b55f0316619115
Vincent Chéry <vincent.chery@m4x.org>
2021-01-07 18:05:40 +0100

    fix environment bind mount

--- datascience/src/pipeline/queries/ocan/navires_hors_cee_peche.sql
+++ datascience/src/pipeline/queries/ocan/navires_hors_cee_peche.sql
@@ -1,6 +1,6 @@
 SELECT
     id_nav_flotteur AS id_nav_flotteur_nep,
-    eng.libelle as fishing_gear_main_nep
+    eng.code as fishing_gear_main_nep
 FROM NAVPRO.NAV_ETR_PECHE nep
 LEFT JOIN COMMUN.C_PCH_CODE_ENGIN_CE eng
 ON nep.idc_pch_engin_prin = eng.idc_pch_engin_ce

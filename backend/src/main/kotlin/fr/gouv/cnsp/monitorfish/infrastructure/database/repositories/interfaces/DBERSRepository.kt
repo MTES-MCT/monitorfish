4f08c75339557b56e87800841984e8084723d76c
Loup Theron <loup-rodney@hotmail.fr>
2021-02-22 13:42:09 +0100

    Test to make delete working

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/database/repositories/interfaces/DBERSRepository.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/database/repositories/interfaces/DBERSRepository.kt
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.transaction.annotation.Propagation
 import java.time.Instant
 import javax.transaction.Transactional
 

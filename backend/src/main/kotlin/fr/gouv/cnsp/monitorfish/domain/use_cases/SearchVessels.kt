2437de9401c755f9783cbb86256d495b210abc4c
Loup Theron <loup-rodney@hotmail.fr>
2021-03-16 13:34:11 +0100

    [#210] Remove vessels without identifications numbers from search usecase

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/SearchVessels.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/SearchVessels.kt
@@ -7,6 +7,11 @@ import fr.gouv.cnsp.monitorfish.domain.repositories.VesselRepository
 @UseCase
 class SearchVessels(private val vesselRepository: VesselRepository) {
     fun execute(searched: String): List<Vessel> {
-        return vesselRepository.search(searched)
+        return vesselRepository.search(searched).filter {
+            !(it.internalReferenceNumber.isNullOrEmpty() &&
+                    it.externalReferenceNumber.isNullOrEmpty() &&
+                    it.ircs.isNullOrEmpty() &&
+                    it.mmsi.isNullOrEmpty())
+        }
     }
 }
---
+++ backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/SearchVesselsUTests.kt
@@ -0,0 +1,31 @@
+package fr.gouv.cnsp.monitorfish.domain.use_cases
+
+import com.nhaarman.mockitokotlin2.any
+import fr.gouv.cnsp.monitorfish.domain.entities.Vessel
+import fr.gouv.cnsp.monitorfish.domain.repositories.VesselRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+class SearchVesselsUTests {
+
+    @MockBean
+    private lateinit var vesselRepository: VesselRepository
+
+    @Test
+    fun `execute Should return no vessel When the is no identification number`() {
+        // Given
+        given(vesselRepository.search(any())).willReturn(listOf(Vessel()))
+
+        // When
+        val vessels = SearchVessels(vesselRepository).execute("DUMMY VESSEL")
+
+        // Then
+        assertThat(vessels).isEmpty()
+    }
+
+}

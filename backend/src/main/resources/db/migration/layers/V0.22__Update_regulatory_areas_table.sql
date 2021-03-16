544602ec6fb70528686f284d6d73a8c46622d739
Loup Theron <loup-rodney@hotmail.fr>
2021-03-16 16:00:54 +0100

    [#80] Add regulatory link to regulatory metadata

---
+++ backend/src/main/resources/db/migration/layers/V0.22__Update_regulatory_areas_table.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.reglementation_peche
+  ADD COLUMN references_reglementaires_old text,
+  ADD COLUMN autre_reglementation text,
+  ADD COLUMN law_type text,
+  ADD COLUMN autre_reglementation_especes text;

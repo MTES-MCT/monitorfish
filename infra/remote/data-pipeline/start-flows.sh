8cf56d88f63a8dffbc813c176bfc976597ea808b
Vincent Chéry <vincent.chery@m4x.org>
2021-03-19 12:53:37 +0100

    Fix start-flows.sh

--- infra/remote/data-pipeline/start-flows.sh
+++ infra/remote/data-pipeline/start-flows.sh
@@ -2,7 +2,7 @@
 
 docker run -t --rm --network=host --name monitorfish-pipeline-flows \
         -v prefect_flows:/home/monitorfish-pipeline/.prefect \
-        -u monitorfish-pipeline:"$(getent group di_etlmf | cut -d: -f3)"
+        -u monitorfish-pipeline:"$(getent group di_etlmf | cut -d: -f3)" \
         --env-file datascience/.env \
         docker.pkg.github.com/mtes-mct/monitorfish/monitorfish-pipeline:v0.1.0_snapshot \
         python main.py

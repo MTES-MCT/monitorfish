d7cdbc120c59b33f30b76cc3316f2b318fe2591b
Vincent <vincent.chery@m4x.org>
2021-03-22 11:56:44 +0100

    Remove old scripts

--- infra/remote/data-pipeline/start-flows.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -t --rm --network=host --name monitorfish-pipeline-flows \
-        -v prefect_flows:/home/monitorfish-pipeline/.prefect \
-        -u monitorfish-pipeline:"$(getent group di_etlmf | cut -d: -f3)" \
-        --env-file datascience/.env \
-        docker.pkg.github.com/mtes-mct/monitorfish/monitorfish-pipeline:v0.1.0_snapshot \
-        python main.py

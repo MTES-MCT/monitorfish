abe96b3912803172a78188d8209c8d730befec09
Vincent Chéry <vincent.chery@m4x.org>
2021-03-23 16:17:04 +0100

    Fix flows run commands

--- infra/remote/data-pipeline/int/start-server.sh
+++ infra/remote/data-pipeline/int/start-server.sh
@@ -3,5 +3,5 @@
 docker run -t --rm --network=host --name monitorfish-pipeline-server \
         -u monitorfish-pipeline:$(getent group docker | cut --delimiter=":" -f3) \
         -v "$(pwd)"/infra/configurations/prefect-server/int:/home/monitorfish-pipeline/.prefect \
-        -v /var/run/docker.sock:/var/run/docker.sock docker.pkg.github.com/mtes-mct/monitorfish/monitorfish-pipeline:$(MONITORFISH_VERSION) \
+        -v /var/run/docker.sock:/var/run/docker.sock docker.pkg.github.com/mtes-mct/monitorfish/monitorfish-pipeline:$MONITORFISH_VERSION \
         prefect server start --use-volume --volume-path ~/.prefect/server/pg_data --postgres-port "5433"

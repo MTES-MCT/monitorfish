eacb40db3e12627971ae580b99b3b4176ccbf3db
Loup Theron <loup-rodney@hotmail.fr>
2021-03-30 11:23:47 +0200

    Fix empty result exception when receiving new position

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/database/repositories/JpaLastPositionRepository.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/database/repositories/JpaLastPositionRepository.kt
@@ -1,12 +1,14 @@
 package fr.gouv.cnsp.monitorfish.infrastructure.database.repositories
 
 import fr.gouv.cnsp.monitorfish.domain.entities.Position
+import fr.gouv.cnsp.monitorfish.domain.exceptions.NoERSMessagesFound
 import fr.gouv.cnsp.monitorfish.domain.repositories.LastPositionRepository
 import fr.gouv.cnsp.monitorfish.infrastructure.database.entities.LastPositionEntity
 import fr.gouv.cnsp.monitorfish.infrastructure.database.entities.PositionEntity
 import fr.gouv.cnsp.monitorfish.infrastructure.database.repositories.interfaces.DBLastPositionRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cache.annotation.Cacheable
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import java.lang.IllegalArgumentException
@@ -32,18 +34,22 @@ class JpaLastPositionRepository(private val dbLastPositionRepository: DBLastPosi
     }
 
     override fun find(internalReferenceNumber: String, externalReferenceNumber: String, ircs: String): Optional<Position> {
-        if(internalReferenceNumber.isNotEmpty()) {
-            return Optional.of(dbLastPositionRepository.findByInternalReferenceNumberEquals(internalReferenceNumber).toPosition())
-        }
+        try {
+            if(internalReferenceNumber.isNotEmpty()) {
+                return Optional.of(dbLastPositionRepository.findByInternalReferenceNumberEquals(internalReferenceNumber).toPosition())
+            }
 
-        if(externalReferenceNumber.isNotEmpty()) {
-            return Optional.of(dbLastPositionRepository.findByExternalReferenceNumberEquals(externalReferenceNumber).toPosition())
-        }
+            if(externalReferenceNumber.isNotEmpty()) {
+                return Optional.of(dbLastPositionRepository.findByExternalReferenceNumberEquals(externalReferenceNumber).toPosition())
+            }
 
-        if(ircs.isNotEmpty()) {
-            return Optional.of(dbLastPositionRepository.findByIrcsEquals(ircs).toPosition())
-        }
+            if(ircs.isNotEmpty()) {
+                return Optional.of(dbLastPositionRepository.findByIrcsEquals(ircs).toPosition())
+            }
 
-        return Optional.empty()
+            return Optional.empty()
+        } catch (e: EmptyResultDataAccessException) {
+            return Optional.empty()
+        }
     }
 }

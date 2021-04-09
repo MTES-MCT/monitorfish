d669b044051b7446c56b892a4dcfb9adf4b697ad
Vincent Chéry <vincent.chery@m4x.org>
2021-04-09 16:48:31 +0200

    Register controls controllers and infractions flows

--- datascience/src/pipeline/schedules.py
+++ datascience/src/pipeline/schedules.py
@@ -3,9 +3,12 @@ from datetime import timedelta
 from prefect.schedules import CronSchedule, IntervalSchedule
 
 from src.pipeline.flows import (
+    controllers,
+    controls,
     ers,
     fishing_gear_codes,
     heartbeat,
+    infractions,
     ports,
     species,
     vessels,
@@ -20,6 +23,9 @@ ers.flow.schedule = IntervalSchedule(interval=timedelta(minutes=1))
 
 ###################### List flows to register with prefect server #####################
 flows_to_register = [
+    controllers.flow,
+    controls.flow,
+    infractions.flow,
     heartbeat.flow,
     vessels.flow,
     fishing_gear_codes.flow,

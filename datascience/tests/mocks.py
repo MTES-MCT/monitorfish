7e64754b609d1bd29ff69749f8311199f7e87d70
Vincent <vincent.chery@m4x.org>
2021-04-15 11:17:56 +0200

    Update controllers test

--- datascience/tests/mocks.py
+++ datascience/tests/mocks.py
@@ -1,10 +1,14 @@
+from pathlib import Path
+from typing import Union
 from unittest.mock import patch
 
 from src.pipeline.generic_tasks import extract
 from src.read_query import read_saved_query
 
 
-def mock_extract_side_effect(db_name, query_filepath, dtypes):
+def mock_extract_side_effect(
+    db_name: str, query_filepath: Union[Path, str], dtypes: Union[None, dict] = None
+    ):
     @patch("src.read_query.pd")
     @patch("src.read_query.create_engine")
     def mock_extract_side_effect_(

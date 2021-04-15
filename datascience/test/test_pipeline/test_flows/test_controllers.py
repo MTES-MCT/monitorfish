f5f3d90611a7473423d0610b89d66909a38a2975
Vincent Chéry <vincent.chery@m4x.org>
2021-04-15 09:41:51 +0200

    Add mock for extract task

--- datascience/test/test_pipeline/test_flows/test_controllers.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from src.pipeline.flows.controllers import flow
-
-
-class TestControllersFlow(unittest.TestCase):
-    def test_flow(self):
-        pass
--- datascience/tests/test_pipeline/test_parsers.py
+++ datascience/tests/test_pipeline/test_parsers.py
@@ -2,15 +2,15 @@ import datetime
 import os
 import unittest
 
-from config import ROOT_DIRECTORY
+from config import TEST_DATA_DIRECTORY
 from src.pipeline.parsers.ers import ERSParsingError, parse_xml_string
 
-TEST_DATA_DIRECTORY = ROOT_DIRECTORY / "test/test_data/ers"
+ERS_TEST_DATA_DIRECTORY = TEST_DATA_DIRECTORY / "ers"
 
 
 class TestLogParsers(unittest.TestCase):
     def parse_file(self, test_file: str, has_data: bool = False):
-        with open(os.path.join(TEST_DATA_DIRECTORY, test_file), "r") as f:
+        with open(os.path.join(ERS_TEST_DATA_DIRECTORY, test_file), "r") as f:
             xml_string = f.read()
         metadata, data_iter = parse_xml_string(xml_string)
         data_list = list(data_iter)

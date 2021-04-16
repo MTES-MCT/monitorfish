4f747bae337f86baa0d2d05467aac31ef6762b38
Vincent <vincent.chery@m4x.org>
2021-04-16 23:00:13 +0200

    Refactor and test infractions flow

--- datascience/tests/mocks.py
+++ datascience/tests/mocks.py
@@ -7,18 +7,21 @@ from src.read_query import read_saved_query
 
 
 def mock_extract_side_effect(
-    db_name: str, query_filepath: Union[Path, str], dtypes: Union[None, dict] = None
+    db_name: str,
+    query_filepath: Union[Path, str],
+    dtypes: Union[None, dict] = None,
+    parse_dates: Union[list, dict, None] = None,
     ):
     @patch("src.read_query.pd")
     @patch("src.read_query.create_engine")
     def mock_extract_side_effect_(
-        db_name, query_filepath, dtypes, mock_create_engine, mock_pd
+        db_name, query_filepath, dtypes, parse_dates, mock_create_engine, mock_pd
     ):
         def read_sql_mock(query, engine, **kwargs):
             return query
 
         mock_pd.read_sql.side_effect = read_sql_mock
 
-        return extract(db_name, query_filepath, dtypes)
+        return extract(db_name, query_filepath, dtypes, parse_dates)
 
-    return mock_extract_side_effect_(db_name, query_filepath, dtypes)
+    return mock_extract_side_effect_(db_name, query_filepath, dtypes, parse_dates)

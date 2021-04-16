3d15a67965f1e3cff4932b9c5b25d0d7b5bd245c
Vincent <vincent.chery@m4x.org>
2021-04-16 23:00:13 +0200

    Refactor and test infractions flow

--- datascience/tests/test_pipeline/test_flows/test_infractions.py
+++ datascience/tests/test_pipeline/test_flows/test_infractions.py
@@ -1,8 +1,44 @@
 import unittest
+from unittest.mock import patch
 
-from src.pipeline.flows.infractions import flow
+import pandas as pd
+
+from src.pipeline.flows.infractions import (
+    flow, 
+    extract_infractions, 
+    clean_infractions,
+    load_infractions
+)
+
+from tests.mocks import mock_extract_side_effect
 
 
 class TestInfractionsFlow(unittest.TestCase):
-    def test_flow(self):
-        pass
+    @patch("src.pipeline.flows.infractions.extract")
+    def test_extract_infractions(self, mock_extract):
+        mock_extract.side_effect = mock_extract_side_effect
+        query = extract_infractions.run()
+        self.assertTrue(isinstance(query, str))
+
+    def test_clean_infractions(self):
+        infractions = pd.DataFrame(
+            {
+                "infraction" : [
+                    "INFRACTION_1", 
+                    "Infraction numéro 2", 
+                    "T.y.p.e d'infraction"
+                ]
+            }
+        )
+
+        cleaned_infractions = clean_infractions.run(infractions)
+
+        self.assertEqual(
+            cleaned_infractions.values.tolist(),
+            [["Infraction_1"], ["Infraction numéro 2"], ["T.y.p.e d'infraction"]]
+        )
+
+    @patch("src.pipeline.flows.infractions.load", autospec=True)
+    def test_load_infractions(self, mock_load):
+        dummy_infractions = pd.DataFrame()
+        load_infractions.run(dummy_infractions)

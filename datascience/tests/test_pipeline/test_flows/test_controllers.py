51396f4ec878d3d9a27eff592da4455703c65592
Vincent <vincent.chery@m4x.org>
2021-04-17 09:59:24 +0200

    Add test on controllers flow

--- datascience/tests/test_pipeline/test_flows/test_controllers.py
+++ datascience/tests/test_pipeline/test_flows/test_controllers.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest.mock import patch
 
+import pandas as pd
+
 from src.pipeline.flows.controllers import extract_controllers, flow, load_controllers
 from tests.mocks import mock_extract_side_effect
 
@@ -12,3 +14,8 @@ class TestControllersFlow(unittest.TestCase):
         mock_extract.side_effect = mock_extract_side_effect
         query = extract_controllers.run()
         self.assertTrue(isinstance(query, str))
+
+    @patch("src.pipeline.flows.controllers.load", autospec=True)
+    def test_load_controllers(self, mock_load):
+        dummy_controllers = pd.DataFrame()
+        load_controllers.run(dummy_controllers)

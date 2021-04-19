ce1a67dc7273059e55b75f12a9edce496c72b5b0
Vincent Chéry <vincent.chery@m4x.org>
2021-04-19 16:28:43 +0200

    Refactor and test controls flow

--- datascience/tests/test_pipeline/test_flows/test_controls.py
+++ datascience/tests/test_pipeline/test_flows/test_controls.py
@@ -1,8 +1,20 @@
 import unittest
+from unittest.mock import patch
 
-from src.pipeline.flows.controls import flow
+import pandas as pd
+
+from src.pipeline.flows.controls import extract_controls, flow, load_controls
+from tests.mocks import mock_extract_side_effect
 
 
 class TestControlsFlow(unittest.TestCase):
-    def test_flow(self):
-        pass
+    @patch("src.pipeline.flows.controls.extract")
+    def test_extract_controls(self, mock_extract):
+        mock_extract.side_effect = mock_extract_side_effect
+        query = extract_controls.run()
+        self.assertTrue(isinstance(query, str))
+
+    @patch("src.pipeline.flows.controls.load", autospec=True)
+    def test_load_controls(self, mock_load):
+        dummy_controls = pd.DataFrame()
+        load_controls.run(dummy_controls)

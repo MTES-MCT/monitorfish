4df2b61d530487e61934a487f40bc4e160964476
Vincent Chéry <vincent.chery@m4x.org>
2021-04-19 17:28:39 +0200

    Remove unused scripts from datascience

--- datascience/scripts/start-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -t --rm --network=host --name monitorfish-pipeline-server \
-        -u monitorfish-pipeline:$(getent group docker | cut --delimiter=":" -f3) \
-        -v /var/run/docker.sock:/var/run/docker.sock monitorfish-pipeline:${VERSION} \
-        prefect server start --use-volume --volume-path ~/.prefect/server/pg_data

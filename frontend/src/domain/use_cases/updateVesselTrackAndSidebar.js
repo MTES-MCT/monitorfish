c32294fa6716e0691c42c475811b699b143efb79
Loup Theron <loup-rodney@hotmail.fr>
2021-04-19 17:15:49 +0200

    Minor renaming

--- frontend/src/domain/use_cases/updateVesselTrackAndSidebar.js
+++ frontend/src/domain/use_cases/updateVesselTrackAndSidebar.js
@@ -1,7 +1,6 @@
 import showVesselTrackAndSidebar from "./showVesselTrackAndSidebar";
 
 const updateVesselTrackAndSidebar = () => (dispatch, getState) => {
-    // TODO This vessel state is not needed
     const vessel = getState().vessel.selectedVesselFeatureAndIdentity
     if(vessel) {
         dispatch(showVesselTrackAndSidebar(vessel, false, true))

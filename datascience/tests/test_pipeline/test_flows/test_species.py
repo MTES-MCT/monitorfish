003e0066f57df2d34691f465cef06dd1a0a2796c
Vincent Chéry <vincent.chery@m4x.org>
2021-04-30 17:10:37 +0200

    Update species flow tests

--- datascience/tests/test_pipeline/test_flows/test_species.py
+++ datascience/tests/test_pipeline/test_flows/test_species.py
@@ -8,12 +8,6 @@ from tests.mocks import mock_extract_side_effect
 
 
 class TestSpeciesFlow(unittest.TestCase):
-    @patch("src.pipeline.flows.species.extract")
-    def test_extract_species(self, mock_extract):
-        mock_extract.side_effect = mock_extract_side_effect
-        query = extract_species.run()
-        self.assertTrue(isinstance(query, str))
-
     @patch("src.pipeline.flows.species.load", autospec=True)
     def test_load_species(self, mock_load):
         dummy_species = pd.DataFrame()

8849f2bc184c550aeb4ee279dcecd8161b1a0eb8
Vincent Chéry <vincent.chery@m4x.org>
2021-05-12 18:57:27 +0200

    Allow ircs to be the only non null identifier in last_positions

--- datascience/src/pipeline/queries/monitorfish/last_positions.sql
+++ datascience/src/pipeline/queries/monitorfish/last_positions.sql
@@ -13,19 +13,20 @@ WITH last_54_hours_positions AS (
         course,
         date_time,
         ROW_NUMBER() OVER (
-            PARTITION BY internal_reference_number, external_reference_number 
-            ORDER BY id DESC) AS rk
+            PARTITION BY internal_reference_number, external_reference_number, ircs
+            ORDER BY date_time DESC) AS rk
     FROM positions
     WHERE date_time > CURRENT_TIMESTAMP - INTERVAL '2 days 6 hours'
     AND (internal_reference_number IS NOT NULL OR 
-         external_reference_number IS NOT NULL)
+         external_reference_number IS NOT NULL OR
+         ircs IS NOT NULL)
 ),
 
 last_two_positions AS (
     SELECT 
         id,
-        COALESCE(internal_reference_number, '') AS internal_reference_number,
-        COALESCE(external_reference_number, '') AS external_reference_number,
+        internal_reference_number,
+        external_reference_number,
         mmsi,
         ircs,
         vessel_name,
@@ -50,11 +51,13 @@ emission_periods AS (
     SELECT
         internal_reference_number,
         external_reference_number,
+        ircs,
         (MAX(date_time) - MIN(date_time)) / NULLIF(COUNT(id) - 1, 0) AS emission_period
     FROM last_two_positions
     GROUP BY 
         internal_reference_number,
-        external_reference_number
+        external_reference_number,
+        ircs
 ),
 
 vessels_ AS (

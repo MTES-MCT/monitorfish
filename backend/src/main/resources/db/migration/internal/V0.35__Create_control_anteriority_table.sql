2e839378746639011037e31b1874d616755876ec
Vincent <vincent.chery@m4x.org>
2021-06-01 10:01:25 +0200

    Create control_anteriority table

---
+++ backend/src/main/resources/db/migration/internal/V0.35__Create_control_anteriority_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE public.control_anteriority (
+    vessel_id INTEGER PRIMARY KEY,
+    cfr VARCHAR(12),
+    ircs VARCHAR(10),
+    external_immatriculation VARCHAR(50),
+    flag_state VARCHAR(5),
+    last_control_datetime_utc TIMESTAMP NOT NULL,
+    last_control_infraction BOOLEAN NOT NULL,
+    number_controls_last_5_years SMALLINT NOT NULL,
+    infraction_rate REAL NOT NULL,
+    diversion_rate REAL NOT NULL,
+    seizure_rate REAL NOT NULL,
+    escort_to_quay_rate REAL NOT NULL
+);
+
+CREATE INDEX ON public.control_anteriority (vessel_id);
+CREATE INDEX ON public.control_anteriority (cfr);
+CREATE INDEX ON public.control_anteriority (ircs);
+CREATE INDEX ON public.control_anteriority (external_immatriculation);

6d30f1f15fd4383f6a082bdeaa63ce9e5d5361e3
audrey bramy <audrey.bramy@gmail.com>
2021-06-04 17:57:52 +0200

    display the div with the good size

--- frontend/src/components/regulatory_zones/RegulatoryZoneSelectedLayer.js
+++ frontend/src/components/regulatory_zones/RegulatoryZoneSelectedLayer.js
@@ -89,7 +89,7 @@ const RegulatoryZoneSelectedLayer = props => {
                 { atLeastOneLayerIsShowed ? <ShowIcon title="Cacher la couche" onClick={() => setShowWholeLayer({ show: false })} /> : <HideIcon title="Afficher la couche" onClick={() => setShowWholeLayer({ show: true })} />}
                 { allowRemoveZone && <CloseIcon title="Supprimer la couche de ma sélection" onClick={() => callRemoveRegulatoryZoneFromMySelection(getRegulatoryLayerName(regulatorySubZones), regulatorySubZones.length)}/> }
             </Zone>
-            <List
+            {<List
                 isOpen={isOpen}
                 name={regulatoryZoneName.replace(/\s/g, '-')}
                 length={regulatorySubZones.length}>
@@ -123,7 +123,7 @@ const RegulatoryZoneSelectedLayer = props => {
                       })
                       : null
                 }
-            </List>
+              </List>}
         </Row>
   )
 }

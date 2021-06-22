2cdba1ddbd840dd238318eeb30d0d18c205f1b7a
Loup Theron <loup-rodney@hotmail.fr>
2021-06-22 10:37:09 +0200

    Refactored layers sidebar and fix #413

--- frontend/src/components/base_layers/BaseLayerItem.js
+++ frontend/src/components/base_layers/BaseLayerItem.js
@@ -2,16 +2,20 @@ import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import { baseLayers } from '../../domain/entities/layers'
 import { Radio } from 'rsuite'
+import { selectBaseLayer } from '../../domain/reducers/Map'
+import { useDispatch } from 'react-redux'
 
-const BaseLayerItem = props => {
+const BaseLayerItem = ({ layer, isShownOnInit }) => {
+  const dispatch = useDispatch()
+
   const firstUpdate = useRef(true)
   const [showLayer_, setShowLayer] = useState(undefined)
 
   useEffect(() => {
     if (showLayer_ === undefined) {
-      setShowLayer(props.isShownOnInit)
+      setShowLayer(isShownOnInit)
     }
-  }, [props.isShownOnInit, showLayer_])
+  }, [isShownOnInit, showLayer_])
 
   useEffect(() => {
     if (firstUpdate.current) {
@@ -20,22 +24,27 @@ const BaseLayerItem = props => {
     }
 
     if (showLayer_) {
-      props.callSelectBaseLayer(baseLayers[props.layer].code)
+      callSelectBaseLayer(baseLayers[layer].code)
     }
   }, [showLayer_])
 
+
+  function callSelectBaseLayer (baseLayer) {
+    dispatch(selectBaseLayer(baseLayer))
+  }
+
   return <>
-        {props.layer
+        {layer
           ? <Row
                 className={'base-layers-selection'}
             >
                     <Radio
                         onChange={() => {
-                          props.callSelectBaseLayer(baseLayers[props.layer].code)
+                          callSelectBaseLayer(baseLayers[layer].code)
                         }}
-                        checked={props.isShownOnInit}
-                        value={props.layer}>
-                        {baseLayers[props.layer].text}
+                        checked={isShownOnInit}
+                        value={layer}>
+                        {baseLayers[layer].text}
                     </Radio>
 
             </Row>

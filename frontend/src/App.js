6257ebc35b59f329c4a4dc27bcc3153d8e8e647d
Loup Theron <loup-rodney@hotmail.fr>
2021-06-22 11:40:56 +0200

    Add some css rules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ function App () {
     case 'Internet Explorer': return getUnsupportedBrowser()
     case 'Edge': if (browserVersion < 79) return getUnsupportedBrowser(); break
     case 'Chrome': if (browserVersion < 69) return getUnsupportedBrowser(); break
-    case 'Firefox': if (browserVersion < 62) return getUnsupportedBrowser(); break
+    case 'Firefox': if (browserVersion > 62) return getUnsupportedBrowser(); break
     case 'Safari': if (browserVersion < 12) return getUnsupportedBrowser(); break
     case 'Opera': if (browserVersion < 56) return getUnsupportedBrowser(); break
   }
@@ -72,13 +72,23 @@ function getUnsupportedBrowser () {
   return <Wrapper>
     <Alert>
       <AlertSVG /><br/>
-      <Title>Cette version de votre navigateur est trop ancienne, MonitorFish ne peut pas fonctionner correctement.</Title><br/>
-      Merci d&apos;utiliser une version de Firefox supérieure à la version 62, ou une version de Chrome supérieure à la version 69.
+      <Text>
+        <Title>Cette version de votre navigateur est trop ancienne, MonitorFish ne peut pas fonctionner correctement.</Title><br/>
+        Merci d&apos;utiliser une version de Firefox supérieure à la version 62, ou une version de Chrome supérieure à la version 69.
+      </Text>
     </Alert>
   </Wrapper>
 }
 
+const Text = styled.span`
+  width: 700px;
+  display: inline-block;
+`
+
 const Title = styled.span`
+  margin-top: 20px;
+  margin-bottom: 15px;
+  display: inline-block;
   font: normal normal bold 28px/31px Marianne;
 `
 
@@ -90,7 +100,7 @@ const Alert = styled.div`
   background: #CAD2D3 0% 0% no-repeat padding-box;
   height: 100vh;
   width: 100vw;
-  padding-top: 35vh
+  padding-top: 35vh;
 `
 
 const Wrapper = styled.div`

361ff09d2a193b82504f12223c351e06c5800f20
Loup Theron <loup-rodney@hotmail.fr>
2021-06-29 17:57:35 +0200

    Add API parameter vesselIdentifier and modify vessel positions fetch

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/repositories/PositionRepository.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/repositories/PositionRepository.kt
@@ -6,10 +6,19 @@ import java.time.ZonedDateTime
 interface PositionRepository {
     fun findAll(): List<Position>
     fun findAllByMmsi(mmsi: String): List<Position>
-    fun findVesselLastPositions(internalReferenceNumber: String,
-                                externalReferenceNumber: String,
-                                ircs: String,
-                                from: ZonedDateTime,
-                                to: ZonedDateTime): List<Position>
+    fun findVesselLastPositionsByIrcs(ircs: String,
+                                      from: ZonedDateTime,
+                                      to: ZonedDateTime): List<Position>
+    fun findVesselLastPositionsByInternalReferenceNumber(internalReferenceNumber: String,
+                                                         from: ZonedDateTime,
+                                                         to: ZonedDateTime): List<Position>
+    fun findVesselLastPositionsByExternalReferenceNumber(externalReferenceNumber: String,
+                                                         from: ZonedDateTime,
+                                                         to: ZonedDateTime): List<Position>
+    fun findVesselLastPositionsWithoutSpecifiedIdentifier(internalReferenceNumber: String,
+                                                          externalReferenceNumber: String,
+                                                          ircs: String,
+                                                          from: ZonedDateTime,
+                                                          to: ZonedDateTime): List<Position>
     fun save(position: Position)
 }

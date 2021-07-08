aa1b23849c0f03b4b34ba95d8d29175e5cea18ce
Loup Theron <loup-rodney@hotmail.fr>
2021-07-08 10:22:27 +0200

    Another port test

--- frontend/cypress/integration/vessel_sidebar.spec.js
+++ frontend/cypress/integration/vessel_sidebar.spec.js
@@ -7,7 +7,7 @@ const port = Cypress.env('PORT') ? Cypress.env('PORT') : 3000
 context('VesselSidebar', () => {
   beforeEach(() => {
     cy.viewport(1280, 1024)
-    cy.visit(`http://0.0.0.0:${port}/#@-824534.42,6082993.21,8.70`)
+    cy.visit(`http://localhost:${port}/#@-824534.42,6082993.21,8.70`)
     cy.wait(2000)
   })
 

45fa17ae2038c405e2cdce65b59d64994bba1d99
audrey bramy <audrey.bramy@gmail.com>
2021-07-16 15:13:13 +0200

    add SelectedValue as parameter

--- frontend/src/components/backoffice/create_reglementation/RegulationRegionLine.js
+++ frontend/src/components/backoffice/create_reglementation/RegulationRegionLine.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {
   ContentLine,
   Label
@@ -7,33 +7,12 @@ import CustomSelectComponent from './CustomSelectComponent'
 import { formatData, FRENCH_REGION_LIST } from '../utils'
 import Tag from '../Tag'
 import MenuItem from '../MenuItem'
-/*
-<ContentLine>
-            <Label>Région</Label>
-            <SelectWrapper>
-              <CustomSelectPicker
-                menuStyle={{ width: 250, overflowY: 'hidden', textOverflow: 'ellipsis' }}
-                style={selectPickerStyle}
-                searchable={false}
-                placeholder='Choisir une région'
-                onChange={addRegionToSelectedRegionList}
-                value={'Choisir une région'}
-                data={formatData(FRENCH_REGION_LIST)}
-                renderMenuItem={(_, item) => renderMenuItem(selectedRegionList.includes(item.value), item, 'Checkbox')}
-                block
-              />
-            </SelectWrapper>
-            <>
-            {
-            selectedRegionList && selectedRegionList.length > 0 &&
-              <SelectedRegionList />
-            }
-            </>
-          </ContentLine>
-*/
 
-const RegulationRegionLine = () => {
-  const [selectedRegionList, setSelectedRegionList] = useState([])
+const RegulationRegionLine = props => {
+  const {
+    selectedRegionList,
+    setSelectedRegionList
+  } = props
 
   const addRegionToSelectedRegionList = (elem) => {
     const newArray = [...selectedRegionList]

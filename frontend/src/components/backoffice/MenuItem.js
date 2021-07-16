c4431b676242b3b9f58c34c106173fa867f02780
audrey bramy <audrey.bramy@gmail.com>
2021-07-16 10:09:55 +0200

    use object as props attribute

--- frontend/src/components/backoffice/MenuItem.js
+++ frontend/src/components/backoffice/MenuItem.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Radio, Checkbox } from 'rsuite'
 
-const MenuItem = (checked, item, tag) => {
-  return <>{tag === 'Radio'
-    ? <Radio checked={checked}>{item.label}</Radio>
-    : tag === 'Checkbox'
-      ? <Checkbox checked={checked}>{item.label}</Checkbox>
-      : null
-    }</>
+const MenuItem = ({ checked, item, tag }) => {
+  return <>{
+    tag === 'Radio'
+      ? <Radio checked={checked}>{item.label}</Radio>
+      : tag === 'Checkbox'
+        ? <Checkbox checked={checked}>{item.label}</Checkbox>
+        : null
+  }</>
 }
 
 export default MenuItem

1d9cb8c04410617049af59631e64952392005895
Vincent <vincent.chery@m4x.org>
2021-07-20 11:48:53 +0200

    Update tests to allow passing parameters to extract task

--- datascience/tests/test_pipeline/test_flows/test_controls.py
+++ datascience/tests/test_pipeline/test_flows/test_controls.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 
 import pandas as pd
+import sqlalchemy
 
 from src.pipeline.flows.controls import extract_controls, flow, load_controls
 from tests.mocks import mock_extract_side_effect
@@ -12,7 +13,7 @@ class TestControlsFlow(unittest.TestCase):
     def test_extract_controls(self, mock_extract):
         mock_extract.side_effect = mock_extract_side_effect
         query = extract_controls.run()
-        self.assertTrue(isinstance(query, str))
+        self.assertTrue(isinstance(query, sqlalchemy.sql.elements.TextClause))
 
     @patch("src.pipeline.flows.controls.load", autospec=True)
     def test_load_controls(self, mock_load):

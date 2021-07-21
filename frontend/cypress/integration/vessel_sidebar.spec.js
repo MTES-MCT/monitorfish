69583ed00e4b01cdff9e9ab477e48fd13925e124
Loup Theron <loup-rodney@hotmail.fr>
2021-07-21 09:09:51 +0200

    Replace cfr check with summary

--- frontend/cypress/integration/vessel_sidebar.spec.js
+++ frontend/cypress/integration/vessel_sidebar.spec.js
@@ -18,8 +18,8 @@ context('VesselSidebar', () => {
     cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 10000 }).should('be.visible')
 
     // Then
-    cy.get('*[data-cy^="vessel-cfr"]').contains("GBR000B14430")
     cy.get('*[data-cy^="vessel-name"]').contains("CABO ORTEGAL (GB)")
+    cy.get('*[data-cy^="vessel-cfr"]').contains("GBR000B14430")
   })
 
   it('Identity Should contain the vessel identity', () => {

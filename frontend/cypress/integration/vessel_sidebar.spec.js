9addec2632771cdb2ee3ce2666c96db5756aa7e2
Loup Theron <loup-rodney@hotmail.fr>
2021-07-21 10:10:34 +0200

    increase timeout

--- frontend/cypress/integration/vessel_sidebar.spec.js
+++ frontend/cypress/integration/vessel_sidebar.spec.js
@@ -8,14 +8,14 @@ context('VesselSidebar', () => {
   beforeEach(() => {
     cy.viewport(1280, 1024)
     cy.visit(`http://localhost:${port}/#@-824534.42,6082993.21,8.70`)
-    cy.get('*[data-cy^="first-loader"]', { timeout: 10000 }).should('not.exist')
+    cy.get('*[data-cy^="first-loader"]', { timeout: 20000 }).should('not.exist')
     cy.url().should('include', '@-82')
   })
 
   it('Resume Should be opened When clicking on a vessel', () => {
     // When
     cy.get('.vessels').click(460, 480)
-    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 20000 }).should('be.visible')
 
     // Then
     cy.get('*[data-cy^="vessel-name"]').contains("CABO ORTEGAL (GB)")
@@ -25,28 +25,28 @@ context('VesselSidebar', () => {
   it('Identity Should contain the vessel identity', () => {
     // Given
     cy.get('.vessels').click(460, 480)
-    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 20000 }).should('be.visible')
 
     // When
-    cy.get('*[data-cy^="vessel-menu-identity"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-menu-identity"]', { timeout: 20000 }).should('be.visible')
     cy.get('*[data-cy^="vessel-menu-identity"]').click()
 
     // Then
-    cy.get('*[data-cy^="vessel-identity-gears"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-identity-gears"]', { timeout: 20000 }).should('be.visible')
     cy.get('*[data-cy^="vessel-identity-gears"]').contains("Sennes danoises (SDN)")
   })
 
   it('Fishing Should contain the vessel fishing resume', () => {
     // Given
     cy.get('.vessels').click(460, 480)
-    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 20000 }).should('be.visible')
 
     // When
     cy.get('*[data-cy^="vessel-menu-fishing"]').click()
-    cy.get('*[data-cy^="vessel-menu-fishing"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-menu-fishing"]', { timeout: 20000 }).should('be.visible')
 
     // Then
-    cy.get('*[data-cy^="vessel-fishing-gears"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-fishing-gears"]', { timeout: 20000 }).should('be.visible')
     cy.get('*[data-cy^="vessel-fishing-gears"]').contains("Trémails et filets maillants combinés (GTN)")
     cy.get('*[data-cy^="vessel-fishing-resume-title"]').contains("1 message - 2256 kg pêchés au total")
   })
@@ -54,10 +54,10 @@ context('VesselSidebar', () => {
   it('Fishing trips Should be walkable', () => {
     // Given
     cy.get('.vessels').click(460, 480)
-    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 20000 }).should('be.visible')
 
     cy.get('*[data-cy^="vessel-menu-fishing"]').click()
-    cy.get('*[data-cy^="vessel-fishing"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-fishing"]', { timeout: 20000 }).should('be.visible')
 
     // Then
     cy.get('*[data-cy^="vessel-fishing-trip-number"]').contains("Marée n°9463715")
@@ -72,11 +72,11 @@ context('VesselSidebar', () => {
   it('Fishing Should contain the vessel DEP message', () => {
     // Given
     cy.get('.vessels').click(460, 480)
-    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 20000 }).should('be.visible')
 
     // When
     cy.get('*[data-cy^="vessel-menu-fishing"]').click()
-    cy.get('*[data-cy^="vessel-fishing"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-fishing"]', { timeout: 20000 }).should('be.visible')
     cy.get('*[data-cy^="vessel-fishing-see-all"]').click()
 
     // Then
@@ -87,11 +87,11 @@ context('VesselSidebar', () => {
   it('Controls Should contain the controls resume', () => {
     // Given
     cy.get('.vessels').click(460, 480)
-    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 20000 }).should('be.visible')
 
     // When
     cy.get('*[data-cy^="vessel-menu-controls"]').click()
-    cy.get('*[data-cy^="vessel-controls"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-controls"]', { timeout: 20000 }).should('be.visible')
 
     // Then
     cy.get('*[data-cy^="vessel-controls-year"]').first().contains("2 contrôles, 2 infractions")
@@ -107,11 +107,11 @@ context('VesselSidebar', () => {
   it('Last SEA and LAND controls Should be presents', () => {
     // Given
     cy.get('.vessels').click(460, 480)
-    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 20000 }).should('be.visible')
 
     // When
     cy.get('*[data-cy^="vessel-menu-controls"]').click()
-    cy.get('*[data-cy^="vessel-controls"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-controls"]', { timeout: 20000 }).should('be.visible')
 
     // Then
     const date = getDate(new Date().toISOString())
@@ -123,7 +123,7 @@ context('VesselSidebar', () => {
   it('Vessel track depth Should be changed', () => {
     // Given
     cy.get('.vessels').click(460, 480)
-    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 20000 }).should('be.visible')
 
     // When
     cy.get('*[data-cy^="vessel-track-depth-selection"]').click()

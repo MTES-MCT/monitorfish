337b7533a9520bf54646e43248d517b9972bd90f
Vincent <vincent.chery@m4x.org>
2021-07-22 09:21:46 +0200

    Add risk factors to last_positions table

---
+++ backend/src/main/resources/db/migration/internal/V0.43__Update_last_positions_table.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.last_positions
+    ADD COLUMN impact_risk_factor DOUBLE PRECISION NOT NULL,
+    ADD COLUMN probability_risk_factor DOUBLE PRECISION NOT NULL,
+    ADD COLUMN detectability_risk_factor DOUBLE PRECISION NOT NULL,
+    ADD COLUMN risk_factor DOUBLE PRECISION NOT NULL;

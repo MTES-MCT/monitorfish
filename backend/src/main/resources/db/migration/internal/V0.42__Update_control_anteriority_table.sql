5ce7f87f7804dc7266d2fe9d8af4ff3568a34248
Vincent <vincent.chery@m4x.org>
2021-07-22 09:59:22 +0200

    Remove not null constraint on some control_anteriority columns

--- backend/src/main/resources/db/migration/internal/V0.42__Update_control_anteriority_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.42__Update_control_anteriority_table.sql
@@ -1,6 +1,6 @@
 ALTER TABLE public.control_anteriority
-    ADD COLUMN number_controls_last_5_years SMALLINT NOT NULL,
-    ADD COLUMN infraction_rate REAL NOT NULL,
-    ADD COLUMN diversion_rate REAL NOT NULL,
-    ADD COLUMN seizure_rate REAL NOT NULL,
-    ADD COLUMN escort_to_quay_rate REAL NOT NULL;
+    ADD COLUMN number_controls_last_5_years SMALLINT,
+    ADD COLUMN infraction_rate REAL,
+    ADD COLUMN diversion_rate REAL,
+    ADD COLUMN seizure_rate REAL,
+    ADD COLUMN escort_to_quay_rate REAL;

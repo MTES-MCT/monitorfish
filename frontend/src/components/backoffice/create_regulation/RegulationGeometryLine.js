828ecfb73a568cafd259701a02abf15efc2c9aca
audrey bramy <audrey.bramy@gmail.com>
2021-07-26 19:21:25 +0200

    add an eye icon to display zone

--- frontend/src/components/backoffice/create_regulation/RegulationGeometryLine.js
+++ frontend/src/components/backoffice/create_regulation/RegulationGeometryLine.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { ContentLine } from '../../commonStyles/Backoffice.style'
 import { Label } from '../../commonStyles/Input.style'
 import CustomSelectComponent from './CustomSelectComponent'
 import MenuItem from './MenuItem'
 import Tag from './Tag'
+import { ReactComponent as ShowIconSVG } from '../../icons/oeil_affiche.svg'
+import { ReactComponent as HideIconSVG } from '../../icons/oeil_masque.svg'
+import { COLORS } from '../../../constants/constants'
 
 const RegulationGeometryLine = props => {
   const {
     setSelectedGeometry,
     geometryIdList,
-    selectedGeometry
+    selectedGeometry,
+    setShowRegulatoryPreview,
+    showRegulatoryPreview
   } = props
 
   return <CustomContentLine>
@@ -26,15 +31,38 @@ const RegulationGeometryLine = props => {
             item={item} tag={'Radio'}/>}
       />
     {selectedGeometry &&
-      <Tag
+      <><Tag
         selectedValue={selectedGeometry}
         onCloseIconClicked={_ => setSelectedGeometry()}
-      />}
+      />
+      { showRegulatoryPreview
+        ? <HideIcon onClick={() => setShowRegulatoryPreview(false)}/>
+        : <ShowIcon onClick={() => setShowRegulatoryPreview(true)}/>
+      }
+      </>
+    }
   </CustomContentLine>
 }
 
 const CustomContentLine = styled(ContentLine)`
   margin-top: 15px;
+`
+
+const baseIcon = css`
+  width: 15px;
+  height: 10px;
+  padding: 8px 5px 7px 5px;
+  border: 1px solid ${COLORS.grayDarker};
+  border-radius: 2px;
+  cursor: pointer;
+`
+
+const ShowIcon = styled(ShowIconSVG)`
+  ${baseIcon}
+`
+
+const HideIcon = styled(HideIconSVG)`
+  ${baseIcon}
 `
 
 export default RegulationGeometryLine

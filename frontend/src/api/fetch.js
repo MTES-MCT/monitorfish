f52c62e7c21aece4bcadaf4a8382071aabb34150
audrey bramy <audrey.bramy@gmail.com>
2021-07-26 16:49:06 +0200

    display regulatory preview when a geometry is selected

--- frontend/src/api/fetch.js
+++ frontend/src/api/fetch.js
@@ -146,10 +146,9 @@ function getAllRegulatoryZonesFromAPI () {
  * @returns {Promise<GeoJSON>} The feature GeoJSON
  */
 function getAllGeometryWithoutProperty () {
-  const filter = 'references_reglementaires IS NULL'
+  const filter = 'references_reglementaires IS NULL AND zones IS NULL AND region IS NULL AND facade IS NULL AND law_type IS NULL AND layer_name IS NULL'
   const REQUEST = `${process.env.REACT_APP_GEOSERVER_LOCAL_URL}/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typename=monitorfish:` +
-  `${Layers.REGULATORY.code}&outputFormat=application/json&propertyName=law_type,layer_name,geometry&CQL_FILTER=` + filter.replace(/'/g, '%27').replace(/ /g, '%20')
-  console.log(REQUEST)
+  `${Layers.REGULATORY.code}&outputFormat=application/json&propertyName=geometry&CQL_FILTER=` + filter.replace(/'/g, '%27').replace(/ /g, '%20')
   return fetch(REQUEST)
     .then(response => {
       if (response.status === OK) {

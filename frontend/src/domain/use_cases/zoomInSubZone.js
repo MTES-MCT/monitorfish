ce5864d8198d47d8d328b5b20620e5189b4b714c
audrey bramy <audrey.bramy@gmail.com>
2021-07-28 14:54:02 +0200

    code refactoring

--- frontend/src/domain/use_cases/zoomInSubZone.js
+++ frontend/src/domain/use_cases/zoomInSubZone.js
@@ -2,29 +2,25 @@ import { animateToRegulatoryLayer } from '../reducers/Map'
 import Layers from '../entities/layers'
 import { getCenter } from 'ol/extent'
 
+const dispatchAnimateToRegulatoryLayer = (center, dispatch, layerName) => {
+  if (center && center.length && !Number.isNaN(center[0]) && !Number.isNaN(center[1])) {
+    dispatch(animateToRegulatoryLayer({
+      name: layerName,
+      center: center
+    }))
+  }
+}
 const zoomInSubZone = ({ subZone, feature }) => (dispatch, getState) => {
   if (subZone) {
     const layerName = `${Layers.REGULATORY.code}:${subZone.layerName}:${subZone.zone}`
     const layerToZoomIn = getState().layer.layers.find(layer => layer.className_ === layerName)
-
     if (layerToZoomIn) {
       const center = getCenter(layerToZoomIn.getSource().getExtent())
-
-      if (center && center.length && !Number.isNaN(center[0]) && !Number.isNaN(center[1])) {
-        dispatch(animateToRegulatoryLayer({
-          name: layerName,
-          center: center
-        }))
-      }
+      dispatchAnimateToRegulatoryLayer(center, dispatch, layerName)
     }
   } else if (feature) {
     const center = getCenter(feature.getGeometry().getExtent())
-    if (center && center.length && !Number.isNaN(center[0]) && !Number.isNaN(center[1])) {
-      dispatch(animateToRegulatoryLayer({
-        name: Layers.REGULATORY_PREVIEW,
-        center: center
-      }))
-    }
+    dispatchAnimateToRegulatoryLayer(center, dispatch, Layers.REGULATORY_PREVIEW)
   }
 }
 

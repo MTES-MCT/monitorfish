0e513dcb61c318a9c351c5c86e639e676dd0a897
audrey bramy <audrey.bramy@gmail.com>
2021-07-28 16:48:05 +0200

    add modal for reference text to come

--- frontend/src/features/commonStyles/Backoffice.style.js
+++ frontend/src/features/commonStyles/Backoffice.style.js
@@ -1,8 +1,25 @@
 import styled from 'styled-components'
+import { COLORS } from '../../constants/constants'
 
 export const ContentLine = styled.div`
   display: flex;
   flex-direction : ${props => props.isFormOpened && props.isInfoTextShown ? 'column' : 'row'};
   align-items: ${props => props.isFormOpened && props.isInfoTextShown ? 'flex-start' : 'center'};
   margin-bottom: 8px;
+`
+
+export const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const SectionTitle = styled.span`
+  text-align: left;
+  font-weight: bold;
+  font-size: 16px;
+  color: ${COLORS.textGray};
+  text-transform: uppercase;
+  width: 100%;
+  border-bottom: 1px solid ${COLORS.grayDarker};
+  margin-bottom: 20px;
 `

fe50a1b14110abd9cc968231abd037328bb65560
audrey bramy <audrey.bramy@gmail.com>
2021-08-31 12:19:06 +0200

    rename Tag properties name

--- frontend/src/features/backoffice/create_regulation/RegulationGeometryLine.js
+++ frontend/src/features/backoffice/create_regulation/RegulationGeometryLine.js
@@ -37,7 +37,7 @@ const RegulationGeometryLine = props => {
       />
     {selectedGeometry &&
       <><Tag
-        selectedValue={selectedGeometry}
+        tagValue={selectedGeometry}
         onCloseIconClicked={onCloseIconClicked}
       />
       <EyeWrapper>

9490be6a87c4232c479cd9ef0a370ea88b7290dc
Loup Theron <loup-rodney@hotmail.fr>
2021-09-03 14:40:58 +0200

    Add ellipsis track

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -6,6 +6,7 @@ export const COLORS = {
   lightGray: '#CCCFD6',
   charcoal: '#3B4559',
   slateGray: '#707785',
+  slateGrayLittleOpacity: 'rgba(112, 119, 133, 0.7)',
   mediumSeaGreen: '#29b361',
   gunMetal: '#282F3E',
   shadowBlue: '#6B839E',

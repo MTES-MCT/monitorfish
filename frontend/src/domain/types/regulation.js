deec6d1e72128ba6d6c33d7a19175488a33c0c98
audrey bramy <audrey.bramy@gmail.com>
2021-09-14 17:44:44 +0200

    add fishingperiod type

--- frontend/src/domain/types/regulation.js
+++ frontend/src/domain/types/regulation.js
@@ -52,3 +52,27 @@
 /**
  * @typedef RegulatoryLawTypes {Object.<string, RegulatoryTopics>}
  **/
+
+/**
+ * @typedef DateInterval
+ * @property {date} from
+ * @property {date} to
+ */
+
+/**
+ * @typedef HourInterval
+ * @property {time} from
+ * @property {time} to
+ */
+
+/**
+ * @typedef FishingPeriod
+ * @property {boolean} authorized
+ * @property {boolean} annualRecurrence
+ * @property {(DateInterval)} dateInterval
+ * @property {date} date
+ * @property {(string)} weekDays
+ * @property {boolean} holidays
+ * @property {HourInterval} timeInterval
+ * @property {boolean} daytime
+ */

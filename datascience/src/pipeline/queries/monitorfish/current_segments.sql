4b192a7f07a94fd20583c98d6ec1c63d324025b0
Vincent Chéry <vincent.chery@m4x.org>
2021-09-17 10:28:17 +0200

    Add segment of highest priority and impact in current_segments

--- datascience/src/pipeline/queries/monitorfish/current_segments.sql
+++ datascience/src/pipeline/queries/monitorfish/current_segments.sql
@@ -7,6 +7,6 @@ SELECT
     species_onboard,
     segments,
     total_weight_onboard,
-    risk_factor as segment_risk_factor,
+    impact_risk_factor,
     control_priority_level
 FROM public.current_segments

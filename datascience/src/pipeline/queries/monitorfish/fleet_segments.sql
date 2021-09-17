5d812421b5f26ce9b40f5d2067ec14054680f467
Vincent Chéry <vincent.chery@m4x.org>
2021-09-17 10:25:37 +0200

    Rename risk_factor to impact_risk_factor in fleet_segments

--- datascience/src/pipeline/queries/monitorfish/fleet_segments.sql
+++ datascience/src/pipeline/queries/monitorfish/fleet_segments.sql
@@ -3,6 +3,6 @@ SELECT
     gears,
     fao_areas,
     target_species || bycatch_species as species,
-    risk_factor,
+    impact_risk_factor,
     control_priority_level
 FROM public.fleet_segments

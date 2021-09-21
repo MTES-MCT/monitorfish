448611822968fd107d6a3c5856b4cf68952ef969
Loup Theron <loup-rodney@hotmail.fr>
2021-09-21 10:51:43 +0200

    FIx cypress test and refactor usage of Date

--- frontend/cypress/integration/vessel_labels.spec.js
+++ frontend/cypress/integration/vessel_labels.spec.js
@@ -26,7 +26,7 @@ context('Vessel labels', () => {
       .click({ timeout: 20000 })
 
     // Then
-    cy.wait(200)
+    cy.wait(400)
     cy.get('*[data-cy^="vessel-label-text"]').first().contains('FRAIS AVIS MODE')
   })
 

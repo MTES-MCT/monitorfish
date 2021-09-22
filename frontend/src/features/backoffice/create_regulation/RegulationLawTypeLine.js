51825fed79a52e020a2ce961cac84d9201c9b83a
audrey bramy <audrey.bramy@gmail.com>
2021-09-22 16:24:37 +0200

    remove add law type functionnality

--- frontend/src/features/backoffice/create_regulation/RegulationLawTypeLine.js
+++ frontend/src/features/backoffice/create_regulation/RegulationLawTypeLine.js
@@ -1,9 +1,7 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React from 'react'
 import Tag from './Tag'
 import { ContentLine } from '../../commonStyles/Backoffice.style'
-import { Label, CustomInput } from '../../commonStyles/Input.style'
-import { ValidateButton, CancelButton, SquareButton } from '../../commonStyles/Buttons.style'
+import { Label } from '../../commonStyles/Input.style'
 import CustomSelectComponent from './CustomSelectComponent'
 import MenuItem from './MenuItem'
 
@@ -14,20 +12,6 @@ const RegulationLawTypeLine = props => {
     selectData,
     lawTypeIsMissing
   } = props
-  const [regulationLawType, setRegulationLawType] = useState()
-  const [regulationBlocNameIsRed, setRegulationBlocNameIsRed] = useState(false)
-  const [isAddRegulationBlocClicked, setIsAddRegulationBlocClicked] = useState(false)
-
-  const addNewRegulationLawType = () => {
-    if (regulationLawType === '') {
-      setRegulationBlocNameIsRed(true)
-    } else {
-      setSelectedValue(regulationLawType)
-      setRegulationLawType('')
-      setIsAddRegulationBlocClicked(false)
-      setRegulationBlocNameIsRed(false)
-    }
-  }
 
   return (
     <ContentLine>
@@ -39,6 +23,7 @@ const RegulationLawTypeLine = props => {
         value={'Choisir un ensemble'}
         onChange={setSelectedValue}
         data={selectData}
+        data-cy={'regulation-lawtype-select'}
         renderMenuItem={(_, item) => <MenuItem checked={item.value === selectedValue} item={item} tag={'Radio'} />}
         valueIsMissing={lawTypeIsMissing}
       />
@@ -48,40 +33,8 @@ const RegulationLawTypeLine = props => {
           onCloseIconClicked={_ => setSelectedValue()}
         />
       }
-      {
-        isAddRegulationBlocClicked
-          ? <CreateRegulationBloc>
-              <CustomInput
-                placeholder='Nommez le nouvel ensemble règlementaire'
-                value={regulationLawType}
-                onChange={setRegulationLawType}
-                width={'250px'}
-                $isRed={regulationBlocNameIsRed}
-              />
-              <ValidateButton
-                disabled={false}
-                isLast={false}
-                onClick={addNewRegulationLawType}>
-                Enregistrer
-              </ValidateButton>
-              <CancelButton
-                disabled={false}
-                isLast={false}
-                onClick={() => setIsAddRegulationBlocClicked(false)}>
-                Annuler
-              </CancelButton>
-            </CreateRegulationBloc>
-          : !selectedValue && <><SquareButton
-              onClick={() => setIsAddRegulationBlocClicked(true)}
-            />
-            <Label>Ajouter un nouvel ensemble</Label></>
-    }
     </ContentLine>
   )
 }
-
-const CreateRegulationBloc = styled.div`
-  display: flex;
-`
 
 export default RegulationLawTypeLine

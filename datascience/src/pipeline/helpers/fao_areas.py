3f4712e3e066c1b7c206c24d1ac50d9d8eee911f
Vincent Chéry <vincent.chery@m4x.org>
2021-10-04 17:40:04 +0200

    Add fao areas list cleaning helper

---
+++ datascience/src/pipeline/helpers/fao_areas.py
@@ -0,0 +1,23 @@
+from typing import List, Sequence
+
+
+def remove_redundant_fao_area_codes(s: Sequence[str]) -> List[str]:
+    """Filters the input sequence of FAO areas to keep only the smallest non
+    overlapping areas.
+
+    This is useful to prune lists of FAO areas that result from intersecting a
+    geometry (ports, vessel position...) with all FAO areas. In such cases we only
+    want to keep the smallest (most precise) FAO areas in the result.
+
+    Args:
+        s (Sequence[str]): list of FAO areas.
+
+    Returns:
+        List[str]: subset of the input sequence.
+
+    Examples:
+        >>> remove_redundant_fao_area_codes(['27.8.a', '27', '37.1'])
+        ['27.8.a', '37.1']
+    """
+    s = set(s)
+    return [a for a in s if True not in {a in t for t in (s - {a})}]

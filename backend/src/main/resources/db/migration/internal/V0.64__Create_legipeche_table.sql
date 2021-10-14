77b7e5638761e192f1be74215bdb12b6566cc7f6
Vincent <vincent.chery@m4x.org>
2021-10-14 14:28:45 +0200

    Create table public.legipeche

---
+++ backend/src/main/resources/db/migration/internal/V0.64__Create_legipeche_table.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS public.legipeche;
+DROP TYPE IF EXISTS public.legipeche_extraction_occurence;
+
+CREATE TYPE public.legipeche_extraction_occurence AS ENUM ('latest', 'previous');
+
+CREATE TABLE public.legipeche (
+    extraction_datetime_utc TIMESTAMP NOT NULL,
+    extraction_occurence legipeche_extraction_occurence NOT NULL,
+    page_title VARCHAR NOT NULL,
+    page_url VARCHAR NOT NULL,
+    document_title VARCHAR,
+    document_url VARCHAR
+);

e6c8edbf5340a1832e1f7b571f351cd81f4e5509
Vincent Chéry <vincent.chery@m4x.org>
2021-10-20 13:54:35 +0200

    Remove useless join in control_charter flow

--- datascience/src/pipeline/queries/monitorfish/control_charter.sql
+++ datascience/src/pipeline/queries/monitorfish/control_charter.sql
@@ -7,29 +7,17 @@ WITH ranked_controls AS (
         ROW_NUMBER() OVER (PARTITION BY vessel_id ORDER BY control_datetime_utc DESC) AS rk
     FROM controls
     WHERE control_datetime_utc IS NOT NULL
-),
-
-latest_control AS (
-    SELECT 
-        vessel_id,
-        infraction,
-        control_datetime_utc as latest_control_datetime_utc,
-        infraction_ids
-    FROM ranked_controls
-    WHERE rk = 1
 )
 
 SELECT 
-    v.id,
-    CASE 
+    vessel_id as id,
+    CASE
         WHEN (
-            lc.latest_control_datetime_utc > CURRENT_TIMESTAMP - INTERVAL '30 days' AND 
-            lc.infraction = false AND 
-            lc.infraction_ids = '{}') 
+            control_datetime_utc > CURRENT_TIMESTAMP - INTERVAL '30 days' AND
+            infraction = false AND
+            infraction_ids = '{}')
         THEN true 
-        ELSE false 
+        ELSE false
     END AS under_charter
-FROM vessels v
-LEFT JOIN latest_control lc
-ON lc.vessel_id = v.id
-
+FROM ranked_controls
+WHERE rk = 1

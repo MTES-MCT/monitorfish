bc9d41f9ffbc80b3eb5dc5a843293b9875b98891
Vincent Chéry <vincent.chery@m4x.org>
2021-10-20 12:08:48 +0200

    Fix charter query

--- datascience/src/pipeline/queries/monitorfish/control_charter.sql
+++ datascience/src/pipeline/queries/monitorfish/control_charter.sql
@@ -1,7 +1,35 @@
+WITH ranked_controls AS (
+    SELECT
+        vessel_id,
+        infraction,
+        infraction_ids,
+        control_datetime_utc,
+        ROW_NUMBER() OVER (PARTITION BY vessel_id ORDER BY control_datetime_utc DESC) AS rk
+    FROM controls
+    WHERE control_datetime_utc IS NOT NULL
+),
+
+latest_control AS (
+    SELECT 
+        vessel_id,
+        infraction,
+        control_datetime_utc as latest_control_datetime_utc,
+        infraction_ids
+    FROM ranked_controls
+    WHERE rk = 1
+)
+
 SELECT 
     v.id,
-    CASE WHEN (c.control_datetime_utc > CURRENT_TIMESTAMP - INTERVAL '30 days' AND c.infraction = false AND c.infraction_ids = '{}') THEN true ELSE false END AS under_charter
+    CASE 
+        WHEN (
+            lc.latest_control_datetime_utc > CURRENT_TIMESTAMP - INTERVAL '30 days' AND 
+            lc.infraction = false AND 
+            lc.infraction_ids = '{}') 
+        THEN true 
+        ELSE false 
+    END AS under_charter
 FROM vessels v
-LEFT JOIN controls c
-ON c.vessel_id = v.id
-GROUP BY 1, 2
+LEFT JOIN latest_control lc
+ON lc.vessel_id = v.id
+

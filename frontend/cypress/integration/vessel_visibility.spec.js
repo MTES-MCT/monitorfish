edb089178e1c7960de72787646423fdcb59c95be
Loup Theron <loup-rodney@hotmail.fr>
2021-10-28 15:42:46 +0200

    Add cypress test

---
+++ frontend/cypress/integration/vessel_visibility.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+const port = Cypress.env('PORT') ? Cypress.env('PORT') : 3000
+
+context('Vessel visibility', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 1024)
+    cy.visit(`http://localhost:${port}/#@-487249.11,6076055.47,15.77`)
+    cy.get('*[data-cy^="first-loader"]', { timeout: 20000 }).should('not.exist')
+    cy.url().should('include', '@-48')
+  })
+
+  it('Vessels at port Should be hidden and showed', () => {
+    // Given
+    cy.get('*[data-cy^="vessel-label-risk-factor"]').should('have.length', 1)
+
+    // When
+    cy.get('*[data-cy^="map-property-trigger"]').eq(2).click({ force: true })
+    cy.wait(1000)
+
+    // Then
+    cy.get('*[data-cy^="vessel-label-risk-factor"]').should('have.length', 12)
+  })
+
+})

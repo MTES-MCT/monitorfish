866e39cac1532beb9d8f4286d7879b69e5b89d9c
Vincent <vincent.chery@m4x.org>
2021-11-02 14:14:18 +0100

    Remove target_number_of_controls_by_air from control_objectives table

--- backend/src/main/resources/db/migration/internal/V0.68__Create_control_objectives_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.68__Create_control_objectives_table.sql
@@ -4,7 +4,6 @@ CREATE TABLE public.control_objectives (
     year INTEGER,
     target_number_of_controls_at_sea INTEGER NOT NULL DEFAULT 0,
     target_number_of_controls_at_port INTEGER NOT NULL DEFAULT 0,
-    target_number_of_controls_by_air INTEGER NOT NULL DEFAULT 0,
     control_priority_level DOUBLE PRECISION NOT NULL DEFAULT 1.0,
     CONSTRAINT facade_segment_year_pkey PRIMARY KEY (facade, segment, year)
 );

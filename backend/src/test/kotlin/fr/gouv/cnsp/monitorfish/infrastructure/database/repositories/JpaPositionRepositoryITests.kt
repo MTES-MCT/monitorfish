7c574c38c362bce52f82efc833c2f45115b2d0ae
Loup Theron <loup-rodney@hotmail.fr>
2021-11-08 09:01:57 +0100

    Fix positions backend integration test

--- backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/database/repositories/JpaPositionRepositoryITests.kt
+++ backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/database/repositories/JpaPositionRepositoryITests.kt
@@ -46,7 +46,7 @@ class JpaPositionRepositoryITests : AbstractDBTests() {
             it.mmsi == "224136470"
         }
         assertThat(sameMMSIPositions).hasSize(1)
-        assertThat(positions).hasSize(69195)
+        assertThat(positions).hasSize(69210)
         assertThat(positions.last().dateTime.toString()).isEqualTo("1970-01-01T00:00:00.999999999Z[UTC]")
     }
 

db7a447ed9dc5224e5499b62b2b3ab465cf29a22
audrey bramy <audrey.bramy@gmail.com>
2021-11-10 10:01:19 +0100

    delete regulation in interface ref

--- frontend/src/domain/use_cases/createOrUpdateRegulationInGeoserver.js
+++ frontend/src/domain/use_cases/createOrUpdateRegulationInGeoserver.js
@@ -1,9 +1,9 @@
-import { updateOrCreateRegulation } from '../../api/fetch'
+import { regulatoryAreaTransaction } from '../../api/fetch'
 import { setRegulationSaved } from '../../features/backoffice/Regulation.slice'
 import { setError } from '../../domain/shared_slices/Global'
 
 const createOrUpdateRegulationInGeoserver = (feature, type) => (dispatch) => {
-  return updateOrCreateRegulation(feature, type)
+  return regulatoryAreaTransaction(feature, type)
     .then(_ => dispatch(setRegulationSaved(true)))
     .catch(e => {
       console.error(e.error)

d1af913ffeaedbca17691b214ace45f9cba9bfbc
audrey bramy <audrey.bramy@gmail.com>
2021-11-10 11:11:07 +0100

    use valid boolean instead of object

--- frontend/src/features/backoffice/Regulation.slice.js
+++ frontend/src/features/backoffice/Regulation.slice.js
@@ -34,26 +34,26 @@ const regulationSlice = createSlice({
     },
     addObjectToRegulatoryTextCheckedMap (state, action) {
       const {
-        /** @type {RegulatoryText | null} */
-        regulatoryText,
+        /** @type {boolean} */
+        valid,
         /** @type {number} */
         id
       } = action.payload
       state.regulatoryTextCheckedMap = {
         ...(state.regulatoryTextCheckedMap || {}),
-        [id]: regulatoryText ? { ...regulatoryText } : null
+        [id]: valid
       }
     },
     addObjectToUpcomingRegulatoryTextCheckedMap (state, action) {
       const {
-        /** @type {RegulatoryText | null} */
-        regulatoryText,
+        /** @type {boolean} */
+        valid,
         /** @type {number} */
         id
       } = action.payload
       state.upcomingRegulatoryTextCheckedMap = {
         ...(state.upcomingRegulatoryTextCheckedMap || {}),
-        [id]: regulatoryText ? { ...regulatoryText } : null
+        [id]: valid
       }
     },
     setUpcomingRegulatoryTextListCheckedMap (state, action) {

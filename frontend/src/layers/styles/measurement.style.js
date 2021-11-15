f5ead9c73480a13c71f8f8bc68ba867bdd6bd2d5
Thomas Brosset <thomas.brosset@gmail.com>
2021-11-15 17:15:27 +0100

    Frontend - ajout style pour centre sur l'outil de mesure
    2 styles sont appliqués, 1 style calcule un pseudo centre de la géométrie
    
    Création d'un style séparé pour les points d'intérêts
    closes #359

--- frontend/src/layers/styles/measurement.style.js
+++ frontend/src/layers/styles/measurement.style.js
@@ -3,8 +3,31 @@ import Fill from 'ol/style/Fill'
 import Stroke from 'ol/style/Stroke'
 import { COLORS } from '../../constants/constants'
 import CircleStyle from 'ol/style/Circle'
+import Point from 'ol/geom/Point'
+import { getCenter } from 'ol/extent'
 
+export const measurementStyleWithCenter = new Style({
+  image: new CircleStyle({
+    radius: 2,
+    fill: new Fill({
+      color: COLORS.staleGray
+    })
+  }),
+  geometry: (feature) => {
+    const extent = feature.getGeometry().getExtent()
+    const center = getCenter(extent)
+    return new Point(center)
+  }
+})
 export const measurementStyle = new Style({
+  stroke: new Stroke({
+    color: COLORS.slateGray,
+    lineDash: [4, 4],
+    width: 2
+  })
+})
+
+export const POIStyle = new Style({
   stroke: new Stroke({
     color: COLORS.slateGray,
     lineDash: [4, 4],
@@ -13,10 +36,10 @@ export const measurementStyle = new Style({
   image: new CircleStyle({
     radius: 2,
     stroke: new Stroke({
-      color: COLORS.slateGray
+      color: COLORS.orange
     }),
     fill: new Fill({
-      color: COLORS.slateGray
+      color: COLORS.red
     })
   })
 })

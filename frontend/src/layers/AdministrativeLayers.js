bc0c28472e58b7b7e39e5937d61f7b60058761ba
Thomas Brosset <thomas.brosset@gmail.com>
2021-11-24 17:56:40 +0100

    Frontend - remove Layers from Redux Store - remove Administrative layers
    relates #518
    Co-authored-by: Loup Theron <louptheron@users.noreply.github.com>

--- frontend/src/layers/AdministrativeLayers.js
+++ frontend/src/layers/AdministrativeLayers.js
@@ -1,48 +1,52 @@
 import { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import layer from '../domain/shared_slices/Layer'
 import Layers, { layersType } from '../domain/entities/layers'
 
-const AdministrativeLayers = ({ map }) => {
-  const layer = useSelector(state => state.layer)
+const AdministrativeLayers = ({ map, namespace = 'homepage' }) => {
+  const { removeLayer } = layer[namespace].actions
+  const stateLayer = useSelector(state => state.layer)
+  const { showedLayers, layers } = stateLayer
+  const dispatch = useDispatch()
   const administrativeLayers = Object.keys(Layers)
     .map(topic => Layers[topic])
     .filter(layer => layer.type === layersType.ADMINISTRATIVE)
 
   useEffect(() => {
-    addOrRemoveAdministrativeLayers()
-  }, [layer.layers])
-
-  function addOrRemoveAdministrativeLayers () {
-    if (map && layer.layers) {
+    if (map && layers && layers.length) {
       addAdministrativeLayersToMap()
+    }
+  }, [layers])
+
+  useEffect(() => {
+    if (map && showedLayers) {
       removeAdministrativeLayersToMap()
     }
-  }
+  }, [showedLayers])
 
   function addAdministrativeLayersToMap () {
-    if (layer.layers.length) {
-      const layersToInsert = layer.layers
-        .filter(layer => !map.getLayers().getArray().some(layer_ => layer === layer_))
-        .filter(showedLayer => administrativeLayers
-          .some(administrativeLayer => showedLayer.name?.includes(administrativeLayer.code)))
+    const layersToInsert = layers
+      .filter(layer => !map.getLayers().getArray().some(layer_ => layer === layer_))
+      .filter(layer => administrativeLayers
+        .some(administrativeLayer => layer.name?.includes(administrativeLayer.code)))
 
-      layersToInsert.forEach(layerToInsert => {
-        if (!layerToInsert) {
-          return
-        }
+    layersToInsert.forEach(layerToInsert => {
+      if (!layerToInsert) {
+        return
+      }
 
-        map.getLayers().push(layerToInsert)
-      })
-    }
+      map.getLayers().push(layerToInsert)
+      dispatch(removeLayer(layerToInsert))
+    })
   }
 
   function removeAdministrativeLayersToMap () {
-    const layers = layer.layers.length ? layer.layers : []
+    const _showedLayers = showedLayers?.length ? showedLayers : []
 
     const layersToRemove = map.getLayers().getArray()
-      .filter(showedLayer => !layers.some(layer_ => showedLayer === layer_))
-      .filter(showedLayer => administrativeLayers
-        .some(administrativeLayer => showedLayer.name?.includes(administrativeLayer.code)))
+      .filter(OLLayer => !_showedLayers.some(layer_ => OLLayer.name === layer_.type)) // les couches dans OL qui ne sont plus dans le state showedLayers
+      .filter(OLLayer => administrativeLayers
+        .some(administrativeLayer => OLLayer.name?.includes(administrativeLayer.code))) // les couches de type administrativeLayer
 
     layersToRemove.forEach(layerToRemove => {
       map.getLayers().remove(layerToRemove)

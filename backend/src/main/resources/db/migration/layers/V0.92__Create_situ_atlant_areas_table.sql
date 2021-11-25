034bbd520a95cb557f11205f22f907e3892ffe2f
Vincent <vincent.chery@m4x.org>
2021-11-25 11:28:18 +0100

    Add migrations for admin layers

---
+++ backend/src/main/resources/db/migration/layers/V0.92__Create_situ_atlant_areas_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE public.situ_atlant_areas (
+    ogc_fid integer PRIMARY KEY,
+    wkb_geometry public.geometry(MultiPolygon,4326),
+    libelle character varying(254)
+);
+
+CREATE INDEX situ_atlant_areas_wkb_geometry_geom_idx 
+    ON public.situ_atlant_areas USING gist (wkb_geometry);

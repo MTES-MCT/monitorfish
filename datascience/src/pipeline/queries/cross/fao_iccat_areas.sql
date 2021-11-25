51911abea1b23aa8cbce2171da57e608508569cb
Vincent <vincent.chery@m4x.org>
2021-11-25 14:08:12 +0100

    Add flow to copy administrative areas to remote

---
+++ datascience/src/pipeline/queries/cross/fao_iccat_areas.sql
@@ -0,0 +1,15 @@
+SELECT 
+    ogc_fid,
+    wkb_geometry,
+    fid,
+    objectid,
+    areatype,
+    defrule,
+    disporder,
+    fill,
+    stroke,
+    rfb,
+    shape_leng,
+    shape_area,
+    ancfeature
+FROM prod.fao_iccat_areas

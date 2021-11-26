6ed81f64f6006e791d077384ccf0675c978fcd11
Vincent <vincent.chery@m4x.org>
2021-11-26 10:31:16 +0100

    Add missing areas to admin_areas flow

---
+++ datascience/src/pipeline/queries/cross/12_miles_areas.sql
@@ -0,0 +1,13 @@
+SELECT 
+    ogc_fid,
+    wkb_geometry,
+    nature,
+    type,
+    descriptio,
+    reference,
+    beginlifes,
+    territory,
+    country,
+    agency,
+    inspireid
+FROM prod."12_miles_areas"
---
+++ datascience/src/pipeline/queries/cross/6_miles_areas.sql
@@ -0,0 +1,14 @@
+SELECT 
+    ogc_fid,
+    wkb_geometry,
+    nature,
+    type,
+    descriptio,
+    reference,
+    beginlifes,
+    territory,
+    country,
+    agency,
+    neighbor,
+    inspireid
+FROM prod."6_miles_areas"

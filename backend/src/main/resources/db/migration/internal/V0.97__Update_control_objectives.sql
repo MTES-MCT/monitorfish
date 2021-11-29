af7dec25c4d2503bfd6c6729f90721e964ef1364
Loup Theron <loup-rodney@hotmail.fr>
2021-11-29 13:07:40 +0100

    Fix constraint migration of control objectives

--- backend/src/main/resources/db/migration/internal/V0.97__Update_control_objectives.sql
+++ backend/src/main/resources/db/migration/internal/V0.97__Update_control_objectives.sql
@@ -1,2 +1,7 @@
 ALTER TABLE public.control_objectives
     ADD COLUMN id serial;
+
+BEGIN;
+    ALTER TABLE control_objectives DROP CONSTRAINT facade_segment_year_pkey;
+    ALTER TABLE control_objectives ADD CONSTRAINT facade_segment_year_unique UNIQUE (facade, segment, year);
+COMMIT;

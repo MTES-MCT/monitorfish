6008d0cd114ec6941e979e0c97ce2606c47f6bf5
Thomas Brosset <thomas.brosset@gmail.com>
2021-12-01 16:00:22 +0100

    Frontend - Remove CFR Option from Vessels Label.
    Make pure controlled component for VesselLabelSelection

--- frontend/src/features/vessel_labels/VesselLabelSelection.js
+++ frontend/src/features/vessel_labels/VesselLabelSelection.js
@@ -1,39 +1,25 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Radio, RadioGroup } from 'rsuite'
 import styled from 'styled-components'
 import { vesselLabel } from '../../domain/entities/vesselLabelLine'
 
 const VesselLabelSelection = props => {
-  const [value, setValue] = useState(null)
-
-  useEffect(() => {
-    if (props.vesselLabel && !value) {
-      setValue(props.vesselLabel)
-    }
-  }, [props.vesselLabel])
-
-  return (
-    <>
-      {value
-        ? <RadioWrapper>
-          <RadioGroup
-            name="vesselLabelRadio"
-            value={value}
-            onChange={value => {
-              setValue(value)
-              props.updateVesselLabel(value)
-            }}
-          >
-            <Radio value={vesselLabel.VESSEL_NATIONALITY}>Nationalité (nom)</Radio>
-            <Radio value={vesselLabel.VESSEL_NAME}>Nom du navire</Radio>
-            <Radio value={vesselLabel.VESSEL_INTERNAL_REFERENCE_NUMBER}>CFR</Radio>
-            <Radio value={vesselLabel.VESSEL_FLEET_SEGMENT}>Segment de flotte</Radio>
-          </RadioGroup>
-        </RadioWrapper>
-        : null
-      }
-    </>
-  )
+  return props.vesselLabel
+    ? (<RadioWrapper>
+      <RadioGroup
+        name="vesselLabelRadio"
+        value={props.vesselLabel}
+        onChange={value => {
+          props.updateVesselLabel(value)
+        }}
+      >
+        <Radio value={vesselLabel.VESSEL_NATIONALITY}>Nationalité (nom)</Radio>
+        <Radio value={vesselLabel.VESSEL_NAME}>Nom du navire</Radio>
+        <Radio value={vesselLabel.VESSEL_FLEET_SEGMENT}>Segment de flotte</Radio>
+      </RadioGroup>
+    </RadioWrapper>
+      )
+    : null
 }
 
 const RadioWrapper = styled.div`

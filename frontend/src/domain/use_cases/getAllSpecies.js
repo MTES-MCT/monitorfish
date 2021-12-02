a4f97f9897e9a366625bc90061ed0e3e1a641e9d
Loup Theron <loup-rodney@hotmail.fr>
2021-12-02 15:28:58 +0100

    Update regulatory species in geoserver and postgres table

--- frontend/src/domain/use_cases/getAllSpecies.js
+++ frontend/src/domain/use_cases/getAllSpecies.js
@@ -3,14 +3,16 @@ import { setError } from '../shared_slices/Global'
 import { batch } from 'react-redux'
 import { setSpeciesAndSpeciesGroups } from '../shared_slices/Species'
 
-const getAllSpecies = () => dispatch => {
-  getAllSpeciesFromAPI().then(speciesAndSpeciesGroups => {
-    batch(() => {
-      dispatch(setSpeciesAndSpeciesGroups(speciesAndSpeciesGroups))
+const getAllSpecies = () => (dispatch, getState) => {
+  if (!getState().species.species?.length || !getState().species.speciesGroups?.length) {
+    getAllSpeciesFromAPI().then(speciesAndSpeciesGroups => {
+      batch(() => {
+        dispatch(setSpeciesAndSpeciesGroups(speciesAndSpeciesGroups))
+      })
+    }).catch(error => {
+      dispatch(setError(error))
     })
-  }).catch(error => {
-    dispatch(setError(error))
-  })
+  }
 }
 
 export default getAllSpecies

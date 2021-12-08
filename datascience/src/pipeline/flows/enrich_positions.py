89d1851fafaef3500eb9e1fb9314f9aa311566df
Vincent Chéry <vincent.chery@m4x.org>
2021-12-08 17:35:29 +0100

    Add enrich_positions flow

--- datascience/src/pipeline/flows/enrich_positions.py
+++ datascience/src/pipeline/flows/enrich_positions.py
@@ -8,7 +8,11 @@ from dateutil.relativedelta import relativedelta
 from prefect import Flow, Parameter, task
 
 from src.pipeline.generic_tasks import extract, load
-from src.pipeline.helpers.spatial import enrich_positions
+from src.pipeline.helpers.spatial import (
+    compute_movement_metrics,
+    detect_fishing_activity,
+    tag_port_movements,
+)
 from src.pipeline.shared_tasks.control_flow import check_flow_not_running
 from src.pipeline.shared_tasks.positions import tag_positions_at_port
 
@@ -22,7 +26,7 @@ class Period:
 @task(checkpoint=False)
 def make_periods() -> List[Period]:
     end = datetime.utcnow()
-    start = end - relativedelta(days=10)
+    start = end - relativedelta(days=1)
     return [Period(start=start, end=end)]
 
 
@@ -49,12 +53,41 @@ def extract_positions(period: Period) -> pd.DataFrame:
 
 
 @task(checkpoint=False)
-def enrich_positions_by_vessel(positions: pd.DataFrame) -> pd.DataFrame:
+def compute_movement_metrics_by_vessel(positions: pd.DataFrame) -> pd.DataFrame:
     return (
-        positions.groupby(["cfr", "ircs", "external_immatriculation"], dropna=False)
-        .apply(enrich_positions)
-        .set_index("id")
+        positions.groupby(
+            ["cfr", "ircs", "external_immatriculation"], dropna=False, group_keys=False
+        )
+        .apply(compute_movement_metrics)
+        .copy(deep=True)
     )
+
+
+@task(checkpoint=False)
+def tag_port_movements_by_vessel(positions: pd.DataFrame) -> pd.DataFrame:
+    return (
+        positions.groupby(
+            ["cfr", "ircs", "external_immatriculation"], dropna=False, group_keys=False
+        )
+        .apply(tag_port_movements)
+        .copy(deep=True)
+    )
+
+
+@task(checkpoint=False)
+def detect_fishing_activity_by_vessel(positions: pd.DataFrame) -> pd.DataFrame:
+    return (
+        positions.groupby(
+            ["cfr", "ircs", "external_immatriculation"], dropna=False, group_keys=False
+        )
+        .apply(detect_fishing_activity)
+        .copy(deep=True)
+    )
+
+
+@task(checkpoint=False)
+def load_fishing_activity(positions: pd.DataFrame):
+    pass
 
 
 with Flow("Enrich positions") as flow:
@@ -62,4 +95,6 @@ with Flow("Enrich positions") as flow:
     periods = make_periods()
     positions = extract_positions.map(periods)
     positions = tag_positions_at_port.map(positions)
-    positions = enrich_positions_by_vessel.map(positions)
+    positions = compute_movement_metrics_by_vessel.map(positions)
+    positions = tag_port_movements_by_vessel.map(positions)
+    positions = detect_fishing_activity_by_vessel.map(positions)

5356e51fd334ee0f060ef83ac892ad509f86b4bd
audrey bramy <audrey.bramy@gmail.com>
2021-12-13 19:32:20 +0100

    add gear section in NewRegulation component

---
+++ frontend/src/features/backoffice/create_regulation/regulatory_gears/RegulatoryGearSection.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import GearForm from './RegulatoryGearForm'
+import SectionTitle from '../../SectionTitle'
+import { Label, CustomInput } from '../../../commonStyles/Input.style'
+
+const RegulatoryGearSection = (props) => {
+  const {
+    regulatoryGears,
+    setRegulatoryGears
+  } = props
+
+  const [show, setShow] = useState(false)
+
+  const setOtherInfo = value => {
+    setRegulatoryGears({
+      ...regulatoryGears,
+      otherInfo: value
+    })
+  }
+
+  return <>
+    <SectionTitle
+      title={'Engins Réglementés'}
+      isOpen={show}
+      setIsOpen={setShow}
+    />
+    <GearForm
+      show={show}
+      regulatoryGears={regulatoryGears}
+      setRegulatoryGears={setRegulatoryGears}
+    />
+    <OtherRemark show={show}>
+      <Label>Mesures Techniques</Label>
+      <CustomInput
+        width={'730px'}
+        value={regulatoryGears?.otherInfo || ''}
+        onChange={setOtherInfo} />
+    </OtherRemark>
+  </>
+}
+
+const OtherRemark = styled.div`
+  display: ${props => props.show ? 'flex' : 'none'};
+  align-items: center;
+  margin-top: 10px;
+`
+
+export default RegulatoryGearSection

3ef3087e0e7e42442e420b5de434db9b27083ac4
Vincent Chéry <vincent.chery@m4x.org>
2021-12-27 15:07:03 +0100

    Refactor enrich_positions as a one task flow

--- datascience/src/pipeline/flows/enrich_positions.py
+++ datascience/src/pipeline/flows/enrich_positions.py
@@ -1,36 +1,23 @@
-from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import List, Union
 
 import pandas as pd
 import prefect
-from dateutil.relativedelta import relativedelta
-from prefect import Flow, Parameter, task
+from prefect import Flow, Parameter, task, unmapped
+from sqlalchemy import text
 
+from src.db_config import create_engine
 from src.pipeline.generic_tasks import extract, load
-from src.pipeline.helpers.spatial import (
-    compute_movement_metrics,
-    detect_fishing_activity,
-    tag_port_movements,
-)
+from src.pipeline.helpers import dates
+from src.pipeline.helpers.dates import Period
+from src.pipeline.helpers.spatial import enrich_positions
+from src.pipeline.processing import prepare_df_for_loading
 from src.pipeline.shared_tasks.control_flow import check_flow_not_running
+from src.pipeline.shared_tasks.dates import make_periods
 from src.pipeline.shared_tasks.positions import tag_positions_at_port
+from src.pipeline.utils import psql_insert_copy
 
 
-@dataclass
-class Period:
-    start: datetime
-    end: datetime
-
-
-@task(checkpoint=False)
-def make_periods() -> List[Period]:
-    end = datetime.utcnow()
-    start = end - relativedelta(days=1)
-    return [Period(start=start, end=end)]
-
-
-@task(checkpoint=False)
 def extract_positions(period: Period) -> pd.DataFrame:
     """
     Extracts all positions of a given Period.
@@ -52,49 +39,125 @@ def extract_positions(period: Period) -> pd.DataFrame:
     )
 
 
-@task(checkpoint=False)
-def compute_movement_metrics_by_vessel(positions: pd.DataFrame) -> pd.DataFrame:
-    return (
-        positions.groupby(
-            ["cfr", "ircs", "external_immatriculation"], dropna=False, group_keys=False
-        )
-        .apply(compute_movement_metrics)
-        .copy(deep=True)
+def enrich_positions_by_vessel(
+    positions: pd.DataFrame,
+    minimum_consecutive_positions: int,
+    fishing_speed_threshold: float,
+) -> pd.DataFrame:
+    return positions.groupby(
+        ["cfr", "ircs", "external_immatriculation"], dropna=False, group_keys=False
+    ).apply(
+        enrich_positions,
+        minimum_consecutive_positions=minimum_consecutive_positions,
+        fishing_speed_threshold=fishing_speed_threshold,
     )
 
 
-@task(checkpoint=False)
-def tag_port_movements_by_vessel(positions: pd.DataFrame) -> pd.DataFrame:
-    return (
-        positions.groupby(
-            ["cfr", "ircs", "external_immatriculation"], dropna=False, group_keys=False
+def load_fishing_activity(positions: pd.DataFrame, period: Period):
+
+    logger = prefect.context.get("logger")
+    e = create_engine("monitorfish_remote")
+
+    with e.begin() as connection:
+        connection.execute(
+            text(
+                "CREATE TEMP TABLE tmp_enriched_positions("
+                "    id INTEGER PRIMARY KEY,"
+                "    is_at_port BOOLEAN,"
+                "    meters_from_previous_position REAL,"
+                "    time_since_previous_position INTERVAL,"
+                "    average_speed REAL,"
+                "    is_fishing BOOLEAN"
+                ")"
+                "ON COMMIT DROP;"
+            )
         )
-        .apply(tag_port_movements)
-        .copy(deep=True)
-    )
 
-
-@task(checkpoint=False)
-def detect_fishing_activity_by_vessel(positions: pd.DataFrame) -> pd.DataFrame:
-    return (
-        positions.groupby(
-            ["cfr", "ircs", "external_immatriculation"], dropna=False, group_keys=False
+        positions = prepare_df_for_loading(
+            positions, logger, timedelta_columns=["time_since_previous_position"]
         )
-        .apply(detect_fishing_activity)
-        .copy(deep=True)
-    )
+
+        columns_to_load = [
+            "id",
+            "is_at_port",
+            "meters_from_previous_position",
+            "time_since_previous_position",
+            "average_speed",
+            "is_fishing",
+        ]
+
+        positions[columns_to_load].to_sql(
+            "tmp_enriched_positions",
+            connection,
+            if_exists="append",
+            index=False,
+            method=psql_insert_copy,
+        )
+
+        connection.execute(
+            text(
+                "UPDATE interim.test_positions p"
+                "SET"
+                "    is_at_port = ep.is_at_port,"
+                "    meters_from_previous_position = COALESCE("
+                "        ep.meters_from_previous_position,"
+                "        p.meters_from_previous_position"
+                "    ),"
+                "    time_since_previous_position = COALESCE("
+                "        ep.time_since_previous_position,"
+                "        p.time_since_previous_position"
+                "    ),"
+                "    average_speed = ep.average_speed,"
+                "    is_fishing = ep.is_fishing"
+                "FROM tmp_enriched_positions ep"
+                "WHERE p.id = ep.id"
+                "AND p.date_time >= :start"
+                "AND p.date_time <= :end"
+                ";"
+            ),
+            start=period.start,
+            end=period.end,
+        )
 
 
 @task(checkpoint=False)
-def load_fishing_activity(positions: pd.DataFrame):
-    pass
+def extract_enrich_load(
+    period: Period, minimum_consecutive_positions: int, fishing_speed_threshold: float
+):
+    """Extract positions for the given `Period`, enrich and update the `positions`
+    table.
+
+    This is all done in one `Task` in order to avoid having tasks returning anything.
+    Indeed Prefect stores all task results in memory until the flow run is done
+    running, which in this case must be avoided in order to benefit from the chunked
+    processing logic in terms of memory consumption.
+    """
+
+    positions = extract_positions(period)
+    positions = tag_positions_at_port.run(positions)
+    positions = enrich_positions_by_vessel(
+        positions,
+        minimum_consecutive_positions=minimum_consecutive_positions,
+        fishing_speed_threshold=fishing_speed_threshold,
+    )
+    load_fishing_activity(positions)
 
 
 with Flow("Enrich positions") as flow:
 
-    periods = make_periods()
-    positions = extract_positions.map(periods)
-    positions = tag_positions_at_port.map(positions)
-    positions = compute_movement_metrics_by_vessel.map(positions)
-    positions = tag_port_movements_by_vessel.map(positions)
-    positions = detect_fishing_activity_by_vessel.map(positions)
+    start_days_ago = Parameter("start_days_ago")
+    end_days_ago = Parameter("end_days_ago")
+    hours_per_chunk = Parameter("hours_per_chunk")
+    chunk_overlap_hours = Parameter("chunk_overlap_hours")
+    minimum_consecutive_positions = Parameter("minimum_consecutive_positions")
+    fishing_speed_threshold = Parameter("fishing_speed_threshold")
+
+    periods = make_periods(
+        start_days_ago, end_days_ago, hours_per_chunk, chunk_overlap_hours
+    )
+
+    extract_enrich_load.map(
+        periods,
+        minimum_consecutive_positions=unmapped(minimum_consecutive_positions),
+        fishing_speed_threshold=unmapped(fishing_speed_threshold),
+    )

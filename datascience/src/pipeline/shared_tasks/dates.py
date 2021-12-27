282efc9671cbe9eadfeec51e1bbb66e3894407b1
Vincent Chéry <vincent.chery@m4x.org>
2021-12-27 14:53:35 +0100

    Add task version of time series chunking util

---
+++ datascience/src/pipeline/shared_tasks/dates.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from typing import List, Union
+
+from prefect import task
+
+from src.pipeline.helpers import dates
+
+
+@task(checkpoint=False)
+def make_periods(
+    start_days_ago: int,
+    end_days_ago: int,
+    hours_per_chunk: int,
+    chunk_overlap_hours: int,
+) -> List[dates.Period]:
+    """
+    `prefect.Task` version of the function `src.pipeline.helpers.dates.make_periods`,
+    with the difference that start and end dates are to be given as a number of days
+    from the current date (instead of `datetime` objects), and chunk duration and
+    overlap are to be given as a number of hours (instead of `timedelta` objects).
+    This is to accomodate for the fact that Prefect flows' parameters must be
+    JSON-serializable, and `datetime` and `timedelta` are not, by default.
+
+    `src.pipeline.helpers.dates.make_periods` is recursive, hence the construction as a
+    python function first and not directly as Prefect `Task`.
+
+    See `src.pipeline.helpers.dates.make_periods` for help.
+    """
+
+    now = datetime.utcnow()
+
+    return dates.make_periods(
+        start_datetime_utc=now - timedelta(days=start_days_ago),
+        end_datetime_utc=now - timedelta(days=end_days_ago),
+        period_duration=timedelta(hours=hours_per_chunk),
+        overlap=timedelta(hours=chunk_overlap_hours),
+    )

98089662d72ee0c3eb8bcc47a8c361be1f110b66
Vincent <vincent.chery@m4x.org>
2021-12-30 15:56:10 +0100

    Add healthcheck on prefect agent process

--- infra/remote/data-pipeline/int/start-flows.sh
+++ infra/remote/data-pipeline/int/start-flows.sh
@@ -5,6 +5,11 @@ docker run -d -t --network=host --name monitorfish-pipeline-flows \
         -u monitorfish-pipeline:"$(getent group di_etlmf | cut -d: -f3)" \
         -v /opt2/monitorfish-data/ers:/opt2/monitorfish-data/ers \
         --env-file datascience/.env \
+        --health-cmd='wget --no-verbose --tries=1 --spider http://localhost:8085/api/health || exit 1'\
+        --health-interval=10s \
+        --health-retries=5 \
+        --health-timeout=2s \
+        --health-start-period=30s \
         --restart always \
         docker.pkg.github.com/mtes-mct/monitorfish/monitorfish-pipeline:$MONITORFISH_VERSION \
         python main.py

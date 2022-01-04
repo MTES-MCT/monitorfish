16752aad42050b297debda18c055620e42e39415
Thomas Brosset <thomas.brosset@gmail.com>
2022-01-04 19:14:51 +0100

    Frontend - update tests

--- frontend/cypress/integration/vessel_sidebar.spec.js
+++ frontend/cypress/integration/vessel_sidebar.spec.js
@@ -11,7 +11,7 @@ context('VesselSidebar', () => {
     cy.visit(`http://localhost:${port}/#@-824534.42,6082993.21,8.70`)
     cy.get('*[data-cy^="first-loader"]', { timeout: 20000 }).should('not.exist')
     cy.url().should('include', '@-82')
-    cy.wait(400)
+    cy.wait(1000)
   })
 
   it('Vessel Should be searched from the search bar', () => {

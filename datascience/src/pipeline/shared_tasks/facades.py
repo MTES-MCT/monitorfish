65d1c919b836abacff71b0084b7b8370dd7e1875
Vincent Chéry <vincent.chery@m4x.org>
2022-01-06 15:35:25 +0100

    Add get_facades_table task

--- datascience/src/pipeline/shared_tasks/facades.py
+++ datascience/src/pipeline/shared_tasks/facades.py
@@ -1,7 +1,11 @@
 import geopandas as gpd
+import prefect
 from prefect import task
+from sqlalchemy import Table
 
+from src.db_config import create_engine
 from src.pipeline.generic_tasks import extract
+from src.pipeline.utils import get_table
 
 
 @task(checkpoint=False)
@@ -19,3 +23,25 @@ def extract_facade_areas() -> gpd.GeoDataFrame:
         geom_col="geometry",
         crs=4326,
     )
+
+
+@task(checkpoint=False)
+def get_facades_table() -> Table:
+    """
+    Return a `sqlalchemy.Table` representing the table in which facades are stored.
+
+    Returns:
+        - ZonesTable: table of positions
+
+    """
+
+    logger = prefect.context.get("logger")
+
+    facades_table = get_table(
+        "facade_areas_subdivided",
+        schema="public",
+        engine=create_engine("monitorfish_remote"),
+        logger=logger,
+    )
+
+    return facades_table

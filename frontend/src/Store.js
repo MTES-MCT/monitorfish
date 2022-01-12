da12c3c7375b1ca2e15bf4e069768e647180674b
AudreyBramy <audrey.bramy@gmail.com>
2022-01-12 15:18:39 +0100

    add redux-persist lib and configure it

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,15 +1,27 @@
 import { backofficeReducers, homeReducers } from './domain/shared_slices'
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+import storage from 'redux-persist/lib/storage' // localStorage
 import thunk from 'redux-thunk'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2
+}
+
 const homeStore = configureStore({
-  reducer: homeReducers,
+  reducer: persistReducer(persistConfig, homeReducers),
   middleware: [thunk]
 })
 
 const backofficeStore = configureStore({
-  reducer: backofficeReducers,
+  reducer: persistReducer(persistConfig, backofficeReducers),
   middleware: [thunk]
 })
 
-export { homeStore, backofficeStore }
+const homePersistor = persistStore(homeStore)
+const backofficePersistor = persistStore(backofficeStore)
+
+export { homeStore, backofficeStore, homePersistor, backofficePersistor }

8565bb15a84bbffa51f1a05d161f890ba35ef6bf
Loup Theron <loup-rodney@hotmail.fr>
2022-01-17 18:15:36 +0100

    WIP: Switch from alerts table to alerts list

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/alerts/type/ThreeMilesTrawlingAlert.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/alerts/type/ThreeMilesTrawlingAlert.kt
@@ -1,8 +1,6 @@
 package fr.gouv.cnsp.monitorfish.domain.entities.alerts.type
 
 class ThreeMilesTrawlingAlert (
-        var speed: Double,
-        var numberOfIncursion: Int,
         var seaFront: String? = null,
         var flagState: String? = null
 ): AlertType(AlertTypeMapping.THREE_MILES_TRAWLING_ALERT)

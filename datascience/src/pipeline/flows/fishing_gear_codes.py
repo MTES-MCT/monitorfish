505844d48540627f68db1661bd35cb8120997cce
Vincent Chéry <vincent.chery@m4x.org>
2022-01-20 09:52:51 +0100

    Take fishing gears data in repo instead of ocan DB

--- datascience/src/pipeline/flows/fishing_gear_codes.py
+++ datascience/src/pipeline/flows/fishing_gear_codes.py
@@ -1,54 +1,19 @@
+import pandas as pd
 import prefect
 from prefect import Flow, task
 
-from src.pipeline.generic_tasks import extract, load
+from config import LIBRARY_LOCATION
+from src.pipeline.generic_tasks import load
 
 
 @task(checkpoint=False)
 def extract_fishing_gear_codes():
-    return extract("ocan", "ocan/codes_engins.sql")
+    return pd.read_csv(LIBRARY_LOCATION / "pipeline/data/fishing_gear_codes.csv")
 
 
 @task(checkpoint=False)
-def clean_fishing_gear_codes(fishing_gear_codes):
-
-    fishing_gear_codes = fishing_gear_codes.set_index("fishing_gear_code")
-
-    # Manual changes
-    name_changes = {
-        "TBS": "Chaluts de fond à crevettes",
-        "TMS": "Chaluts pélagiques à crevettes",
-    }
-
-    for (gear_code, name) in name_changes.items():
-        fishing_gear_codes.loc[gear_code, "fishing_gear"] = name
-
-    category_changes = {
-        "OTP": "Chaluts",
-        "NS": "Engin inconnu",
-        "DRM": "Dragues",
-        "OTS": "Engin inconnu",
-        "SUX": "Filets tournants",
-        "FG": "Engins divers",
-        "HAR": "Engins divers",
-        "FCN": "Engins divers",
-        "SSC": "Sennes traînantes",
-        "SDN": "Sennes traînantes",
-        "SX": "Sennes traînantes",
-        "PS": "Sennes tournantes coulissantes",
-        "PS2": "Sennes tournantes coulissantes",
-        "PS1": "Sennes tournantes coulissantes",
-        "SV": "Sennes tournantes coulissantes",
-        "SPR": "Sennes tournantes coulissantes",
-        "SB": "Sennes tournantes coulissantes",
-    }
-
-    for (gear_code, category) in category_changes.items():
-        fishing_gear_codes.loc[gear_code, "fishing_gear_category"] = category
-
-    fishing_gear_codes = fishing_gear_codes.reset_index()
-
-    return fishing_gear_codes
+def extract_fishing_gear_codes_groups():
+    return pd.read_csv(LIBRARY_LOCATION / "pipeline/data/fishing_gear_codes_groups.csv")
 
 
 @task(checkpoint=False)
@@ -63,7 +28,20 @@ def load_fishing_gear_codes(fishing_gear_codes):
     )
 
 
+@task(checkpoint=False)
+def load_fishing_gear_codes_groups(fishing_gear_codes_groups):
+    load(
+        fishing_gear_codes_groups,
+        table_name="fishing_gear_codes_groups",
+        schema="public",
+        db_name="monitorfish_remote",
+        logger=prefect.context.get("logger"),
+        how="replace",
+    )
+
+
 with Flow("Fishing gears") as flow:
     fishing_gear_codes = extract_fishing_gear_codes()
-    fishing_gear_codes = clean_fishing_gear_codes(fishing_gear_codes)
+    fishing_gear_codes_groups = extract_fishing_gear_codes_groups()
+    load_fishing_gear_codes_groups(fishing_gear_codes_groups)
     load_fishing_gear_codes(fishing_gear_codes)

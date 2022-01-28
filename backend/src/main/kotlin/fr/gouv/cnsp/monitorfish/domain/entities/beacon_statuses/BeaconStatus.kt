75e456a487bb1545eee3de1c59f5490a4f21b43f
Loup Theron <loup-rodney@hotmail.fr>
2022-01-28 10:30:44 +0100

    Add the risk factor to the beacon status details

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/beacon_statuses/BeaconStatus.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/beacon_statuses/BeaconStatus.kt
@@ -1,6 +1,7 @@
 package fr.gouv.cnsp.monitorfish.domain.entities.beacon_statuses
 
 import fr.gouv.cnsp.monitorfish.domain.entities.VesselIdentifier
+import fr.gouv.cnsp.monitorfish.domain.entities.last_position.LastPosition
 import java.time.ZonedDateTime
 
 data class BeaconStatus(
@@ -16,4 +17,17 @@ data class BeaconStatus(
         val malfunctionStartDateTime: ZonedDateTime,
         val malfunctionEndDateTime: ZonedDateTime?,
         val vesselStatusLastModificationDateTime: ZonedDateTime,
-        var riskFactor: Double? = null)
+        var riskFactor: Double? = null) {
+    companion object {
+        fun getVesselFromBeaconStatus(beaconStatus: BeaconStatus): (LastPosition) -> Boolean {
+            return { lastPosition ->
+                when (beaconStatus.vesselIdentifier) {
+                    VesselIdentifier.INTERNAL_REFERENCE_NUMBER -> lastPosition.internalReferenceNumber == beaconStatus.internalReferenceNumber
+                    VesselIdentifier.IRCS -> lastPosition.ircs == beaconStatus.ircs
+                    VesselIdentifier.EXTERNAL_REFERENCE_NUMBER -> lastPosition.externalReferenceNumber == beaconStatus.externalReferenceNumber
+                    else -> false
+                }
+            }
+        }
+    }
+}

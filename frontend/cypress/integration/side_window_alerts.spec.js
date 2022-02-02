6dd626d686ab9e8c1b1321249645cbfff15dd41a
Loup Theron <loup-rodney@hotmail.fr>
2022-02-02 18:08:59 +0100

    Fix vessel identifier not taken into the alert object

--- frontend/cypress/integration/side_window_alerts.spec.js
+++ frontend/cypress/integration/side_window_alerts.spec.js
@@ -21,7 +21,7 @@ context('Alerts', () => {
 
     // Show vessel on map
     cy.intercept('GET', 'bff/v1/vessels/find?internalReferenceNumber=FAK000999999&externalReferenceNumber=DONTSINK' +
-      '&IRCS=CALLME&vesselIdentifier=UNDEFINED&trackDepth=TWELVE_HOURS&afterDateTime=&beforeDateTime=').as('showVesselPositionsOnMap')
+      '&IRCS=CALLME&vesselIdentifier=INTERNAL_REFERENCE_NUMBER&trackDepth=TWELVE_HOURS&afterDateTime=&beforeDateTime=').as('showVesselPositionsOnMap')
     cy.intercept('GET', 'bff/v1/ers/find?internalReferenceNumber=FAK000999999&externalReferenceNumber=DONTSINK' +
       '&IRCS=CALLME&voyageRequest=LAST&tripNumber=').as('showVesselVoyageOnMap')
     cy.get('*[data-cy="side-window-alerts-show-vessel"]').first().click()

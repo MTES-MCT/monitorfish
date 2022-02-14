9fefdee8d3a3ccdd08d3a1fb520ee47a0ead9976
AudreyBramy <audrey.bramy@gmail.com>
2022-02-14 16:32:48 +0100

    update next_id

--- backend/src/main/resources/db/migration/layers/V0.115__Update_regulatory_areas_table.sql
+++ backend/src/main/resources/db/migration/layers/V0.115__Update_regulatory_areas_table.sql
@@ -1,2 +1,14 @@
 ALTER TABLE public.reglementation_peche
     ADD COLUMN next_id bigint
+
+DROP VIEW reglementation_peche_view;
+
+CREATE OR REPLACE VIEW reglementation_peche_view AS SELECT id, law_type,
+                                                           facade, layer_name, zones, region, date_fermeture,
+                                                           date_ouverture, fishing_period, periodes, engins, engins_interdits,
+                                                           mesures_techniques, especes, species, quantites, taille,
+                                                           especes_interdites, autre_reglementation_especes,
+                                                           documents_obligatoires, autre_reglementation,
+                                                           references_reglementaires, geometry_simplified, row_hash, next_id
+                                                           FROM reglementation_peche;
+ALTER TABLE reglementation_peche_view RENAME COLUMN geometry_simplified to geometry;

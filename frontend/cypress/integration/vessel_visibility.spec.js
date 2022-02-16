25ae1ccf9103cc9d1e2a8bf617b10441e2683252
Loup Theron <loup-rodney@hotmail.fr>
2022-02-16 15:05:02 +0100

    Fix few cypress tests

--- frontend/cypress/integration/vessel_visibility.spec.js
+++ frontend/cypress/integration/vessel_visibility.spec.js
@@ -5,6 +5,9 @@ const port = Cypress.env('PORT') ? Cypress.env('PORT') : 3000
 context('Vessel visibility', () => {
   beforeEach(() => {
     cy.viewport(1280, 1024)
+    cy.visit(`http://localhost:${port}/#@-487249.11,6076055.47,15.77`)
+    cy.get('*[data-cy^="first-loader"]', { timeout: 20000 }).should('not.exist')
+    cy.url().should('include', '@-48')
   })
 
   it('Vessels at port Should be hidden and showed', () => {

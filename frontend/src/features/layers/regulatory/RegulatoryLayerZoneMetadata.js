ba14dd1448b0f5e1049d7677174b5ac24a711003
Loup Theron <loup-rodney@hotmail.fr>
2022-02-21 13:23:29 +0100

    Add outdated regulation warning

--- frontend/src/features/layers/regulatory/RegulatoryLayerZoneMetadata.js
+++ frontend/src/features/layers/regulatory/RegulatoryLayerZoneMetadata.js
@@ -2,7 +2,6 @@ import React, { useCallback } from 'react'
 import styled from 'styled-components'
 import { COLORS } from '../../../constants/constants'
 import { ReactComponent as REGPaperSVG } from '../../icons/reg_paper_dark.svg'
-import { ReactComponent as AlertSVG } from '../../icons/Picto_alerte.svg'
 import { FingerprintSpinner } from 'react-epic-spinners'
 import closeRegulatoryZoneMetadata from '../../../domain/use_cases/closeRegulatoryZoneMetadata'
 import { useDispatch, useSelector } from 'react-redux'
@@ -13,6 +12,7 @@ import MetadataFishingPeriod from './metadata/MetadataFishingPeriod'
 import MetadataGears from './metadata/MetadataGears'
 import MetadataSpecies from './metadata/MetadataSpecies'
 import MetadataRegulatoryReferences from './metadata/MetadataRegulatoryReferences'
+import OutdatedRegulatoryReferences from './metadata/OutdatedRegulatoryReferences'
 
 const RegulatoryLayerZoneMetadata = () => {
   const dispatch = useDispatch()
@@ -34,7 +34,8 @@ const RegulatoryLayerZoneMetadata = () => {
       regulatoryZoneMetadataPanelIsOpen={regulatoryZoneMetadataPanelIsOpen}>
       {
         regulatoryZoneMetadata
-          ? <><Header>
+          ? <>
+            <Header>
               <REGPaperIcon/>
               <RegulatoryZoneName title={getTitle(regulatoryZoneMetadata)}>
                 {getTitle(regulatoryZoneMetadata)}
@@ -44,17 +45,15 @@ const RegulatoryLayerZoneMetadata = () => {
                 onClick={onCloseIconClicked}
               />
             </Header>
-            <Warning>
-              <WarningIcon/>
-              Travail en cours, bien vérifier dans Légipêche la validité de la référence et des infos réglementaires
-            </Warning>
+            <OutdatedRegulatoryReferences/>
             <Content>
-              <Identification />
-              <MetadataFishingPeriod />
-              <MetadataGears />
-              <MetadataSpecies />
-              <MetadataRegulatoryReferences />
-            </Content></>
+              <Identification/>
+              <MetadataFishingPeriod/>
+              <MetadataGears/>
+              <MetadataSpecies/>
+              <MetadataRegulatoryReferences/>
+            </Content>
+          </>
           // eslint-disable-next-line react/forbid-component-props
           : <FingerprintSpinner color={COLORS.background} className={'radar'} size={100}/>
       }
@@ -102,23 +101,6 @@ const Content = styled.div`
   background: ${COLORS.background};
   overflow-y: auto;
   max-height: 72vh;
-`
-
-const Warning = styled.div`
-  font-size: 13px;
-  color: ${COLORS.gunMetal};
-  background: ${COLORS.orange};
-  display: flex;
-  text-align: left;
-  font: normal normal bold 13px/18px Marianne;
-  padding: 10px;
-`
-
-const WarningIcon = styled(AlertSVG)`
-  width: 30px;
-  flex: 57px;
-  height: 30px;
-  margin: 4px 10px 0px 0;
 `
 
 const REGPaperIcon = styled(REGPaperSVG)`

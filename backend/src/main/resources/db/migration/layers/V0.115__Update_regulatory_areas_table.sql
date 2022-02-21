d2d9bf04ee89844af593435e41bec55c5edada77
AudreyBramy <audrey.bramy@gmail.com>
2022-02-21 16:15:48 +0100

    add gears property in update regulatory area table

--- backend/src/main/resources/db/migration/layers/V0.115__Update_regulatory_areas_table.sql
+++ backend/src/main/resources/db/migration/layers/V0.115__Update_regulatory_areas_table.sql
@@ -6,7 +6,7 @@ DROP VIEW reglementation_peche_view;
 CREATE OR REPLACE VIEW reglementation_peche_view AS SELECT id, law_type,
                                                            facade, layer_name, zones, region, date_fermeture,
                                                            date_ouverture, fishing_period, periodes, engins, engins_interdits,
-                                                           mesures_techniques, especes, species, quantites, taille,
+                                                           mesures_techniques, especes, species, gears, quantites, taille,
                                                            especes_interdites, autre_reglementation_especes,
                                                            documents_obligatoires, autre_reglementation,
                                                            references_reglementaires, geometry_simplified, row_hash, next_id

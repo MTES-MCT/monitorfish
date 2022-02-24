09796bb913c1625ff5903ce5ae583dfc18f1f3cb
Loup Theron <loup-rodney@hotmail.fr>
2022-02-24 16:01:47 +0100

    Update some columns of regulatory areas

--- backend/src/main/resources/db/migration/layers/V0.116__Update_regulatory_areas_table.sql
+++ backend/src/main/resources/db/migration/layers/V0.116__Update_regulatory_areas_table.sql
@@ -6,14 +6,14 @@ ALTER TABLE public.reglementation_peche
     DROP COLUMN periodes,
     DROP COLUMN engins,
     DROP COLUMN engins_interdits,
-    DROP COLUMN mesures_techniques,
     DROP COLUMN especes,
     DROP COLUMN quantites,
     DROP COLUMN taille,
     DROP COLUMN especes_interdites,
     DROP COLUMN autre_reglementation_especes,
     DROP COLUMN documents_obligatoires,
-    DROP COLUMN autre_reglementation;
+    DROP COLUMN autre_reglementation,
+    DROP COLUMN references_reglementaires_a_venir;
 
 ALTER TABLE public.reglementation_peche
     RENAME COLUMN layer_name to topic;
@@ -21,6 +21,8 @@ ALTER TABLE public.reglementation_peche
     RENAME COLUMN zones to zone;
 ALTER TABLE public.reglementation_peche
     RENAME COLUMN references_reglementaires to regulatory_references;
+ALTER TABLE public.reglementation_peche
+    RENAME COLUMN mesures_techniques to regulatory_details;
 
 UPDATE public.reglementation_peche
     SET topic = REPLACE(topic, '_', ' ');

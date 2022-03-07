7b26cb50cc8601719ad76c14c36d18c6040372be
Vincent <vincent.chery@m4x.org>
2022-03-07 18:42:20 +0100

    Add Enum to map Poseidon beacon statuses to MF  objects

--- backend/src/main/resources/db/migration/internal/V0.120__Update_vessels_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.120__Update_vessels_table.sql
@@ -1,4 +1,4 @@
-CREATE TYPE public.beacon_status AS ENUM ('Activée', 'Désactivée', 'En test', 'Non agréée', 'Non surveillée');
+CREATE TYPE public.beacon_status AS ENUM ('ACTIVATED', 'DEACTIVATED', 'IN_TEST', 'NON_APPROVED', 'UNSUPERVISED');
 
 ALTER TABLE public.vessels
     ADD COLUMN beacon_status beacon_status;

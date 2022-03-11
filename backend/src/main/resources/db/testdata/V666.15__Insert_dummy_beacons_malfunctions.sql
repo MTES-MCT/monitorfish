02bae4fb659b6e1cccf898c97358b1a44b8635a0
Loup Theron <loup-rodney@hotmail.fr>
2022-03-11 11:12:17 +0100

    Remvoe hardcoded beacon malfunction flag state

--- backend/src/main/resources/db/testdata/V666.15__Insert_dummy_beacons_malfunctions.sql
+++ backend/src/main/resources/db/testdata/V666.15__Insert_dummy_beacons_malfunctions.sql
@@ -1,11 +1,11 @@
-INSERT INTO beacon_malfunctions (internal_reference_number, ircs, external_reference_number, vessel_identifier, vessel_name, vessel_status, stage, priority, malfunction_start_date_utc, malfunction_end_date_utc, vessel_status_last_modification_date_utc) VALUES
-('FAK000999999','CALLME','DONTSINK', 'INTERNAL_REFERENCE_NUMBER', 'PHENOMENE', 'ACTIVITY_DETECTED', 'INITIAL_ENCOUNTER', true, NOW() - ('1 WEEK')::interval, null, NOW()),
-('FAK000999999','CALLME','DONTSINK', 'INTERNAL_REFERENCE_NUMBER', 'PHENOMENE', 'AT_SEA', 'END_OF_MALFUNCTION', true, NOW() - ('8 WEEKS')::interval, NOW() - ('6 WEEKS')::interval, NOW() - ('6 WEEKS')::interval),
-('U_W0NTFINDME','QGDF','TALK2ME', 'IRCS','MALOTRU', 'NO_NEWS', 'FOUR_HOUR_REPORT', false, NOW() - ('2 WEEK')::interval, null, NOW() - ('1 WEEK')::interval),
-('FR263418260','IR12A','08FR65324', 'EXTERNAL_REFERENCE_NUMBER','LE b@TO', 'AT_PORT', 'INITIAL_ENCOUNTER', true, NOW() - ('3 WEEK')::interval, null, NOW() - ('2 WEEK')::interval),
-('FR263465414','IR123','08FR65465', 'EXTERNAL_REFERENCE_NUMBER','LE b@TO 2', 'AT_PORT', 'ARCHIVED', true, NOW() - ('3 WEEK')::interval, null, NOW() - ('2 WEEK')::interval),
-('FR263454484','FE4864','8FR6541', 'EXTERNAL_REFERENCE_NUMBER','NO NAME', 'AT_SEA', 'RELAUNCH_REQUEST', true, NOW() - ('3 WEEK')::interval, null, NOW() - ('2 WEEK')::interval),
-('ABC000939217','SC6082','RU460262', 'INTERNAL_REFERENCE_NUMBER','FRAIS AVIS MODE', 'AT_SEA', 'INITIAL_ENCOUNTER', true, NOW() - ('3 WEEK')::interval, null, NOW() - ('2 WEEK')::interval),
-('ABC000717263','QP6933','IG860866', 'INTERNAL_REFERENCE_NUMBER','ÉCRASER TON IMPOSSIBLE', 'AT_SEA', 'INITIAL_ENCOUNTER', true, NOW() - ('1 WEEK 5 DAYS')::interval, null, NOW() - ('1 WEEK 5 DAYS')::interval),
-('ABC000480070','TR0588','ZQ658496', 'INTERNAL_REFERENCE_NUMBER','DURER REJETER RECONNAÎTRE', 'AT_PORT', 'INITIAL_ENCOUNTER', true, NOW() - ('1 WEEK 7 DAYS')::interval, null, NOW() - ('1 WEEK 6 DAYS')::interval),
-('ABC000640738','IJU5217','EJ600773', 'INTERNAL_REFERENCE_NUMBER','PROFITER ESPRIT DEPUIS', 'AT_PORT', 'INITIAL_ENCOUNTER', true, NOW() - ('1 WEEK 3 DAYS')::interval, null, NOW() - ('1 WEEK 3 DAYS')::interval);
+INSERT INTO beacon_malfunctions (internal_reference_number, ircs, external_reference_number, flag_state, vessel_identifier, vessel_name, vessel_status, stage, priority, malfunction_start_date_utc, malfunction_end_date_utc, vessel_status_last_modification_date_utc) VALUES
+('FAK000999999','CALLME','DONTSINK', 'GB', 'INTERNAL_REFERENCE_NUMBER', 'PHENOMENE', 'ACTIVITY_DETECTED', 'INITIAL_ENCOUNTER', true, NOW() - ('1 WEEK')::interval, null, NOW()),
+('FAK000999999','CALLME','DONTSINK', 'GB', 'INTERNAL_REFERENCE_NUMBER', 'PHENOMENE', 'AT_SEA', 'END_OF_MALFUNCTION', true, NOW() - ('8 WEEKS')::interval, NOW() - ('6 WEEKS')::interval, NOW() - ('6 WEEKS')::interval),
+('U_W0NTFINDME','QGDF','TALK2ME', 'FR', 'IRCS','MALOTRU', 'NO_NEWS', 'FOUR_HOUR_REPORT', false, NOW() - ('2 WEEK')::interval, null, NOW() - ('1 WEEK')::interval),
+('FR263418260','IR12A','08FR65324', 'FR', 'EXTERNAL_REFERENCE_NUMBER','LE b@TO', 'AT_PORT', 'INITIAL_ENCOUNTER', true, NOW() - ('3 WEEK')::interval, null, NOW() - ('2 WEEK')::interval),
+('FR263465414','IR123','08FR65465', 'FR', 'EXTERNAL_REFERENCE_NUMBER','LE b@TO 2', 'AT_PORT', 'ARCHIVED', true, NOW() - ('3 WEEK')::interval, null, NOW() - ('2 WEEK')::interval),
+('FR263454484','FE4864','8FR6541', 'FR', 'EXTERNAL_REFERENCE_NUMBER','NO NAME', 'AT_SEA', 'RELAUNCH_REQUEST', true, NOW() - ('3 WEEK')::interval, null, NOW() - ('2 WEEK')::interval),
+('ABC000939217','SC6082','RU460262', 'FR', 'INTERNAL_REFERENCE_NUMBER','FRAIS AVIS MODE', 'AT_SEA', 'INITIAL_ENCOUNTER', true, NOW() - ('3 WEEK')::interval, null, NOW() - ('2 WEEK')::interval),
+('ABC000717263','QP6933','IG860866', 'FR', 'INTERNAL_REFERENCE_NUMBER','ÉCRASER TON IMPOSSIBLE', 'AT_SEA', 'INITIAL_ENCOUNTER', true, NOW() - ('1 WEEK 5 DAYS')::interval, null, NOW() - ('1 WEEK 5 DAYS')::interval),
+('ABC000480070','TR0588','ZQ658496', 'FR', 'INTERNAL_REFERENCE_NUMBER','DURER REJETER RECONNAÎTRE', 'AT_PORT', 'INITIAL_ENCOUNTER', true, NOW() - ('1 WEEK 7 DAYS')::interval, null, NOW() - ('1 WEEK 6 DAYS')::interval),
+('ABC000640738','IJU5217','EJ600773', 'FR', 'INTERNAL_REFERENCE_NUMBER','PROFITER ESPRIT DEPUIS', 'AT_PORT', 'INITIAL_ENCOUNTER', true, NOW() - ('1 WEEK 3 DAYS')::interval, null, NOW() - ('1 WEEK 3 DAYS')::interval);

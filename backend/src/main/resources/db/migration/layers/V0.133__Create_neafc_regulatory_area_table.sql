c7a13ae0fa2c9ee6535da4b4b3aec81d848f437f
Loup Theron <loup-rodney@hotmail.fr>
2022-04-11 11:41:01 +0200

    Add style to effort zone layer

--- backend/src/main/resources/db/migration/layers/V0.133__Create_neafc_regulatory_area_table.sql
+++ backend/src/main/resources/db/migration/layers/V0.133__Create_neafc_regulatory_area_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "public"."neafc_regulatory_area" (
     "ogc_fid" SERIAL,
+    wkb_geometry public.geometry(MultiPolygon,4326),
     CONSTRAINT "neafc_regulatory_area_pk" PRIMARY KEY ("ogc_fid"));
-SELECT AddGeometryColumn('public','neafc_regulatory_area','wkb_geometry',4326,'MULTIPOLYGON',2);
 
 CREATE INDEX "neafc_regulatory_area_wkb_geometry_geom_idx" ON "public"."neafc_regulatory_area" USING GIST ("wkb_geometry");

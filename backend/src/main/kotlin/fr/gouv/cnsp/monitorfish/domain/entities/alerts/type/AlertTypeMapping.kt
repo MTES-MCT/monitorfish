61caa24adf3c8d49d8facf8062ac7bb8a6afaa4c
Vincent <vincent.chery@m4x.org>
2022-04-19 18:13:23 +0200

    Add french EEZ fishing alerts to backend

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/alerts/type/AlertTypeMapping.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/alerts/type/AlertTypeMapping.kt
@@ -2,7 +2,8 @@ package fr.gouv.cnsp.monitorfish.domain.entities.alerts.type
 
 enum class AlertTypeMapping(private val clazz: Class<out AlertType>) : IHasImplementation {
     PNO_LAN_WEIGHT_TOLERANCE_ALERT(PNOAndLANWeightToleranceAlert::class.java),
-    THREE_MILES_TRAWLING_ALERT(ThreeMilesTrawlingAlert::class.java);
+    THREE_MILES_TRAWLING_ALERT(ThreeMilesTrawlingAlert::class.java),
+    FRENCH_EEZ_FISHING_ALERT(FrenchEEZFishingAlert::class.java);
 
     override fun getImplementation(): Class<out AlertType> {
         return clazz

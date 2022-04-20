0f386205d63635592a735de272a9a936406a4586
Loup Theron <loup-rodney@hotmail.fr>
2022-04-20 14:46:20 +0200

    Do not show the fishing period if it is empty

--- frontend/src/features/layers/regulatory/metadata/MetadataFishingPeriod.js
+++ frontend/src/features/layers/regulatory/metadata/MetadataFishingPeriod.js
@@ -6,22 +6,38 @@ import { fishingPeriodToString } from '../../../../domain/entities/regulatory'
 import ReactMarkdown from 'react-markdown'
 
 const MetadataFishingPeriod = () => {
-  const { fishingPeriod } = useSelector(state => state.regulatory.regulatoryZoneMetadata)
+  const {
+    fishingPeriod,
+    fishingPeriodText
+  } = useSelector(state => {
+    return {
+      fishingPeriod: state.regulatory.regulatoryZoneMetadata.fishingPeriod,
+      fishingPeriodText: fishingPeriodToString(state.regulatory.regulatoryZoneMetadata.fishingPeriod)
+    }
+  })
 
-  return <>{fishingPeriod && fishingPeriod.authorized !== undefined &&
-    <Section data-cy={'regulatory-layers-metadata-fishing-period'}>
-      <SectionTitle>{fishingPeriod.authorized
-        ? <GreenCircle margin={'0 5px 0 0'} />
-        : <RedCircle margin={'0 5px 0 0'} />}
-      Période de pêche {fishingPeriod.authorized ? 'autorisée' : 'interdites'}</SectionTitle>
-      {fishingPeriodToString(fishingPeriod)}
-      {
-        fishingPeriod.otherInfo &&
-        <ReactMarkdown>
-          {fishingPeriod.otherInfo}
-        </ReactMarkdown>
-      }
-    </Section>}</>
+  return <>
+    {
+      fishingPeriod && fishingPeriod.authorized !== undefined && fishingPeriodText &&
+      <Section data-cy={'regulatory-layers-metadata-fishing-period'}>
+        <SectionTitle>
+          {
+            fishingPeriod.authorized
+              ? <GreenCircle margin={'0 5px 0 0'} />
+              : <RedCircle margin={'0 5px 0 0'} />
+          }
+          Période de pêche {fishingPeriod.authorized ? 'autorisée' : 'interdites'}
+        </SectionTitle>
+        {fishingPeriodText}
+        {
+          fishingPeriod.otherInfo &&
+          <ReactMarkdown>
+            {fishingPeriod.otherInfo}
+          </ReactMarkdown>
+        }
+      </Section>
+    }
+  </>
 }
 
 export default MetadataFishingPeriod

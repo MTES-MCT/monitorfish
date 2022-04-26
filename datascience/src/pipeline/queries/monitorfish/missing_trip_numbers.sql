cad9d36b894f742456a672189b50482646865ddc
Vincent <vincent.chery@m4x.org>
2022-04-26 14:58:58 +0200

    Fix missing_trip_numbers query

--- datascience/src/pipeline/queries/monitorfish/missing_trip_numbers.sql
+++ datascience/src/pipeline/queries/monitorfish/missing_trip_numbers.sql
@@ -1,15 +1,18 @@
--- For each vessel and for each year, messages are sorted according to the time and 
--- date of the activity recorded, and not to the time and date of sending the message,
--- because they can be sent in advance or a posteriori - except for PNOs, for which we
--- use the time and date at which the message is sent.
 
--- After sorting the messages in this way, we consider that a new trip number starts 
--- at each DEP or just after each LAN message.
+WITH vessels_with_missing_trip_numbers AS (
+    SELECT DISTINCT cfr
+    FROM logbook_reports
+    WHERE
+        operation_datetime_utc < current_timestamp AND
+        operation_type IN ('DAT', 'COR')
+    AND trip_number IS NULL
+),
 
-WITH t1 AS (
+t1 AS (
     SELECT 
         id,
         cfr,
+        operation_type,
         log_type,
         CASE 
             WHEN log_type = 'PNO' THEN LEAST(
@@ -27,14 +30,14 @@ WITH t1 AS (
                     WHEN log_type = 'EOF' THEN value->>'endOfFishingDatetimeUtc'
                     WHEN log_type = 'RTP' THEN value->>'returnDatetimeUtc'
                 END)::timestamptz,
-                report_datetime_utc,
-                operation_datetime_utc
+                report_datetime_utc
             )
-        END AS order_datetime_utc
+        END AS order_datetime_utc,
+        operation_datetime_utc
     FROM logbook_reports
     WHERE operation_datetime_utc < CURRENT_TIMESTAMP
     AND operation_type IN ('DAT', 'COR')
-    AND trip_number IS NULL
+    AND cfr IN (SELECT cfr FROM vessels_with_missing_trip_numbers)
 ),
 
 
@@ -45,6 +48,7 @@ t2 AS (
         COALESCE((LAG(log_type, 1) OVER cfr_window) = 'LAN', FALSE) AS follows_lan,
         RANK() OVER cfr_window AS rk
     FROM t1
+    WHERE operation_type = 'DAT'
     WINDOW cfr_window AS (PARTITION BY cfr ORDER BY order_datetime_utc)
 ),
 

7f21d08efadf526cb000ab174437b8590148e3eb
Loup Theron <loup-rodney@hotmail.fr>
2022-05-11 10:14:29 +0200

    Move class converter to infra to respect clean archi

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/dtos/UpdateFleetSegmentFields.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/dtos/UpdateFleetSegmentFields.kt
@@ -1,7 +1,5 @@
 package fr.gouv.cnsp.monitorfish.domain.use_cases.dtos
 
-import fr.gouv.cnsp.monitorfish.infrastructure.api.input.UpdateFleetSegmentDataInput
-
 data class UpdateFleetSegmentFields(
         var segment: String? = null,
         var segmentName: String? = null,
@@ -10,16 +8,4 @@ data class UpdateFleetSegmentFields(
         var targetSpecies: List<String>? = null,
         var bycatchSpecies: List<String>? = null,
         var impactRiskFactor: Double? = null
-) {
-    companion object {
-        fun fromUpdateFleetSegmentDataInput(updateFleetSegmentDataInput: UpdateFleetSegmentDataInput) = UpdateFleetSegmentFields(
-                segment = updateFleetSegmentDataInput.segment,
-                segmentName = updateFleetSegmentDataInput.segmentName,
-                gears = updateFleetSegmentDataInput.gears,
-                faoAreas = updateFleetSegmentDataInput.faoAreas,
-                targetSpecies = updateFleetSegmentDataInput.targetSpecies,
-                bycatchSpecies = updateFleetSegmentDataInput.bycatchSpecies,
-                impactRiskFactor = updateFleetSegmentDataInput.impactRiskFactor,
-        )
-    }
-}
+)

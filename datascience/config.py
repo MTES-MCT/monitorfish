9b9d293ac599961750293fb1f4655774131f0501
Vincent <vincent.chery@m4x.org>
2022-05-13 09:54:48 +0200

    Add last_positions healthcheck

--- datascience/config.py
+++ datascience/config.py
@@ -101,6 +101,7 @@ BACKOFFICE_URL = MONITORFISH_URL + "backoffice/"
 # Backend endpoints
 BFF_ENDPOINT = MONITORFISH_URL + "bff/v1/"
 BEACON_MALFUNCTIONS_ENDPOINT = BFF_ENDPOINT + "beacon_malfunctions/"
+HEALTHCHECK_ENDPOINT = BFF_ENDPOINT + "healthcheck/"
 
 # Flows healthcheck URL
 FLOWS_HEALTHCHECK_URL = "http://localhost:8085"

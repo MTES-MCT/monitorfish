41f12f900bd2e568b1a3db3867b51a48df5b57c7
Vincent <vincent.chery@m4x.org>
2022-05-16 18:57:36 +0200

    Create DB user 'metabase'

---
+++ infra/remote/db_users/metabase.sql
@@ -0,0 +1,9 @@
+CREATE ROLE readaccess;
+GRANT CONNECT ON DATABASE monitorfishdb TO readaccess;
+GRANT USAGE ON SCHEMA public TO readaccess;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readaccess;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readaccess;
+
+CREATE USER metabase WITH PASSWORD '***';
+GRANT readaccess TO metabase;

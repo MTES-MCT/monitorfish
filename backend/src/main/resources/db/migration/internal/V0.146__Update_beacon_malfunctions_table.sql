1f4597cf2ac1ab5d43f8aa888f4d43bee090cdea
Vincent <vincent.chery@m4x.org>
2022-06-06 18:22:03 +0200

    Add latitude and longitude to beacon_malfunctions table

--- backend/src/main/resources/db/migration/internal/V0.146__Update_beacon_malfunctions_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.146__Update_beacon_malfunctions_table.sql
@@ -1,3 +1,5 @@
 ALTER TABLE public.beacon_malfunctions
     ADD COLUMN vessel_id INTEGER,
-    ADD COLUMN notification_requested beacon_malfunction_notification_type;
+    ADD COLUMN notification_requested beacon_malfunction_notification_type,
+    ADD COLUMN latitude double precision,
+    ADD COLUMN longitude double precision;

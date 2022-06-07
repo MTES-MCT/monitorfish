b9dcc7dae4eb4591dbc17203a92a0916bd9445cd
Vincent <vincent.chery@m4x.org>
2022-06-07 10:43:39 +0200

    Add images and attachments in email helpers

--- datascience/src/pipeline/flows/regulations_checkup.py
+++ datascience/src/pipeline/flows/regulations_checkup.py
@@ -18,8 +18,8 @@ from config import (
     EMAIL_STYLESHEETS_LOCATION,
     EMAIL_TEMPLATES_LOCATION,
 )
-from src.pipeline.emails.emails import create_html_email, send_email
 from src.pipeline.generic_tasks import extract
+from src.pipeline.helpers.emails import create_html_email, send_email
 from src.pipeline.processing import try_get_factory
 
 ####################################### Helpers #######################################
---
+++ datascience/src/pipeline/helpers/emails.py
@@ -0,0 +1,84 @@
+import smtplib
+from email.message import EmailMessage
+from email.mime.image import MIMEImage
+from mimetypes import guess_type
+from pathlib import Path
+from typing import List, Union
+
+from config import (
+    MONITORFISH_EMAIL_ADDRESS,
+    MONITORFISH_EMAIL_SERVER_PORT,
+    MONITORFISH_EMAIL_SERVER_URL,
+)
+
+
+def create_html_email(
+    to: Union[str, List[str]],
+    subject: str,
+    html: str,
+    from_: str = MONITORFISH_EMAIL_ADDRESS,
+    images: List[Path] = None,
+    attachments: List = None,
+) -> EmailMessage:
+    """Creates a `email.EmailMessage` ready to be sent with the defined parameters.
+
+    Args:
+        to (Union[str, List[str]]): email address or list of email addresses of
+          recipient(s)
+        subject (str): Subject of the email.
+        html (str): html representation of the email's content.
+        from_ (str, optional): From field. Defaults to `MONITORFISH_EMAIL_ADDRESS`.
+        images (List[Path], optional): List of `Path` to images on the server's file
+          system to attach to the email. These images can be displayed in the html body
+          of the email by referencing them in the `src` attribute of `<img>` tag as
+          `cid:<image_name>`, where `<image_name>` is the image file's name.
+
+          For example: `/a/b/c/my_image_123.png` can be included in the html message
+          as :
+
+            `<img src="cid:my_image_123.png">` in the html message.
+
+          Defaults to None.
+        attachements (List): List of attachments to add to the email.
+
+    Returns:
+        EmailMessage
+    """
+
+    if isinstance(to, list):
+        to = ", ".join(to)
+
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = from_
+    msg["To"] = to
+
+    msg.add_related(html, subtype="html")
+
+    if images:
+        for image in images:
+
+            (mimetype, _) = guess_type(image)
+            (_, subtype) = mimetype.split("/")
+
+            with open(image, "rb") as fp:
+                img = MIMEImage(fp.read(), _subtype=subtype)
+            img.add_header("Content-ID", f"<{image.name}>")
+            msg.add_related(img)
+
+    if attachments:
+        for attachment in attachments:
+            msg.add_attachment(attachment)
+
+    return msg
+
+
+def send_email(msg: EmailMessage):
+
+    assert MONITORFISH_EMAIL_SERVER_URL is not None
+    assert MONITORFISH_EMAIL_SERVER_PORT is not None
+
+    with smtplib.SMTP(
+        host=MONITORFISH_EMAIL_SERVER_URL, port=MONITORFISH_EMAIL_SERVER_PORT
+    ) as s:
+        s.send_message(msg)

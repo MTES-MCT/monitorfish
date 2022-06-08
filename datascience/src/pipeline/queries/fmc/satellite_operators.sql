9a47d83af104d0584e7925ff3ca0abf10a2a7dad
Vincent <vincent.chery@m4x.org>
2022-06-08 17:32:31 +0200

    Add satellite operators in vessels flow

---
+++ datascience/src/pipeline/queries/fmc/satellite_operators.sql
@@ -0,0 +1,13 @@
+SELECT
+    os.id_fmc_operateur_satellite AS id,
+    os.nom AS name,
+    LISTAGG(mcd.adresse, ', ') WITHIN GROUP (ORDER BY mcd.adresse) emails
+FROM FMC2.FMC_OPERATEUR_SATELLITE os
+LEFT JOIN FMC2.FMC_MOYEN_COMM_DEST mcd
+ON mcd.id_fmc_destinataire = os.id_fmc_destinataire
+LEFT JOIN FMC2.FMC_CODE_MOYEN_COMM cmc
+ON cmc.idc_fmc_moyen_comm = mcd.idc_fmc_moyen_comm
+WHERE cmc.libelle = 'Email'
+GROUP BY
+    os.id_fmc_operateur_satellite,
+    os.nom

eac0d49c562504f0ed1850c75ca6940fd677993e
Vincent <vincent.chery@m4x.org>
2022-06-08 17:34:17 +0200

    Update test data

---
+++ datascience/tests/test_data/remote_database/V666.18__Reset_test_beacon_malfunction_notifications.sql
@@ -0,0 +1,8 @@
+DELETE FROM beacon_malfunction_notifications;
+
+INSERT INTO beacon_malfunction_notifications (
+    id, beacon_malfunction_id,                                                                         date_time_utc,                         notification_type, communication_means, recipient_function, recipient_name, recipient_address_or_number
+)
+VALUES
+(    1,                     1, (NOW() AT TIME ZONE 'UTC')::TIMESTAMP - INTERVAL '1 month 2 days 23 hours 57 minutes', 'MALFUNCTION_AT_SEA_INITIAL_NOTIFICATION',               'FAX',   'VESSEL_CAPTAIN', 'Captain Cook',                '0100000000'),
+(    2,                     2,                 (NOW() AT TIME ZONE 'UTC')::TIMESTAMP - INTERVAL '9 hours 57 minutes', 'MALFUNCTION_AT_SEA_INITIAL_NOTIFICATION',               'SMS',   'VESSEL_CAPTAIN',           null,                '0111111111');

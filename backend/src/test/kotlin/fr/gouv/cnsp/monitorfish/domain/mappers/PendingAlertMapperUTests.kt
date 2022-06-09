721239e48c8f5c11f4ae96d41f04771890b9e13d
Loup Theron <loup-rodney@hotmail.fr>
2022-06-09 10:26:52 +0200

    Remove unused natinf in pending alerts test data

---
+++ backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/domain/mappers/PendingAlertMapperUTests.kt
@@ -0,0 +1,37 @@
+package fr.gouv.cnsp.monitorfish.domain.mappers
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import fr.gouv.cnsp.monitorfish.config.MapperConfiguration
+import fr.gouv.cnsp.monitorfish.domain.entities.alerts.type.AlertType
+import fr.gouv.cnsp.monitorfish.domain.entities.alerts.type.ThreeMilesTrawlingAlert
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.json.JsonTest
+import org.springframework.context.annotation.Import
+
+@Import(MapperConfiguration::class)
+@JsonTest
+class PendingAlertMapperUTests {
+
+    @Autowired
+    private lateinit var mapper: ObjectMapper
+
+    @Test
+    fun `getReportingValueFromJSON Should deserialize a THREE_MILES_TRAWLING_ALERT and append the NATINF code`() {
+        // Given
+        val alert = "{\"type\": \"THREE_MILES_TRAWLING_ALERT\", \"seaFront\": \"MEMN\", \"flagState\": \"FR\", \"riskFactor\": 1.2311444133}"
+
+        // When
+        val parsedAlert = mapper.readValue(alert, AlertType::class.java)
+
+        // Then
+        assertThat(parsedAlert).isInstanceOf(ThreeMilesTrawlingAlert::class.java)
+        parsedAlert as ThreeMilesTrawlingAlert
+        assertThat(parsedAlert.seaFront).isEqualTo("MEMN")
+        assertThat(parsedAlert.flagState).isEqualTo("FR")
+        assertThat(parsedAlert.riskFactor).isEqualTo(1.2311444133)
+        assertThat(parsedAlert.natinfCode).isEqualTo("7059")
+    }
+
+}

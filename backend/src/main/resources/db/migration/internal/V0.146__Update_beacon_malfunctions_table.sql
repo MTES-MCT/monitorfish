3bfcf32be32cb760024b3862e81901c1964bc5a0
Vincent <vincent.chery@m4x.org>
2022-06-13 11:00:33 +0200

    Create index beacon_malfunctions_notification_requested_idx

--- backend/src/main/resources/db/migration/internal/V0.146__Update_beacon_malfunctions_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.146__Update_beacon_malfunctions_table.sql
@@ -3,3 +3,7 @@ ALTER TABLE public.beacon_malfunctions
     ADD COLUMN notification_requested beacon_malfunction_notification_type,
     ADD COLUMN latitude double precision,
     ADD COLUMN longitude double precision;
+
+CREATE INDEX beacon_malfunctions_notification_requested_idx
+ON beacon_malfunctions (notification_requested)
+WHERE notification_requested IS NOT NULL;

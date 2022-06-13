7419fb835e6adc79245a321b9980dc9a6111e5ce
Vincent <vincent.chery@m4x.org>
2022-06-13 11:01:25 +0200

    Update test data

--- datascience/tests/test_data/remote_database/V666.19__Reset_test_satellite_operators.sql
+++ datascience/tests/test_data/remote_database/V666.19__Reset_test_satellite_operators.sql
@@ -4,4 +4,4 @@ INSERT INTO satellite_operators (
     id, name, emails
 ) VALUES
 (    1, 'SAT', '{email1@sat.op,email2@sat.op}'),
-(    2, 'SRV', '{contact@src.gps}');
+(    2, 'SRV', '{contact@srv.gps}');

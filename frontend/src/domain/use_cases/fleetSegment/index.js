46d3c2ca15f5458ce61e75d87cf40e4651b34efa
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-07-12 17:12:08 +0200

    Fix minor issues in backoffice fleet segments

---
+++ frontend/src/domain/use_cases/fleetSegment/index.js
@@ -0,0 +1,11 @@
+import createFleetSegment from './createFleetSegment'
+import deleteFleetSegment from './deleteFleetSegment'
+import getAllFleetSegments from './getAllFleetSegments'
+import updateFleetSegment from './updateFleetSegment'
+
+export {
+  createFleetSegment,
+  deleteFleetSegment,
+  getAllFleetSegments,
+  updateFleetSegment
+}

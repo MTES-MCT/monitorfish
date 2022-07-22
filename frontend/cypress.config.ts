7ee30f8abf7a2f93b29b770e62285ee26c7b95d6
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-07-22 14:26:56 +0200

    Disable video capture on for CI in e2e_test job

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'cypress'
 
+const IS_CI = Boolean(process.env.CI)
+
 export default defineConfig({
-  viewportWidth: 1280,
-  viewportHeight: 1024,
-  scrollBehavior: false,
   projectId: '9b7q8z',
-  retries: {
-    runMode: 5,
-    openMode: 0
+  e2e: {
+    excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents (on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
   },
   env: {
     'cypress-plugin-snapshots': {
@@ -18,13 +22,14 @@ export default defineConfig({
       updateSnapshots: false
     }
   },
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents (on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-    excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
-  }
+  retries: {
+    runMode: 5,
+    openMode: 0
+  },
+  screenshotOnRunFailure: true,
+  scrollBehavior: false,
+  video: !IS_CI,
+  viewportWidth: 1280,
+  viewportHeight: 1024,
+  waitForAnimations: true
 })

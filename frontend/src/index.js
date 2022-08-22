12c9a61b61f951270d289b9d1af974e2b1adcd3e
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-08-22 08:10:10 +0200

    Add UI page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,30 @@
-import React from 'react'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
 import { createRoot } from 'react-dom/client'
-import 'rsuite/dist/rsuite.css'
+
+import { App } from './App'
+import GlobalFonts from './fonts/fonts'
+
 import 'mini.css'
 import 'nouislider/distribute/nouislider.css'
+import 'rsuite/dist/rsuite.css'
 import './index.css'
 import 'ol/ol.css'
 import './App.css'
 
-import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
-
-import App from './App'
-import GlobalFonts from './fonts/fonts'
-
 if (!(process.env.NODE_ENV === 'development')) {
   Sentry.init({
     dsn: 'https://a5f3272efa794bb9ada2ffea90f2fec5@sentry.incubateur.net/8',
-    integrations: [new Integrations.BrowserTracing({
-      tracingOrigins: ['monitorfish-test.csam.e2.rie.gouv.fr', 'monitorfish.din.developpement-durable.gouv.fr']
-    })],
+    integrations: [
+      new Integrations.BrowserTracing({
+        tracingOrigins: ['monitorfish-test.csam.e2.rie.gouv.fr', 'monitorfish.din.developpement-durable.gouv.fr'],
+      }),
+    ],
 
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 1.0
+    tracesSampleRate: 1.0,
   })
 }
 
@@ -32,6 +33,7 @@ const root = createRoot(container)
 
 root.render(
   <>
-    <GlobalFonts/>
-    <App/>
-  </>)
+    <GlobalFonts />
+    <App />
+  </>,
+)

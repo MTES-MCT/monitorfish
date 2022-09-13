c289de545d5127db448a35a9b22ae02c42a52229
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-09-13 10:32:44 +0200

    Remove frontend domain common types

--- frontend/src/domain/types/common.d.ts
+++
@@ -1,5 +0,0 @@
-// TODO Use type-fest to replace this file later on.
-
-declare namespace Common {
-  export type ValueOf<ObjectType, ValueType extends keyof ObjectType = keyof ObjectType> = ObjectType[ValueType]
-}

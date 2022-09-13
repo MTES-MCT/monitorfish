3d1d61c5a4734ce78080a1513c49bad19f99198b
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-09-13 14:51:26 +0200

    Type Map shared slice state

--- frontend/src/domain/shared_slices/Map.ts
+++ frontend/src/domain/shared_slices/Map.ts
@@ -1,9 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
+
 import { getLocalStorageState } from '../../utils'
-import { VesselTrackDepth } from '../entities/vesselTrackDepth'
-import { vesselLabel } from '../entities/vesselLabelLine'
 import { baseLayers } from '../entities/layers'
 import { CoordinatesFormat } from '../entities/map'
+import { vesselLabel } from '../entities/vesselLabelLine'
+import { VesselTrackDepth } from '../entities/vesselTrackDepth'
 
 const vesselLabelsShowedOnMapLocalStorageKey = 'vesselLabelsShowedOnMap'
 const vesselsLastPositionVisibilityLocalStorageKey = 'vesselsLastPositionVisibility'
@@ -17,38 +18,67 @@ const riskFactorLocalStorageKey = 'riskFactor'
 const coordinatesFormatLocalStorageKey = 'coordinatesFormat'
 const hideVesselsAtPortLocalStorageKey = 'hideVesselsAtPort'
 
-const mapSlice = createSlice({
-  name: 'map',
-  initialState: {
-    // End of vessels map properties
-    animateToCoordinates: null,
-    animateToExtent: null,
-    animateToRegulatoryLayer: null,
-    coordinatesFormat: getLocalStorageState(CoordinatesFormat.DEGREES_MINUTES_SECONDS, coordinatesFormatLocalStorageKey),
-    defaultVesselTrackDepth: getLocalStorageState(VesselTrackDepth.TWELVE_HOURS, vesselTrackDepthLocalStorageKey),
-    doNotAnimate: false,
-    extent: getLocalStorageState(null, savedMapExtentLocalStorageKey),
-    hideVesselsAtPort: getLocalStorageState(true, hideVesselsAtPortLocalStorageKey),
-    interaction: null,
-    riskFactorShowedOnMap: getLocalStorageState(true, riskFactorLocalStorageKey),
-    selectedBaseLayer: getLocalStorageState(baseLayers.LIGHT.code, baseLayerLocalStorageKey),
-    showingVesselsEstimatedPositions: getLocalStorageState(true, estimatedPositionsLocalStorageKey),
-    vesselLabel: getLocalStorageState(vesselLabel.VESSEL_NAME, vesselLabelLocalStorageKey),
-    vesselLabelsShowedOnMap: getLocalStorageState(false, vesselLabelsShowedOnMapLocalStorageKey),
-    // Vessels map properties
-    vesselsLastPositionVisibility: getLocalStorageState({
-      opacityReduced: 6,
-      hidden: 48
-    }, vesselsLastPositionVisibilityLocalStorageKey),
-    view: getLocalStorageState({
-      zoom: null,
-      center: null
-    }, savedMapViewLocalStorageKey)
-  },
-  reducers: {
-    doNotAnimate (state, action) {
-      state.doNotAnimate = action.payload
+export type MapState = {
+  /** End of vessels map properties */
+  animateToCoordinates: null
+  animateToExtent: boolean
+  animateToRegulatoryLayer: null
+  coordinatesFormat: string
+  defaultVesselTrackDepth: VesselTrackDepth
+  doNotAnimate: boolean
+  extent: null
+  hideVesselsAtPort: boolean
+  interaction: null
+  riskFactorShowedOnMap: boolean
+  selectedBaseLayer: string
+  showingVesselsEstimatedPositions: boolean
+  vesselLabel: string
+  vesselLabelsShowedOnMap: boolean
+  /** Vessels map properties */
+  vesselsLastPositionVisibility: {
+    hidden: 48
+    opacityReduced: 6
+  }
+  view: {
+    center: null
+    zoom: null
+  }
+}
+const INITIAL_STATE: MapState = {
+  animateToCoordinates: null,
+  animateToExtent: false,
+  animateToRegulatoryLayer: null,
+  coordinatesFormat: getLocalStorageState(CoordinatesFormat.DEGREES_MINUTES_SECONDS, coordinatesFormatLocalStorageKey),
+  defaultVesselTrackDepth: getLocalStorageState(VesselTrackDepth.TWELVE_HOURS, vesselTrackDepthLocalStorageKey),
+  doNotAnimate: false,
+  extent: getLocalStorageState(null, savedMapExtentLocalStorageKey),
+  hideVesselsAtPort: getLocalStorageState(true, hideVesselsAtPortLocalStorageKey),
+  interaction: null,
+  riskFactorShowedOnMap: getLocalStorageState(true, riskFactorLocalStorageKey),
+  selectedBaseLayer: getLocalStorageState(baseLayers.LIGHT.code, baseLayerLocalStorageKey),
+  showingVesselsEstimatedPositions: getLocalStorageState(true, estimatedPositionsLocalStorageKey),
+  vesselLabel: getLocalStorageState(vesselLabel.VESSEL_NAME, vesselLabelLocalStorageKey),
+  vesselLabelsShowedOnMap: getLocalStorageState(false, vesselLabelsShowedOnMapLocalStorageKey),
+  vesselsLastPositionVisibility: getLocalStorageState(
+    {
+      hidden: 48,
+      opacityReduced: 6
     },
+    vesselsLastPositionVisibilityLocalStorageKey
+  ),
+  view: getLocalStorageState(
+    {
+      center: null,
+      zoom: null
+    },
+    savedMapViewLocalStorageKey
+  )
+}
+
+const mapSlice = createSlice({
+  initialState: INITIAL_STATE,
+  name: 'map',
+  reducers: {
     /**
      * Animate map to the specified OpenLayers coordinates
      * @param {Object} state
@@ -56,52 +86,74 @@ const mapSlice = createSlice({
      *   payload: import('ol/coordinate').Coordinate
      * }} action - The OpenLayers internal [longitude, latitude] coordinates
      */
-    animateToCoordinates (state, action) {
+    animateToCoordinates(state, action) {
       state.animateToCoordinates = action.payload
     },
-    resetAnimateToCoordinates (state) {
-      state.animateToCoordinates = null
-    },
+
     /**
      * Animate map to the vessel track extent stored in the Vessel reduced
      * @param {Object=} state
      */
-    animateToExtent (state) {
+    animateToExtent(state) {
       state.animateToExtent = true
     },
-    resetAnimateToExtent (state) {
-      state.animateToExtent = null
-    },
-    animateToRegulatoryLayer (state, action) {
+
+    animateToRegulatoryLayer(state, action) {
       state.animateToRegulatoryLayer = action.payload
     },
-    resetAnimateToRegulatoryLayer (state) {
+
+    doNotAnimate(state, action) {
+      state.doNotAnimate = action.payload
+    },
+    resetAnimateToCoordinates(state) {
+      state.animateToCoordinates = null
+    },
+    resetAnimateToExtent(state) {
+      state.animateToExtent = false
+    },
+    resetAnimateToRegulatoryLayer(state) {
       state.animateToRegulatoryLayer = null
     },
-    setVesselLabelsShowedOnMap (state, action) {
-      window.localStorage.setItem(vesselLabelsShowedOnMapLocalStorageKey, JSON.stringify(action.payload))
-      state.vesselLabelsShowedOnMap = action.payload
+    /**
+     * Reset the interaction with the OpenLayers map
+     * @param {Object=} state
+     */
+    resetInteraction(state) {
+      state.interaction = null
     },
-    setRiskFactorShowedOnMap (state, action) {
-      window.localStorage.setItem(riskFactorLocalStorageKey, JSON.stringify(action.payload))
-      state.riskFactorShowedOnMap = action.payload
-    },
-    setVesselsLastPositionVisibility (state, action) {
-      window.localStorage.setItem(vesselsLastPositionVisibilityLocalStorageKey, JSON.stringify(action.payload))
-      state.vesselsLastPositionVisibility = action.payload
-    },
-    setDefaultVesselTrackDepth (state, action) {
-      window.localStorage.setItem(vesselTrackDepthLocalStorageKey, JSON.stringify(action.payload))
-      state.defaultVesselTrackDepth = action.payload
-    },
-    setVesselLabel (state, action) {
-      window.localStorage.setItem(vesselLabelLocalStorageKey, JSON.stringify(action.payload))
-      state.vesselLabel = action.payload
-    },
-    selectBaseLayer (state, action) {
+
+    selectBaseLayer(state, action) {
       window.localStorage.setItem(baseLayerLocalStorageKey, JSON.stringify(action.payload))
       state.selectedBaseLayer = action.payload
     },
+
+    /**
+     * Set the coordinate format in the whole application (as DMS, DMD or DD)
+     * @param {Object} state
+     * @param {{
+     * payload: CoordinatesFormat}} action - The coordinate format
+     */
+    setCoordinatesFormat(state, action) {
+      window.localStorage.setItem(coordinatesFormatLocalStorageKey, JSON.stringify(action.payload))
+      state.coordinatesFormat = action.payload
+    },
+
+    setDefaultVesselTrackDepth(state, action) {
+      window.localStorage.setItem(vesselTrackDepthLocalStorageKey, JSON.stringify(action.payload))
+      state.defaultVesselTrackDepth = action.payload
+    },
+
+    /**
+     * Show or hide the vessels located in a port
+     * @param {Object} state
+     * @param {{
+     * payload: boolean}} action - true if the vessels at port are hidden
+     */
+    setHideVesselsAtPort(state, action) {
+      window.localStorage.setItem(hideVesselsAtPortLocalStorageKey, JSON.stringify(action.payload))
+      state.hideVesselsAtPort = action.payload
+    },
+
     /**
      * Start an interaction with the OpenLayers map, hence use the mouse to draw geometries
      * @param {Object} state
@@ -112,68 +164,62 @@ const mapSlice = createSlice({
      *   }
      * }} action - The interaction type (see InteractionTypes enum) and listener (see layersType enum)
      */
-    setInteraction (state, action) {
+    setInteraction(state, action) {
       state.interaction = action.payload
     },
-    /**
-     * Reset the interaction with the OpenLayers map
-     * @param {Object=} state
-     */
-    resetInteraction (state) {
-      state.interaction = null
+
+    setRiskFactorShowedOnMap(state, action) {
+      window.localStorage.setItem(riskFactorLocalStorageKey, JSON.stringify(action.payload))
+      state.riskFactorShowedOnMap = action.payload
     },
-    /**
-     * Set the coordinate format in the whole application (as DMS, DMD or DD)
-     * @param {Object} state
-     * @param {{
-     * payload: CoordinatesFormat}} action - The coordinate format
-     */
-    setCoordinatesFormat (state, action) {
-      window.localStorage.setItem(coordinatesFormatLocalStorageKey, JSON.stringify(action.payload))
-      state.coordinatesFormat = action.payload
+
+    setVesselLabel(state, action) {
+      window.localStorage.setItem(vesselLabelLocalStorageKey, JSON.stringify(action.payload))
+      state.vesselLabel = action.payload
     },
+
+    setVesselLabelsShowedOnMap(state, action) {
+      window.localStorage.setItem(vesselLabelsShowedOnMapLocalStorageKey, JSON.stringify(action.payload))
+      state.vesselLabelsShowedOnMap = action.payload
+    },
+
+    setVesselsLastPositionVisibility(state, action) {
+      window.localStorage.setItem(vesselsLastPositionVisibilityLocalStorageKey, JSON.stringify(action.payload))
+      state.vesselsLastPositionVisibility = action.payload
+    },
+
     /**
      * Show or hide the vessels current estimated positions
      * @param {Object} state
      * @param {{
      * payload: boolean}} action
      */
-    showVesselsEstimatedPositions (state, action) {
+    showVesselsEstimatedPositions(state, action) {
       window.localStorage.setItem(estimatedPositionsLocalStorageKey, JSON.stringify(action.payload))
       state.showingVesselsEstimatedPositions = action.payload
-    },
-    /**
-     * Show or hide the vessels located in a port
-     * @param {Object} state
-     * @param {{
-     * payload: boolean}} action - true if the vessels at port are hidden
-     */
-    setHideVesselsAtPort (state, action) {
-      window.localStorage.setItem(hideVesselsAtPortLocalStorageKey, JSON.stringify(action.payload))
-      state.hideVesselsAtPort = action.payload
     }
   }
 })
 
 export const {
   animateToCoordinates,
-  resetAnimateToCoordinates,
   animateToExtent,
-  resetAnimateToExtent,
   animateToRegulatoryLayer,
+  doNotAnimate,
+  resetAnimateToCoordinates,
+  resetAnimateToExtent,
   resetAnimateToRegulatoryLayer,
+  resetInteraction,
+  selectBaseLayer,
+  setCoordinatesFormat,
+  setDefaultVesselTrackDepth,
+  setHideVesselsAtPort,
+  setInteraction,
+  setRiskFactorShowedOnMap,
+  setVesselLabel,
   setVesselLabelsShowedOnMap,
   setVesselsLastPositionVisibility,
-  setDefaultVesselTrackDepth,
-  setVesselLabel,
-  selectBaseLayer,
-  setInteraction,
-  resetInteraction,
-  showVesselsEstimatedPositions,
-  doNotAnimate,
-  setCoordinatesFormat,
-  setRiskFactorShowedOnMap,
-  setHideVesselsAtPort
+  showVesselsEstimatedPositions
 } = mapSlice.actions
 
-export default mapSlice.reducer
+export const mapReducer = mapSlice.reducer

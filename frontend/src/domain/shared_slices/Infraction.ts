a47cb6c309c0cdc05caf8efa2c0fcde543846c41
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-09-14 14:13:09 +0200

    Type Infraction shared slice state

--- frontend/src/domain/shared_slices/Infraction.ts
+++ frontend/src/domain/shared_slices/Infraction.ts
@@ -1,19 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+import type { Infraction } from '../types/control'
+
+export type InfractionState = {
+  infractions: Infraction[]
+}
+const INITIAL_STATE: InfractionState = {
+  infractions: []
+}
+
 const infractionSlice = createSlice({
+  initialState: INITIAL_STATE,
   name: 'infraction',
-  initialState: {
-    infractions: []
-  },
   reducers: {
-    setInfractions (state, action) {
+    setInfractions(state, action) {
       state.infractions = action.payload
     }
   }
 })
 
-export const {
-  setInfractions
-} = infractionSlice.actions
+export const { setInfractions } = infractionSlice.actions
 
-export default infractionSlice.reducer
+export const infractionReducer = infractionSlice.reducer

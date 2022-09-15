62588029ee70de106ad44a714ae571728a51c91f
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-09-15 16:03:54 +0200

    Lint frontend alert entity file

--- frontend/src/domain/entities/alerts.ts
+++ frontend/src/domain/entities/alerts.ts
@@ -1,35 +1,35 @@
-import { getDate } from '../../utils'
 import Fuse from 'fuse.js'
 import _ from 'lodash'
 
+import { getDate } from '../../utils'
+
 export const AlertType = {
-  PNO_LAN_WEIGHT_TOLERANCE_ALERT: {
-    code: 'PNO_LAN_WEIGHT_TOLERANCE_ALERT',
-    name: 'Tolérance 10% non respectée',
-    isOperationalAlert: false,
-    nameWithAlertDetails: (percentOfTolerance, minimumWeightThreshold) => {
-      return `Tolérance de ${percentOfTolerance}% non respectée, appliquée pour un poids minimum de ${minimumWeightThreshold}kg.`
-    }
-  },
-  THREE_MILES_TRAWLING_ALERT: {
-    code: 'THREE_MILES_TRAWLING_ALERT',
-    name: '3 milles - Chaluts',
-    isOperationalAlert: true
-  },
   FRENCH_EEZ_FISHING_ALERT: {
     code: 'FRENCH_EEZ_FISHING_ALERT',
-    name: 'Pêche en ZEE française par un navire tiers',
-    isOperationalAlert: true
-  },
-  TWELVE_MILES_FISHING_ALERT: {
-    code: 'TWELVE_MILES_FISHING_ALERT',
-    name: '12 milles - Pêche sans droits historiques',
-    isOperationalAlert: true
+    isOperationalAlert: true,
+    name: 'Pêche en ZEE française par un navire tiers'
   },
   MISSING_FAR_ALERT: {
     code: 'MISSING_FAR_ALERT',
-    name: 'Non-emission de message "FAR"',
-    isOperationalAlert: true
+    isOperationalAlert: true,
+    name: 'Non-emission de message "FAR"'
+  },
+  PNO_LAN_WEIGHT_TOLERANCE_ALERT: {
+    code: 'PNO_LAN_WEIGHT_TOLERANCE_ALERT',
+    isOperationalAlert: false,
+    name: 'Tolérance 10% non respectée',
+    nameWithAlertDetails: (percentOfTolerance, minimumWeightThreshold) =>
+      `Tolérance de ${percentOfTolerance}% non respectée, appliquée pour un poids minimum de ${minimumWeightThreshold}kg.`
+  },
+  THREE_MILES_TRAWLING_ALERT: {
+    code: 'THREE_MILES_TRAWLING_ALERT',
+    isOperationalAlert: true,
+    name: '3 milles - Chaluts'
+  },
+  TWELVE_MILES_FISHING_ALERT: {
+    code: 'TWELVE_MILES_FISHING_ALERT',
+    isOperationalAlert: true,
+    name: '12 milles - Pêche sans droits historiques'
   }
 }
 
@@ -37,55 +37,47 @@ export const operationalAlertTypes = Object.keys(AlertType)
   .map(alertTypeName => AlertType[alertTypeName])
   .filter(alertType => alertType.isOperationalAlert)
 
-export const getAlertNameFromType = type => {
-  return AlertType[type]
-    ? AlertType[type].name
-    : 'Alerte inconnue'
-}
+export const getAlertNameFromType = type => (AlertType[type] ? AlertType[type].name : 'Alerte inconnue')
 
 export const AlertsSubMenu = {
+  MED: {
+    code: 'MED',
+    name: 'MED'
+  },
   MEMN: {
-    name: 'MEMN',
-    code: 'MEMN'
+    code: 'MEMN',
+    name: 'MEMN'
   },
   NAMO: {
-    name: 'NAMO',
-    code: 'NAMO'
-  },
-  SA: {
-    name: 'SA',
-    code: 'SA'
-  },
-  MED: {
-    name: 'MED',
-    code: 'MED'
+    code: 'NAMO',
+    name: 'NAMO'
   },
   OUTREMEROA: {
-    name: 'OUTRE-MER OA',
-    code: 'OUTREMEROA'
+    code: 'OUTREMEROA',
+    name: 'OUTRE-MER OA'
   },
   OUTREMEROI: {
-    name: 'OUTRE-MER OI',
-    code: 'OUTREMEROI'
+    code: 'OUTREMEROI',
+    name: 'OUTRE-MER OI'
+  },
+  SA: {
+    code: 'SA',
+    name: 'SA'
   }
 }
 
 export const AlertsMenuSeaFrontsToSeaFrontList = {
+  MED: {
+    menuSeaFront: 'MED',
+    seaFronts: ['MED']
+  },
   MEMN: {
     menuSeaFront: 'MEMN',
     seaFronts: ['MEMN']
   },
-  SA: {
-    menuSeaFront: 'SA',
-    seaFronts: ['SA']
-  },
   NAMO: {
     menuSeaFront: 'NAMO',
     seaFronts: ['NAMO']
-  },
-  MED: {
-    menuSeaFront: 'MED',
-    seaFronts: ['MED']
   },
   OUTREMEROA: {
     menuSeaFront: 'OUTREMEROA',
@@ -94,20 +86,24 @@ export const AlertsMenuSeaFrontsToSeaFrontList = {
   OUTREMEROI: {
     menuSeaFront: 'OUTREMEROI',
     seaFronts: ['Sud Océan Indien']
+  },
+  SA: {
+    menuSeaFront: 'SA',
+    seaFronts: ['SA']
   }
 }
 
 export const SilencedAlertPeriod = {
-  THIS_OCCURRENCE: 'THIS_OCCURRENCE',
-  ONE_HOUR: 'ONE_HOUR',
-  TWO_HOURS: 'TWO_HOURS',
-  SIX_HOURS: 'SIX_HOURS',
-  TWELVE_HOURS: 'TWELVE_HOURS',
+  CUSTOM: 'CUSTOM',
   ONE_DAY: 'ONE_DAY',
-  ONE_WEEK: 'ONE_WEEK',
+  ONE_HOUR: 'ONE_HOUR',
   ONE_MONTH: 'ONE_MONTH',
+  ONE_WEEK: 'ONE_WEEK',
   ONE_YEAR: 'ONE_YEAR',
-  CUSTOM: 'CUSTOM'
+  SIX_HOURS: 'SIX_HOURS',
+  THIS_OCCURRENCE: 'THIS_OCCURRENCE',
+  TWELVE_HOURS: 'TWELVE_HOURS',
+  TWO_HOURS: 'TWO_HOURS'
 }
 
 export const getSilencedAlertPeriodText = silencedAlertPeriodRequest => {
@@ -140,30 +136,26 @@ export const getSilencedAlertPeriodText = silencedAlertPeriodRequest => {
       return 'pendant 1 année'
     }
     case SilencedAlertPeriod.CUSTOM: {
-      return `du ${getDate(silencedAlertPeriodRequest.afterDateTime)} au ${getDate(silencedAlertPeriodRequest.beforeDateTime)}`
+      return `du ${getDate(silencedAlertPeriodRequest.afterDateTime)} au ${getDate(
+        silencedAlertPeriodRequest.beforeDateTime
+      )}`
     }
   }
 }
 
-const alertTypeKey = ["value", "type"]
+const alertTypeKey = ['value', 'type']
 export const alertSearchOptions = {
-  includeScore: true,
   distance: 50,
-  threshold: 0.4,
-  keys: [
-    'vesselName',
-    'internalReferenceNumber',
-    'externalReferenceNumber',
-    'ircs',
-    alertTypeKey,
-  ],
   getFn: (alert, path) => {
     const value = Fuse.config.getFn(alert, path)
 
-    if (_.isEqual(path,alertTypeKey)) {
+    if (_.isEqual(path, alertTypeKey)) {
       return getAlertNameFromType(alert.value.type)
     }
 
     return value
-  }
+  },
+  includeScore: true,
+  keys: ['vesselName', 'internalReferenceNumber', 'externalReferenceNumber', 'ircs', alertTypeKey],
+  threshold: 0.4
 }

438659d650ae3711a0d216a69de6a9a11c48291f
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-09-20 10:31:42 +0200

    Refactor frontend alert api

--- frontend/src/domain/types/alert.ts
+++ frontend/src/domain/types/alert.ts
@@ -1,48 +1,64 @@
-export type Alert = {
-  creationDate: string
+// TODO Find a way to associate `Alert`, `PendingAlert` and `SilencedAlert` via data/extra props.
+
+export enum AlertType {
+  'ACTIVE' = 'ACTIVE',
+  'SILENCED' = 'SILENCED'
+}
+
+export type LEGACY_Alert = {
   externalReferenceNumber: string
   id: string
   internalReferenceNumber: string
   ircs: string
-  tripNumber: number
-  type: string
-  value: PendingAlert | PNOAndLANWeightToleranceAlert
+  value: AlertValueForPending | AlertValueForPNOAndLANWeightTolerance
   vesselIdentifier: string
   vesselName: string
 }
 
-export type PendingAlert = {
+export type LEGACY_ActiveAlert = LEGACY_Alert & {
+  creationDate: string
+  infraction: {
+    infraction: string
+    infractionCategory: string
+    natinfCode: string
+    regulation: string
+  }
+  tripNumber: string
+}
+
+export type ActiveAlert = LEGACY_ActiveAlert & {
+  type: AlertType.ACTIVE
+}
+
+export type LEGACY_SilencedAlert = LEGACY_Alert & {
+  isReactivated: boolean | null
+  silencedAfterDate: Date
+  silencedBeforeDate: Date
+}
+
+export type SilencedAlert = LEGACY_SilencedAlert & {
+  type: AlertType.SILENCED
+}
+
+export type AlertValueForPending = {
   flagState: string
   natinfCode: string | null
   seaFront: string
   speed: number
-  type: string
+  type: '' // ???
 }
 
-export type SilencedAlert = {
-  externalReferenceNumber: string
-  id: string
-  internalReferenceNumber: string
-  ircs: string
-  isReactivated: boolean | null
-  silencedAfterDate: Date
-  silencedBeforeDate: Date
-  value: PendingAlert | PNOAndLANWeightToleranceAlert
-  vesselIdentifier: string
-  vesselName: string
-}
-
-export type PNOAndLANWeightToleranceAlert = {
-  catchesOverTolerance: PNOAndLANCatches[]
+export type AlertValueForPNOAndLANWeightTolerance = {
+  catchesOverTolerance: PNOAndLANWeightToleranceCatches[]
   lanOperationNumber: string
   minimumWeightThreshold: number
   name: string
   percentOfTolerance: number
   pnoOperationNumber: string
-  type: string
+  type: 'THREE_MILES_TRAWLING_ALERT' | 'FRENCH_EEZ_FISHING_ALERT' | 'TWELVE_MILES_FISHING_ALERT' | 'MISSING_FAR_ALERT'
 }
 
-export type PNOAndLANCatches = {
+export type PNOAndLANWeightToleranceCatches = {
   lan: Object
   pno: Object
 }

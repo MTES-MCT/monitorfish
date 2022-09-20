475422cba38cf05142c6edcbfd35c473b67685ed
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-09-20 09:35:32 +0200

    Add boolean prefix ESLint rule mitigated via warn instead of error

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
+const BOOLEAN_CAMEL_PREFIXES = ['can', 'did', 'is', 'had', 'has', 'must', 'should', 'was', 'will']
+const BOOLEAN_UPPER_PREFIXES = ['CAN_', 'DID_', 'IS_', 'HAD_', 'HAS_', 'MUST_', 'SHOULD_', 'WAS_', 'WILL_']
+
 module.exports = {
   extends: '@ivangabriele/eslint-config-typescript-react',
   parser: '@typescript-eslint/parser',
@@ -12,6 +15,65 @@ module.exports = {
     browser: true
   },
   rules: {
+    // We must add PascalCase in formats because ESLint trim the prefix before evaluating the case
+    // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/naming-convention.md#format-options
+    // > Note: As documented above, the prefix is trimmed before format is validated,
+    // > therefore PascalCase must be used to allow variables such as isEnabled using the prefix is.
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE']
+      },
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase']
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+        filter: {
+          regex: '^LEGACY_',
+          match: false
+        }
+      },
+      {
+        selector: 'accessor',
+        types: ['boolean'],
+        format: ['camelCase', 'PascalCase'],
+        prefix: BOOLEAN_CAMEL_PREFIXES
+      },
+      {
+        selector: 'classProperty',
+        types: ['boolean'],
+        format: ['camelCase', 'PascalCase'],
+        prefix: BOOLEAN_CAMEL_PREFIXES
+      },
+      {
+        selector: 'objectLiteralProperty',
+        types: ['boolean'],
+        format: ['camelCase', 'PascalCase'],
+        prefix: BOOLEAN_CAMEL_PREFIXES
+      },
+      {
+        selector: 'parameter',
+        types: ['boolean'],
+        format: ['camelCase', 'PascalCase'],
+        prefix: BOOLEAN_CAMEL_PREFIXES
+      },
+      {
+        selector: 'parameterProperty',
+        types: ['boolean'],
+        format: ['camelCase', 'PascalCase'],
+        prefix: BOOLEAN_CAMEL_PREFIXES
+      },
+      {
+        selector: 'variable',
+        types: ['boolean'],
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        prefix: [...BOOLEAN_CAMEL_PREFIXES, ...BOOLEAN_UPPER_PREFIXES]
+      }
+    ],
     '@typescript/no-use-before-define': 'off',
     '@typescript-eslint/no-restricted-imports': [
       'error',
@@ -23,9 +85,8 @@ module.exports = {
     ],
     '@typescript-eslint/no-use-before-define': 'off',
 
-
     // See https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/default_props/#you-may-not-need-defaultprops
-    "react/require-default-props": "off",
+    'react/require-default-props': 'off',
     'react/react-in-jsx-scope': 'off'
   },
   overrides: [

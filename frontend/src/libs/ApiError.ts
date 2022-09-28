53bea0d9e54d10d0edef9830e50e83d0b759c669
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-09-28 17:07:10 +0200

    Remove useless type prop in ApiError

--- frontend/src/libs/ApiError.ts
+++ frontend/src/libs/ApiError.ts
@@ -1,10 +1,6 @@
 export class ApiError extends Error {
-  public type: 'API'
-
   constructor(message: string, err: any) {
     super(message)
-
-    this.type = 'API'
 
     // eslint-disable-next-line no-console
     console.error(err)

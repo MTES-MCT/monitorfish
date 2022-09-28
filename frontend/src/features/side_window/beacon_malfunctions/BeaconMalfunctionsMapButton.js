99e98617d0b28b568313827c8a1e35e904309577
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-09-28 17:56:39 +0200

    Fix SIDE_WINDOW_MENU const in BeaconMalfunctionsMapButton

--- frontend/src/features/side_window/beacon_malfunctions/BeaconMalfunctionsMapButton.js
+++ frontend/src/features/side_window/beacon_malfunctions/BeaconMalfunctionsMapButton.js
@@ -5,47 +5,47 @@ import { ReactComponent as BeaconMalfunctionsSVG } from '../../icons/Icone_VMS.s
 import { COLORS } from '../../../constants/constants'
 import { MapButtonStyle } from '../../commonStyles/MapButton.style'
 import { closeSideWindow, openSideWindowTab } from '../../../domain/shared_slices/Global'
-import { sideWindowMenu } from '../../../domain/entities/sideWindow'
+import { SIDE_WINDOW_MENU } from '../constants'
 
 const BeaconMalfunctionsMapButton = () => {
   const dispatch = useDispatch()
-  const {
-    regulatoryZoneMetadataPanelIsOpen
-  } = useSelector(state => state.regulatory)
-  const {
-    healthcheckTextWarning,
-    previewFilteredVesselsMode,
-    openedSideWindowTab,
-    sideWindowIsOpen
-  } = useSelector(state => state.global)
+  const { regulatoryZoneMetadataPanelIsOpen } = useSelector(state => state.regulatory)
+  const { healthcheckTextWarning, previewFilteredVesselsMode, openedSideWindowTab, sideWindowIsOpen } = useSelector(
+    state => state.global
+  )
 
-  return <BeaconMalfunctionsButton
-    data-cy={'beacon-malfunction-button'}
-    title={'Avaries VMS'}
-    isVisible={openedSideWindowTab === sideWindowMenu.BEACON_MALFUNCTIONS.code}
-    regulatoryZoneMetadataPanelIsOpen={regulatoryZoneMetadataPanelIsOpen}
-    healthcheckTextWarning={healthcheckTextWarning}
-    isHidden={previewFilteredVesselsMode}
-    onClick={() => {
-      if (!sideWindowIsOpen || (sideWindowIsOpen && openedSideWindowTab !== sideWindowMenu.BEACON_MALFUNCTIONS.code)) {
-        dispatch(openSideWindowTab(sideWindowMenu.BEACON_MALFUNCTIONS.code))
-        return
-      }
+  return (
+    <BeaconMalfunctionsButton
+      data-cy={'beacon-malfunction-button'}
+      title={'Avaries VMS'}
+      isVisible={openedSideWindowTab === SIDE_WINDOW_MENU.BEACON_MALFUNCTIONS.code}
+      regulatoryZoneMetadataPanelIsOpen={regulatoryZoneMetadataPanelIsOpen}
+      healthcheckTextWarning={healthcheckTextWarning}
+      isHidden={previewFilteredVesselsMode}
+      onClick={() => {
+        if (
+          !sideWindowIsOpen ||
+          (sideWindowIsOpen && openedSideWindowTab !== SIDE_WINDOW_MENU.BEACON_MALFUNCTIONS.code)
+        ) {
+          dispatch(openSideWindowTab(SIDE_WINDOW_MENU.BEACON_MALFUNCTIONS.code))
+          return
+        }
 
-      if (sideWindowIsOpen && openedSideWindowTab === sideWindowMenu.BEACON_MALFUNCTIONS.code) {
-        dispatch(closeSideWindow())
-      }
-    }}
-  >
-    <BeaconMalfunctionsIcon/>
-  </BeaconMalfunctionsButton>
+        if (sideWindowIsOpen && openedSideWindowTab === SIDE_WINDOW_MENU.BEACON_MALFUNCTIONS.code) {
+          dispatch(closeSideWindow())
+        }
+      }}
+    >
+      <BeaconMalfunctionsIcon />
+    </BeaconMalfunctionsButton>
+  )
 }
 
 const BeaconMalfunctionsButton = styled(MapButtonStyle)`
   position: absolute;
   display: inline-block;
   color: ${COLORS.blue};
-  background: ${props => props.isVisible ? COLORS.shadowBlue : COLORS.charcoal};
+  background: ${props => (props.isVisible ? COLORS.shadowBlue : COLORS.charcoal)};
   padding: 2px 2px 2px 2px;
   top: 162px;
   left: 12px;
@@ -53,8 +53,9 @@ const BeaconMalfunctionsButton = styled(MapButtonStyle)`
   height: 40px;
   width: 40px;
 
-  :hover, :focus {
-      background: ${props => props.isVisible ? COLORS.shadowBlue : COLORS.charcoal};
+  :hover,
+  :focus {
+    background: ${props => (props.isVisible ? COLORS.shadowBlue : COLORS.charcoal)};
   }
 `
 

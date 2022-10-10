b87ddc7845d38c69fa060c0a1ede2dfde6a7d7ed
Loup Theron <loup-rodney@hotmail.fr>
2022-10-10 16:50:59 +0200

    Prevent dropping to ARCHIVED and group by date time without nano seconds

--- backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/GetBeaconMalfunctionUTests.kt
+++ backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/GetBeaconMalfunctionUTests.kt
@@ -40,31 +40,41 @@ class GetBeaconMalfunctionUTests {
         given(beaconMalfunctionsRepository.find(1))
             .willReturn(BeaconMalfunction(1, "FR224226850", "1236514", "IRCS",
                 null, VesselIdentifier.INTERNAL_REFERENCE_NUMBER, "BIDUBULE", VesselStatus.AT_SEA, Stage.END_OF_MALFUNCTION,
-                 ZonedDateTime.now(), null, ZonedDateTime.now(),
+                ZonedDateTime.now(), null, ZonedDateTime.now(),
                 beaconNumber = "123465", beaconStatusAtMalfunctionCreation = BeaconStatus.ACTIVATED))
         given(beaconMalfunctionsRepository.findAllByVesselIdentifierEquals(eq(VesselIdentifier.INTERNAL_REFERENCE_NUMBER), eq("FR224226850"), any()))
             .willReturn(listOf(
                 BeaconMalfunction(1, "FR224226850", "1236514", "IRCS",
                     null, VesselIdentifier.INTERNAL_REFERENCE_NUMBER, "BIDUBULE", VesselStatus.AT_SEA, Stage.END_OF_MALFUNCTION,
-                     ZonedDateTime.now(), null, ZonedDateTime.now(),
+                    ZonedDateTime.now(), null, ZonedDateTime.now(),
                     beaconNumber = "123465", beaconStatusAtMalfunctionCreation = BeaconStatus.ACTIVATED),
                 BeaconMalfunction(2, "FR224226850", "1236514", "IRCS",
                     null, VesselIdentifier.INTERNAL_REFERENCE_NUMBER, "BIDUBULE", VesselStatus.AT_SEA, Stage.INITIAL_ENCOUNTER,
-                     ZonedDateTime.now(), null, ZonedDateTime.now(),
+                    ZonedDateTime.now(), null, ZonedDateTime.now(),
                     beaconNumber = "123465", beaconStatusAtMalfunctionCreation = BeaconStatus.ACTIVATED)))
         given(beaconMalfunctionCommentsRepository.findAllByBeaconMalfunctionId(1)).willReturn(listOf(BeaconMalfunctionComment(
             beaconMalfunctionId = 1, comment = "A comment", userType = BeaconMalfunctionCommentUserType.SIP, dateTime = now)))
         given(beaconMalfunctionActionsRepository.findAllByBeaconMalfunctionId(1)).willReturn(listOf(BeaconMalfunctionAction(
             beaconMalfunctionId = 1, propertyName = BeaconMalfunctionActionPropertyName.VESSEL_STATUS, nextValue = VesselStatus.ACTIVITY_DETECTED.toString(), previousValue = VesselStatus.AT_PORT.toString(), dateTime = now)))
         given(beaconMalfunctionNotificationsRepository.findAllByBeaconMalfunctionId(1)).willReturn(
-            listOf(
+            listOf(BeaconMalfunctionNotification(
+                id = 1, beaconMalfunctionId = 1, dateTimeUtc = now,
+                notificationType = BeaconMalfunctionNotificationType.MALFUNCTION_AT_PORT_INITIAL_NOTIFICATION,
+                communicationMeans = CommunicationMeans.SMS,
+                recipientFunction = BeaconMalfunctionNotificationRecipientFunction.VESSEL_CAPTAIN,
+                recipientName = "Jack Sparrow", recipientAddressOrNumber = "0000000000", success = true),
                 BeaconMalfunctionNotification(
-                    id = 1, beaconMalfunctionId = 1, dateTimeUtc = now,
-                    notificationType = BeaconMalfunctionNotificationType.MALFUNCTION_AT_PORT_INITIAL_NOTIFICATION,
+                    id = 2, beaconMalfunctionId = 1, dateTimeUtc = now.plusDays(2),
+                    notificationType = BeaconMalfunctionNotificationType.MALFUNCTION_AT_PORT_REMINDER,
                     communicationMeans = CommunicationMeans.SMS,
                     recipientFunction = BeaconMalfunctionNotificationRecipientFunction.VESSEL_CAPTAIN,
-                    recipientName = "Jack Sparrow", recipientAddressOrNumber = "0000000000", success = true
-                )
+                    recipientName = "Jack Sparrow", recipientAddressOrNumber = "0000000000", success = true),
+                BeaconMalfunctionNotification(
+                    id = 3, beaconMalfunctionId = 1, dateTimeUtc = now.plusNanos(123),
+                    notificationType = BeaconMalfunctionNotificationType.MALFUNCTION_AT_PORT_INITIAL_NOTIFICATION,
+                    communicationMeans = CommunicationMeans.EMAIL,
+                    recipientFunction = BeaconMalfunctionNotificationRecipientFunction.VESSEL_CAPTAIN,
+                    recipientName = "Jack Sparrow", recipientAddressOrNumber = "0000000000", success = true)
             )
         )
 
@@ -84,13 +94,20 @@ class GetBeaconMalfunctionUTests {
         assertThat(beaconMalfunctions.resume?.lastBeaconMalfunctionVesselStatus).isEqualTo(VesselStatus.AT_SEA)
         assertThat(beaconMalfunctions.actions).hasSize(1)
         assertThat(beaconMalfunctions.comments).hasSize(1)
-        assertThat(beaconMalfunctions.notifications).hasSize(1)
+
+        assertThat(beaconMalfunctions.notifications).hasSize(2)
         assertThat(beaconMalfunctions.notifications[0].beaconMalfunctionId).isEqualTo(1)
-        assertThat(beaconMalfunctions.notifications[0].notificationType).isEqualTo(
-            BeaconMalfunctionNotificationType.MALFUNCTION_AT_PORT_INITIAL_NOTIFICATION
-        )
-        assertThat(beaconMalfunctions.notifications[0].dateTimeUtc).isEqualTo(now)
+        assertThat(beaconMalfunctions.notifications[0].notificationType).isEqualTo(BeaconMalfunctionNotificationType.MALFUNCTION_AT_PORT_INITIAL_NOTIFICATION)
+        assertThat(beaconMalfunctions.notifications[0].dateTimeUtc).isEqualTo(now.withNano(0))
+        assertThat(beaconMalfunctions.notifications[0].notifications).hasSize(2)
         assertThat(beaconMalfunctions.notifications[0].notifications[0].recipientName).isEqualTo("Jack Sparrow")
+
+        assertThat(beaconMalfunctions.notifications[1].beaconMalfunctionId).isEqualTo(1)
+        assertThat(beaconMalfunctions.notifications[1].notificationType).isEqualTo(BeaconMalfunctionNotificationType.MALFUNCTION_AT_PORT_REMINDER)
+        assertThat(beaconMalfunctions.notifications[1].dateTimeUtc).isEqualTo(now.withNano(0).plusDays(2))
+        assertThat(beaconMalfunctions.notifications[1].notifications).hasSize(1)
+        assertThat(beaconMalfunctions.notifications[1].notifications[0].recipientName).isEqualTo("Jack Sparrow")
+
         assertThat(beaconMalfunctions.beaconMalfunction.internalReferenceNumber).isEqualTo("FR224226850")
     }
 }

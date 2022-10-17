e19faffc7e01fd78a199f05a7f779b2b5755ade5
Loup Theron <loup-rodney@hotmail.fr>
2022-10-17 14:12:07 +0200

    Add dml to PendingAlert class

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/alerts/type/MissingFARAlert.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/alerts/type/MissingFARAlert.kt
@@ -2,6 +2,7 @@ package fr.gouv.cnsp.monitorfish.domain.entities.alerts.type
 
 class MissingFARAlert(
     override var seaFront: String? = null,
+    override var dml: String? = null,
     var flagState: String? = null,
     var riskFactor: Double? = null
-) : AlertType(AlertTypeMapping.MISSING_FAR_ALERT, seaFront, "27689")
+) : AlertType(AlertTypeMapping.MISSING_FAR_ALERT, seaFront, dml, "27689")

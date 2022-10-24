990c73e83c830b33e369f9a872c9c9b0c5524b3c
Vincent <vincent.chery@m4x.org>
2022-10-24 17:21:01 +0200

    Remove malfunctions of non ACTIVATED beacons from last_positions

--- datascience/src/pipeline/queries/monitorfish/beacon_malfunctions_for_last_positions.sql
+++ datascience/src/pipeline/queries/monitorfish/beacon_malfunctions_for_last_positions.sql
@@ -1,7 +1,11 @@
 SELECT
-    id,
-    internal_reference_number AS cfr,
-    external_reference_number AS external_immatriculation,
-    ircs
-FROM beacon_malfunctions
-WHERE stage NOT IN ('END_OF_MALFUNCTION', 'ARCHIVED')
+    bm.id,
+    bm.internal_reference_number AS cfr,
+    bm.external_reference_number AS external_immatriculation,
+    bm.ircs
+FROM beacon_malfunctions bm
+JOIN beacons b
+ON b.beacon_number = bm.beacon_number
+WHERE
+    bm.stage NOT IN ('END_OF_MALFUNCTION', 'ARCHIVED') AND
+    b.beacon_status = 'ACTIVATED'

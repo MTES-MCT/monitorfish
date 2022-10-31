96c00a8df5a85228e305fc673eb43d6001338c2b
Vincent <vincent.chery@m4x.org>
2022-10-31 16:04:32 +0100

    Handle url aliases in get_unknown_links

--- datascience/tests/test_data/remote_database/V666.24__Reset_test_regulations.sql
+++ datascience/tests/test_data/remote_database/V666.24__Reset_test_regulations.sql
@@ -6,5 +6,5 @@ INSERT INTO public.regulations (
     (1, 'Reg. Facade 1',   'Morbihan - bivalves', 'Secteur 1',                                                                                                                                 'null'),
     (2, 'Reg. Facade 1',   'Morbihan - bivalves', 'Secteur 2', '[{"url": "http://some.url", "reference": "some regulation"}, {"url": "http://some.other.url", "reference": "some other regulation"}]'),
     (3, 'Reg. Facade 2', 'Mediterranée - filets',    'Zone A',                                                                                                                                 'null'),
-    (4, 'Reg. Facade 2', 'Mediterranée - filets',    'Zone B',                                                                    '[{"url": "http://regulation.url", "reference": "Med regulation"}]'),
+    (4, 'Reg. Facade 2', 'Mediterranée - filets',    'Zone B',                           '[{"url": "http://regulation.intranets.developpement-durable.ader.gouv.fr", "reference": "Med regulation"}]'),
     (5, 'Reg. Facade 2', 'Mediterranée - filets',    'Zone C',                                                    '[{"url": "http://dead_link.regulation.url", "reference": "Dead link regulation"}]');

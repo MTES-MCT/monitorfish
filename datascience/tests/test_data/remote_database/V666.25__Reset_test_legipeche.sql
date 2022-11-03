2a20d8a2c5f13d2c8eb12f5939ebc9f3c7e9f87f
Vincent <vincent.chery@m4x.org>
2022-11-03 17:24:24 +0100

    Use legipeche article ids in regulations checkup flow

--- datascience/tests/test_data/remote_database/V666.25__Reset_test_legipeche.sql
+++ datascience/tests/test_data/remote_database/V666.25__Reset_test_legipeche.sql
@@ -1,17 +1,17 @@
 DELETE FROM public.legipeche;
 
 INSERT INTO public.legipeche (
-    extraction_datetime_utc, extraction_occurence,            page_title,                          page_url,         document_title,         document_url
+    extraction_datetime_utc, extraction_occurence,              page_title,                                                           page_url,            document_title,         document_url
 ) VALUES
-    (      '2021-3-2 14:25',           'previous',       'Some old page', 'http://dead_link.regulation.url',    'Some old reg text',  'http://some.thing'),
-    (      '2021-3-2 14:25',           'previous', 'Med. sea regulation',            'http://regulation.i2',         'Med reg text',     'http://med.reg'),
-    (      '2021-3-2 14:25',           'previous', 'Bretagne regulation',                 'http://some.url',    'Bretagne reg text',     'http://bzh.reg'),
-    (      '2021-3-2 14:25',           'previous',  'Bretagne other reg',           'http://some.other.url', 'Bretagne other reg 1', 'http://bzh.other_1'),
-    (      '2021-3-2 14:25',           'previous',  'Bretagne other reg',           'http://some.other.url', 'Bretagne other reg 2', 'http://bzh.other_2'),
-    (      '2021-3-3 14:25',             'latest',  'Bretagne other reg',           'http://some.other.url', 'Bretagne other reg 1', 'http://bzh.other_1'),
-    (      '2021-3-3 14:25',             'latest',  'Bretagne other reg',           'http://some.other.url', 'Bretagne other reg 3', 'http://bzh.other_3'),
-    (      '2021-3-3 14:25',             'latest', 'Bretagne regulation',                 'http://some.url',    'Bretagne reg text',     'http://bzh.reg'),
-    (      '2021-3-2 14:25',           'previous',   'Unused regulation',               'http://unused.url',      'Unused reg text',  'http://unused.reg'),
-    (      '2021-3-3 14:25',             'latest', 'Med. sea regulation',            'http://regulation.i2',         'Med reg text',     'http://med.reg'),
-    (      '2021-3-3 14:25',             'latest',   'Unused regulation',               'http://unused.url',      'Unused reg text',  'http://unused.reg'),
-    (      '2021-3-3 14:25',             'latest', 'Unused regulation 2',             'http://unused_2.url',      'Unused reg text', 'http://unused2.reg');
+    (      '2021-3-2 14:25',           'previous',         'Some old page',          'http://legipeche.metier.i2/deleted-regulation-a671.html',       'Some old reg text',  'http://some.thing'),
+    (      '2021-3-2 14:25',           'previous',   'Med. sea regulation', 'http://legipeche.metier.i2/regulation-with-unstable-url-a689.html',            'Med reg text',     'http://med.reg'),
+    (      '2021-3-2 14:25',           'previous',   'Bretagne regulation',             'http://legipeche.metier.i2/some-regulation-a666.html',       'Bretagne reg text',     'http://bzh.reg'),
+    (      '2021-3-2 14:25',           'previous', 'Bretagne modified reg',         'http://legipeche.metier.i2/modified-regulation-a668.html', 'Bretagne modified reg 1', 'http://bzh.other_1'),
+    (      '2021-3-2 14:25',           'previous', 'Bretagne modified reg',         'http://legipeche.metier.i2/modified-regulation-a668.html', 'Bretagne modified reg 2', 'http://bzh.other_2'),
+    (      '2021-3-3 14:25',             'latest', 'Bretagne modified reg',         'http://legipeche.metier.i2/modified-regulation-a668.html', 'Bretagne modified reg 1', 'http://bzh.other_1'),
+    (      '2021-3-3 14:25',             'latest', 'Bretagne modified reg',         'http://legipeche.metier.i2/modified-regulation-a668.html', 'Bretagne modified reg 3', 'http://bzh.other_3'),
+    (      '2021-3-3 14:25',             'latest',   'Bretagne regulation',             'http://legipeche.metier.i2/some-regulation-a666.html',       'Bretagne reg text',     'http://bzh.reg'),
+    (      '2021-3-2 14:25',           'previous',     'Unused regulation',           'http://legipeche.metier.i2/unused-regulation-a670.html',         'Unused reg text',  'http://unused.reg'),
+    (      '2021-3-3 14:25',             'latest',   'Med. sea regulation', 'http://legipeche.metier.i2/regulation-with-unstable-url-a689.html',            'Med reg text',     'http://med.reg'),
+    (      '2021-3-3 14:25',             'latest',     'Unused regulation',           'http://legipeche.metier.i2/unused-regulation-a670.html',         'Unused reg text',  'http://unused.reg'),
+    (      '2021-3-3 14:25',             'latest',   'Unused regulation 2',     'http://legipeche.metier.i2/other-unused-regulation-a675.html',         'Unused reg text', 'http://unused2.reg');

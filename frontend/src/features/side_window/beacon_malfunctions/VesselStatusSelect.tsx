107a456164ba8a174ec813e4a2ea39e0f258cf3f
Loup Theron <loup-rodney@hotmail.fr>
2022-11-16 17:49:15 +0100

    Add status filter in beacon malfunctions

--- frontend/src/features/side_window/beacon_malfunctions/VesselStatusSelect.tsx
+++ frontend/src/features/side_window/beacon_malfunctions/VesselStatusSelect.tsx
@@ -1,46 +1,71 @@
-import { useRef } from 'react'
+import { useEffect } from 'react'
 import { SelectPicker } from 'rsuite'
 
 import { VESSEL_STATUS } from '../../../domain/entities/beaconMalfunction/constants'
+import { theme } from '../../../ui/theme'
 import { VesselStatusSelectValue } from './VesselStatusSelectValue'
 
 import type { BeaconMalfunction } from '../../../domain/types/beaconMalfunction'
 
 type VesselStatusSelectProps = {
-  beaconMalfunction: BeaconMalfunction
+  beaconMalfunction: BeaconMalfunction | undefined
   domRef: any
-  isAbsolute: boolean
-  updateVesselStatus: (beaconMalfunction: BeaconMalfunction, status: string | null) => void
+  isAbsolute?: boolean
+  isCleanable?: boolean
+  marginTop?: number | undefined
+  updateVesselStatus: (beaconMalfunction: BeaconMalfunction | undefined, status: string | null) => void
   // TODO Type vesselStatus in constants.tsx
-  vesselStatus: { color: string; icon: JSX.Element; label: string; textColor: string; value: string }
+  vesselStatus: { color: string; icon: JSX.Element; label: string; textColor: string; value: string } | undefined
 }
-
 export function VesselStatusSelect({
   beaconMalfunction,
   domRef,
   isAbsolute = false,
+  isCleanable = false,
   updateVesselStatus,
+  marginTop = undefined,
   vesselStatus
 }: VesselStatusSelectProps) {
-  const selectMenuRef = useRef<HTMLSpanElement>(null)
+  useEffect(() => {
+    if (domRef.current) {
+      // TODO Use styled-component and avoid useEffect to update these elements style.
+      const selectElement = domRef.current.querySelector('.rs-picker-select') as HTMLElement
+      if (selectElement?.style) {
+        selectElement.style.background = vesselStatus?.color || theme.color.white
+      }
+
+      const toggleElement = domRef.current.querySelector(
+        '*[data-cy="side-window-beacon-malfunctions-vessel-status"]'
+      ) as HTMLElement
+      if (toggleElement?.style) {
+        toggleElement.style.color = vesselStatus?.textColor || theme.color.charcoal
+      }
+
+      const icons = domRef.current.querySelectorAll('.rs-icon') as HTMLElement[]
+      icons.forEach(icon => {
+        if (icon?.style) {
+          // eslint-disable-next-line no-param-reassign
+          icon.style.color = vesselStatus?.textColor || theme.color.charcoal
+        }
+      })
+    }
+  }, [vesselStatus, beaconMalfunction, domRef])
 
   return (
-    <>
-      <SelectPicker
-        cleanable={false}
-        container={() => domRef.current}
-        data={VESSEL_STATUS}
-        menuStyle={
-          isAbsolute
-            ? { marginLeft: 40, marginTop: 120, position: 'absolute' }
-            : { marginLeft: -5, marginTop: -67, position: 'relative' }
-        }
-        onChange={status => updateVesselStatus(beaconMalfunction, status)}
-        renderValue={(_, item) => <VesselStatusSelectValue item={item} />}
-        searchable={false}
-        value={vesselStatus.value}
-      />
-      <span ref={selectMenuRef} />
-    </>
+    <SelectPicker
+      cleanable={isCleanable}
+      container={() => domRef.current}
+      data={VESSEL_STATUS}
+      menuStyle={
+        isAbsolute
+          ? { marginLeft: 40, marginTop: 120, position: 'absolute' }
+          : { marginLeft: -5, marginTop: marginTop || -67, position: 'relative' }
+      }
+      onChange={status => updateVesselStatus(beaconMalfunction, status)}
+      placeholder="Statut"
+      renderValue={(_, item) => <VesselStatusSelectValue item={item} />}
+      searchable={false}
+      value={vesselStatus?.value || null}
+    />
   )
 }

f323541aa2eca9b3c8763e424e3cb64d5490b707
Loup Theron <loup-rodney@hotmail.fr>
2022-11-17 15:53:50 +0100

    Fix measurement test and refactor custom circle range component

---
+++ frontend/src/features/map/tools/measurements/CustomCircleRange.tsx
@@ -0,0 +1,205 @@
+import { transform } from 'ol/proj'
+import { useCallback, useMemo } from 'react'
+import styled from 'styled-components'
+
+import { COLORS } from '../../../../constants/constants'
+import { coordinatesAreDistinct, getCoordinates } from '../../../../coordinates'
+import {
+  CoordinatesFormat,
+  MeasurementType,
+  OPENLAYERS_PROJECTION,
+  WSG84_PROJECTION
+} from '../../../../domain/entities/map'
+import { setMapToolOpened } from '../../../../domain/shared_slices/Global'
+import {
+  resetCircleMeasurementInDrawing,
+  setCircleMeasurementInDrawing,
+  setCircleMeasurementToAdd,
+  setMeasurementTypeToAdd
+} from '../../../../domain/shared_slices/Measurement'
+import { useAppDispatch } from '../../../../hooks/useAppDispatch'
+import { useAppSelector } from '../../../../hooks/useAppSelector'
+import { SetCoordinates } from '../../../coordinates/SetCoordinates'
+import { MapToolBox } from '../MapToolBox'
+
+export function CustomCircleRange() {
+  const dispatch = useAppDispatch()
+  const { circleMeasurementInDrawing, measurementTypeToAdd } = useAppSelector(state => state.measurement)
+  const { healthcheckTextWarning } = useAppSelector(state => state.global)
+
+  const circleCoordinates = useMemo(() => {
+    if (measurementTypeToAdd !== MeasurementType.CIRCLE_RANGE || !circleMeasurementInDrawing?.coordinates?.length) {
+      return []
+    }
+
+    return getCoordinates(
+      circleMeasurementInDrawing?.coordinates,
+      OPENLAYERS_PROJECTION,
+      CoordinatesFormat.DECIMAL_DEGREES,
+      false
+    ).map(coordinate => parseFloat(coordinate.replace(/°/g, '')))
+  }, [measurementTypeToAdd, circleMeasurementInDrawing])
+
+  const circleRadius = useMemo(() => {
+    if (measurementTypeToAdd !== MeasurementType.CIRCLE_RANGE || !circleMeasurementInDrawing?.measurement) {
+      return ''
+    }
+
+    return circleMeasurementInDrawing?.measurement.replace('r = ', '').replace('nm', '')
+  }, [measurementTypeToAdd, circleMeasurementInDrawing])
+
+  /**
+   * Compare with previous coordinates and update interest point coordinates
+   * @param {number[]} nextCoordinates - Coordinates ([latitude, longitude]) to update, in decimal format.
+   * @param {number[]} coordinates - Previous coordinates ([latitude, longitude]), in decimal format.
+   */
+  const updateCoordinates = (nextCoordinates, coordinates) => {
+    if (!nextCoordinates?.length) {
+      return
+    }
+
+    if (!coordinates?.length || coordinatesAreDistinct(nextCoordinates, coordinates)) {
+      updateCustomCircleRange(nextCoordinates, circleRadius)
+    }
+  }
+
+  const updateCustomCircleRange = useCallback(
+    (nextCoordinates, nextCircleRadius) => {
+      // Convert to [longitude, latitude] and OpenLayers projection
+      const updatedCoordinates = transform(
+        [nextCoordinates[1], nextCoordinates[0]],
+        WSG84_PROJECTION,
+        OPENLAYERS_PROJECTION
+      )
+
+      dispatch(
+        setCircleMeasurementInDrawing({
+          coordinates: updatedCoordinates,
+          measurement: nextCircleRadius
+        })
+      )
+    },
+    [dispatch]
+  )
+
+  const addCustomCircleRange = useCallback(
+    (nextCoordinates, nextCircleRadius) => {
+      dispatch(
+        setCircleMeasurementToAdd({
+          circleCoordinatesToAdd: nextCoordinates,
+          circleRadiusToAdd: nextCircleRadius
+        })
+      )
+    },
+    [dispatch]
+  )
+
+  const cancelAddCircleRange = useCallback(() => {
+    dispatch(setMeasurementTypeToAdd(null))
+    dispatch(resetCircleMeasurementInDrawing())
+    dispatch(setMapToolOpened(undefined))
+  }, [dispatch])
+
+  return (
+    <Wrapper
+      healthcheckTextWarning={!!healthcheckTextWarning}
+      isOpen={measurementTypeToAdd === MeasurementType.CIRCLE_RANGE}
+    >
+      <Header>Définir une valeur</Header>
+      <Body>
+        <p>Coordonnées</p>
+        <SetCoordinates coordinates={circleCoordinates} updateCoordinates={updateCoordinates} />
+        <p>Distance (rayon)</p>
+        <input
+          data-cy="measurement-circle-radius-input"
+          onChange={e => updateCustomCircleRange(circleCoordinates, e.target.value)}
+          style={{ width: 62 }}
+          type="text"
+          value={circleRadius}
+        />
+        <span>(Nm)</span>
+        <br />
+        <OkButton
+          data-cy="measurement-circle-add"
+          onClick={() => addCustomCircleRange(circleCoordinates, circleRadius)}
+        >
+          OK
+        </OkButton>
+        <CancelButton onClick={cancelAddCircleRange}>Annuler</CancelButton>
+      </Body>
+    </Wrapper>
+  )
+}
+
+const CancelButton = styled.button`
+  border: 1px solid ${COLORS.charcoal};
+  width: 130px;
+  padding: 5px 12px;
+  margin: 15px 0 0 15px;
+  font-size: 13px;
+  color: ${COLORS.gunMetal};
+
+  :disabled {
+    border: 1px solid ${COLORS.lightGray};
+    color: ${COLORS.lightGray};
+  }
+`
+
+const OkButton = styled.button`
+  background: ${COLORS.charcoal};
+  width: 130px;
+  padding: 5px 12px;
+  margin: 15px 0 0;
+  font-size: 13px;
+  color: ${COLORS.gainsboro};
+
+  :hover,
+  :focus {
+    background: ${COLORS.charcoal};
+  }
+`
+
+const Body = styled.div`
+  margin: 10px 15px;
+  text-align: left;
+  font-size: 13px;
+  color: ${COLORS.slateGray};
+
+  p {
+    margin: 0;
+    font-size: 13px;
+  }
+
+  p:nth-of-type(2) {
+    margin-top: 15px;
+    font-size: 13px;
+  }
+
+  span {
+    margin-left: 7px;
+  }
+
+  input {
+    color: ${p => p.theme.color.gunMetal};
+    margin-top: 7px;
+    background: ${p => p.theme.color.gainsboro};
+    border: none;
+    height: 27px;
+    padding-left: 8px;
+  }
+`
+
+const Header = styled.div`
+  background: ${COLORS.charcoal};
+  color: ${COLORS.gainsboro};
+  padding: 9px 0 7px 15px;
+  font-size: 16px;
+  text-align: left;
+  border-top-left-radius: 2px;
+  border-top-right-radius: 2px;
+`
+
+const Wrapper = styled(MapToolBox)`
+  width: 306px;
+  top: 249px;
+`

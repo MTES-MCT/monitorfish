2ea978d3e993e32ba412cfb2db8ff3468306461d
Loup Theron <loup-rodney@hotmail.fr>
2022-11-18 11:35:28 +0100

    Add stubbed monitorenv missions api

--- infra/docker/env.sh
+++ infra/docker/env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sed -i 's#__REACT_APP_MONITORENV_URL__#'"$REACT_APP_MONITORENV_URL"'#g' /home/monitorfish/public/env.js
 sed -i 's#__REACT_APP_GEOSERVER_LOCAL_URL__#'"$REACT_APP_GEOSERVER_LOCAL_URL"'#g' /home/monitorfish/public/env.js
 sed -i 's#__REACT_APP_GEOSERVER_REMOTE_URL__#'"$REACT_APP_GEOSERVER_REMOTE_URL"'#g' /home/monitorfish/public/env.js
 sed -i 's#__REACT_APP_CYPRESS_TEST__#'"$REACT_APP_CYPRESS_TEST"'#g' /home/monitorfish/public/env.js

694498ecc3bbbe712653fc6659bd549bff286586
Vincent <vincent.chery@m4x.org>
2022-11-18 17:26:55 +0100

    Remove silenced_after_date from pipeline

--- datascience/tests/test_data/remote_database/V666.19__Reset_test_silenced_alerts.sql
+++ datascience/tests/test_data/remote_database/V666.19__Reset_test_silenced_alerts.sql
@@ -2,15 +2,15 @@ DELETE FROM silenced_alerts;
 
 INSERT INTO silenced_alerts (
     vessel_name, internal_reference_number, external_reference_number, ircs, vessel_identifier,
-    silenced_before_date, silenced_after_date, value
+    silenced_before_date, value
 ) VALUES 
 (
     'MYNAMEIS', 'ABC000658985', 'OHMYGOSH', 'OGMJ', 'INTERNAL_REFERENCE_NUMBER',
-    NOW() + ('15 DAYS')::interval, NULL,
+    NOW() + ('15 DAYS')::interval,
     '{"type": "THREE_MILES_TRAWLING_ALERT", "seaFront": "Facade B", "flagState": "FR"}'
 ),
 (
     'DEVINER FIGURE CONSCIENCE', 'ABC000542519', 'RO237719', 'FQ7058', 'INTERNAL_REFERENCE_NUMBER',
-    NOW() + ('15 DAYS')::interval, NULL,
+    NOW() + ('15 DAYS')::interval,
     '{"type": "MISSING_FAR_ALERT", "seaFront": "Facade A", "flagState": "FR"}'
 );

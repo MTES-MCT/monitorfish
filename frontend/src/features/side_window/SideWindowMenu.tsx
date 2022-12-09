6da1e47b042731b1f28e9d24efa79ec444f76d50
Loup Theron <loup-rodney@hotmail.fr>
2022-12-09 14:34:29 +0100

    Add feature flag on missions to hide it in prod

--- frontend/src/features/side_window/SideWindowMenu.tsx
+++ frontend/src/features/side_window/SideWindowMenu.tsx
@@ -1,10 +1,13 @@
 import styled from 'styled-components'
 
+import { getEnvironmentVariable } from '../../api/api'
 import { openSideWindowTab } from '../../domain/shared_slices/Global'
 import { useAppDispatch } from '../../hooks/useAppDispatch'
 import { ReactComponent as AlertsSVG } from '../icons/Icone_alertes.svg'
 import { ReactComponent as BeaconMalfunctionsSVG } from '../icons/Icone_VMS.svg'
 import { SideWindowMenuKey } from './constants'
+
+const IS_DEV_ENV = getEnvironmentVariable('REACT_APP_IS_DEV_ENV')
 
 export type SideWindowMenuProps = {
   selectedMenu?: string
@@ -24,15 +27,17 @@ export function SideWindowMenu({ selectedMenu }: SideWindowMenuProps) {
       >
         <AlertsIcon />
       </MenuButton>
-      <MenuButton
-        aria-label={SideWindowMenuKey.MISSION_LIST}
-        onClick={() => dispatch(openSideWindowTab(SideWindowMenuKey.MISSION_LIST))}
-        role="menuitem"
-        selected={selectedMenu === SideWindowMenuKey.MISSION_LIST}
-        title={SideWindowMenuKey.MISSION_LIST}
-      >
-        <BeaconMalfunctionsIcon />
-      </MenuButton>
+      {IS_DEV_ENV && (
+        <MenuButton
+          aria-label={SideWindowMenuKey.MISSION_LIST}
+          onClick={() => dispatch(openSideWindowTab(SideWindowMenuKey.MISSION_LIST))}
+          role="menuitem"
+          selected={selectedMenu === SideWindowMenuKey.MISSION_LIST}
+          title={SideWindowMenuKey.MISSION_LIST}
+        >
+          <BeaconMalfunctionsIcon />
+        </MenuButton>
+      )}
       <MenuButton
         data-cy="side-window-menu-beacon-malfunctions"
         onClick={() => dispatch(openSideWindowTab(SideWindowMenuKey.BEACON_MALFUNCTIONS))}
@@ -47,24 +52,24 @@ export function SideWindowMenu({ selectedMenu }: SideWindowMenuProps) {
 }
 
 const Menu = styled.div`
-  width: 66px;
-  height: 100vh;
   background: ${p => p.theme.color.charcoal};
+  color: ${p => p.theme.color.gainsboro};
   flex-shrink: 0;
   font-size: 11px;
-  color: ${p => p.theme.color.gainsboro};
+  height: 100vh;
   padding: 0;
+  width: 66px;
 `
 
 const MenuButton = styled.div<{
   selected?: boolean
 }>`
-  text-align: center;
   background: ${p => (p.selected ? p.theme.color.blueGray[100] : 'none')};
-  padding: 7px 5px;
-  height: 50px;
-  cursor: pointer;
   border-bottom: 0.5px solid ${p => p.theme.color.slateGray};
+  cursor: pointer;
+  height: 50px;
+  padding: 7px 5px;
+  text-align: center;
 `
 
 const AlertsIcon = styled(AlertsSVG)`

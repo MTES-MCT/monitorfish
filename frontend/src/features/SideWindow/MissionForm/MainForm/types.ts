aac0d4af5903f700e2274518a7f7a7b831b1c19b
Loup Theron <loup-rodney@hotmail.fr>
2023-01-17 16:20:15 +0100

    Use new control units structure in missions

--- frontend/src/features/SideWindow/MissionForm/MainForm/types.ts
+++ frontend/src/features/SideWindow/MissionForm/MainForm/types.ts
@@ -1,8 +1,8 @@
-import type { MissionType, Mission, MissionUnit } from '../../../../domain/types/mission'
+import type { MissionType, Mission, ControlUnit } from '../../../../domain/types/mission'
 import type { DateRange } from '@mtes-mct/monitor-ui'
 
 export type FormValues = Partial<Omit<Mission, 'endDate' | 'startDate' | 'units'>> & {
   dateRange?: DateRange
   type: MissionType
-  units: Partial<MissionUnit>[]
+  units: Partial<ControlUnit>[]
 }

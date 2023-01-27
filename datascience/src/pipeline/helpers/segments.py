f87a9e0ddace3c31fadb7115ad226bc6cccb42b5
Vincent <vincent.chery@m4x.org>
2023-01-27 11:06:23 +0100

    Add attribute_segments_to_catches_by_year helper

--- datascience/src/pipeline/helpers/segments.py
+++ datascience/src/pipeline/helpers/segments.py
@@ -152,3 +152,41 @@ def attribute_segments_to_catches(
     )
 
     return segmented_catches
+
+
+def attribute_segments_to_catches_by_year(
+    catches: pd.DataFrame,
+    segments: pd.DataFrame,
+    *,
+    append_unassigned_catches: bool = False,
+    unassigned_catches_segment_label: str = "Aucun"
+) -> pd.DataFrame:
+    """
+    Same as `attribute_segments_to_catches`, but with an additionnal condition on the
+    year. Both DataFrames must have a `year` column, in addition to the other
+    requirements of `attribute_segments_to_catches`.
+    """
+
+    segmented_catches = []
+
+    for year in catches.year.unique():
+        year_catches = (
+            catches[catches.year == year].copy(deep=True).reset_index(drop=True)
+        )
+
+        year_segments = (
+            segments[segments.year == year]
+            .drop(columns=["year"])
+            .copy(deep=True)
+            .reset_index(drop=True)
+        )
+
+        segmented_catches.append(
+            attribute_segments_to_catches(
+                catches=year_catches,
+                segments=year_segments,
+                append_unassigned_catches=append_unassigned_catches,
+                unassigned_catches_segment_label=unassigned_catches_segment_label,
+            )
+        )
+    return pd.concat(segmented_catches).reset_index(drop=True)
--- datascience/tests/test_pipeline/test_helpers/test_segments.py
+++ datascience/tests/test_pipeline/test_helpers/test_segments.py
@@ -3,6 +3,7 @@ import pytest
 
 from src.pipeline.helpers.segments import (
     attribute_segments_to_catches,
+    attribute_segments_to_catches_by_year,
     catch_area_isin_fao_area,
 )
 
@@ -56,6 +57,13 @@ def catches():
 
 
 @pytest.fixture
+def catches_by_year(catches):
+    catches_with_year = catches.copy(deep=True)
+    catches_with_year["year"] = [2020, 2021, 2021, 2021, 2022, 2022, 2022, 2022]
+    return catches_with_year
+
+
+@pytest.fixture
 def segments():
     return pd.DataFrame(
         {
@@ -105,6 +113,16 @@ def segments():
             ],
         }
     )
+
+
+@pytest.fixture
+def segments_by_year(segments):
+    segments_with_year = segments.copy(deep=True)
+    segments_with_year["year"] = [2021, 2021, 2021, 2021, 2021, 2022, 2022, 2022, 2022]
+    segments_with_year["segment"] = segments_with_year[["segment", "year"]].apply(
+        lambda row: row["segment"] + "_" + str(row["year"]), axis=1
+    )
+    return segments_with_year
 
 
 @pytest.fixture
@@ -176,6 +194,71 @@ def expected_segmented_catches():
     )
 
 
+@pytest.fixture
+def expected_segmented_catches_by_year():
+    return pd.DataFrame(
+        {
+            "segment": [
+                "Pas de segment",
+                "segment_all_criteria_2021",
+                "Pas de segment",
+                "segment_gear_species_2021",
+                "segment_area_species_2021",
+                "Pas de segment",
+                "segment_gear_only_2022",
+                "segment_species_only_2022",
+                "segment_area_only_2022",
+            ],
+            "species": [
+                "spe_1",
+                "spe_2",
+                "spe_4",
+                "spe_1",
+                "spe_1",
+                "spe_1",
+                "spe_1",
+                "spe_4",
+                "spe_5",
+            ],
+            "gear": [
+                "gear_1",
+                "gear_1",
+                "gear_1",
+                "gear_2",
+                "gear_2",
+                "gear_1",
+                "gear_3",
+                "gear_4",
+                "gear_4",
+            ],
+            "fao_area_of_segment": [
+                None,
+                "27.7",
+                None,
+                None,
+                "28.8",
+                None,
+                None,
+                None,
+                "28.8.a",
+            ],
+            "some_id": ["A", "A", "A", "B", "B", "C", "D", "D", "E"],
+            "fao_area_of_catch": [
+                "27.7.b",
+                "27.7.b",
+                "27.7.b",
+                "28.8",
+                "28.8",
+                "27.8",
+                "28.8.f",
+                "37.8",
+                "28.8.a.5",
+            ],
+            "year": [2020, 2021, 2021, 2021, 2021, 2022, 2022, 2022, 2022],
+        }
+    )
+
+
 def test_attribute_segments_to_catches(catches, segments, expected_segmented_catches):
 
     segmented_catches = attribute_segments_to_catches(
@@ -200,3 +283,33 @@ def test_attribute_segments_to_catches_with_unsassigned_catches(
         unassigned_catches_segment_label="Pas de segment",
     )
     pd.testing.assert_frame_equal(segmented_catches, expected_segmented_catches)
+
+
+def test_attribute_segments_to_catches_by_year(
+    catches_by_year, segments_by_year, expected_segmented_catches_by_year
+):
+    segmented_catches = attribute_segments_to_catches_by_year(
+        catches_by_year, segments_by_year
+    )
+    pd.testing.assert_frame_equal(
+        segmented_catches,
+        expected_segmented_catches_by_year[
+            expected_segmented_catches_by_year.segment != "Pas de segment"
+        ].reset_index(drop=True),
+        check_like=True,
+    )
+
+
+def test_attribute_segments_to_catches_by_year_with_unsassigned_catches(
+    catches_by_year, segments_by_year, expected_segmented_catches_by_year
+):
+    segmented_catches = attribute_segments_to_catches_by_year(
+        catches_by_year,
+        segments_by_year,
+        append_unassigned_catches=True,
+        unassigned_catches_segment_label="Pas de segment",
+    )
+
+    pd.testing.assert_frame_equal(
+        segmented_catches, expected_segmented_catches_by_year, check_like=True
+    )

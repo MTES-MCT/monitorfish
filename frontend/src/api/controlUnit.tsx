855f83328ad29e9db3612fcc218c4d6669a3b811
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-01-30 09:01:24 +0100

    Remove most Makefile dev commands & drop /infra/dev

--- frontend/src/api/controlUnit.tsx
+++ frontend/src/api/controlUnit.tsx
@@ -1,10 +1,6 @@
 import { monitorenvApi } from '.'
 
-// import { getEnvironmentVariable } from './utils'
-
 import type { ControlUnit } from '../domain/types/controlUnit'
-
-// const MONITORENV_URL = getEnvironmentVariable('REACT_APP_MONITORENV_URL')
 
 export const controlUnitApi = monitorenvApi.injectEndpoints({
   endpoints: builder => ({

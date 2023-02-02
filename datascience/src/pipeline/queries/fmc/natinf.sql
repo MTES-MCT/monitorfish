26db63ba82c6def7d90f664fa492ef43c4d41c33
Vincent <vincent.chery@m4x.org>
2023-02-02 11:09:57 +0100

    Remove duplicate natinfs

--- datascience/src/pipeline/queries/fmc/natinf.sql
+++ datascience/src/pipeline/queries/fmc/natinf.sql
@@ -1,9 +1,21 @@
+WITH t1 AS (
+    SELECT
+        inf.idc_fmc_natinf as id,
+        inf.code as natinf_code,
+        inf.texte_reglementaire as regulation,
+        typinf.libelle as infraction_category,
+        inf.libelle as infraction,
+        ROW_NUMBER() OVER (PARTITION BY inf.code ORDER BY inf.texte_reglementaire) AS row_num
+    FROM FMC2.FMC_CODE_NATINF inf
+    LEFT JOIN FMC2.FMC_CODE_TYPE_INFRACTION typinf
+    ON inf.idc_fmc_type_infraction = typinf.idc_fmc_type_infraction
+)
+
 SELECT
-    inf.idc_fmc_natinf as id,
-    inf.code as natinf_code,
-    inf.texte_reglementaire as regulation,
-    typinf.libelle as infraction_category,
-    inf.libelle as infraction
-FROM FMC2.FMC_CODE_NATINF inf
-LEFT JOIN FMC2.FMC_CODE_TYPE_INFRACTION typinf
-ON inf.idc_fmc_type_infraction = typinf.idc_fmc_type_infraction
+    id,
+    natinf_code,
+    regulation,
+    infraction_category,
+    infraction
+FROM t1
+WHERE row_num=1

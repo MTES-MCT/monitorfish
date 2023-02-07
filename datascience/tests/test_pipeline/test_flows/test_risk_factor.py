ad24b1bc505f3599ab8cf369eb4e9d087033d71f
Vincent <vincent.chery@m4x.org>
2023-02-07 15:45:54 +0100

    Update risk_factor flow

--- datascience/tests/test_pipeline/test_flows/test_risk_factor.py
+++ datascience/tests/test_pipeline/test_flows/test_risk_factor.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 
 from src.db_config import create_engine
 from src.pipeline.flows.risk_factor import flow
@@ -8,7 +9,31 @@ from tests.mocks import mock_check_flow_not_running
 flow.replace(flow.get_tasks("check_flow_not_running")[0], mock_check_flow_not_running)
 
 
-def test_risk_factor_flow(reset_test_data):
+@pytest.fixture
+def risk_factors() -> pd.DataFrame:
+    return pd.DataFrame(
+        columns=pd.Index(
+            [
+                "vessel_id",
+                "cfr",
+                "ircs",
+                "external_immatriculation",
+                "probability_risk_factor",
+                "detectability_risk_factor",
+                "risk_factor",
+            ]
+        ),
+        data=[
+            [1, "ABC000306959", "LLUK", "RV348407", 1.0, 1.322876, 1.334146],
+            [2, "ABC000542519", None, None, 2.0, 2.000000, 2.168944],
+            [3, "ABC000055481", "IL2468", "AS761555", 1.0, 1.322876, 1.150163],
+            [4, "CFR_OF_LOGBK", "OLY7853", "SB125334", 1.0, 1.322876, 1.334146],
+            [None, "UNKONWN_VESS", None, None, 2.0, 2.0, 1.741101],
+        ],
+    )
+
+
+def test_risk_factor_flow(reset_test_data, risk_factors):
 
     query = """SELECT
         vessel_id,
@@ -55,25 +80,4 @@ def test_risk_factor_flow(reset_test_data):
     assert len(control_anteriority) == 3
     assert set(control_anteriority.ircs) == {"LLUK", "OLY7853", "IL2468"}
 
-    expected_risk_factors = pd.DataFrame(
-        columns=pd.Index(
-            [
-                "vessel_id",
-                "cfr",
-                "ircs",
-                "external_immatriculation",
-                "probability_risk_factor",
-                "detectability_risk_factor",
-                "risk_factor",
-            ]
-        ),
-        data=[
-            [1, "ABC000306959", "LLUK", "RV348407", 1.0, 1.322876, 1.334146],
-            [2, "ABC000542519", None, None, 2.0, 2.000000, 2.168944],
-            [3, "ABC000055481", "IL2468", "AS761555", 1.0, 1.322876, 1.150163],
-            [4, "CFR_OF_LOGBK", "OLY7853", "SB125334", 1.0, 1.322876, 1.334146],
-            [None, "UNKONWN_VESS", None, None, 2.0, 2.0, 1.741101],
-        ],
-    )
-
-    pd.testing.assert_frame_equal(loaded_risk_factors, expected_risk_factors)
+    pd.testing.assert_frame_equal(loaded_risk_factors, risk_factors)

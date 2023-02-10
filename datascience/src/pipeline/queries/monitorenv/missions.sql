afbabad85aaa8260d4e133b90478c1939b4ed0a5
Vincent <vincent.chery@m4x.org>
2023-02-10 11:12:21 +0100

    Add mission_order to missions flow

--- datascience/src/pipeline/queries/monitorenv/missions.sql
+++ datascience/src/pipeline/queries/monitorenv/missions.sql
@@ -8,7 +8,8 @@ SELECT
     mission_nature,
     deleted,
     mission_source,
-    closed
+    closed,
+    mission_order
 FROM missions
 WHERE 
     start_datetime_utc >= CURRENT_TIMESTAMP AT TIME ZONE 'UTC' - INTERVAL ':number_of_months months'

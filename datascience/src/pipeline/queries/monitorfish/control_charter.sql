5575db3f040368bf05eae1ddb01b686f9467fd9d
Vincent <vincent.chery@m4x.org>
2023-02-10 09:18:21 +0100

    Update contorl_charter computation query

--- datascience/src/pipeline/queries/monitorfish/control_charter.sql
+++ datascience/src/pipeline/queries/monitorfish/control_charter.sql
@@ -1,27 +1,16 @@
-WITH ranked_controls AS (
-    SELECT
-        vessel_id,
-        infraction,
-        infraction_ids,
-        control_datetime_utc,
-        ROW_NUMBER() OVER (PARTITION BY vessel_id ORDER BY control_datetime_utc DESC) AS rk
-    FROM controls c
-    JOIN vessels v
-    ON c.vessel_id = v.id
-    WHERE
-        control_datetime_utc IS NOT NULL and
-        v.flag_state = 'FR'
-)
-
-SELECT 
-    vessel_id as id,
-    CASE
-        WHEN (
-            control_datetime_utc > CURRENT_TIMESTAMP - INTERVAL '30 days' AND
-            infraction = false AND
-            infraction_ids = '{}')
-        THEN true 
-        ELSE false
-    END AS under_charter
-FROM ranked_controls
-WHERE rk = 1
+SELECT DISTINCT ON (vessel_id)
+    vessel_id AS id,
+    jsonb_array_length(
+        CASE WHEN jsonb_typeof(logbook_infractions) = 'array' THEN logbook_infractions ELSE '[]' END ||
+        CASE WHEN jsonb_typeof(gear_infractions) = 'array' THEN gear_infractions ELSE '[]' END ||
+        CASE WHEN jsonb_typeof(species_infractions) = 'array' THEN species_infractions ELSE '[]' END ||
+        CASE WHEN jsonb_typeof(other_infractions) = 'array' THEN other_infractions ELSE '[]' END
+    ) > 0 AS under_charter
+FROM mission_actions a
+LEFT JOIN vessels v
+ON a.vessel_id = v.id
+WHERE
+    action_type IN ('SEA_CONTROL', 'LAND_CONTROL', 'AIR_CONTROL') AND
+    v.flag_state = 'FR' AND
+    action_datetime_utc > CURRENT_TIMESTAMP - INTERVAL '1 month'
+ORDER BY vessel_id, action_datetime_utc DESC

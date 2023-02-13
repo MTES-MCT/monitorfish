d066a090aaee7ab6971f91be73d13643a2ac7f76
Loup Theron <loup-rodney@hotmail.fr>
2023-02-13 19:06:59 +0100

    Fix datascience test data

--- datascience/tests/test_data/remote_database/V666.20__Reset_test_reportings.sql
+++ datascience/tests/test_data/remote_database/V666.20__Reset_test_reportings.sql
@@ -1,5 +1,5 @@
 DELETE FROM reportings;
 
 INSERT INTO reportings (
-       type, vessel_id, internal_reference_number, external_reference_number,       ircs,      vessel_name, vessel_identifier,               creation_date, validation_date, archived, deleted,                                                                                                                              value) VALUES
-(   'ALERT',         6,                      NULL,               'ZZTOPACDC', 'ZZ000000', 'I DO 4H REPORT',            'IRCS', NOW() - ('1 DAY')::interval,           NOW(),    false,   false, '{"seaFront": "NAMO", "flagState": "FR", "riskFactor": 3.5647, "type": "THREE_MILES_TRAWLING_ALERT", "natinfCode": "7059"}'::jsonb);
+       type, vessel_id, internal_reference_number, external_reference_number,       ircs,      vessel_name, vessel_identifier,               creation_date, validation_date, archived, deleted, flag_state,                                                                                                                              value) VALUES
+(   'ALERT',         6,                      NULL,               'ZZTOPACDC', 'ZZ000000', 'I DO 4H REPORT',            'IRCS', NOW() - ('1 DAY')::interval,           NOW(),    false,   false, 'FR'      ,'{"seaFront": "NAMO", "riskFactor": 3.5647, "type": "THREE_MILES_TRAWLING_ALERT", "natinfCode": "7059"}'::jsonb);
--- datascience/tests/test_data/remote_database/V666.8__Reset_test_pending_alerts.sql
+++ datascience/tests/test_data/remote_database/V666.8__Reset_test_pending_alerts.sql
@@ -2,13 +2,13 @@ DELETE FROM pending_alerts;
 
 INSERT INTO pending_alerts (
     vessel_name, internal_reference_number, external_reference_number, ircs, vessel_identifier,
-    creation_date, trip_number, 
+    creation_date, trip_number, flag_state,
     value,
     alert_config_name
-) VALUES 
+) VALUES
 (
     'L''AMBRE', 'FRA000614250', 'GV614250', 'FUJW', 'INTERNAL_REFERENCE_NUMBER',
-    '2021-12-23 16:03:00+00', NULL, 
-    '{"type": "THREE_MILES_TRAWLING_ALERT", "seaFront": "NAMO", "flagState": "FR"}',
+    '2021-12-23 16:03:00+00', NULL, 'FR',
+    '{"type": "THREE_MILES_TRAWLING_ALERT", "seaFront": "NAMO"}',
     'ALERTE_1'
 );

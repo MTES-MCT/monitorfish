7f2b1e4e48e45734a63394b08a1c78b9cda72f7d
Loup Theron <loup-rodney@hotmail.fr>
2023-02-13 16:09:08 +0100

    Make side window reportigns Cypress tests as conditonal to prevent race condition

--- frontend/cypress/e2e/side_window_reporting.spec.ts
+++ frontend/cypress/e2e/side_window_reporting.spec.ts
@@ -31,24 +31,36 @@ context('Reportings', () => {
     cy.get('*[data-cy="side-window-reporting-tab"]').click()
     cy.get('[data-cy="side-window-sub-menu-NAMO"]').click()
     cy.wait(200)
-    cy.get('*[data-cy^="side-window-sub-menu-NAMO-number"]').contains('3')
-    cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', 3)
-    cy.get('*[data-cy="side-window-current-reportings"]').first().contains('MARIAGE ÎLE HASARD')
-    cy.get('*[data-cy="side-window-current-reportings"]').last().contains('RENCONTRER VEILLER APPARTEMENT"')
 
-    // When we re-order by date
-    cy.get('[data-cy="table-order-by-validationDate"]').click()
+    /**
+     * We need to wrap this test as the reporting list might have 3 or 4 value, as another test might convert
+     * an OBSERVATION to an INFRACTION_SUSPICION in the same running Cypress machine
+     */
+    cy.get('*[data-cy="side-window-current-reportings"]').then($reportingList => {
+      const numberOfReportings = Cypress.$($reportingList).length
+      cy.log(`${numberOfReportings} reportings found in the list`)
 
-    // Then it is re-ordered
-    cy.get('*[data-cy="side-window-current-reportings"]').first().contains('RENCONTRER VEILLER APPARTEMENT"')
-    cy.get('*[data-cy="side-window-current-reportings"]').last().contains('MARIAGE ÎLE HASARD')
+      assert(numberOfReportings === 3 || numberOfReportings === 4, 'There Should be 3 or 4 reportings')
+      cy.get('*[data-cy^="side-window-sub-menu-NAMO-number"]').contains(numberOfReportings)
 
-    // When we search by vessel name
-    cy.get('*[data-cy^="side-window-reporting-search"]').type('RENCONTRER')
+      cy.wrap($reportingList).first().contains('MARIAGE ÎLE HASARD')
+      cy.wrap($reportingList).last().contains('RENCONTRER VEILLER APPARTEMENT"')
 
-    // Then there is only the searched vessel
-    cy.get('*[data-cy="side-window-current-reportings"]').first().contains('RENCONTRER VEILLER APPARTEMENT"')
-    cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', 1)
+      // When we re-order by date
+      cy.get('[data-cy="table-order-by-validationDate"]').click()
+
+      // Then it is re-ordered
+      // We do not use `$reportingList` as we need to get the updated DOM element
+      cy.get('*[data-cy="side-window-current-reportings"]').first().contains('RENCONTRER VEILLER APPARTEMENT"')
+      cy.get('*[data-cy="side-window-current-reportings"]').last().contains('MARIAGE ÎLE HASARD')
+
+      // When we search by vessel name
+      cy.get('*[data-cy^="side-window-reporting-search"]').type('RENCONTRER')
+
+      // Then there is only the searched vessel
+      cy.get('*[data-cy="side-window-current-reportings"]').first().contains('RENCONTRER VEILLER APPARTEMENT"')
+      cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', numberOfReportings === 4 ? 2 : 1)
+    })
   })
 
   it('Reportings Should be deleted', () => {
@@ -57,21 +69,34 @@ context('Reportings', () => {
     cy.get('*[data-cy="side-window-reporting-tab"]').click()
     cy.get('[data-cy="side-window-sub-menu-NAMO"]').click()
     cy.wait(200)
-    cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', 3)
 
-    // When
-    // Select and delete a reporting
-    cy.get('.rs-checkbox-wrapper').eq(1).click()
-    cy.get('.rs-checkbox-wrapper').last().click()
-    cy.get('*[data-cy="delete-reporting-cards"]').click({ force: true })
-    cy.wait('@deleteReportings').then(({ request, response }) => {
-      expect(request.body.toString()).contains('1,7')
-      expect(response && response.statusCode).equal(200)
+    /**
+     * We need to wrap this test as the reporting list might have 3 or 4 value, as another test might convert
+     * an OBSERVATION to an INFRACTION_SUSPICION in the same running Cypress machine
+     */
+    cy.get('*[data-cy="side-window-current-reportings"]').then($reportingList => {
+      const numberOfReportings = Cypress.$($reportingList).length
+      cy.log(`${numberOfReportings} reportings found in the list`)
+      assert(numberOfReportings === 3 || numberOfReportings === 4, 'There Should be 3 or 4 reportings')
+
+      cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', numberOfReportings)
+
+      // When
+      // Select reporting
+      cy.get('.rs-checkbox-wrapper').eq(1).click()
+      // Select reporting
+      cy.get('.rs-checkbox-wrapper').last().click()
+      // Delete two reportings
+      cy.get('*[data-cy="delete-reporting-cards"]').click({ force: true })
+      cy.wait('@deleteReportings').then(({ request, response }) => {
+        expect(request.body.toString()).contains('1,7')
+        expect(response && response.statusCode).equal(200)
+      })
+
+      // Then
+      // Should delete the row
+      cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', numberOfReportings - 2)
     })
-
-    // Then
-    // Should delete the row
-    cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', 1)
   })
 
   it('A Reporting Should be edited', () => {
@@ -79,31 +104,42 @@ context('Reportings', () => {
     cy.intercept('PUT', 'bff/v1/reportings/6/update').as('updateReporting')
     cy.get('*[data-cy="side-window-reporting-tab"]').click()
     cy.get('[data-cy="side-window-sub-menu-NAMO"]').click()
-    cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', 1)
 
-    // When
-    cy.get('[data-cy="side-window-edit-reporting"]').click()
-    cy.get('[data-cy="new-reporting-title"]').type(
-      '{backspace}{backspace}{backspace}{backspace}{backspace}{backspace} km'
-    )
-    cy.get('[data-cy="new-reporting-select-natinf"]').click()
-    cy.wait(200)
-    cy.get('.rs-picker-search-bar-input').type('maille{enter}')
-    cy.wait(200)
-    cy.get('[data-cy="new-reporting-create-button"]').click()
+    /**
+     * We need to wrap this test as the reporting list might have 1 ou 2 value, as another test might convert
+     * an OBSERVATION to an INFRACTION_SUSPICION in the same running Cypress machine
+     */
+    cy.get('*[data-cy="side-window-current-reportings"]').then($reportingList => {
+      const numberOfReportings = Cypress.$($reportingList).length
+      cy.log(`${numberOfReportings} reportings found in the list`)
+      assert(numberOfReportings === 1 || numberOfReportings === 2, 'There Should be 1 or 2 reportings')
 
-    // Then
-    cy.wait('@updateReporting').then(({ request, response }) => {
-      expect(request.body.natinfCode).contains('23581')
-      expect(request.body.title).contains('Suspicion de chalutage dans les 3  km')
-      expect(response && response.statusCode).equal(200)
+      cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', numberOfReportings)
+
+      // When
+      cy.get('[data-cy="side-window-edit-reporting"]').click()
+      cy.get('[data-cy="new-reporting-title"]').type(
+        '{backspace}{backspace}{backspace}{backspace}{backspace}{backspace} km'
+      )
+      cy.get('[data-cy="new-reporting-select-natinf"]').click()
+      cy.wait(200)
+      cy.get('.rs-picker-search-bar-input').type('maille{enter}')
+      cy.wait(200)
+      cy.get('[data-cy="new-reporting-create-button"]').click()
+
+      // Then
+      cy.wait('@updateReporting').then(({ request, response }) => {
+        expect(request.body.natinfCode).contains('23581')
+        expect(request.body.title).contains('Suspicion de chalutage dans les 3  km')
+        expect(response && response.statusCode).equal(200)
+      })
+      cy.wait(200)
+
+      cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', numberOfReportings)
+      // The DML is modified as this is the DML fetched from the vessels table
+      cy.get('*[data-cy="side-window-current-reportings"]').first().contains('DML 56')
+      cy.get('*[data-cy="side-window-current-reportings"]').first().contains('23581')
     })
-    cy.wait(200)
-
-    cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', 1)
-    // The DML is modified as this is the DML fetched from the vessels table
-    cy.get('*[data-cy="side-window-current-reportings"]').first().contains('DML 56')
-    cy.get('*[data-cy="side-window-current-reportings"]').first().contains('23581')
   })
 
   it('A Reporting Should be edited with the reporting type modified ', () => {
@@ -111,21 +147,32 @@ context('Reportings', () => {
     cy.intercept('PUT', 'bff/v1/reportings/6/update').as('updateReporting')
     cy.get('*[data-cy="side-window-reporting-tab"]').click()
     cy.get('[data-cy="side-window-sub-menu-NAMO"]').click()
-    cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', 1)
 
-    // When
-    cy.get('[data-cy="side-window-edit-reporting"]').click()
-    cy.get('[data-cy="new-reporting-select-observation-reporting-type"]').click()
-    cy.wait(500)
-    cy.get('[data-cy="new-reporting-create-button"]').click()
+    /**
+     * We need to wrap this test as the reporting list might have 1 ou 2 value, as another test might convert
+     * an OBSERVATION to an INFRACTION_SUSPICION in the same running Cypress machine
+     */
+    cy.get('*[data-cy="side-window-current-reportings"]').then($reportingList => {
+      const numberOfReportings = Cypress.$($reportingList).length
+      cy.log(`${numberOfReportings} reportings found in the list`)
+      assert(numberOfReportings === 1 || numberOfReportings === 2, 'There Should be 1 or 2 reportings')
 
-    // Then
-    cy.wait('@updateReporting').then(({ request, response }) => {
-      expect(request.body.type).contains('OBSERVATION')
-      expect(response && response.statusCode).equal(200)
+      cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', numberOfReportings)
+
+      // When
+      cy.get('[data-cy="side-window-edit-reporting"]').click()
+      cy.get('[data-cy="new-reporting-select-observation-reporting-type"]').click()
+      cy.wait(500)
+      cy.get('[data-cy="new-reporting-create-button"]').click()
+
+      // Then
+      cy.wait('@updateReporting').then(({ request, response }) => {
+        expect(request.body.type).contains('OBSERVATION')
+        expect(response && response.statusCode).equal(200)
+      })
+      cy.wait(50)
+
+      cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', numberOfReportings - 1)
     })
-    cy.wait(50)
-
-    cy.get('*[data-cy="side-window-current-reportings"]').should('have.length', 0)
   })
 })

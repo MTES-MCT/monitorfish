2f8f0e2c14f040660f1a601e10a27898610da904
Loup Theron <loup-rodney@hotmail.fr>
2023-02-14 14:25:24 +0100

    Fix contron unit migration in reporting when multiple controllers found

--- backend/src/main/resources/db/migration/internal/V0.201__Update_reportings_control_units.sql
+++ backend/src/main/resources/db/migration/internal/V0.201__Update_reportings_control_units.sql
@@ -3,6 +3,8 @@ SET value = jsonb_set(value, '{unit}', to_jsonb((
     SELECT controllers.id
     FROM controllers
     WHERE controllers.controller = r.value->>'unit'
+    ORDER BY id DESC
+    LIMIT 1
 )))
 WHERE value->>'unit' != '';
 

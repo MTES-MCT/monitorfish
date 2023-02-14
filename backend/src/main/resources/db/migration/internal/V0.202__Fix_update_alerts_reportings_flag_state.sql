6707625066f78f06e4b5be9941371c8c4645478b
Loup Theron <loup-rodney@hotmail.fr>
2023-02-14 17:12:07 +0100

    Fix typo

--- backend/src/main/resources/db/migration/internal/V0.202__Fix_update_alerts_reportings_flag_state.sql
+++ backend/src/main/resources/db/migration/internal/V0.202__Fix_update_alerts_reportings_flag_state.sql
@@ -3,8 +3,8 @@ UPDATE silenced_alerts
    WHERE value->>'flagState' is not null;
 
 UPDATE pending_alerts
-   SET flag_state = value->>'flagState';
-   WHERE value->>'flagState' is not null
+   SET flag_state = value->>'flagState'
+   WHERE value->>'flagState' is not null;
 
 ALTER TABLE reportings
     ADD COLUMN flag_state varchar(2);

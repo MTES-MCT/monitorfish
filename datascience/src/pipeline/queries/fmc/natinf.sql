71b1d01f103b50504119776803c0b2d0c596029b
Vincent <vincent.chery@m4x.org>
2023-02-16 19:04:21 +0100

    Update infractions flow

--- datascience/src/pipeline/queries/fmc/natinf.sql
+++ datascience/src/pipeline/queries/fmc/natinf.sql
@@ -1,13 +1,14 @@
 WITH t1 AS (
     SELECT
-        inf.code as natinf_code,
+        CAST(TRIM(BOTH ' ' FROM inf.code) AS INTEGER) as natinf_code,
         inf.texte_reglementaire as regulation,
         typinf.libelle as infraction_category,
         inf.libelle as infraction,
-        ROW_NUMBER() OVER (PARTITION BY inf.code ORDER BY inf.texte_reglementaire) AS row_num
+        ROW_NUMBER() OVER (PARTITION BY TRIM(BOTH ' ' FROM inf.code) ORDER BY inf.texte_reglementaire) AS row_num
     FROM FMC2.FMC_CODE_NATINF inf
     LEFT JOIN FMC2.FMC_CODE_TYPE_INFRACTION typinf
     ON inf.idc_fmc_type_infraction = typinf.idc_fmc_type_infraction
+    WHERE REGEXP_LIKE(TRIM(BOTH ' ' FROM inf.code), '^[0-9]+$') -- exclude natinfs that are not castable to integers
 )
 
 SELECT

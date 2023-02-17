3c4c67644e9a874673e35bef20a3552c1f74258a
Vincent <vincent.chery@m4x.org>
2023-02-17 15:21:59 +0100

    Migrate reportings and silenced_alerts natinfs to integers

---
+++ backend/src/main/resources/db/migration/internal/V0.204.1__Update_silenced_alerts_table.sql
@@ -0,0 +1,12 @@
+UPDATE public.silenced_alerts
+SET value = jsonb_strip_nulls(
+    jsonb_set(
+        value,
+        '{natinfCode}',
+        CASE
+            WHEN value->>'natinfCode' IS NULL OR value->>'natinfCode' = '' THEN 'null'
+            ELSE to_jsonb((value->>'natinfCode')::INTEGER)
+        END,
+        false
+    )
+);

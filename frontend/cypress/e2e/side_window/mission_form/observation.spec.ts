aeaa5e5fde3286a546067a65db51dd03d10b22b6
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-02-20 12:33:19 +0100

    Enable e2e test for mission form observation

--- frontend/cypress/e2e/side_window/mission_form/observation.spec.ts
+++ frontend/cypress/e2e/side_window/mission_form/observation.spec.ts
@@ -4,7 +4,7 @@ import { Mission } from 'src/domain/types/mission'
 
 import { fillSideWindowMissionFormBase, openSideWindowNewMission } from './utils'
 
-context.skip('Side Window > Mission Form > Observation', () => {
+context('Side Window > Mission Form > Observation', () => {
   beforeEach(() => {
     openSideWindowNewMission()
 
@@ -39,9 +39,47 @@ context.skip('Side Window > Mission Form > Observation', () => {
         assert.fail('`interception.response` is undefined.')
       }
 
-      // console.log(JSON.stringify(interception.response))
-
-      assert.deepInclude(interception.request.body, {})
+      assert.deepInclude(interception.request.body, {
+        // actionDatetimeUtc: '2023-02-20T12:31:46.093Z',
+        actionType: 'OBSERVATION',
+        controlQualityComments: null,
+        controlUnits: [],
+        diversion: null,
+        emitsAis: null,
+        emitsVms: null,
+        facade: null,
+        feedbackSheetRequired: null,
+        gearInfractions: [],
+        gearOnboard: [],
+        id: null,
+        isFromPoseidon: null,
+        latitude: null,
+        licencesAndLogbookObservations: null,
+        licencesMatchActivity: null,
+        logbookInfractions: [],
+        logbookMatchesActivity: null,
+        longitude: null,
+        missionId: 1,
+        numberOfVesselsFlownOver: null,
+        otherComments: 'Une observation.',
+        otherInfractions: [],
+        portLocode: null,
+        portName: null,
+        segments: [],
+        seizureAndDiversion: null,
+        seizureAndDiversionComments: null,
+        separateStowageOfPreservedSpecies: false,
+        speciesInfractions: [],
+        speciesObservations: null,
+        speciesOnboard: [],
+        speciesSizeControlled: false,
+        speciesWeightControlled: false,
+        unitWithoutOmegaGauge: null,
+        userTrigram: 'Marlin',
+        vesselId: null,
+        vesselName: null,
+        vesselTargeted: false
+      })
       assert.isString(interception.request.body.actionDatetimeUtc)
 
       cy.get('h1').should('contain.text', 'Missions et contrôles')

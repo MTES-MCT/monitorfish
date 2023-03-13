87ed2c9aca911bdacae3e8af95c40c2a60f844ae
Loup Theron <loup-rodney@hotmail.fr>
2023-03-13 15:50:28 +0100

    Rename mapClickEvent to mapClick

--- frontend/src/domain/use_cases/map/clickOnMapFeature.ts
+++ frontend/src/domain/use_cases/map/clickOnMapFeature.ts
@@ -5,23 +5,23 @@ import { showVessel } from '../vessel/showVessel'
 import { showVesselTrack } from '../vessel/showVesselTrack'
 
 import type { VesselLastPositionFeature } from '../../entities/vessel/types'
-import type { MapClickEvent } from '../../types/map'
+import type { MapClick } from '../../types/map'
 
-export const clickOnMapFeature = (clickEvent: MapClickEvent) => (dispatch, getState) => {
+export const clickOnMapFeature = (mapClick: MapClick) => (dispatch, getState) => {
   const { previewFilteredVesselsMode } = getState().global
-  if (!clickEvent.feature) {
+  if (!mapClick.feature) {
     return
   }
 
-  const clickedFeatureId = clickEvent.feature.getId()?.toString()
+  const clickedFeatureId = mapClick.feature.getId()?.toString()
   if (!clickedFeatureId) {
     return
   }
 
   if (clickedFeatureId.includes(LayerProperties.REGULATORY.code)) {
     const zone = {
-      topic: clickEvent.feature.getProperties().topic,
-      zone: clickEvent.feature.getProperties().zone
+      topic: mapClick.feature.getProperties().topic,
+      zone: mapClick.feature.getProperties().zone
     }
     dispatch(showRegulatoryZoneMetadata(zone, false))
 
@@ -33,9 +33,9 @@ export const clickOnMapFeature = (clickEvent: MapClickEvent) => (dispatch, getSt
   }
 
   if (clickedFeatureId.includes(LayerProperties.VESSELS.code)) {
-    const clickedVessel = (clickEvent.feature as VesselLastPositionFeature).vesselProperties
+    const clickedVessel = (mapClick.feature as VesselLastPositionFeature).vesselProperties
 
-    if (clickEvent.ctrlKeyPressed) {
+    if (mapClick.ctrlKeyPressed) {
       dispatch(showVesselTrack(clickedVessel, false, null))
     } else {
       dispatch(showVessel(clickedVessel, false, false))

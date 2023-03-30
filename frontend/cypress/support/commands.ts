2c289b6d554df46ff29bc8db4867ec377b0591da
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-03-30 11:13:42 +0200

    Remove most custom Cypress commands to use monitor-ui ones

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -1,10 +1,7 @@
-import { clickButton } from './commands/clickButton'
-import { clickOutside } from './commands/clickOutside'
-import { fill } from './commands/fill'
+import '@mtes-mct/monitor-ui/cypress'
+
 import { fillDateRangePicker } from './commands/fillDateRangePicker'
-import { forceClick } from './commands/forceClick'
 import { getComputedStyle } from './commands/getComputedStyle'
-import { getDataCy } from './commands/getDataCy'
 
 function unquote(str: string): string {
   return str.replace(/(^")|("$)/g, '')
@@ -39,34 +36,6 @@ Cypress.Commands.add('cleanFiles', () => {
   cy.exec(`rm -f cypress/downloads/*`)
 })
 
-// Cypress typings are wrong and only allow a boolean for `prevSubject` despite the doc:
-// https://docs.cypress.io/api/cypress-api/custom-commands#Dual-Commands
-Cypress.Commands.add('clickButton', { prevSubject: 'optional' } as any, clickButton)
-
 Cypress.Commands.add('getComputedStyle', getComputedStyle)
 
-Cypress.Commands.add(
-  'clickLink',
-  (linkText: string): Cypress.Chainable<JQuery<HTMLAnchorElement>> => cy.get('a').contains(linkText).click()
-)
-
-/**
- * @description
- * Useful to close modals.
- */
-Cypress.Commands.add('clickOutside', clickOutside)
-
-/**
- * @example
- * ```ts
- *   cy.fill('Password', 'P422W0Rd')
- * ```
- */
-Cypress.Commands.add('fill', fill)
-
 Cypress.Commands.add('fillDateRangePicker', fillDateRangePicker)
-
-// Maybe because of https://github.com/cypress-io/cypress/issues/19564
-Cypress.Commands.add('forceClick', { prevSubject: true }, forceClick)
-
-Cypress.Commands.add('getDataCy', getDataCy)
--- frontend/cypress/support/commands/forceClick.ts
+++
@@ -1,9 +0,0 @@
-export function forceClick([subject]: Cypress.Chainable<Cypress.JQueryWithSelector>[]): Cypress.Chainable<
-  JQuery<HTMLElement>
-> {
-  if (!subject) {
-    throw new Error(`Could not find subject.`)
-  }
-
-  return subject.click({ force: true })
-}

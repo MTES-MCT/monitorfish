5054750a9d3ae316b143d48c053d6aba82ff8117
Loup Theron <loup-rodney@hotmail.fr>
2023-03-31 11:26:59 +0200

    Update e2e.ts

--- frontend/cypress/support/e2e.ts
+++ frontend/cypress/support/e2e.ts
@@ -28,7 +28,7 @@ declare global {
 
 Cypress.on('uncaught:exception', err => {
   // We ignore uncaught exceptions `Error: ResizeObserver loop completed with undelivered notifications.`
-  // since they only seem to happen (sporadically) within e2e tes
+  // since they only seem to happen (sporadically) within e2e tests
   if (err.message.includes('ResizeObserver loop completed with undelivered notifications.')) {
     return false
   }

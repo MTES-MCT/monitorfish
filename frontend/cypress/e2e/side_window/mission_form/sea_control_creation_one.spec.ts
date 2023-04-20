cf68dfef4dc52eda92dd055c1873e1919b51c864
Loup Theron <loup-rodney@hotmail.fr>
2023-04-20 10:00:52 +0200

    Update and rename sea_control_creation.spec.ts to sea_control_creation_one.spec.ts

--- frontend/cypress/e2e/side_window/mission_form/sea_control_creation_one.spec.ts
+++ frontend/cypress/e2e/side_window/mission_form/sea_control_creation_one.spec.ts
@@ -1,7 +1,7 @@
 import { fillSideWindowMissionFormBase, openSideWindowNewMission } from './utils'
 import { Mission } from '../../../../src/domain/entities/mission/types'
 
-context('Side Window > Mission Form > Sea Control Creation', () => {
+context('Side Window > Mission Form > Sea Control Creation One', () => {
   beforeEach(() => {
     openSideWindowNewMission()
     fillSideWindowMissionFormBase(Mission.MissionTypeLabel.SEA)

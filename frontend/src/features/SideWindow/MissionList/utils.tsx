1659ada55f7745c84f989f30b819ac1698631f9f
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-04-20 08:43:07 +0200

    Style mission list status tag

--- frontend/src/features/SideWindow/MissionList/utils.tsx
+++ frontend/src/features/SideWindow/MissionList/utils.tsx
@@ -1,5 +1,6 @@
 import { Tag, TagBullet, customDayjs } from '@mtes-mct/monitor-ui'
 import { filter } from 'ramda'
+import styled from 'styled-components'
 
 import { MissionDateRangeFilter } from './types'
 import { ALERTS_MENU_SEA_FRONT_TO_SEA_FRONTS } from '../../../domain/entities/alerts/constants'
@@ -130,33 +131,44 @@ export const renderStatus = (missionStatus: Mission.MissionStatus): JSX.Element 
   switch (missionStatus) {
     case Mission.MissionStatus.UPCOMING:
       return (
-        <Tag bullet={TagBullet.DISK} bulletColor="#52B0FF" style={{ color: '#52B0FF' }}>
+        <StyledTag bullet={TagBullet.DISK} bulletColor="#52B0FF" style={{ color: '#52B0FF' }}>
           {Mission.MissionStatusLabel.UPCOMING}
-        </Tag>
+        </StyledTag>
       )
 
     case Mission.MissionStatus.IN_PROGRESS:
       return (
-        <Tag bullet={TagBullet.DISK} bulletColor="#3660FA" style={{ color: '#3660FA' }}>
+        <StyledTag bullet={TagBullet.DISK} bulletColor="#3660FA" style={{ color: '#3660FA' }}>
           {Mission.MissionStatusLabel.IN_PROGRESS}
-        </Tag>
+        </StyledTag>
       )
 
     case Mission.MissionStatus.DONE:
       return (
-        <Tag bullet={TagBullet.DISK} bulletColor="#1400AD" style={{ color: '#1400AD' }}>
+        <StyledTag bullet={TagBullet.DISK} bulletColor="#1400AD" style={{ color: '#1400AD' }}>
           {Mission.MissionStatusLabel.DONE}
-        </Tag>
+        </StyledTag>
       )
 
     case Mission.MissionStatus.CLOSED:
       return (
-        <Tag bullet={TagBullet.DISK} bulletColor="#463939" style={{ color: '#463939' }}>
+        <StyledTag bullet={TagBullet.DISK} bulletColor="#463939" style={{ color: '#463939' }}>
           {Mission.MissionStatusLabel.CLOSED}
-        </Tag>
+        </StyledTag>
       )
 
     default:
       throw new FrontendError("`missionStatus` doesn't match `MissionStatus` enum.")
   }
 }
+
+const StyledTag = styled(Tag)`
+  align-items: flex-end;
+  display: flex;
+  line-height: 1;
+
+  > span {
+    height: 10px;
+    width: 10px;
+  }
+`

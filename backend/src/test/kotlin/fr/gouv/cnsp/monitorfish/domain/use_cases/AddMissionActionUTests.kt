ff8a9cad7f30825dc603f6088ed84a1dae598401
Loup Theron <loup-rodney@hotmail.fr>
2023-04-24 11:33:41 +0200

    Add vesselId check when creating a control action

---
+++ backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/AddMissionActionUTests.kt
@@ -0,0 +1,96 @@
+package fr.gouv.cnsp.monitorfish.domain.use_cases
+
+import com.nhaarman.mockitokotlin2.anyOrNull
+import com.nhaarman.mockitokotlin2.given
+import fr.gouv.cnsp.monitorfish.domain.entities.mission_actions.MissionAction
+import fr.gouv.cnsp.monitorfish.domain.entities.mission_actions.MissionActionType
+import fr.gouv.cnsp.monitorfish.domain.repositories.MissionActionsRepository
+import fr.gouv.cnsp.monitorfish.domain.use_cases.mission_actions.AddMissionAction
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.time.ZonedDateTime
+import java.util.*
+
+@ExtendWith(SpringExtension::class)
+class AddMissionActionUTests {
+
+    @MockBean
+    private lateinit var missionActionsRepository: MissionActionsRepository
+
+    @Test
+    fun `execute Should throw an exception When the vesselId is missing in a control`() {
+        // Given
+        val action = MissionAction(
+            id = null,
+            vesselId = null,
+            missionId = 1,
+            actionDatetimeUtc = ZonedDateTime.now(),
+            portLocode = "AEFAT",
+            actionType = MissionActionType.LAND_CONTROL,
+            gearOnboard = listOf(),
+            seizureAndDiversion = true,
+            isDeleted = false,
+        )
+
+        // When
+        val throwable = catchThrowable {
+            AddMissionAction(missionActionsRepository).execute(action)
+        }
+
+        // Then
+        assertThat(throwable).isNotNull()
+        assertThat(throwable.message).isEqualTo("A control must specify a vessel: the `vesselId` must be given.")
+    }
+
+    @Test
+    fun `execute Should throw an exception When the id is not null`() {
+        // Given
+        val action = MissionAction(
+            id = 1,
+            vesselId = null,
+            missionId = 1,
+            actionDatetimeUtc = ZonedDateTime.now(),
+            portLocode = "AEFAT",
+            actionType = MissionActionType.LAND_CONTROL,
+            gearOnboard = listOf(),
+            seizureAndDiversion = true,
+            isDeleted = false,
+        )
+
+        // When
+        val throwable = catchThrowable {
+            AddMissionAction(missionActionsRepository).execute(action)
+        }
+
+        // Then
+        assertThat(throwable).isNotNull()
+        assertThat(throwable.message).isEqualTo("An action creation must have no id: the `id` must be null.")
+    }
+
+    @Test
+    fun `execute Should not throw an exception When the vesselId is given in a control`() {
+        // Given
+        val action = MissionAction(
+            id = null,
+            vesselId = 1,
+            missionId = 1,
+            actionDatetimeUtc = ZonedDateTime.now(),
+            portLocode = "AEFAT",
+            actionType = MissionActionType.LAND_CONTROL,
+            gearOnboard = listOf(),
+            seizureAndDiversion = true,
+            isDeleted = false,
+        )
+        given(missionActionsRepository.save(anyOrNull())).willReturn(action)
+
+        // When
+        val returnedAction = AddMissionAction(missionActionsRepository).execute(action)
+
+        // Then
+        assertThat(returnedAction).isNotNull
+    }
+}

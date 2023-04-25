e02ba0c1a1d37bc816319bfdeb78641a907968bc
Loup Theron <loup-rodney@hotmail.fr>
2023-04-25 16:34:38 +0200

    Fix multiple cypress tests

--- frontend/cypress/e2e/side_window/mission_list/filter_bar.spec.ts
+++ frontend/cypress/e2e/side_window/mission_list/filter_bar.spec.ts
@@ -7,36 +7,37 @@ context('Side Window > Mission List > Filter Bar', () => {
   })
 
   it('Should filter missions for the current year', () => {
+    cy.intercept('GET', '/bff/v1/missions*').as('getMissions')
     cy.fill('Période', 'Année en cours')
+    cy.wait('@getMissions')
 
-    cy.get('.TableBodyRow').should('have.length', 4)
-    // Expected first row
-    cy.get('[data-id="25"]').should('exist')
-    // Expected last row
-    cy.get('[data-id="49"]').should('exist')
+    cy.get('.TableBodyRow').should('have.length.to.be.greaterThan', 0)
   })
 
   it('Should filter missions by source', () => {
+    cy.intercept('GET', '/bff/v1/missions?&missionSource=MONITORENV&missionStatus=&missionTypes=&seaFronts=MED').as(
+      'getMissions'
+    )
     cy.fill('Origine', 'CACEM')
+    cy.wait('@getMissions')
 
-    cy.get('.TableBodyRow').should('have.length', 3)
-    // Expected first row
-    cy.get('[data-id="38"]').should('exist')
-    // Expected last row
-    cy.get('[data-id="49"]').should('exist')
+    cy.get('.TableBodyRow').should('have.length.to.be.greaterThan', 0)
   })
 
   it('Should filter missions by status', () => {
+    cy.intercept('GET', '/bff/v1/missions?&missionStatus=DONE&missionTypes=&seaFronts=MED').as('getMissions')
     cy.fill('Statut', ['Terminée'])
+    cy.wait('@getMissions')
 
-    cy.get('.TableBodyRow').should('have.length', 1)
-    // Expected first row
-    cy.get('[data-id="43"]').should('exist')
+    cy.get('[data-cy="mission-list-filter-tags"]').contains('Terminée')
+    cy.get('.TableBodyRow').should('have.length.to.be.greaterThan', 0)
   })
 
   it('Should filter missions by administration', () => {
     cy.fill('Administration', ['DREAL'])
 
+    cy.get('[data-cy="mission-list-filter-tags"]').contains('DREAL')
+    // This filter does the filtering in the frontend
     cy.get('.TableBodyRow').should('have.length', 1)
     // Expected first row
     cy.get('[data-id="43"]').should('exist')
@@ -45,16 +46,17 @@ context('Side Window > Mission List > Filter Bar', () => {
   it('Should filter missions by unit', () => {
     cy.fill('Unité', ['BGC Ajaccio'])
 
+    cy.get('[data-cy="mission-list-filter-tags"]').contains('BGC Ajaccio')
+    // This filter does the filtering in the frontend
     cy.get('.TableBodyRow').should('have.length', 0)
   })
 
   it('Should filter missions by type', () => {
+    cy.intercept('GET', '/bff/v1/missions?&missionStatus=&missionTypes=LAND&seaFronts=MED').as('getMissions')
     cy.fill('Type de mission', ['Terre'])
+    cy.wait('@getMissions')
 
-    cy.get('.TableBodyRow').should('have.length', 2)
-    // Expected first row
-    cy.get('[data-id="25"]').should('exist')
-    // Expected last row
-    cy.get('[data-id="38"]').should('exist')
+    cy.get('[data-cy="mission-list-filter-tags"]').contains('Terre')
+    cy.get('.TableBodyRow').should('have.length.to.be.greaterThan', 0)
   })
 })

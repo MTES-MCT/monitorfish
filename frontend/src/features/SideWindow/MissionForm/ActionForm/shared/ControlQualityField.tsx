061c3ee0eb6270b7e6a585ecfca3d58ba993376b
Loup Theron <loup-rodney@hotmail.fr>
2023-04-27 10:52:32 +0200

    Refactor two missions action columns to control check enum

--- frontend/src/features/SideWindow/MissionForm/ActionForm/shared/ControlQualityField.tsx
+++ frontend/src/features/SideWindow/MissionForm/ActionForm/shared/ControlQualityField.tsx
@@ -1,12 +1,21 @@
 import { FormikCheckbox, FormikMultiRadio, FormikTextarea } from '@mtes-mct/monitor-ui'
 
-import { BOOLEAN_AS_OPTIONS } from '../../../../../constants'
+import { MissionAction } from '../../../../../domain/types/missionAction'
 import { FieldsetGroup } from '../../shared/FieldsetGroup'
 
 export function ControlQualityField() {
   return (
     <FieldsetGroup isLight legend="Qualité du contrôle">
-      <FormikMultiRadio isInline label="Navire ciblé par le CNSP" name="vesselTargeted" options={BOOLEAN_AS_OPTIONS} />
+      <FormikMultiRadio
+        isInline
+        label="Navire ciblé par le CNSP"
+        name="vesselTargeted"
+        options={[
+          { label: 'Oui', value: MissionAction.ControlCheck.YES },
+          { label: 'Non', value: MissionAction.ControlCheck.NO },
+          { label: 'Non concerné', value: MissionAction.ControlCheck.NOT_APPLICABLE }
+        ]}
+      />
       <FormikCheckbox label="Unité sans jauge oméga" name="unitWithoutOmegaGauge" />
       <FormikTextarea
         label="Observations sur le déroulé du contrôle"

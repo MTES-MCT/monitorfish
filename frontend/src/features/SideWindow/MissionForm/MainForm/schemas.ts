0b7450a23ee6013a83b6b1ab31ddde9d73651d75
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-05-30 10:09:29 +0200

    Refactor MainFormSchema endDateTimeUtc rule in mission form

--- frontend/src/features/SideWindow/MissionForm/MainForm/schemas.ts
+++ frontend/src/features/SideWindow/MissionForm/MainForm/schemas.ts
@@ -1,20 +1,13 @@
 /* eslint-disable sort-keys-fix/sort-keys-fix */
 
 import { customDayjs } from '@mtes-mct/monitor-ui'
-import { identity } from 'lodash/fp'
 import { object, string } from 'yup'
 
 export const MainFormSchema = object({
   startDateTimeUtc: string().required('La date de début de mission est un champ obligatoire.'),
-  endDateTimeUtc: string().when('startDateTimeUtc', {
-    // When `startDateTimeUtc` is defined
-    is: (startDateTimeUtc: string) => !!startDateTimeUtc,
-    // `endDateTimeUtc` should be after `startDateTimeUtc`
-    then: schema =>
-      schema.test({
-        message: 'La date de fin de mission doit être postérieure à la date de début.',
-        test: (endDateTimeUtc, context) => customDayjs(endDateTimeUtc).isAfter(context.parent.startDateTimeUtc)
-      }),
-    otherwise: identity
+  endDateTimeUtc: string().test({
+    message: 'La date de fin de mission doit être postérieure à la date de début.',
+    test: (endDateTimeUtc, context) =>
+      context.parent.startDateTimeUtc ? customDayjs(endDateTimeUtc).isAfter(context.parent.startDateTimeUtc) : true
   })
 })

da2edc2ad816d66efa8235565677a4a0036a22d4
Loup Theron <loup-rodney@hotmail.fr>
2023-05-31 10:12:44 +0200

    Update dependencies and controller test

--- backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/bff/ForeignFMCsControllerITests.kt
+++ backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/bff/ForeignFMCsControllerITests.kt
@@ -1,21 +1,21 @@
 package fr.gouv.cnsp.monitorfish.infrastructure.api.bff
 
+import fr.gouv.cnsp.monitorfish.config.WebSecurityConfig
 import fr.gouv.cnsp.monitorfish.domain.entities.beacon_malfunctions.ForeignFMC
 import fr.gouv.cnsp.monitorfish.domain.use_cases.beacon_malfunction.GetAllForeignFMCs
 import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.BDDMockito.given
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.context.annotation.Import
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@ExtendWith(SpringExtension::class)
+@Import(WebSecurityConfig::class)
 @WebMvcTest(value = [(ForeignFMCsController::class)])
 class ForeignFMCsControllerITests {
 

36828243c047ce442a08d08538c8f6eb446f25e2
Loup Theron <loup-rodney@hotmail.fr>
2023-06-02 16:07:30 +0200

    Add sentry in backend and fix http requests logging to Kibana

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/MonitorFishApplication.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/MonitorFishApplication.kt
@@ -1,5 +1,6 @@
 package fr.gouv.cnsp.monitorfish
 
+import io.sentry.Sentry
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,16 @@ import org.springframework.boot.runApplication
 class MonitorFishApplication
 
 fun main(args: Array<String>) {
-    runApplication<MonitorFishApplication>(*args)
+    val ctx = runApplication<MonitorFishApplication>(*args)
+
+    val isSentryEnabled: String? = ctx.environment.getProperty("monitorfish.sentry.enabled")
+
+    if (isSentryEnabled == "true") {
+        Sentry.init { options ->
+            options.dsn = "https://a5f3272efa794bb9ada2ffea90f2fec5@sentry.incubateur.net/8"
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 1.0
+        }
+    }
 }

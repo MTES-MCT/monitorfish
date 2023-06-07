eef0e57ec1ddff5d9c02a936f4256b957761c432
Loup Theron <loup-rodney@hotmail.fr>
2023-06-07 14:54:26 +0200

    FIx various unit tests

--- frontend/src/domain/shared_slices/Mission.ts
+++ frontend/src/domain/shared_slices/Mission.ts
@@ -1,14 +1,14 @@
-import type {PayloadAction} from '@reduxjs/toolkit'
-import {createSlice, current} from '@reduxjs/toolkit'
-import {isEqual} from 'lodash/fp'
+import { createSlice, current } from '@reduxjs/toolkit'
+import { isEqual } from 'lodash/fp'
 
-import type {FilterValues} from '../../features/SideWindow/MissionList/types'
-import {MissionDateRangeFilter, MissionFilterType} from '../../features/SideWindow/MissionList/types'
-import {Mission} from '../entities/mission/types'
-import {SeaFrontGroup} from '../entities/seaFront/constants'
+import { MissionDateRangeFilter, MissionFilterType } from '../../features/SideWindow/MissionList/types'
+import { Mission } from '../entities/mission/types'
+import { SeaFrontGroup } from '../entities/seaFront/constants'
 
-import type {MissionActionFormValues, MissionMainFormValues} from '../../features/SideWindow/MissionForm/types'
-import type {GeoJSON} from '../types/GeoJSON'
+import type { MissionActionFormValues, MissionMainFormValues } from '../../features/SideWindow/MissionForm/types'
+import type { FilterValues } from '../../features/SideWindow/MissionList/types'
+import type { GeoJSON } from '../types/GeoJSON'
+import type { PayloadAction } from '@reduxjs/toolkit'
 
 export interface MissionState {
   /**

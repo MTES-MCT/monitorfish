f280065c6ecf6e7fccbab2798af45651b0e01908
Loup Theron <loup-rodney@hotmail.fr>
2023-06-08 19:01:12 +0200

    Remove last migration (duplicate)

--- backend/src/main/resources/db/migration/internal/V0.220.1__Update_control_objectives_table.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO control_objectives (facade, segment, year, target_number_of_controls_at_sea,
-                                target_number_of_controls_at_port, control_priority_level)
-SELECT 'Corse' as facade, segment, year, target_number_of_controls_at_sea,
-        target_number_of_controls_at_port, control_priority_level FROM control_objectives
-WHERE facade = 'MED';
-

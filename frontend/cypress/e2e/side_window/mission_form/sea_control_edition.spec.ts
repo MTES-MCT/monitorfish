ab4a08561f6313c6dc7ab213154704e32401de88
Loup Theron <loup-rodney@hotmail.fr>
2023-06-12 17:24:11 +0200

    Fix Cypress test with district code check

--- frontend/cypress/e2e/side_window/mission_form/sea_control_edition.spec.ts
+++ frontend/cypress/e2e/side_window/mission_form/sea_control_edition.spec.ts
@@ -46,7 +46,7 @@ context('Side Window > Mission Form > Sea Control Edition', () => {
         actionType: 'SEA_CONTROL',
         controlQualityComments: null,
         controlUnits: [],
-        districtCode: 'AY',
+        districtCode: null,
         diversion: null,
         emitsAis: null,
         emitsVms: 'NOT_APPLICABLE',

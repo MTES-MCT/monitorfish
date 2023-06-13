4f67ca231d13911322f3c4e02871d5c554080626
Loup Theron <loup-rodney@hotmail.fr>
2023-06-13 16:01:12 +0200

    Fix super user context setup on /ext path

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,4 +1,4 @@
-import { Navigate, createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
 
 import { Backoffice } from './features/Backoffice'
 import { ControlObjectiveList } from './features/Backoffice/ControlObjectiveList'

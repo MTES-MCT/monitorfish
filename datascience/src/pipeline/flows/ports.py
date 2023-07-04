0df4ba4398cdf547f030e66ae7b891445b341566
Vincent <vincent.chery@m4x.org>
2023-07-04 15:30:12 +0200

    WIP

--- datascience/src/pipeline/flows/ports.py
+++ datascience/src/pipeline/flows/ports.py
@@ -1,7 +1,9 @@
 import io
 import os
+import re
 from ast import literal_eval
 from datetime import date
+from itertools import product
 from pathlib import Path
 from time import sleep
 
@@ -15,7 +17,7 @@ from config import LIBRARY_LOCATION, PORTS_URL, PROXIES
 from src.db_config import create_engine
 from src.pipeline.generic_tasks import extract, load
 from src.pipeline.helpers.fao_areas import remove_redundant_fao_area_codes
-from src.pipeline.helpers.spatial import geocode
+from src.pipeline.helpers.spatial import geocode, geocode_google
 from src.pipeline.processing import coalesce
 from src.pipeline.utils import psql_insert_copy
 from src.read_query import read_query, read_table
@@ -44,7 +46,7 @@ def make_lat_lon(lat_lon: str):
         lat_sign = 1 if north_south == "N" else -1
 
         lon_degrees = float(lat_lon[-6:-3])
-        lon_minutes = float(lat_lon[-3 - 1])
+        lon_minutes = float(lat_lon[-3:-1])
         east_west = lat_lon[-1]
         lon_sign = 1 if east_west == "E" else -1
 
@@ -169,24 +171,15 @@ def extract_circabc_locations(csv_filepath):
 
 @task(checkpoint=False)
 def clean_circabc(locations):
-    # Extract latitude longitude from string value like "9060N 18060W"
-    positions = locations.apply(
-        lambda row: pd.Series(
-            make_lat_lon(row.Coordinates), index=["latitude", "longitude"], dtype=object
-        ),
-        result_type="expand",
-        axis=1,
-    )
-
-    locations = locations.join(positions)
-
     rename_cols = {
         "Code": "locode",
         "Name": "port_name",
-        "FishingPort": "is_fishing_port",
-        "LandingPlace": "is_landing_place",
-        "CommercialPort": "is_commercial_port",
+        "Latitude": "latitude",
+        "Longitude": "longitude",
     }
+    locations = locations.rename(columns=rename_cols)
+
+    locations["country_code_iso2"] = locations.locode.map(lambda s: s[:2])
 
     keep_cols = [
         "country_code_iso2",
@@ -194,15 +187,21 @@ def clean_circabc(locations):
         "port_name",
         "latitude",
         "longitude",
-        "is_fishing_port",
-        "is_landing_place",
-        "is_commercial_port",
     ]
-
-    locations = locations.rename(columns=rename_cols)
-    locations["country_code_iso2"] = locations.locode.map(lambda s: s[:2])
     locations = locations[keep_cols]
+
     locations["port_name"] = locations.port_name.map(str.title)
+
+    def replace_commas(str_or_float):
+        return (
+            str_or_float.replace(",", ".")
+            if type(str_or_float) is str
+            else str_or_float
+        )
+
+    locations["latitude"] = locations.latitude.map(replace_commas).astype(float)
+    locations["longitude"] = locations.longitude.map(replace_commas).astype(float)
+
     return locations
 
 
@@ -274,6 +273,106 @@ def combine_columns_into_value(ports):
 
 
 @task(checkpoint=False)
+def clean_ports(ports: pd.DataFrame) -> pd.DataFrame:
+    """
+    Rename ambiguous port names and change incorrect country codes
+    """
+
+    ports = ports.copy(deep=True)
+
+    # Incorrect country codes to change
+    country_codes_to_change = {
+        "FRXFK": "TF",
+        "FRPPN": "GP",
+        "FRFDE": "MQ",
+        "GPMSB": "MF",
+        "FRRN4": "RE",
+    }
+
+    # Ports with an ambiguous name  to humans and / or to the Google geocoding API
+    ports_to_rename = {
+        "FRDUU": "Le Douhet (Saint-Georges-d'Oléron)",
+        "FRBEC": "Bec d'Ambès (Bayon-sur-Gironde)",
+        "FRKOC": "Kérascouët (Hôpital-Camfrout)",
+        "FRPCV": "Porscav (Lampaul-Plouarzel)",
+        "FRKSL": "Kastell Ac'h (Plouguerneau)",
+        "FRKSI": "Koréjou (Plouguerneau)",
+        "FRHR3": "Locmaria (Groix)",
+        "FRRUB": "Port de Roubary (Gatteville-le-Phare)",
+        "GBNPO": "Newport, Isle of Wight",
+        "FRMSM": "Mont-Saint-Martin, Meuthe-et-Moselle",
+        "FRHOE": "Hœrdt",
+        "FRCA7": "Port du canal (Gujan-Mestras)",
+        "FRBN4": "Port du Bonhomme (La Guérinière)",
+        "FRCE9": "Port du Collet (Les Moutiers-en-Retz)",
+        "FRMI4": "Port du Morin, (L'Épine, Île de Noirmoutier)",
+        "FRLF3": "La Fosse (Barbâtre)",
+        "FRCH9": "Les Champs (Bouin)",
+        "FRSAB": "Sablon (Metz)",
+        "FRBE5": "Port de la Belle Etoile (Saint-Androny)",
+        "FRAYZ": "Ayse",
+        "FRPDW": "Pain de Sucre (Bourg)",
+        "FRHYR": "Hyères",
+        "FRDDT": "Le Pradet",
+        "FRG9J": "Gajan, Gard",
+        "FRHHE": "Montoulieu, Hérault",
+        "FRART": "Artix, Pyrénées-Atlantiques",
+        "FRHOU": "Hourcade (Villenave-d'Ornon)",
+        "FRPN6": "Port Nord (Fouras)",
+        "FR5RA": "Saint-Maur, Indre",
+        "FRCS9": "Port de Cassy (Lanton)",
+        "FRFAC": "Facture-Biganos",
+        "FRIMI": "Île aux Moines",
+        "FRMI2": "Mindin (Saint-Brevin-les-Pins)",
+        "FRGV6": "Port de la Gravette (La Plaine-sur-Mer)",
+        "FRAR5": "Port d'Argenton (Landunvez)",
+        "FRRE2": "Port du Rocher (La Teste-de-Buch)",
+        "FRPF2": "Le Four (Lège-Cap-Ferret)",
+        "FRBR5": "Port De La Barbotière (Gujan-Mestras)",
+        "FRVI2": "Port de la Vigne (Lège-Cap-Ferret)",
+        "FRE56": "Sainte-Hélène, Morbihan",
+        "FRLO4": "Port de Larros (Gujan-Mestras)",
+        "FR2SS": "Saint-Symphorien, Sarthe",
+        "FRTOO": "Toull Broc'h (Ploubazlanec)",
+        "FRMR4": "La Martinière (Le Pellerin)",
+        "FRCR2": "Carro (Martigues)",
+    }
+
+    for locode, country_code in country_codes_to_change.items():
+        ports.loc[ports.locode == locode, "country_code_iso2"] = country_code
+
+    for locode, name in ports_to_rename.items():
+        ports.loc[ports.locode == locode, "port_name"] = name
+
+    return ports
+
+
+@task(checkpoint=False)
+def parse_ports_names(ports):
+    pattern = re.compile(r"^(?P<location_or_city>.*?)(\((?P<city>.*)\))?$")
+
+    def get_location_and_city(s: str) -> pd.Series:
+        m = pattern.match(s)
+        groups = m.groupdict()
+        location_or_city = groups["location_or_city"]
+        city = groups["city"]
+
+        if city:
+            city = city.strip()
+            location = location_or_city.strip()
+        else:
+            city = location_or_city.strip()
+            location = None
+
+        return pd.Series([location, city], index=["location", "city"])
+
+    location_and_city = ports.port_name.apply(get_location_and_city)
+
+    ports = ports.join(location_and_city)
+    return ports
+
+
+@task(checkpoint=False)
 def load_port_codes(ports):
     engine = create_engine("monitorfish_remote")
     ports.to_sql(
@@ -292,42 +391,129 @@ with Flow(
     unece_ports = extract_unece_ports()
     ports = merge_circabc_unece(circabc_ports, unece_ports)
     ports = combine_columns_into_value(ports)
+    ports = clean_ports(ports)
+    ports = parse_ports_names(ports)
     load_port_codes(ports)
 
 
 # ************* Geocoding to improve the precision of latitude longitude **************
+"""
+In this flow, we geocode a subset of the full ports lists :
+- Ports of selected countries
+- Ports flagged as 'active'
+'Active' ports are ports in which there has been at least a PNO, a LAN or a land
+control in recent years
+
+The list of 'active' ports can be extracted from the production environment with the
+ following query :
+
+
+WITH pno_lan_ports AS (
+    SELECT DISTINCT value->>'port' AS locode
+    FROM logbook_reports
+    WHERE
+        log_type IN ('PNO', 'LAN') AND
+        operation_datetime_utc > CURRENT_TIMESTAMP - INTERVAL '2 years'
+),
+
+control_ports AS (
+    SELECT DISTINCT port_locode AS locode
+    FROM mission_actions
+    WHERE
+        action_type = 'LAND_CONTROL' AND
+        action_datetime_utc > CURRENT_TIMESTAMP - INTERVAL '5 years'
+)
+
+SELECT COALESCE(pno_lan_ports.locode, control_ports.locode) AS locode
+FROM pno_lan_ports
+FULL OUTER JOIN control_ports
+ON pno_lan_ports.locode =  control_ports.locode
+"""
 
 
 def geocode_row(row):
-    credit_exhausted = True  # max rate 1/s if no geolocationIQ credit is available
+    limit_rate = True  # limit rate to 1 query / second
     country_code_iso2 = row["country_code_iso2"]
+
+    osm_fr_countries = {"RE", "YT", "MQ", "GF", "GP"}
+    if country_code_iso2 in osm_fr_countries:
+        country_code_iso2 = "FR"
     region = row["region"]
-    port_name = row["port_name"]
-    if pd.isna(port_name):
-        lat, lon = None, None
-    else:
+    location = row["location"]
+    city = row["city"]
+
+    success = False
+    first_attempt_failed = False
+
+    backends = ["Nominatim", "LocationIQ"]
+    query_strings = [
+        f"{city}",
+    ]
+    if region:
+        query_strings.insert(0, f"{city}, {region}")
+    if location:
+        if region:
+            query_strings.insert(0, f"{location}, {city}, {region}")
+        else:
+            query_strings.insert(0, f"{location}, {city}")
+
+    attempts = list(product(backends, query_strings))
+
+    for backend, query_string in attempts:
+        if first_attempt_failed:
+            print(f"Retrying {query_string} on {backend}...")
+
         try:
-            if credit_exhausted:
+            if limit_rate:
                 sleep(1)
+
             lat, lon = geocode(
-                city=port_name, country_code_iso2=country_code_iso2, county=region
+                query_string=query_string,
+                country_code_iso2=country_code_iso2,
+                backend=backend,
             )
-        except requests.HTTPError:
+
+            success = True
+            break
+
+        except (requests.HTTPError, ValueError):
+            first_attempt_failed = True
+
+    if not success:
+        print("Could not geocode", city)
+        lat, lon, backend = None, None, "Geocoding failed"
+
+    return pd.Series(
+        [lat, lon, backend],
+        index=["geocoded_latitude", "geocoded_longitude", "geocoding_api"],
+    )
+
+
+def geocode_row_google(row):
+    country_code_iso2 = row["country_code_iso2"]
+    location = row["location"]
+    city = row["city"]
+
+    if location:
+        address = f"{location}, {city}"
+    else:
+        address = city
+
+    try:
+        lat, lon = geocode_google(address=address, country=country_code_iso2)
+    except (AssertionError, requests.HTTPError):
+        if location:
+            print(f"Retrying with city only : {city}")
             try:
-                if credit_exhausted:
-                    sleep(1)
-                print("Retring without county for port", port_name)
-                lat, lon = geocode(city=port_name, country_code_iso2=country_code_iso2)
-            except Exception:
-                try:
-                    if credit_exhausted:
-                        sleep(1)
-                    print("Retring with port name alone for port", port_name)
-                    lat, lon = geocode(city=port_name)
-                except Exception:
-                    print("Could not geocode", port_name)
-                    lat, lon = None, None
-    print(port_name, lat, lon)
+                lat, lon = geocode_google(address=city, country=country_code_iso2)
+
+            except (AssertionError, requests.HTTPError):
+                print(f"Failed geocoding : {city}")
+                lat, lon = None, None
+        else:
+            print(f"Failed geocoding : {city}")
+            lat, lon = None, None
+
     return pd.Series([lat, lon], index=["geocoded_latitude", "geocoded_longitude"])
 
 
@@ -338,8 +524,41 @@ def extract_port_codes():
 
 
 @task(checkpoint=False)
+def extract_active_ports_locodes() -> set:
+    csv_file_path = "../../../data/external/monitorfish_prod/active_ports_locodes.csv"
+    ports_locodes = pd.read_csv(csv_file_path)
+    ports_locodes_set = set(ports_locodes.locode)
+    return ports_locodes_set
+
+
+@task(checkpoint=False)
+def flag_active_ports(ports: pd.DataFrame, active_ports_locodes: set) -> pd.DataFrame:
+    ports = ports.copy(deep=True)
+    ports["is_active"] = ports.locode.isin(active_ports_locodes)
+    return ports
+
+
+@task(checkpoint=False)
 def geocode_ports(ports):
-    positions = ports.apply(geocode_row, axis=1, result_type="expand")
+
+    country_codes_to_geocode = [
+        "FR",
+        "MQ",
+        "GP",
+        "YT",
+        "VE",
+        "MG",
+        "RE",
+        "BE",
+        "FO",
+        "GF",
+    ]
+
+    ports_to_geocode = ports.loc[
+        ports.is_active | ports.country_code_iso2.isin(country_codes_to_geocode)
+    ]
+
+    positions = ports_to_geocode.apply(geocode_row_google, axis=1, result_type="expand")
     geocoded_ports = ports.join(positions)
     return geocoded_ports
 
@@ -348,7 +567,7 @@ def geocode_ports(ports):
 def load_geocoded_ports(geocoded_ports):
     engine = create_engine("monitorfish_remote")
     geocoded_ports.to_sql(
-        "geocoded_ports",
+        "geocoded_ports_google",
         engine,
         schema="interim",
         if_exists="replace",
@@ -356,10 +575,39 @@ def load_geocoded_ports(geocoded_ports):
     )
 
 
+@task(checkpoint=False)
+def compute_distance_between_geocoded_and_initial_positions():
+    e = create_engine("monitorfish_remote")
+    with e.begin() as conn:
+
+        conn.execute(
+            """
+            ALTER TABLE interim.geocoded_ports_google
+            ADD COLUMN distance_km DOUBLE PRECISION
+        """
+        )
+
+        conn.execute(
+            """
+            UPDATE interim.geocoded_ports_google
+            SET distance_km = ST_Distance(
+                    ST_SetSRID(ST_Point(longitude, latitude), 4326)::geography,
+                    ST_SetSRID(
+                        ST_Point(geocoded_longitude, geocoded_latitude),
+                        4326
+                    )::geography
+                ) / 1000
+        """
+        )
+
+
 with Flow("Geocode ports") as flow_geocode_ports:
     ports = extract_port_codes()
+    active_ports_locodes = extract_active_ports_locodes()
+    ports = flag_active_ports(ports, active_ports_locodes)
     geocoded_ports = geocode_ports(ports)
-    load_geocoded_ports(geocoded_ports)
+    loaded = load_geocoded_ports(geocoded_ports)
+    compute_distance_between_geocoded_and_initial_positions(upstream_tasks=[loaded])
 
 
 # **************** Flow : combine all data together & add manual fixes ****************

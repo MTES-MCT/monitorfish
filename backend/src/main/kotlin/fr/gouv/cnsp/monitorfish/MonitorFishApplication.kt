d3ee5d74a2f1fff0ce8566f13ac8d0300b354691
Loup Theron <loup-rodney@hotmail.fr>
2023-07-10 09:44:32 +0200

    Add sentry to log4j and fix ts sentry types

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/MonitorFishApplication.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/MonitorFishApplication.kt
@@ -11,10 +11,11 @@ fun main(args: Array<String>) {
     val ctx = runApplication<MonitorFishApplication>(*args)
 
     val isSentryEnabled: String? = ctx.environment.getProperty("monitorfish.sentry.enabled")
+    val sentryDsn: String? = ctx.environment.getProperty("sentry.dsn")
 
     if (isSentryEnabled == "true") {
         Sentry.init { options ->
-            options.dsn = "https://a5f3272efa794bb9ada2ffea90f2fec5@sentry.incubateur.net/8"
+            options.dsn = sentryDsn
             // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
             // We recommend adjusting this value in production.
             options.tracesSampleRate = 1.0

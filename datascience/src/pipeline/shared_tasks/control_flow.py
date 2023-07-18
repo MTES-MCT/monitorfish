b4d9ab83540ccb5ea36f29f27f38be4493b36d6b
Vincent Chéry <vincent.chery@m4x.org>
2023-07-18 15:37:34 +0200

    Add clean_flow_runs flow

--- datascience/src/pipeline/shared_tasks/control_flow.py
+++ datascience/src/pipeline/shared_tasks/control_flow.py
@@ -1,10 +1,89 @@
+from datetime import datetime, tzinfo
 from pathlib import Path
+from typing import List
 
 import prefect
 from prefect import task
 from prefect.engine.signals import SKIP
 from prefect.tasks.control_flow.filter import FilterTask
 from prefect.utilities.graphql import with_args
+
+from config import FLOWS_LABEL, PREFECT_SERVER_URL
+from src.pipeline.entities.prefect import FlowRun
+
+
+@task(checkpoint=False)
+def get_flow_runs(
+    states: List[str], max_state_datetime_utc: datetime = None
+) -> List[FlowRun]:
+    """
+    This task fetches flow runs that match the states and max_state_datetime_utc criteria.
+    """
+    logger = prefect.context.get("logger")
+    prefect_client = prefect.Client(api_server=PREFECT_SERVER_URL)
+
+    filters = [{"labels": {"_contains": FLOWS_LABEL}}]
+
+    if states:
+        filters.append({"state": {"_in": states}})
+
+    if max_state_datetime_utc:
+        try:
+            assert isinstance(max_state_datetime_utc, datetime)
+        except AssertionError:
+            raise ValueError("max_state_datetime_utc must be a datetime object")
+
+        try:
+            assert isinstance(max_state_datetime_utc.tzinfo, tzinfo)
+        except AssertionError:
+            raise ValueError("max_state_datetime_utc must be timezone aware")
+
+        filters.append({"state_timestamp": {"_lt": max_state_datetime_utc.isoformat()}})
+
+    query = {
+        "query": {
+            with_args("flow_run", {"where": {"_and": filters}},): {
+                "id",
+                "flow {name}",
+                "state",
+                "state_timestamp",
+            }
+        }
+    }
+
+    logger.info(f"Sending query {query}")
+    response = prefect_client.graphql(query)
+
+    logger.info("Parsing results")
+    flow_runs = response.get("data", {}).get("flow_run", [])
+    flow_runs = [
+        FlowRun(
+            flow_run_id=run["id"],
+            flow_name=run["flow"]["name"],
+            state=run["state"],
+            state_timestamp=datetime.fromisoformat(run["state_timestamp"]),
+        )
+        for run in flow_runs
+    ]
+
+    logger.info(f"Returning {len(flow_runs)} flow runs.")
+
+    return flow_runs
+
+
+@task(checkpoint=False)
+def cancel_flow_run(flow_run: FlowRun):
+    logger = prefect.context.get("logger")
+    prefect_client = prefect.Client(api_server=PREFECT_SERVER_URL)
+
+    logger.info(
+        (
+            f"Cancelling flow run {flow_run.flow_run_id} of flow {flow_run.flow_name} "
+            f"which has been in state {flow_run.state} since {flow_run.state_timestamp}."
+        )
+    )
+
+    prefect_client.cancel_flow_run(flow_run.flow_run_id)
 
 
 @task(checkpoint=False)

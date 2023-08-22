1b34b391cd197ef843c03d0ee7f997c784656e91
Loup Theron <loup-rodney@hotmail.fr>
2023-08-22 16:35:50 +0200

    Migrate validate api used by the flow on public api. Remove MISSING_FAR from returned operational alerts of frontend

---
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/public_api/OperationalAlertController.kt
@@ -0,0 +1,29 @@
+package fr.gouv.cnsp.monitorfish.infrastructure.api.public_api
+
+import fr.gouv.cnsp.monitorfish.domain.use_cases.alert.ValidateOperationalAlert
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.websocket.server.PathParam
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/operational_alerts")
+@Tag(name = "Public APIs for Operational alerts")
+class OperationalAlertController(
+    private val validateOperationalAlert: ValidateOperationalAlert,
+) {
+
+    @PutMapping(value = ["/{id}/validate"])
+    @Operation(summary = "Validate an operational alert")
+    fun validateAlert(
+        @PathParam("Alert id")
+        @PathVariable(name = "id")
+        id: Int,
+    ) {
+        return validateOperationalAlert.execute(id)
+    }
+
+}

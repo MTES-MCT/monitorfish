59a9152052fea5feda8034b3757d8b287f989161
Loup Theron <loup-rodney@hotmail.fr>
2023-08-29 16:21:40 +0200

    Fix ts types

--- frontend/src/features/SideWindow/Alert/SilencedAlerts/types.ts
+++ frontend/src/features/SideWindow/Alert/SilencedAlerts/types.ts
@@ -1,15 +1,10 @@
-import type { SilencedAlertData } from '../../../../domain/entities/alerts/types'
+import type {SilencedAlertData} from '../../../../domain/entities/alerts/types'
 
 export type SilencedAlertFormValues = Partial<SilencedAlertData>
 
 export const emptySilencedAlert: SilencedAlertFormValues = {
   externalReferenceNumber: null,
-  flagState: undefined,
   internalReferenceNumber: null,
   ircs: null,
-  silencedBeforeDate: undefined,
-  value: undefined,
   vesselId: null,
-  vesselIdentifier: undefined,
-  vesselName: undefined
 }

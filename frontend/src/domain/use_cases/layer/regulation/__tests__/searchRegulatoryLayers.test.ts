6eca022f12c05bfac2d38840b2d56b6e20d5fabf
Loup Theron <loup-rodney@hotmail.fr>
2023-09-01 11:30:32 +0200

    Improve regulation search relevancy

--- frontend/src/domain/use_cases/layer/regulation/__tests__/searchRegulatoryLayers.test.ts
+++ frontend/src/domain/use_cases/layer/regulation/__tests__/searchRegulatoryLayers.test.ts
@@ -40,8 +40,28 @@ describe('searchRegulatoryLayers()', () => {
     // @ts-ignore
     const fuse = new Fuse(regulatoryZones, REGULATION_SEARCH_OPTIONS)
 
-    const items = fuse.search<RegulatoryZone>('Chalut').map(result => result.item)
+    const items = fuse.search<RegulatoryZone>('chalu').map(result => result.item)
+
+    expect(items).toHaveLength(2)
+  })
+
+  it('should search Merlu with the zone field', () => {
+    // @ts-ignore
+    const fuse = new Fuse(regulatoryZones, REGULATION_SEARCH_OPTIONS)
+
+    const items = fuse.search<RegulatoryZone>('merl').map(result => result.item)
+
+    // It returns Merlu and Merlan
+    expect(items).toHaveLength(2)
+  })
+
+  it('should search with the regulatory reference number', () => {
+    // @ts-ignore
+    const fuse = new Fuse(regulatoryZones, REGULATION_SEARCH_OPTIONS)
+
+    const items = fuse.search<RegulatoryZone>('789').map(result => result.item)
 
     expect(items).toHaveLength(1)
+    expect(items[0]?.zone).toEqual('Interdiction')
   })
 })

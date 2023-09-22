61b3c7f49b101345769b5d8d992c35a9e54337d1
Loup Theron <loup-rodney@hotmail.fr>
2023-09-22 11:35:33 +0200

    Fix types import

--- frontend/src/features/Vessel/useCases/showVesselsLastPosition.ts
+++ frontend/src/features/Vessel/useCases/showVesselsLastPosition.ts
@@ -1,8 +1,9 @@
 import { applyFilterToVessels } from './applyFilterToVessels'
-import { VesselLastPosition } from '../../../domain/entities/vessel/types'
 import { resetIsUpdatingVessels } from '../../../domain/shared_slices/Global'
 import { setVesselsFromAPI, setVesselsSpeciesAndDistricts } from '../../../domain/shared_slices/Vessel'
 import getUniqueSpeciesAndDistricts from '../../../domain/use_cases/species/getUniqueSpeciesAndDistricts'
+
+import type { VesselLastPosition } from '../../../domain/entities/vessel/types'
 
 export const showVesselsLastPosition = (vessels: VesselLastPosition[]) => async dispatch => {
   dispatch(setVesselsFromAPI(vessels))

d2d74aa170b1b94130d160899d38b2fa1cc9c1fa
Loup Theron <loup-rodney@hotmail.fr>
2023-09-25 09:51:38 +0200

    Remove logbook from vessel api file

--- frontend/src/api/vessel.ts
+++ frontend/src/api/vessel.ts
@@ -1,13 +1,9 @@
-import { HTTPError } from 'ky'
+import {HttpStatusCode} from './constants'
+import {monitorfishApiKy} from './index'
+import {ApiError} from '../libs/ApiError'
 
-import { HttpStatusCode } from './constants'
-import { monitorfishApiKy } from './index'
-import { NavigateTo } from '../features/Logbook/constants'
-import { ApiError } from '../libs/ApiError'
-
-import type { TrackRequest, VesselAndPositions, VesselIdentity, VesselPosition } from '../domain/entities/vessel/types'
-import type { CurrentAndArchivedReportingsOfSelectedVessel } from '../domain/types/reporting'
-import type { VesselVoyage } from '../features/Logbook/Logbook.types'
+import type {TrackRequest, VesselAndPositions, VesselIdentity, VesselPosition} from '../domain/entities/vessel/types'
+import type {CurrentAndArchivedReportingsOfSelectedVessel} from '../domain/types/reporting'
 
 const VESSEL_POSITIONS_ERROR_MESSAGE = "Nous n'avons pas pu récupérer les informations du navire"
 const VESSEL_SEARCH_ERROR_MESSAGE = "Nous n'avons pas pu récupérer les navires dans notre base"
@@ -91,36 +87,6 @@ async function searchVesselsFromAPI(searched: string) {
 }
 
 /**
- * Get vessel logbook.
- * If the vessel has no logbook, an NOT_FOUND (404) API http code is returned from the API.
- *
- * @throws {@link ApiError}
- */
-async function getVesselLogbookFromAPI(
-  vesselIdentity: VesselIdentity,
-  voyageRequest: NavigateTo | undefined,
-  tripNumber: number | undefined
-) {
-  const internalReferenceNumber = vesselIdentity.internalReferenceNumber || ''
-  const nextTripNumber = tripNumber || ''
-  const nextVoyageRequest = voyageRequest || ''
-
-  try {
-    return await monitorfishApiKy
-      .get(
-        `/bff/v1/vessels/logbook/find?internalReferenceNumber=${internalReferenceNumber}&voyageRequest=${nextVoyageRequest}&tripNumber=${nextTripNumber}`
-      )
-      .json<VesselVoyage>()
-  } catch (err) {
-    if (err instanceof HTTPError && err.response.status === HttpStatusCode.NOT_FOUND) {
-      return undefined
-    }
-
-    throw new ApiError(LOGBOOK_ERROR_MESSAGE, err)
-  }
-}
-
-/**
  * Get vessel reporting
  *
  * @throws {@link ApiError}
@@ -144,6 +110,5 @@ export {
   searchVesselsFromAPI,
   getVesselPositionsFromAPI,
   getVesselFromAPI,
-  getVesselLogbookFromAPI,
   getVesselReportingsFromAPI
 }

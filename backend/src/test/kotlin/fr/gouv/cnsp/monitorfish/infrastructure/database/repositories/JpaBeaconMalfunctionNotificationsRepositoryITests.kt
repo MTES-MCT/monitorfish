9236d9ce0ab20e71e0505ca00db14b8fb078607d
Loup Theron <loup-rodney@hotmail.fr>
2023-10-17 18:03:42 +0200

    Replace maven with gradle

--- backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/database/repositories/JpaBeaconMalfunctionNotificationsRepositoryITests.kt
+++ backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/database/repositories/JpaBeaconMalfunctionNotificationsRepositoryITests.kt
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.transaction.annotation.Transactional
 
-@RunWith(SpringRunner::class)
 class JpaBeaconMalfunctionNotificationsRepositoryITests : AbstractDBTests() {
 
     @Autowired

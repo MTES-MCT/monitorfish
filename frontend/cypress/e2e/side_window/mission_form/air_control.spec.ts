993af4615aceec84c9eafb1e8503b0c88816c1c5
Loup Theron <loup-rodney@hotmail.fr>
2023-11-01 14:58:40 +0100

    Add missing group form field in cypress test

--- frontend/cypress/e2e/side_window/mission_form/air_control.spec.ts
+++ frontend/cypress/e2e/side_window/mission_form/air_control.spec.ts
@@ -31,6 +31,7 @@ context('Side Window > Mission Form > Air Control', () => {
     // Infractions
     cy.clickButton('Ajouter une infraction')
     cy.fill('Type d’infraction', 'Avec PV')
+    cy.fill('Groupe', 'Autre infraction')
     cy.fill('NATINF', '23581')
     cy.fill('Observations sur l’infraction', 'Une observation sur l’infraction.')
     cy.clickButton('Valider l’infraction')

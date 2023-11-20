2bc939e5b9702ca2f099ef88c7caafd743e3d343
Loup Theron <loup-rodney@hotmail.fr>
2023-11-20 14:01:23 +0100

    Use a global SSE channel

--- frontend/src/features/SideWindow/MissionForm/apis.ts
+++ frontend/src/features/SideWindow/MissionForm/apis.ts
@@ -1,8 +1,9 @@
 import {logSoftError} from '@mtes-mct/monitor-ui'
 
-import {MONITORENV_API_URL, monitorenvApi, monitorfishApi} from '../../../api'
 import {Mission} from '../../../domain/entities/mission/types'
-import {ReconnectingEventSource} from '../../../libs/ReconnectingEventSource'
+import { addNewMissionListener, missionEventListener, removeMissionListener } from './sse'
+import { monitorenvApi, monitorfishApi } from '../../../api'
+import { Mission } from '../../../domain/entities/mission/types'
 
 import type {ControlUnit} from '../../../domain/types/ControlUnit'
 import {FrontendApiError} from "../../../libs/FrontendApiError";
@@ -48,40 +49,24 @@ export const monitorenvMissionApi = monitorenvApi.injectEndpoints({
     }),
 
     getMission: builder.query<Mission.Mission, Mission.Mission['id']>({
+      keepUnusedDataFor: 0,
       providesTags: [{ type: 'Missions' }],
       query: id => `/v1/missions/${id}`,
       transformErrorResponse: response => new FrontendApiError(GET_MISSION_ERROR_MESSAGE, response),
       async onCacheEntryAdded(id, { cacheDataLoaded, cacheEntryRemoved, updateCachedData }) {
-        const url = `${MONITORENV_API_URL}/api/v1/missions/${id}/sse`
-
         try {
-          const eventSource = new ReconnectingEventSource(url)
-          // eslint-disable-next-line no-console
-          console.log(`SSE: listening for updates of mission id ${id}...`)
-
-          // wait for the initial query to resolve before proceeding
           await cacheDataLoaded
 
-          const listener = (event: MessageEvent) => {
-            const mission = JSON.parse(event.data) as Mission.Mission
-            // eslint-disable-next-line no-console
-            console.log(`SSE: received an update for mission id ${mission.id}.`)
-
-            updateCachedData(() => mission)
-          }
-
-          eventSource.addEventListener('MISSION_UPDATE', listener)
+          const listener = missionEventListener(id, updateCachedData)
+          addNewMissionListener(id, listener)
 
           // cacheEntryRemoved will resolve when the cache subscription is no longer active
           await cacheEntryRemoved
 
-          // perform cleanup steps once the `cacheEntryRemoved` promise resolves
-          eventSource.close()
+          // perform cleanup once the `cacheEntryRemoved` promise resolves
+          removeMissionListener(id)
         } catch (e) {
           logSoftError({
-            context: {
-              url
-            },
             isSideWindowError: true,
             message: "SSE: Can't connect or receive messages",
             originalError: e

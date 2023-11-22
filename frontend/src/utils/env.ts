e75ebe6fba526e6bfe8d17a19ed11065db6f9e68
Loup Theron <loup-rodney@hotmail.fr>
2023-11-22 16:51:41 +0100

    Add rights to folder

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -5,16 +5,19 @@ import type { Self } from '../domain/types/self'
 /**
  * Get the environment variable:
  * - injected by the `env.sh` script at runtime in `window`
- * - or from `import.meta.env` when running locally
+ * - or from `import.meta.env`
  */
 export function env(name: Env) {
   if ((self as Self).env) {
     const injectedValue = (self as Self).env[name]
-    if (injectedValue === 'true' || injectedValue === 'false') {
-      return Boolean(injectedValue)
-    }
 
-    return injectedValue
+    if (injectedValue) {
+      if (injectedValue === 'true' || injectedValue === 'false') {
+        return Boolean(injectedValue)
+      }
+
+      return injectedValue
+    }
   }
 
   const valueFromProcess = import.meta.env[name]

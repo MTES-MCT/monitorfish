d14b6daa7de4ded7c0146d3f51db36f3b9e67e7d
Loup Theron <loup-rodney@hotmail.fr>
2023-11-22 19:48:29 +0100

    Change pkg target

--- infra/docker/env.sh
+++ infra/docker/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+set -e
 
-ls /home/monitorfish/
-cat /home/monitorfish/import-meta-env
+ls -al /home/monitorfish/
 ./home/monitorfish/import-meta-env -x /home/monitorfish/.env.example -p /home/monitorfish/public/index.html
 
 exec "$@"

959e794d114ba1579a99db53a8db14f2788d9624
Loup Theron <loup-rodney@hotmail.fr>
2023-11-22 19:35:13 +0100

    Add logging

--- infra/docker/env.sh
+++ infra/docker/env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ls /home/monitorfish/
+cat /home/monitorfish/import-meta-env
 ./home/monitorfish/import-meta-env -x /home/monitorfish/.env.example -p /home/monitorfish/public/index.html
 
 exec "$@"

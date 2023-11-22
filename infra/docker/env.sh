0f56d0d8a96f4f24b1b619eff7fdb397a5f431eb
Loup Theron <loup-rodney@hotmail.fr>
2023-11-22 14:01:13 +0100

    Rework Dockerfile

--- infra/docker/env.sh
+++ infra/docker/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-./home/monitorfish/import-meta-env -x /home/monitorfish/.env.example -o /home/monitorfish/public/**/*
+./home/monitorfish/import-meta-env -x /home/monitorfish/.env.example -p /home/monitorfish/public/**/*
 
 exec "$@"

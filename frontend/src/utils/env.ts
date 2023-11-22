b779d1fd2d430d7e9e60150de309c09c4ec55e38
Loup Theron <loup-rodney@hotmail.fr>
2023-11-22 17:16:25 +0100

    Add local env

---
+++ frontend/src/utils/env.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-restricted-globals */
+import type { Env } from '../domain/types/env'
+import type { Self } from '../domain/types/self'
+
+/**
+ * Get the environment variable:
+ * - injected by the `env.sh` script at runtime in `window`
+ * - or from `import.meta.env`
+ */
+export function env(name: Env) {
+  if ((self as Self).env) {
+    const injectedValue = (self as Self).env[name]
+
+    if (injectedValue) {
+      if (injectedValue === 'true' || injectedValue === 'false') {
+        return Boolean(injectedValue)
+      }
+
+      return injectedValue
+    }
+  }
+
+  const valueFromProcess = import.meta.env[name]
+  if (valueFromProcess === 'true' || valueFromProcess === 'false') {
+    return Boolean(valueFromProcess)
+  }
+
+  return valueFromProcess
+}
+
+/* eslint-enable no-restricted-globals */

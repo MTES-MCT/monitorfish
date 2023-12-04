6a798e143225afba587d3ae02a18dc4b700fd71c
Loup Theron <loup-rodney@hotmail.fr>
2023-12-04 15:56:31 +0100

    Add public MissionActions controller

---
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/public_api/PublicMissionActionsController.kt
@@ -0,0 +1,30 @@
+package fr.gouv.cnsp.monitorfish.infrastructure.api.public_api
+
+import fr.gouv.cnsp.monitorfish.domain.use_cases.mission_actions.GetMissionActions
+import fr.gouv.cnsp.monitorfish.infrastructure.api.outputs.MissionActionDataOutput
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/mission_actions")
+@Tag(name = "APIs for mission actions")
+class PublicMissionActionsController(
+    private val getMissionActions: GetMissionActions,
+) {
+
+    @GetMapping("")
+    @Operation(summary = "Get mission actions of specified mission")
+    fun getMissionActions(
+        @Parameter(description = "Mission id")
+        @RequestParam(name = "missionId")
+        missionId: Int,
+    ): List<MissionActionDataOutput> {
+        return getMissionActions.execute(missionId).map { MissionActionDataOutput.fromMissionAction(it) }
+    }
+
+}

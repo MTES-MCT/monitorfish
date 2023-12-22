e6d4ffdd4363981215227af744956e5a2348213e
Loup Theron <loup-rodney@hotmail.fr>
2023-12-22 15:10:15 +0100

    Add cypress wait

--- frontend/cypress/e2e/nav_monitorfish.spec.ts
+++ frontend/cypress/e2e/nav_monitorfish.spec.ts
@@ -4,6 +4,7 @@ context('Light MonitorFish', () => {
     cy.intercept('/bff/v1/authorization/current', { statusCode: 401 }).as('getIsSuperUser')
     cy.visit('/light#@-824534.42,6082993.21,8.70')
     cy.wait('@getIsSuperUser')
+    cy.wait(200)
 
     // Then
     // Vessel sidebar is minimized

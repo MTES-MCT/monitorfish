d7f6991173d7ea90845794db83a3dad64b5fbe64
Vincent <vincent.chery@m4x.org>
2024-01-18 11:35:44 +0100

    Rename mirror_pg_data

--- data_warehouse/forklift/pipeline/flows/proxy_pg_database.py
+++ data_warehouse/forklift/pipeline/flows/proxy_pg_database.py
@@ -8,13 +8,13 @@ from forklift.pipeline.shared_tasks.control_flow import check_flow_not_running
 
 
 @task(checkpoint=False)
-def mirror_pg_database(database: str, schema: str, database_name_in_dw: str):
+def create_proxy_pg_database(database: str, schema: str, database_name_in_dw: str):
     """
-    Creates a PostgreSQL engine database in Clickhouse mirroring a remote Postgres
+    Creates a PostgreSQL engine database in Clickhouse proxying a remote Postgres
     database.
 
-    Data is not physically copied to Clickhouse, this just creates a "mirror" in
-    Clickhouse. This mirror can be queried from Clickhouse as if it were a database in
+    Data is not physically copied to Clickhouse, this just creates a proxy in
+    Clickhouse. This proxy can be queried from Clickhouse as if it were a database in
     Clickhouse, but the query is actually delegated to the original remote Postgres
     instance, and results are streamed back to Clickhouse.
 
@@ -23,17 +23,17 @@ def mirror_pg_database(database: str, schema: str, database_name_in_dw: str):
     various sources and performing the join in Clickhouse after collecting the data
     from the different sources.
 
-    The performances of queries on these mirrored databases is however that of databses
+    The performances of queries on these proxyed databases is however that of databases
     which actually execute the queries (plus network overhead), which will be much
     slower than querying data that actually resides in Clickhouse. It is therefore
     recommended to use this mechanism mainly for syncing to tables physically created
     in Clickhouse, and not for user-facing fonctionnality (i.e. dashboards).
 
     Args:
-        database (str): Database to mirror. Possible values : 'monitorfish_remote',
+        database (str): Database to proxy. Possible values : 'monitorfish_remote',
           'monitorenv_remote'
-        schema (str): schema to mirror.
-        database_name_in_dw (str): Name of the mirror database to create in Clickhouse.
+        schema (str): schema to proxy.
+        database_name_in_dw (str): Name of the proxy database to create in Clickhouse.
 
     Raises:
         ValueError: if database credentials for the database are not found in the
@@ -66,13 +66,13 @@ def mirror_pg_database(database: str, schema: str, database_name_in_dw: str):
     client.command(sql)
 
 
-with Flow("Mirror PostgreSQL database") as flow:
+with Flow("Proxy PostgreSQL database") as flow:
     flow_not_running = check_flow_not_running()
     with case(flow_not_running, True):
         database = Parameter("database")
         schema = Parameter("schema")
         database_name_in_dw = Parameter("database_name_in_dw")
-        mirror_pg_database(
+        create_proxy_pg_database(
             database=database, schema=schema, database_name_in_dw=database_name_in_dw
         )
 

96668d0533e7b0011e897ff3930e4e40385f6662
Vincent <vincent.chery@m4x.org>
2024-02-29 15:35:32 +0100

    Update pno_types test data

--- datascience/tests/test_data/remote_database/V666.32__Insert_test_pno_types.sql
+++ datascience/tests/test_data/remote_database/V666.32__Insert_test_pno_types.sql
@@ -11,11 +11,13 @@ INSERT INTO public.pno_types (
 ALTER SEQUENCE pno_types_id_seq RESTART WITH 5;
 
 INSERT INTO public.pno_type_rules (
-     pno_type_id,                 species,                                                    fao_areas,                                    cgpm_areas,  gears, flag_states, minimum_quantity_kg) VALUES 
-    (          1, '{HKE,BSS,COD,ANF,SOL}', '{27.3.a,27.4,27.6,27.7,27.8.a,27.8.b,27.8.c,27.8.d,27.9.a}',                                          '{}',   '{}',        '{}',                   0),
-    (          1,                 '{HKE}',                                                       '{37}', '{30.01,30.05,30.06,30.07,30.09,30.10,30.11}',   '{}',        '{}',                   0),
-    (          1,     '{HER,MAC,HOM,WHB}',                                           '{27,34.1.2,34.2}',                                          '{}',   '{}',        '{}',               10000),
-    (          2, '{HKE,BSS,COD,ANF,SOL}', '{27.3.a,27.4,27.6,27.7,27.8.a,27.8.b,27.8.c,27.8.d,27.9.a}',                                          '{}',   '{}',        '{}',                2000),
-    (          2,     '{HER,MAC,HOM,WHB}',                                           '{27,34.1.2,34.2}',                                          '{}',   '{}',        '{}',               10000),
-    (          3,                    '{}',                                                         '{}',                                          '{}',   '{}', '{GBR,VEN}',                   0),
-    (          4,                    '{}',                                                         '{}',                                          '{}', '{SB}',        '{}',                   0);
+     id, pno_type_id,                 species,                                                    fao_areas,                                    cgpm_areas,  gears, flag_states, minimum_quantity_kg) VALUES 
+    ( 1,           1, '{HKE,BSS,COD,ANF,SOL}', '{27.3.a,27.4,27.6,27.7,27.8.a,27.8.b,27.8.c,27.8.d,27.9.a}',                                          '{}',   '{}',        '{}',                   0),
+    ( 2,           1,                 '{HKE}',                                                       '{37}', '{30.01,30.05,30.06,30.07,30.09,30.10,30.11}',   '{}',        '{}',                   0),
+    ( 3,           1,     '{HER,MAC,HOM,WHB}',                                           '{27,34.1.2,34.2}',                                          '{}',   '{}',        '{}',               10000),
+    ( 4,           2, '{HKE,BSS,COD,ANF,SOL}', '{27.3.a,27.4,27.6,27.7,27.8.a,27.8.b,27.8.c,27.8.d,27.9.a}',                                          '{}',   '{}',        '{}',                2000),
+    ( 5,           2,     '{HER,MAC,HOM,WHB}',                                           '{27,34.1.2,34.2}',                                          '{}',   '{}',        '{}',               10000),
+    ( 6,           3,                    '{}',                                                         '{}',                                          '{}',   '{}', '{GBR,VEN}',                   0),
+    ( 7,           4,                    '{}',                                                         '{}',                                          '{}', '{SB}',        '{}',                   0);
+
+ALTER SEQUENCE pno_type_rules_id_seq RESTART WITH 8;

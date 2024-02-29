c1be2e102501f6fef7f5cdb23550a2233369face
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-02-29 03:30:00 +0100

    Fix fleet segments e2e tests

--- frontend/cypress/e2e/backoffice/fleet_segments.spec.ts
+++ frontend/cypress/e2e/backoffice/fleet_segments.spec.ts
@@ -14,7 +14,10 @@ context('Fleet segments', () => {
     cy.wait(1000)
   })
 
-  it('Should render the fleet segments', () => {
+  it('Should list, create, update and delete fleet segments', () => {
+    // -------------------------------------------------------------------------
+    cy.log('Should render the fleet segments')
+
     // Then
     cy.get('.rs-table-row').should('have.length', 44)
     cy.get('[data-cy="row-ATL01-impactRiskFactor"]').contains(1.7)
@@ -27,9 +30,10 @@ context('Fleet segments', () => {
     cy.get('.rs-table-cell-content').eq(14).contains('27.8')
     cy.get('.rs-table-cell-content').eq(14).contains('27.9')
     cy.get('.rs-table-cell-content').eq(14).contains('27.10')
-  })
 
-  it('Should update the impact factor field', () => {
+    // -------------------------------------------------------------------------
+    cy.log('Should update the impact factor field')
+
     // When
     cy.intercept('PUT', `/bff/v1/fleet_segments?year=${currentYear}&segment=ATL01`).as('updateFleetSegment')
     cy.get('[data-cy="row-ATL01-impactRiskFactor"]').click()
@@ -46,9 +50,10 @@ context('Fleet segments', () => {
     cy.wait('@fleetSegments')
     cy.wait(50)
     cy.get('[data-cy="row-ATL01-impactRiskFactor"]').contains(1.2)
-  })
 
-  it('Should update the segment field', () => {
+    // -------------------------------------------------------------------------
+    cy.log('Should update the segment field')
+
     // When
     cy.intercept('PUT', `/bff/v1/fleet_segments?year=${currentYear}&segment=ATL01`).as('updateFleetSegment')
     cy.get('[data-cy="row-ATL01-segment"]').type('{backspace}')
@@ -73,9 +78,12 @@ context('Fleet segments', () => {
     cy.request('PUT', `/bff/v1/fleet_segments?year=${currentYear}&segment=ATL036`, {
       segment: 'ATL01'
     })
-  })
 
-  it('Should update the segment name field', () => {
+    // -------------------------------------------------------------------------
+    cy.log('Should update the segment name field')
+
+    cy.visit('/backoffice/fleet_segments')
+
     // When
     cy.intercept('PUT', `/bff/v1/fleet_segments?year=${currentYear}&segment=ATL01`).as('updateFleetSegment')
     cy.get('[data-cy="row-ATL01-segmentName"]').type('{backspace}').wait(500)
@@ -99,9 +107,12 @@ context('Fleet segments', () => {
     cy.request('PUT', `/bff/v1/fleet_segments?year=${currentYear}&segment=ATL01`, {
       segmentName: 'All Trawls 3'
     })
-  })
 
-  it('Should update the gears field', () => {
+    // -------------------------------------------------------------------------
+    cy.log('Should update the gears field')
+
+    cy.visit('/backoffice/fleet_segments')
+
     // Given
     cy.intercept('PUT', `/bff/v1/fleet_segments?year=${currentYear}&segment=ATL01`).as('updateFleetSegment')
     cy.get('.rs-table-cell-content').eq(11).click()
@@ -159,9 +170,10 @@ context('Fleet segments', () => {
     cy.wait(50)
     cy.get('.rs-table-cell-content').eq(11).contains('MSP')
     cy.get('.rs-table-cell-content').eq(11).contains('NO')
-  })
 
-  it('Should delete a fleet segment', () => {
+    // -------------------------------------------------------------------------
+    cy.log('Should delete a fleet segment')
+
     // Given
     cy.get('.rs-table-row').should('have.length', 44)
     cy.intercept('DELETE', `/bff/v1/fleet_segments?year=${currentYear}&segment=FR_DRB`).as('deleteFleetSegment')
@@ -181,9 +193,10 @@ context('Fleet segments', () => {
     cy.wait(50)
     cy.get('.rs-table-row').should('have.length', 43)
     cy.get('*[data-cy="delete-row-FR_DRB"]').should('not.exist')
-  })
 
-  it('Should create a fleet segment', () => {
+    // -------------------------------------------------------------------------
+    cy.log('Should create a fleet segment')
+
     // Given
     cy.get('[role="row"]').should('have.length', 43)
     cy.intercept('POST', '/bff/v1/fleet_segments').as('createFleetSegment')
@@ -195,37 +208,16 @@ context('Fleet segments', () => {
     cy.fill('Nom', 'SEGMENT007')
     cy.fill('Note d’impact', '2.7')
     cy.fill('Description', 'Malotru’s segment')
-
-    cy.get('[data-cy="create-fleet-segment-gears"]').click({ force: true })
-    cy.get('[data-key="DHS"]').click()
-    cy.wait(500)
-    cy.get('[data-key="FCN"]').scrollIntoView().click()
-
-    cy.get('[data-cy="create-fleet-segment-targeted-species"]').click({ force: true })
-    cy.get('[data-cy="create-fleet-segment-targeted-species"] input').type('COD', { force: true })
-    cy.wait(200)
-    cy.get('[data-key="COD"]').click({ force: true })
-    cy.get('[data-cy="create-fleet-segment-targeted-species"] input').type('SOL', { force: true })
-    cy.wait(200)
-    cy.get('[data-key="SOL"]').click({ force: true })
-
-    // TODO Investigate why this is not working
-    // cy.get('[data-cy="create-fleet-segment-incidental-species"]').click({ force: true })
-    // cy.get('[data-cy="create-fleet-segment-incidental-species"] input').type('ANE', { force: true })
+    cy.fill('Engins', ['DHS', 'FCN'])
+    cy.fill('Espèces ciblées', ['COD', 'SOL'])
+    // TODO Investigate why this is not working.
     // cy.get('[data-key="ANE"]').click()
+    // cy.fill('Prises accessoires', ['ANE', 'BFT'])
+    cy.fill('Prises accessoires', ['FRF'])
+    cy.fill('Zones FAO', ['21.1.A', '21.1.B'])
 
-    cy.get(':nth-child(9) > .rs-picker-tag-wrapper > .rs-picker-search > .rs-picker-search-input > input').type('BF', {
-      force: true
-    })
-    cy.wait(200)
-    cy.get('[data-key="BFT"]').scrollIntoView().click({ force: true })
+    cy.clickButton('Créer')
 
-    cy.get('*[data-cy="create-fleet-segment-fao-zones"]').click({ force: true })
-    cy.wait(200)
-    cy.get('[data-key="21.1.A"]').click({ force: true })
-    cy.get('[data-key="21.1.B"]').click({ force: true })
-
-    cy.get('*[data-cy="create-fleet-segment"]').click()
     cy.wait('@createFleetSegment')
 
     // Then

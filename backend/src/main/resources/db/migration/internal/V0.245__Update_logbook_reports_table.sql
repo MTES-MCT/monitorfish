8b62154bc6e271de9486031499a9618541c67853
Vincent <vincent.chery@m4x.org>
2024-03-01 18:00:35 +0100

    Update logbook_reports migration

--- backend/src/main/resources/db/migration/internal/V0.245__Update_logbook_reports_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.245__Update_logbook_reports_table.sql
@@ -2,4 +2,4 @@ ALTER TABLE public.logbook_reports
     ADD COLUMN enriched BOOLEAN NOT NULL DEFAULT false,
     ADD COLUMN trip_gears jsonb,
     ADD COLUMN pno_types jsonb,
-    ADD COLUMN trip_segments VARCHAR[];
+    ADD COLUMN trip_segments jsonb;

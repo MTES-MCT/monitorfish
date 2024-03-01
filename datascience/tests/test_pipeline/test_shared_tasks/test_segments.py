682cdeb1c07218890b0787ae48f7edb8785fbd2b
Vincent <vincent.chery@m4x.org>
2024-03-01 14:24:25 +0100

    Mose segments test data to fixture

--- datascience/tests/test_pipeline/test_shared_tasks/test_segments.py
+++ datascience/tests/test_pipeline/test_shared_tasks/test_segments.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import pandas as pd
+import pytest
 
 from src.pipeline.shared_tasks.segments import (
     extract_all_segments,
@@ -9,32 +10,8 @@ from src.pipeline.shared_tasks.segments import (
 )
 
 
-def test_extract_segments_of_year(reset_test_data):
-    current_year = datetime.utcnow().year
-    segments = extract_segments_of_year.run(current_year)
-
-    expected_segments = pd.DataFrame(
-        {
-            "segment": ["SWW01/02/03", "SWW04"],
-            "segment_name": ["Bottom trawls", "Midwater trawls"],
-            "gears": [
-                ["OTB", "OTT", "PTB", "OT", "PT", "TBN", "TBS", "TX", "TB"],
-                ["OTM", "PTM"],
-            ],
-            "fao_areas": [["27.8.c", "27.8", "27.9"], ["27.8.c", "27.8"]],
-            "species": [["ANF", "HKE", "LEZ", "MNZ", "NEP", "SOL"], ["HKE"]],
-            "impact_risk_factor": [3.0, 2.1],
-        }
-    )
-
-    pd.testing.assert_frame_equal(
-        segments.sort_values("segment").reset_index(drop=True),
-        expected_segments.sort_values("segment").reset_index(drop=True),
-    )
-
-
-def test_extract_all_segments(reset_test_data):
-    segments = extract_all_segments.run()
+@pytest.fixture
+def expected_all_segments() -> pd.DataFrame:
     current_year = datetime.utcnow().year
     expected_segments = pd.DataFrame(
         {
@@ -67,11 +44,36 @@ def test_extract_all_segments(reset_test_data):
             "impact_risk_factor": [3.0, 2.1, 3.0, 2.1],
         }
     )
+    return expected_segments
+
+
+def test_extract_segments_of_year(reset_test_data):
+    current_year = datetime.utcnow().year
+    segments = extract_segments_of_year.run(current_year)
+
+    expected_segments = pd.DataFrame(
+        {
+            "segment": ["SWW01/02/03", "SWW04"],
+            "segment_name": ["Bottom trawls", "Midwater trawls"],
+            "gears": [
+                ["OTB", "OTT", "PTB", "OT", "PT", "TBN", "TBS", "TX", "TB"],
+                ["OTM", "PTM"],
+            ],
+            "fao_areas": [["27.8.c", "27.8", "27.9"], ["27.8.c", "27.8"]],
+            "species": [["ANF", "HKE", "LEZ", "MNZ", "NEP", "SOL"], ["HKE"]],
+            "impact_risk_factor": [3.0, 2.1],
+        }
+    )
 
     pd.testing.assert_frame_equal(
-        segments.sort_values(["year", "segment"]).reset_index(drop=True),
-        expected_segments.sort_values(["year", "segment"]).reset_index(drop=True),
+        segments.sort_values("segment").reset_index(drop=True),
+        expected_segments.sort_values("segment").reset_index(drop=True),
     )
+
+
+def test_extract_all_segments(reset_test_data, expected_all_segments):
+    segments = extract_all_segments.run()
+    pd.testing.assert_frame_equal(segments, expected_all_segments)
 
 
 def test_unnest_segments():

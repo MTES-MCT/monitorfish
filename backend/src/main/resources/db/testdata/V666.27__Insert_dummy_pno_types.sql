9c0cef62397efaa736cf4b86a2056a9ac659a0d7
Vincent <vincent.chery@m4x.org>
2024-03-02 09:23:02 +0100

    Update backend pno_types test data

--- backend/src/main/resources/db/testdata/V666.27__Insert_dummy_pno_types.sql
+++ backend/src/main/resources/db/testdata/V666.27__Insert_dummy_pno_types.sql
@@ -1,10 +1,11 @@
 INSERT INTO public.pno_types (
-     id                                name, minimum_notification_period, has_designated_ports) VALUES 
-    ( 1,                   'Préavis type 1',                           4,                 true),
-    ( 2,                   'Préavis type 2',                           4,                 true),
-    (10,             'Préavis par pavillon',                           4,                 true),
-    (12,                'Préavis par engin',                           4,                 true);
+    id                                name, minimum_notification_period, has_designated_ports) VALUES 
+    (1,                   'Préavis type 1',                           4,                 true),
+    (2,                   'Préavis type 2',                           4,                 true),
+    (3,             'Préavis par pavillon',                           4,                 true),
+    (4,                'Préavis par engin',                           4,                 true);
 
+ALTER SEQUENCE pno_types_id_seq RESTART WITH 5;
 
 INSERT INTO public.pno_type_rules (
      pno_type_id,                 species,                                                    fao_areas,                                    cgpm_areas,  gears, flag_states, minimum_quantity_kg) VALUES 
@@ -13,5 +14,5 @@ INSERT INTO public.pno_type_rules (
     (          1,     '{HER,MAC,HOM,WHB}',                                           '{27,34.1.2,34.2}',                                          '{}',   '{}',        '{}',               10000),
     (          2, '{HKE,BSS,COD,ANF,SOL}', '{27.3.a,27.4,27.6,27.7,27.8.a,27.8.b,27.8.c,27.8.d,27.9.a}',                                          '{}',   '{}',        '{}',                2000),
     (          2,     '{HER,MAC,HOM,WHB}',                                           '{27,34.1.2,34.2}',                                          '{}',   '{}',        '{}',               10000),
-    (         10,                    '{}',                                                         '{}',                                          '{}',   '{}', '{GBR,VEN}',                   0),
-    (         12,                    '{}',                                                         '{}',                                          '{}', '{SB}',        '{}',                   0);
+    (          3,                    '{}',                                                         '{}',                                          '{}',   '{}', '{GBR,VEN}',                   0),
+    (          4,                    '{}',                                                         '{}',                                          '{}', '{SB}',        '{}',                   0);

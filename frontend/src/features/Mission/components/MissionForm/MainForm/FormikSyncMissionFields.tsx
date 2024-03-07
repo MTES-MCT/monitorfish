428fb450e61e47fd22e80ed58eba9c21255d8fea
Loup Theron <loup-rodney@hotmail.fr>
2024-03-07 09:49:37 +0100

    Fix invalidate of tags

--- frontend/src/features/Mission/components/MissionForm/MainForm/FormikSyncMissionFields.tsx
+++ frontend/src/features/Mission/components/MissionForm/MainForm/FormikSyncMissionFields.tsx
@@ -1,8 +1,8 @@
-import { useDeepCompareEffect } from '@hooks/useDeepCompareEffect'
 import { logInDev } from '@utils/logInDev'
 import { diff } from 'deep-object-diff'
 import { useFormikContext } from 'formik'
 import { omit } from 'lodash'
+import { useEffect } from 'react'
 
 import { useListenToMissionEventUpdatesById } from '../hooks/useListenToMissionEventUpdatesById'
 import { MISSION_EVENT_UNSYNCHRONIZED_PROPERTIES_IN_FORM } from '../sse'
@@ -19,26 +19,24 @@ export function FormikSyncMissionFields({ missionId }: FormikSyncMissionFormProp
   const { setFieldValue, values } = useFormikContext<MissionMainFormValues>()
   const missionEvent = useListenToMissionEventUpdatesById(missionId)
 
-  useDeepCompareEffect(
+  useEffect(
     () => {
       if (!missionEvent) {
         return
       }
 
-      ;(async () => {
-        const receivedDiff = diff(
-          omit(values, MISSION_EVENT_UNSYNCHRONIZED_PROPERTIES_IN_FORM),
-          omit(missionEvent, MISSION_EVENT_UNSYNCHRONIZED_PROPERTIES_IN_FORM)
-        )
+      const receivedDiff = diff(
+        omit(values, MISSION_EVENT_UNSYNCHRONIZED_PROPERTIES_IN_FORM),
+        omit(missionEvent, MISSION_EVENT_UNSYNCHRONIZED_PROPERTIES_IN_FORM)
+      )
 
-        /**
-         * We iterate and use `setFieldValue` on each diff key to avoid a global re-render of the <MainForm/> component
-         */
-        Object.keys(receivedDiff).forEach(key => {
-          logInDev(`SSE: setting form key "${key}" to ${JSON.stringify(missionEvent[key])}`)
-          setFieldValue(key, missionEvent[key])
-        })
-      })()
+      /**
+       * We iterate and use `setFieldValue` on each diff key to avoid a global re-render of the <MainForm/> component
+       */
+      Object.keys(receivedDiff).forEach(key => {
+        logInDev(`SSE: setting form key "${key}" to ${JSON.stringify(missionEvent[key])}`)
+        setFieldValue(key, missionEvent[key])
+      })
     },
 
     // We don't want to trigger infinite re-renders since `setFieldValue` changes after each rendering

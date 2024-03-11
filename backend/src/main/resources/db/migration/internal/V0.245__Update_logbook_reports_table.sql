717b7455408a6315eb2f3b5aa0e58a1af1e0b7bc
Vincent <vincent.chery@m4x.org>
2024-03-11 14:58:20 +0100

    Move pno_types info into  field

--- backend/src/main/resources/db/migration/internal/V0.245__Update_logbook_reports_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.245__Update_logbook_reports_table.sql
@@ -1,5 +1,4 @@
 ALTER TABLE public.logbook_reports
     ADD COLUMN enriched BOOLEAN NOT NULL DEFAULT false,
     ADD COLUMN trip_gears jsonb,
-    ADD COLUMN pno_types jsonb,
     ADD COLUMN trip_segments jsonb;

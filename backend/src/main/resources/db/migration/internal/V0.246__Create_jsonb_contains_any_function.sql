5c5ed81d79421c1893c2cfd04782fd538ff32d44
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-03-15 09:51:30 +0100

    Split internal migration file

--- backend/src/main/resources/db/migration/internal/V0.246__Create_jsonb_contains_any_function.sql
+++ backend/src/main/resources/db/migration/internal/V0.246__Create_jsonb_contains_any_function.sql
@@ -45,11 +45,3 @@ RETURNS boolean AS $$
         RETURN FALSE;
     END;
 $$ LANGUAGE plpgsql IMMUTABLE;
-
-CREATE OR REPLACE FUNCTION jsonb_to_timestamp(json_data jsonb, key text)
-RETURNS timestamp AS $$
-    BEGIN
-        RETURN (SELECT TO_TIMESTAMP(json_data ->> key, 'YYYY-MM-DD"T"HH24:MI:SS"Z"'));
-    END;
-$$ LANGUAGE plpgsql IMMUTABLE;
-

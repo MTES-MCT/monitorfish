aa2aaeddbaf4319946a44862cb2e3200843c592f
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-03-15 11:03:51 +0100

    Restore default null values in Backend Port entity

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/port/Port.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/port/Port.kt
@@ -3,9 +3,9 @@ package fr.gouv.cnsp.monitorfish.domain.entities.port
 data class Port(
     val locode: String,
     val name: String,
-    val facade: String?,
-    val faoAreas: List<String>,
-    val latitude: Double?,
-    val longitude: Double?,
-    val region: String?,
+    val facade: String? = null,
+    val faoAreas: List<String> = emptyList(),
+    val latitude: Double? = null,
+    val longitude: Double? = null,
+    val region: String? = null,
 )

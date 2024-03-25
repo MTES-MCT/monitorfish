3098ede3e3c8982e807b5432328719b1a71f6ed4
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-03-25 01:07:45 +0100

    Add GetPrioNotificationTypes use case unit test in Backend

---
+++ backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/prior_notification/GetPriorNotificationsUTests.kt
@@ -0,0 +1,29 @@
+package fr.gouv.cnsp.monitorfish.domain.use_cases.prior_notification
+
+import com.nhaarman.mockitokotlin2.given
+import fr.gouv.cnsp.monitorfish.domain.repositories.*
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+class GetPriorNotificationsUTests {
+    @MockBean
+    private lateinit var logbookReportRepository: LogbookReportRepository
+
+    @Test
+    fun `execute Should return a list of prior notifications`() {
+        // Given
+        given(logbookReportRepository.findDistinctPriorNotificationTypes()).willReturn(
+            listOf("Préavis de Type A", "Préavis de Type B"),
+        )
+
+        // When
+        val result = GetPriorNotificationTypes(logbookReportRepository).execute()
+
+        // Then
+        Assertions.assertThat(result).isEqualTo(listOf("Préavis de Type A", "Préavis de Type B"))
+    }
+}

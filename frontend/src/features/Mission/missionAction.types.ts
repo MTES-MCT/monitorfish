094afb29f4e915040af2356a409e55854e144a4a
Loup Theron <loup-rodney@hotmail.fr>
2024-04-02 16:39:07 +0200

    wip: add completion tag

--- frontend/src/features/Mission/missionAction.types.ts
+++ frontend/src/features/Mission/missionAction.types.ts
@@ -5,6 +5,7 @@ export namespace MissionAction {
     actionDatetimeUtc: string
     actionType: MissionActionType
     closedBy: string | undefined
+    completion: CompletionStatus
     controlQualityComments: string | undefined
     controlUnits: LegacyControlUnit.LegacyControlUnit[]
     districtCode: string | undefined
@@ -158,5 +159,17 @@ export namespace MissionAction {
     nbFish: number | undefined
     speciesCode: string
     underSized: boolean | undefined
+  }
+
+  export enum CompletionStatus {
+    COMPLETED = 'COMPLETED',
+    TO_COMPLETE = 'TO_COMPLETE'
+  }
+
+  export enum FrontCompletionStatus {
+    COMPLETED = 'COMPLETED',
+    TO_COMPLETE = 'TO_COMPLETE',
+    TO_COMPLETE_MISSION_ENDED = 'TO_COMPLETE_MISSION_ENDED',
+    UP_TO_DATE = 'UP_TO_DATE'
   }
 }

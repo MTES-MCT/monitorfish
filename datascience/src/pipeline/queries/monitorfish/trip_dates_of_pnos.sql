c504514ab9149952797dbe65f6ccd489674c0ff7
Vincent <vincent.chery@m4x.org>
2024-04-04 11:52:55 +0200

    Get trip dates of all PNOs

--- datascience/src/pipeline/queries/monitorfish/trip_dates_of_pnos.sql
+++ datascience/src/pipeline/queries/monitorfish/trip_dates_of_pnos.sql
@@ -1,26 +1,9 @@
-WITH deleted_corrected_or_rejected_messages AS (
-    SELECT referenced_report_id
-    FROM logbook_reports
-    WHERE
-        operation_datetime_utc >= :pno_emission_start_datetime_utc - INTERVAL '1 day'
-        AND operation_datetime_utc <= :pno_emission_end_datetime_utc + INTERVAL '1 week'
-        AND 
-            (
-            operation_type IN ('COR', 'DEL')
-            OR (
-                operation_type = 'RET'
-                AND value->>'returnStatus' = '002'
-            )
-        )
-)
-
 SELECT
     MIN((r.value->>'tripStartDate')::TIMESTAMPTZ) AS min_trip_start_date,
     MAX((r.value->>'predictedArrivalDatetimeUtc')::TIMESTAMPTZ) AS max_predicted_arrival_datetime_utc
 FROM logbook_reports r
 WHERE
-    --operation_datetime_utc AT TIME ZONE 'UTC' >= :pno_emission_start_datetime_utc
-    operation_datetime_utc  <= :pno_emission_end_datetime_utc + INTERVAL '2 hours 41 minutes'
+    operation_datetime_utc >= :pno_emission_start_datetime_utc
+    AND operation_datetime_utc <= :pno_emission_end_datetime_utc
     AND log_type = 'PNO'
     AND NOT enriched
-    AND report_id NOT IN (SELECT referenced_report_id FROM deleted_corrected_or_rejected_messages)

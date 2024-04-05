eba60952dd70a58b2de48c7a90ab720d2c036540
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-04-05 10:24:56 +0200

    Move prior notification toggling to slice

--- frontend/src/features/PriorNotification/components/PriorNotificationList/ButtonsGroupRow.tsx
+++ frontend/src/features/PriorNotification/components/PriorNotificationList/ButtonsGroupRow.tsx
@@ -1,12 +1,20 @@
+import { priorNotificationActions } from '@features/PriorNotification/slice'
+import { useMainAppDispatch } from '@hooks/useMainAppDispatch'
 import { Accent, Icon, IconButton } from '@mtes-mct/monitor-ui'
 import { noop } from 'lodash'
 import styled from 'styled-components'
 
 export function ButtonsGroupRow({ id }) {
+  const dispatch = useMainAppDispatch()
+
+  const openPriorNotificationDetail = () => {
+    dispatch(priorNotificationActions.openPriorNotificationDetail(id))
+  }
+
   return (
     <ButtonsGroup>
       <IconButton accent={Accent.TERTIARY} Icon={Icon.ViewOnMap} onClick={() => noop(id)} />
-      <IconButton accent={Accent.TERTIARY} Icon={Icon.Display} onClick={() => noop(id)} />
+      <IconButton accent={Accent.TERTIARY} Icon={Icon.Display} onClick={openPriorNotificationDetail} />
     </ButtonsGroup>
   )
 }

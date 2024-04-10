e1e181d56d7f538f13d5ae19e7e42dbdb35f837b
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-04-11 01:49:16 +0200

    Replace @ModelAttribute by @RequestParam in PriorNotificationController

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/bff/PriorNotificationController.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/bff/PriorNotificationController.kt
@@ -1,12 +1,13 @@
 package fr.gouv.cnsp.monitorfish.infrastructure.api.bff
 
+import fr.gouv.cnsp.monitorfish.domain.filters.LogbookReportFilter
 import fr.gouv.cnsp.monitorfish.domain.use_cases.prior_notification.GetPriorNotification
 import fr.gouv.cnsp.monitorfish.domain.use_cases.prior_notification.GetPriorNotificationTypes
 import fr.gouv.cnsp.monitorfish.domain.use_cases.prior_notification.GetPriorNotifications
-import fr.gouv.cnsp.monitorfish.infrastructure.api.input.LogbookReportFilterDataInput
 import fr.gouv.cnsp.monitorfish.infrastructure.api.outputs.PriorNotificationDataOutput
 import fr.gouv.cnsp.monitorfish.infrastructure.api.outputs.PriorNotificationDetailDataOutput
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.websocket.server.PathParam
 import org.springframework.web.bind.annotation.*
@@ -22,9 +23,59 @@ class PriorNotificationController(
     @GetMapping("")
     @Operation(summary = "Get all prior notifications")
     fun getAll(
-        @ModelAttribute filter: LogbookReportFilterDataInput,
+        @Parameter(description = "Vessels flag states (countries Alpha3 codes).")
+        @RequestParam(name = "flagStates")
+        flagStates: List<String>? = null,
+        @Parameter(description = "Vessels that are less than 12 meters in length.")
+        @RequestParam(name = "isLessThanTwelveMetersVessel")
+        isLessThanTwelveMetersVessel: Boolean? = null,
+        @Parameter(description = "Vessels that were last controlled after the given date.")
+        @RequestParam(name = "lastControlledAfter")
+        lastControlledAfter: String? = null,
+        @Parameter(description = "Vessels that were last controlled before the given date.")
+        @RequestParam(name = "lastControlledBefore")
+        lastControlledBefore: String? = null,
+        @Parameter(description = "Landings port locodes.")
+        @RequestParam(name = "portLocodes")
+        portLocodes: List<String>? = null,
+        @Parameter(description = "Prior notification types.")
+        @RequestParam(name = "priorNotificationTypes")
+        priorNotificationTypes: List<String>? = null,
+        @Parameter(description = "Search query (vessel name).")
+        @RequestParam(name = "searchQuery")
+        searchQuery: String? = null,
+        @Parameter(description = "Caught species codes.")
+        @RequestParam(name = "specyCodes")
+        specyCodes: List<String>? = null,
+        @Parameter(description = "Trip segment segments.")
+        @RequestParam(name = "tripSegmentSegments")
+        tripSegmentSegments: List<String>? = null,
+        @Parameter(description = "Trip gear codes.")
+        @RequestParam(name = "tripGearCodes")
+        tripGearCodes: List<String>? = null,
+        @Parameter(description = "Vessels that will arrive after the given date.")
+        @RequestParam(name = "willArriveAfter")
+        willArriveAfter: String? = null,
+        @Parameter(description = "Vessels that will arrive before the given date.")
+        @RequestParam(name = "willArriveBefore")
+        willArriveBefore: String? = null,
     ): List<PriorNotificationDataOutput> {
-        return getPriorNotifications.execute(filter.toLogbookReportFilter()).mapNotNull {
+        val logbookReportFilter = LogbookReportFilter(
+            flagStates = flagStates,
+            isLessThanTwelveMetersVessel = isLessThanTwelveMetersVessel,
+            lastControlledAfter = lastControlledAfter,
+            lastControlledBefore = lastControlledBefore,
+            portLocodes = portLocodes,
+            priorNotificationTypes = priorNotificationTypes,
+            searchQuery = searchQuery,
+            specyCodes = specyCodes,
+            tripSegmentSegments = tripSegmentSegments,
+            tripGearCodes = tripGearCodes,
+            willArriveAfter = willArriveAfter,
+            willArriveBefore = willArriveBefore,
+        )
+
+        return getPriorNotifications.execute(logbookReportFilter).mapNotNull {
             PriorNotificationDataOutput.fromPriorNotification(it)
         }
     }

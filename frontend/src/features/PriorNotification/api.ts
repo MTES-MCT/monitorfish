ebebb5fd484e2547fed1dd548a5a85717f2ec64d
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-04-10 14:28:41 +0200

    Revert "Split prior notification type controller & route from non-type one"
    This reverts commit 9ca7ed13d30a2483eb83d4088658c83e53bac2f5.

--- frontend/src/features/PriorNotification/api.ts
+++ frontend/src/features/PriorNotification/api.ts
@@ -5,7 +5,7 @@ import { monitorfishApi } from '../../api/api'
 import type { PriorNotification } from './PriorNotification.types'
 import type { LogbookMessage } from '@features/Logbook/LogbookMessage.types'
 
-const priorNotificationApi = monitorfishApi.injectEndpoints({
+export const priorNotificationApi = monitorfishApi.injectEndpoints({
   endpoints: builder => ({
     getPriorNotification: builder.query<PriorNotification.PriorNotificationDetail, string>({
       providesTags: () => [{ type: 'PriorNotifications' }],
@@ -15,19 +15,14 @@ const priorNotificationApi = monitorfishApi.injectEndpoints({
     getPriorNotifications: builder.query<PriorNotification.PriorNotification[], LogbookMessage.ApiFilter>({
       providesTags: () => [{ type: 'PriorNotifications' }],
       query: filter => getUrlOrPathWithQueryParams(`/prior_notifications`, filter)
-    })
-  })
-})
+    }),
 
-export const { useGetPriorNotificationQuery, useGetPriorNotificationsQuery } = priorNotificationApi
-
-const priorNotificationTypeApi = monitorfishApi.injectEndpoints({
-  endpoints: builder => ({
     getPriorNotificationTypes: builder.query<string[], void>({
-      providesTags: () => [{ type: 'PriorNotifications' }],
-      query: () => `/prior_notification_types`
+      providesTags: () => [{ type: 'PriorNotificationTypes' }],
+      query: () => '/prior_notifications/types'
     })
   })
 })
 
-export const { useGetPriorNotificationTypesQuery } = priorNotificationTypeApi
+export const { useGetPriorNotificationQuery, useGetPriorNotificationsQuery, useGetPriorNotificationTypesQuery } =
+  priorNotificationApi

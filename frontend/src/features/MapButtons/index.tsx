b858c58dbfe9079817d7336271090180774788eb
Loup Theron <loup-rodney@hotmail.fr>
2024-04-17 16:35:02 +0200

    wip: Add logout button and do not fetch user when not super-user url

--- frontend/src/features/MapButtons/index.tsx
+++ frontend/src/features/MapButtons/index.tsx
@@ -1,3 +1,4 @@
+import { Logout } from '@features/Logout/components/Logout'
 import { LegacyRsuiteComponentsWrapper } from 'ui/LegacyRsuiteComponentsWrapper'
 
 import { AlertsMapButton } from './AlertsMapButton'
@@ -50,6 +51,7 @@ export function MapButtons() {
         {isVesselLabelsMapButtonDisplayed && <VesselLabelsMapButton />}
         {isFavoriteVesselsMapButtonDisplayed && <FavoriteVessels />}
         {isSuperUser && isFavoriteVesselsMapButtonDisplayed && <MissionsMenu />}
+        <Logout />
       </LegacyRsuiteComponentsWrapper>
 
       {isSuperUser && <ControlUnitListDialogButton />}

f4a1613d44c0bdb7069bfbcbfa02b4f9852a3055
Loup Theron <loup-rodney@hotmail.fr>
2024-04-19 11:46:45 +0200

    Add it_token_hint to signout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,8 +27,10 @@ export function App({ auth }: AppProps) {
   const userAuthorization = useGetUserAuthorization()
 
   const logout = useCallback(() => {
+    const idTokenHint = auth?.user?.id_token
+
     auth?.removeUser()
-    auth?.signoutRedirect()
+    auth?.signoutRedirect({ id_token_hint: idTokenHint })
   }, [auth])
 
   const userAccount = useMemo(

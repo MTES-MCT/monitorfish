d2725105444e1465fc04d06da70dc2fed853e8e3
Loup Theron <loup-rodney@hotmail.fr>
2024-04-23 16:50:45 +0200

    Add display of env actions notes content

---
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/outputs/EnvMissionActionDataOutput.kt
@@ -0,0 +1,22 @@
+package fr.gouv.cnsp.monitorfish.infrastructure.api.outputs
+
+import fr.gouv.cnsp.monitorfish.domain.entities.mission.env_mission_action.EnvMissionAction
+import fr.gouv.cnsp.monitorfish.domain.entities.mission.env_mission_action.EnvMissionActionType
+import java.time.ZonedDateTime
+import java.util.*
+
+data class EnvMissionActionDataOutput(
+    val id: UUID,
+    val actionStartDateTimeUtc: ZonedDateTime? = null,
+    val actionType: EnvMissionActionType,
+    val observations: String? = null,
+) {
+    companion object {
+        fun fromEnvMissionAction(envMissionAction: EnvMissionAction) = EnvMissionActionDataOutput(
+            id = envMissionAction.id,
+            actionStartDateTimeUtc = envMissionAction.actionStartDateTimeUtc,
+            actionType = envMissionAction.actionType,
+            observations = envMissionAction.observations,
+        )
+    }
+}

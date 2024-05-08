90c0bf4b97daa8c3c7490f38179aa0b9df38774b
Loup Theron <loup-rodney@hotmail.fr>
2024-05-08 17:23:16 +0200

    Add missing env vars

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -8,12 +8,15 @@ interface ImportMetaEnv {
   readonly FRONTEND_MISSION_FORM_AUTO_SAVE_ENABLED: string
   readonly FRONTEND_MISSION_FORM_AUTO_UPDATE_ENABLED: string
   readonly FRONTEND_MONITORENV_URL: string
+  readonly FRONTEND_MONITORFISH_VERSION?: string
   readonly FRONTEND_OIDC_AUTHORITY: string
   readonly FRONTEND_OIDC_CLIENT_ID: string
   readonly FRONTEND_OIDC_ENABLED: string
   readonly FRONTEND_OIDC_REDIRECT_URI: string
   readonly FRONTEND_PRIOR_NOTIFICATION_LIST_ENABLED: string
   readonly FRONTEND_SENTRY_DSN?: string
+  readonly FRONTEND_SENTRY_ENV?: string
+  readonly FRONTEND_SENTRY_TRACING_ORIGINS?: string
   readonly FRONTEND_SHOM_KEY: string
 }
 

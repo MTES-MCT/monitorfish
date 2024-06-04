d41ebf49f4de9b1f64540c91f2119c2f9b329445
Vincent <vincent.chery@m4x.org>
2024-06-04 10:42:17 +0200

    Test extract_pnos_to_distribute

--- datascience/src/pipeline/queries/monitorfish/pnos_to_distribute.sql
+++ datascience/src/pipeline/queries/monitorfish/pnos_to_distribute.sql
@@ -18,29 +18,44 @@ acknowledged_messages AS (
 )
 
 SELECT
-    id,
-    operation_number,
-    operation_datetime_utc,
-    operation_type,
-    report_id,
-    report_datetime_utc,
-    cfr,
-    ircs,
-    external_identification,
-    vessel_name,
-    flag_state,
-    value,
-    trip_gears,
-    trip_segments
-FROM logbook_reports
+    r.id,
+    r.operation_number,
+    r.operation_datetime_utc,
+    r.operation_type,
+    r.report_id,
+    r.report_datetime_utc,
+    r.cfr,
+    r.ircs,
+    r.external_identification,
+    r.vessel_name,
+    r.flag_state,
+    r.value->>'purpose' AS purpose,
+    r.value->'catchOnboard' AS catch_onboard,
+    r.value->>'port' AS port_locode,
+    p.port_name,
+    (r.value->>'predictedArrivalDatetimeUtc')::TIMESTAMPTZ AT TIME ZONE 'UTC' AS predicted_arrival_datetime_utc,
+    r.trip_gears,
+    r.trip_segments,
+    v.length AS vessel_length,
+    v.mmsi,
+    rf.risk_factor,
+    rf.last_control_datetime_utc
+FROM logbook_reports r
+LEFT JOIN vessels v
+ON v.cfr = r.cfr
+LEFT JOIN risk_factors rf
+ON rf.cfr = r.cfr
+LEFT JOIN ports p
+ON p.locode = r.value->>'port'
 WHERE
     operation_datetime_utc >= :start_datetime_utc
     AND operation_datetime_utc < :end_datetime_utc
     AND log_type='PNO'
-    AND (value->>'isPnoToDistribute')::BOOLEAN IS true
+    AND (value->>'isBeingSent')::BOOLEAN IS true
     AND report_id NOT IN (SELECT referenced_report_id FROM deleted_messages)
     AND (
         transmission_format = 'FLUX'
+        OR (value->>'isVerified')::BOOLEAN IS true
         OR (
             transmission_format = 'ERS'
             AND report_id IN (SELECT referenced_report_id FROM acknowledged_messages)

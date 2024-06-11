449ce06eabef68bd7ae222bf25d21bca0c1da2ca
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-06-11 17:48:12 +0200

    Add missing expected landing date validation in prior notification form

--- frontend/src/features/PriorNotification/components/PriorNotificationForm/constants.ts
+++ frontend/src/features/PriorNotification/components/PriorNotificationForm/constants.ts
@@ -13,18 +13,25 @@ const FISHING_CATCH_VALIDATION_SCHEMA: ObjectSchema<PriorNotification.PriorNotif
 })
 
 export const FORM_VALIDATION_SCHEMA: ObjectSchema<FormValues> = object({
-  authorTrigram: string().trim().required(),
+  authorTrigram: string().trim().required('Veuillez indiquer votre trigramme.'),
   didNotFishAfterZeroNotice: boolean().required(),
-  expectedArrivalDate: string().required(),
-  expectedLandingDate: string(),
-  faoArea: string().required(),
-  fishingCatches: array().of(FISHING_CATCH_VALIDATION_SCHEMA.required()).ensure().required().min(1),
+  expectedArrivalDate: string().required("Veuillez indiquer la date d'arrivée estimée."),
+  expectedLandingDate: string().when('$isExpectedLandingDateSameAsExpectedArrivalDate', {
+    is: false,
+    then: schema => schema.required('Veuillez indiquer la date de débarquement prévue.')
+  }),
+  faoArea: string().required('Veuillez indiquer la zone FAO.'),
+  fishingCatches: array()
+    .of(FISHING_CATCH_VALIDATION_SCHEMA.required())
+    .ensure()
+    .required()
+    .min(1, 'Veuillez sélectionner au moins une espèce.'),
   isExpectedLandingDateSameAsExpectedArrivalDate: boolean().required(),
   note: string(),
-  portLocode: string().required(),
-  sentAt: string().required(),
-  tripGearCodes: array().of(string().required()).ensure().required().min(1),
-  vesselId: number().required()
+  portLocode: string().required("Veuillez indiquer le port d'arrivée."),
+  sentAt: string().required('Veuillez indiquer la date de réception du préavis.'),
+  tripGearCodes: array().of(string().required()).ensure().required().min(1, 'Veuillez sélectionner au moins un engin.'),
+  vesselId: number().required('Veuillez indiquer le navire concerné.')
 })
 
 export const INITIAL_FORM_VALUES: FormValues = {

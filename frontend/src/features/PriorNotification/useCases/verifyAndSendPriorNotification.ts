99dfc5ab6e9d8e2e0c1624b5ed7eac7ebd5cc714
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-06-20 04:43:05 +0200

    Link prior notification verify & send action to manual prior notification form

--- frontend/src/features/PriorNotification/useCases/verifyAndSendPriorNotification.ts
+++ frontend/src/features/PriorNotification/useCases/verifyAndSendPriorNotification.ts
@@ -6,18 +6,17 @@ import { displayOrLogError } from 'domain/use_cases/error/displayOrLogError'
 import { priorNotificationApi } from '../priorNotificationApi'
 import { priorNotificationActions } from '../slice'
 
-import type { PriorNotification } from '../PriorNotification.types'
 import type { MainAppThunk } from '@store'
 
-export const updateEditedPriorNotificationComputedValues =
-  (requestData: PriorNotification.ManualPriorNotificationComputeRequestData): MainAppThunk<Promise<void>> =>
+export const verifyAndSendPriorNotification =
+  (reportId: string): MainAppThunk<Promise<void>> =>
   async dispatch => {
     try {
-      const nextPriorNotificationComputedValues = await dispatch(
-        priorNotificationApi.endpoints.computePriorNotification.initiate(requestData)
+      const updatedPriorNotificationDetail = await dispatch(
+        priorNotificationApi.endpoints.verifyAndSendPriorNotification.initiate(reportId)
       ).unwrap()
 
-      dispatch(priorNotificationActions.setEditedPriorNotificationComputedValues(nextPriorNotificationComputedValues))
+      dispatch(priorNotificationActions.setEditedPriorNotificationDetail(updatedPriorNotificationDetail))
     } catch (err) {
       if (err instanceof FrontendApiError) {
         dispatch(displayOrLogError(err, undefined, true, DisplayedErrorKey.SIDE_WINDOW_PRIOR_NOTIFICATION_CARD_ERROR))

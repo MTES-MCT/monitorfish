aae026d78548d511a11fce5d7f6c855e7cf571c5
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-06-20 17:10:38 +0200

    Complete PriorNotificationController unit tests

--- backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/fakers/PriorNotificationFaker.kt
+++ backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/fakers/PriorNotificationFaker.kt
@@ -1,49 +1,30 @@
 package fr.gouv.cnsp.monitorfish.fakers
 
-import fr.gouv.cnsp.monitorfish.domain.entities.logbook.LogbookMessage
 import fr.gouv.cnsp.monitorfish.domain.entities.logbook.LogbookMessageTyped
-import fr.gouv.cnsp.monitorfish.domain.entities.logbook.LogbookOperationType
-import fr.gouv.cnsp.monitorfish.domain.entities.logbook.LogbookTransmissionFormat
 import fr.gouv.cnsp.monitorfish.domain.entities.logbook.messages.PNO
 import fr.gouv.cnsp.monitorfish.domain.entities.prior_notification.PriorNotification
 import java.time.ZonedDateTime
 
 class PriorNotificationFaker {
     companion object {
-        fun fakePriorNotification(): PriorNotification {
+        fun fakePriorNotification(index: Int = 1): PriorNotification {
             return PriorNotification(
-                reportId = "FAKE_REPORT_ID_1",
-                authorTrigram = null,
-                createdAt = null,
+                reportId = "FAKE_REPORT_ID_$index",
+                authorTrigram = "ABC",
+                createdAt = ZonedDateTime.now().toString(),
                 didNotFishAfterZeroNotice = false,
                 isManuallyCreated = false,
                 logbookMessageTyped = LogbookMessageTyped(
                     clazz = PNO::class.java,
-                    logbookMessage = LogbookMessage(
-                        id = 1,
-                        reportId = "FAKE_REPORT_ID_1",
-                        referencedReportId = null,
-                        analyzedByRules = emptyList(),
-                        isDeleted = false,
-                        integrationDateTime = ZonedDateTime.now(),
-                        isCorrectedByNewerMessage = false,
-                        isEnriched = true,
-                        message = PNO(),
-                        messageType = "PNO",
-                        operationDateTime = ZonedDateTime.now(),
-                        operationNumber = "1",
-                        operationType = LogbookOperationType.DAT,
-                        reportDateTime = ZonedDateTime.now(),
-                        transmissionFormat = LogbookTransmissionFormat.ERS,
-                    ),
+                    logbookMessage = LogbookMessageFaker.fakePnoLogbookMessage(index),
                 ),
-                port = null,
+                port = PortFaker.fakePort(),
                 reportingCount = null,
                 seafront = null,
-                sentAt = null,
+                sentAt = ZonedDateTime.now().toString(),
                 state = null,
-                updatedAt = null,
-                vessel = null,
+                updatedAt = ZonedDateTime.now().toString(),
+                vessel = VesselFaker.fakeVessel(),
                 vesselRiskFactor = null,
             )
         }

77e6fdea47250eeabdb6039dd3c6443b46d0ecef
Vincent <vincent.chery@m4x.org>
2024-06-24 17:50:01 +0200

    Handle duplicate ers equipements

--- datascience/src/pipeline/queries/fmc/vessels_logbook_equipment.sql
+++ datascience/src/pipeline/queries/fmc/vessels_logbook_equipment.sql
@@ -1,3 +1,20 @@
+WITH e_sacapt_equipment_statuses AS (
+    SELECT
+        id_fmc_navire,
+        equipe_esacapt,
+        ROW_NUMBER() OVER (PARTITION BY id_fmc_navire ORDER BY DATE_PASSAGE_OFFICIEL DESC) AS rk
+    FROM fmc2.fmc_equipement_ers
+    WHERE est_courant = 1
+),
+
+latest_esacapt_status AS (
+    SELECT
+        id_fmc_navire,
+        equipe_esacapt
+    FROM e_sacapt_equipment_statuses
+    WHERE rk = 1
+)
+
 SELECT
     n.id_nav_flotteur AS id,
     ceq.libelle AS logbook_equipment_status,
@@ -5,6 +22,6 @@ SELECT
 FROM fmc2.fmc_navire n
 LEFT JOIN fmc2.fmc_code_statut_equipemen ceq
 ON ceq.idc_fmc_statut_equipemen = n.idc_fmc_statut_equipemen
-LEFT JOIN fmc2.fmc_equipement_ers eq
-ON eq.id_fmc_navire = n.id_fmc_navire AND eq.est_courant = 1
+LEFT JOIN latest_esacapt_status eq
+ON eq.id_fmc_navire = n.id_fmc_navire
 WHERE n.est_actif = 1

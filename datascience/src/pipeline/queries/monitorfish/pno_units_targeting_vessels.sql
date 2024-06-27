cda12734d61613bd3d1cf66ec52fe7992d8dd81b
Vincent <vincent.chery@m4x.org>
2024-06-27 11:32:14 +0200

    Fix flaky test

--- datascience/src/pipeline/queries/monitorfish/pno_units_targeting_vessels.sql
+++ datascience/src/pipeline/queries/monitorfish/pno_units_targeting_vessels.sql
@@ -1,7 +1,7 @@
 SELECT
     s.vessel_id,
     v.cfr,
-    ARRAY_AGG(control_unit_id) AS control_unit_ids_targeting_vessel
+    ARRAY_AGG(control_unit_id ORDER BY control_unit_id) AS control_unit_ids_targeting_vessel
 FROM pno_vessels_subscriptions s
 JOIN vessels v
 ON v.id = s.vessel_id

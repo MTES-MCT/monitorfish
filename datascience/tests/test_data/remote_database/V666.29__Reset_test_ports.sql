2f46d6f9812280072b851507a7373fe7701e99fc
Vincent <vincent.chery@m4x.org>
2024-07-02 18:30:30 +0200

    Compute and save risk factor upon logbook pno enrichment

--- datascience/tests/test_data/remote_database/V666.29__Reset_test_ports.sql
+++ datascience/tests/test_data/remote_database/V666.29__Reset_test_ports.sql
@@ -1,13 +1,14 @@
 DELETE FROM public.ports;
 
 INSERT INTO public.ports (
-    country_code_iso2, region,     locode,                    port_name, is_active) VALUES 
-    (            'FR',   '56',     'FAKE', 'Fake Port initially active',      true),
-    (            'FR',   '56', 'PNO_PORT',         'Fake Port with PNO',     false),
-    (            'FR',   '56', 'LAN_PORT',         'Fake Port with LAN',     false),
-    (            'FR',  '999',    'FRCQF', 'Somewhere over the rainbow',     false),
-    (            'FR',  '999',    'FRBES',    'Somewhere over the hill',     false),
-    (            'FR',  '999',    'FRDPE',  'Somewhere over the clouds',     false),
-    (            'FR',  '999',    'FRDKK',   'Somewhere over the swell',     false),
-    (            'FR',  '999',    'FRLEH',   'Somewhere over the ocean',     false),
-    (            'FR',  '999',    'FRZJZ',     'Somewhere over the top',     false);
+    country_code_iso2, region,     locode,                    port_name, is_active, facade) VALUES 
+    (            'FR',   '56',     'FAKE', 'Fake Port initially active',      true,   NULL),
+    (            'FR',   '56', 'PNO_PORT',         'Fake Port with PNO',     false,   NULL),
+    (            'FR',   '56', 'LAN_PORT',         'Fake Port with LAN',     false,   NULL),
+    (            'FR',  '999',    'FRCQF', 'Somewhere over the rainbow',     false,   NULL),
+    (            'FR',  '999',    'FRBES',    'Somewhere over the hill',     false,   NULL),
+    (            'FR',  '999',    'FRDPE',  'Somewhere over the clouds',     false,   NULL),
+    (            'FR',  '999',    'FRDKK',   'Somewhere over the swell',     false,   NULL),
+    (            'FR',  '999',    'FRLEH',   'Somewhere over the ocean',     false,   NULL),
+    (            'FR',  '999',    'FRZJZ',     'Somewhere over the top',     false,   NULL),
+    (            'FR',  '999',    'GBPHD',           'Port with facade',     false, 'MEMN');

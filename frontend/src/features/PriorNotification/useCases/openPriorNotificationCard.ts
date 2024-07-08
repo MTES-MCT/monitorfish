fde7c853b49de6edeab699f0e2a4c4b605151394
Loup Theron <loup-rodney@hotmail.fr>
2024-07-08 17:28:48 +0200

    Fix sidebar not showing when there is an error in the api

--- frontend/src/features/PriorNotification/useCases/openPriorNotificationCard.ts
+++ frontend/src/features/PriorNotification/useCases/openPriorNotificationCard.ts
@@ -55,6 +55,8 @@ export const openPriorNotificationCard =
       dispatch(priorNotificationActions.openPriorNotificationCard())
     } catch (err) {
       if (err instanceof FrontendApiError) {
+        dispatch(priorNotificationActions.openPriorNotificationCard())
+
         dispatch(
           displayOrLogError(
             err,

b965e08b8a9701c2b76c427a0e8abd883a5b3499
Loup Theron <loup-rodney@hotmail.fr>
2024-08-02 15:10:28 +0200

    Add test controller

---
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/public_api/SentryController.kt
@@ -0,0 +1,17 @@
+package fr.gouv.cnsp.monitorfish.infrastructure.api.public_api
+
+import org.springframework.boot.info.BuildProperties
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class VersionController(val buildProperties: BuildProperties) {
+
+    @GetMapping("/version")
+    fun version(): Map<String, String> {
+        return mapOf(
+            "version" to buildProperties.version,
+            "commit" to buildProperties.get("commit.hash"),
+        )
+    }
+}

51d4c55a859744fd015b6f5a01402335600c8fdf
Vincent <vincent.chery@m4x.org>
2024-08-07 11:33:35 +0200

    Remove UPDATE from migration

--- backend/src/main/resources/db/migration/internal/V0.270__Update_logbook_reports_tables.sql
+++ backend/src/main/resources/db/migration/internal/V0.270__Update_logbook_reports_tables.sql
@@ -1,19 +1,21 @@
 ALTER TABLE public.logbook_reports
 ADD COLUMN is_test_message BOOLEAN NOT NULL DEFAULT false;
 
-WITH test_messages AS (
-    SELECT
-        operation_number
-    FROM logbook_raw_messages
-    WHERE (
-        xpath(
-            '//ers:OPS/attribute::TS',
-            xml_message::xml,
-            ARRAY[ARRAY['ers', 'http://ec.europa.eu/fisheries/schema/ers/v3']]
-        )
-    )[1]::VARCHAR = '1'
-)
+-- To execute manually after deployment, in order to avoid a down time :
 
-UPDATE logbook_reports
-SET is_test_message = true
-WHERE operation_number IN (SELECT operation_number FROM test_messages);
+-- WITH test_messages AS (
+--     SELECT
+--         operation_number
+--     FROM logbook_raw_messages
+--     WHERE (
+--         xpath(
+--             '//ers:OPS/attribute::TS',
+--             xml_message::xml,
+--             ARRAY[ARRAY['ers', 'http://ec.europa.eu/fisheries/schema/ers/v3']]
+--         )
+--     )[1]::VARCHAR = '1'
+-- )
+-- 
+-- UPDATE logbook_reports
+-- SET is_test_message = true
+-- WHERE operation_number IN (SELECT operation_number FROM test_messages);

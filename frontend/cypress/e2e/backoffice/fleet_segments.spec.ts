68bc2a48a18a40fd7f1d559ac32b46867d50ac5e
Loup Theron <loup-rodney@hotmail.fr>
2024-08-09 11:14:58 +0200

    Fix few tests

--- frontend/cypress/e2e/backoffice/fleet_segments.spec.ts
+++ frontend/cypress/e2e/backoffice/fleet_segments.spec.ts
@@ -8,7 +8,7 @@ dayjs.extend(utc)
 const currentYear = dayjs().utc().year()
 context('Fleet segments', () => {
   beforeEach(() => {
-    cy.intercept('GET', `/bff/v1/fleet_segments/${currentYear}`).as('fleetSegments')
+    cy.intercept('GET', `/bff/v1/fleet_segments/backoffice/${currentYear}`).as('fleetSegments')
     cy.visit('/backoffice/fleet_segments')
     cy.wait('@fleetSegments')
     cy.wait(1000)
@@ -35,7 +35,7 @@ context('Fleet segments', () => {
     cy.log('Should update the segment')
 
     // When
-    cy.intercept('PUT', `/bff/v1/fleet_segments?year=${currentYear}&segment=ATL01`).as('updateFleetSegment')
+    cy.intercept('PUT', `/bff/v1/fleet_segments/backoffice?year=${currentYear}&segment=ATL01`).as('updateFleetSegment')
 
     cy.get('[aria-rowindex="2"]').find('[title="Editer la ligne"]').click()
     cy.fill('Nom', 'ATL036')
@@ -92,7 +92,7 @@ context('Fleet segments', () => {
 
   it('Should create a new fleet segment and delete it', () => {
     // Given
-    cy.intercept('POST', '/bff/v1/fleet_segments').as('createFleetSegment')
+    cy.intercept('POST', '/bff/v1/fleet_segments/backoffice').as('createFleetSegment')
 
     // When
     cy.clickButton('Ajouter un segment')
@@ -137,7 +137,9 @@ context('Fleet segments', () => {
     // -------------------------------------------------------------------------
     cy.log('Should delete a fleet segment')
 
-    cy.intercept('DELETE', `/bff/v1/fleet_segments?year=${currentYear}&segment=ABC123`).as('deleteFleetSegment')
+    cy.intercept('DELETE', `/bff/v1/fleet_segments/backoffice?year=${currentYear}&segment=ABC123`).as(
+      'deleteFleetSegment'
+    )
     cy.get('[aria-rowindex="2"]').find('[title="Supprimer la ligne"]').click()
     cy.wait('@deleteFleetSegment')
 

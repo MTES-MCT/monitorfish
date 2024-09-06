7c3b8e49637a97a0d6a9640e5e8f244e960fd46e
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-06 08:19:16 +0200

    Add logbook pno form upload create/delete e2e test

--- frontend/cypress/e2e/side_window/logbook_prior_notification_form/behavior.spec.ts
+++ frontend/cypress/e2e/side_window/logbook_prior_notification_form/behavior.spec.ts
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs'
+
 import { editSideWindowPriorNotification } from './utils'
 
 context('Side Window > Logbook Prior Notification Form > Behavior', () => {
@@ -11,5 +13,28 @@ context('Side Window > Logbook Prior Notification Form > Behavior', () => {
 
     cy.contains('button', 'Télécharger').should('be.disabled')
     cy.contains('button', 'Diffuser').should('be.disabled')
+  })
+
+  it('Should not update the logbook prior notification before the form is filled', () => {
+    cy.intercept('PUT', '/bff/v1/prior_notifications/logbook/FAKE_OPERATION_116*', cy.spy().as('updateForm'))
+
+    editSideWindowPriorNotification(`LE MARIN`, 'FAKE_OPERATION_116')
+
+    cy.contains(`LE MARIN D'EAU DOUCE (CFR111)`).should('be.visible')
+
+    cy.get('@updateForm').should('not.have.been.called')
+
+    cy.fill("Points d'attention identifiés par le CNSP", 'Une note')
+
+    cy.get('@updateForm').should('have.been.calledOnce')
+
+    // Reset
+    const operationDate = dayjs().subtract(6, 'hours').toISOString()
+    cy.request('PUT', `/bff/v1/prior_notifications/logbook/FAKE_OPERATION_116?operationDate=${operationDate}`, {
+      body: {
+        authorTrigram: null,
+        note: null
+      }
+    })
   })
 })

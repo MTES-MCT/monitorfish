2b776e31076712936b96f05f60b867adcb4d0576
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-10 17:14:49 +0200

    Remove default instantiation values from Port entity in Backend

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/port/Port.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/port/Port.kt
@@ -3,17 +3,18 @@ package fr.gouv.cnsp.monitorfish.domain.entities.port
 data class Port(
     val locode: String,
     /** ISO Alpha-2 country code */
-    val countryCode: String? = null,
+    val countryCode: String?,
     val name: String,
-    val facade: String? = null,
+    val facade: String?,
     val faoAreas: List<String>,
-    val latitude: Double? = null,
-    val longitude: Double? = null,
-    val region: String? = null,
+    val latitude: Double?,
+    val longitude: Double?,
+    val region: String?,
 ) {
-    private val FRENCH_PORT_COUNTRY_CODES = listOf<String>(
-        "TF", "MQ", "GF", "YT", "GP", "RE", "NC", "PF", "BL", "MF", "PM", "WF",
-    )
+    private val FRENCH_PORT_COUNTRY_CODES =
+        listOf<String>(
+            "TF", "MQ", "GF", "YT", "GP", "RE", "NC", "PF", "BL", "MF", "PM", "WF",
+        )
 
     fun isFrenchOrUnknown(): Boolean {
         return this.countryCode === null || FRENCH_PORT_COUNTRY_CODES.contains(countryCode)

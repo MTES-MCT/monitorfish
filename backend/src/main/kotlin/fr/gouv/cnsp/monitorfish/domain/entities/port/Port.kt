08ed9c207f24ec357ad7edd236f186d780b6ed71
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-10 16:42:49 +0200

    Exclude foreign prior notifications

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/port/Port.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/port/Port.kt
@@ -2,10 +2,20 @@ package fr.gouv.cnsp.monitorfish.domain.entities.port
 
 data class Port(
     val locode: String,
+    /** ISO Alpha-2 country code */
+    val countryCode: String? = null,
     val name: String,
     val facade: String? = null,
-    val faoAreas: List<String> = emptyList(),
+    val faoAreas: List<String>,
     val latitude: Double? = null,
     val longitude: Double? = null,
     val region: String? = null,
-)
+) {
+    private val FRENCH_PORT_COUNTRY_CODES = listOf<String>(
+        "TF", "MQ", "GF", "YT", "GP", "RE", "NC", "PF", "BL", "MF", "PM", "WF",
+    )
+
+    fun isFrenchOrUnknown(): Boolean {
+        return this.countryCode === null || FRENCH_PORT_COUNTRY_CODES.contains(countryCode)
+    }
+}

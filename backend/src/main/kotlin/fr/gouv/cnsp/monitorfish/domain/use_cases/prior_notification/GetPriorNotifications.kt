dbc3f576198d7282243511b135c90a8b6511240a
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-11 06:28:31 +0200

    Add unit tests for PriorNotification.isPriorNotificationZero getter in Backend

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/prior_notification/GetPriorNotifications.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/prior_notification/GetPriorNotifications.kt
@@ -113,11 +113,11 @@ class GetPriorNotifications(
             measureTimedValue {
                 PriorNotificationStats(
                     perSeafrontGroupCount =
-                    SeafrontGroup.entries.associateWith { seafrontGroupEntry ->
-                        priorNotifications.count { priorNotification ->
-                            seafrontGroupEntry.hasSeafront(priorNotification.seafront)
-                        }
-                    },
+                        SeafrontGroup.entries.associateWith { seafrontGroupEntry ->
+                            priorNotifications.count { priorNotification ->
+                                seafrontGroupEntry.hasSeafront(priorNotification.seafront)
+                            }
+                        },
                 )
             }
         logger.info("TIME_RECORD - 'extraData' took $extraDataTimeTaken.")

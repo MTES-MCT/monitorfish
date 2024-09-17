eb17ffa4f947d0e7703454e19fc015189ae90c71
Loup Theron <loup-rodney@hotmail.fr>
2024-09-17 17:55:19 +0200

    Fix caching of control units API request

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/control_units/GetAllControlUnits.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/control_units/GetAllControlUnits.kt
@@ -3,7 +3,6 @@ package fr.gouv.cnsp.monitorfish.domain.use_cases.control_units
 import fr.gouv.cnsp.monitorfish.config.UseCase
 import fr.gouv.cnsp.monitorfish.domain.entities.mission.ControlUnit
 import fr.gouv.cnsp.monitorfish.domain.repositories.ControlUnitRepository
-import kotlinx.coroutines.runBlocking
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -14,8 +13,6 @@ class GetAllControlUnits(
     private val logger: Logger = LoggerFactory.getLogger(GetAllControlUnits::class.java)
 
     fun execute(): List<ControlUnit> {
-        return runBlocking {
-            return@runBlocking controlUnitsRepository.findAll(this).await()
-        }
+        return controlUnitsRepository.findAll()
     }
 }

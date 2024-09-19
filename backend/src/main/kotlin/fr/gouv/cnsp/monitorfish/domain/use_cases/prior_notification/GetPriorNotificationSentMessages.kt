e42a9e067094f620b29ba59a0d39488475442305
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-19 03:49:36 +0200

    Add GetPriorNotificationSentMessages use case

---
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/prior_notification/GetPriorNotificationSentMessages.kt
@@ -0,0 +1,14 @@
+package fr.gouv.cnsp.monitorfish.domain.use_cases.prior_notification
+
+import fr.gouv.cnsp.monitorfish.config.UseCase
+import fr.gouv.cnsp.monitorfish.domain.entities.prior_notification.PriorNotificationSentMessage
+import fr.gouv.cnsp.monitorfish.domain.repositories.*
+
+@UseCase
+class GetPriorNotificationSentMessages(
+    private val priorNotificationSentMessageRepository: PriorNotificationSentMessageRepository,
+) {
+    fun execute(reportId: String): List<PriorNotificationSentMessage> {
+        return priorNotificationSentMessageRepository.findAllByReportId(reportId)
+    }
+}

94fe78f9f9a1ad717e1ae98c4f3ed0267bc9409d
Vincent <vincent.chery@m4x.org>
2024-09-23 11:43:10 +0200

    Add email_address to facades

--- datascience/tests/test_data/remote_database/V666.9__Reset_test_facade_areas_subdivided.sql
+++ datascience/tests/test_data/remote_database/V666.9__Reset_test_facade_areas_subdivided.sql
@@ -1,7 +1,7 @@
 DELETE FROM facade_areas_subdivided;
 
 INSERT INTO facade_areas_subdivided (
-    facade, geometry
+    facade, email_address,                                                                                          geometry
 ) VALUES
-('NAMO', ST_Polygon('LINESTRING(10.0 45.0, -10.0 45.0, -10.0 0.0, 10.0 0.0, 10.0 45.0)'::geometry, 4326)),
-('SA',   ST_Polygon('LINESTRING(10.0 45.0, -10.0 45.0, -10.0 50.0, 10.0 50.0, 10.0 45.0)'::geometry, 4326));
+(   'NAMO',  'namo@email',   ST_Polygon('LINESTRING(10.0 45.0, -10.0 45.0, -10.0 0.0, 10.0 0.0, 10.0 45.0)'::geometry, 4326)),
+(     'SA',    'sa@email', ST_Polygon('LINESTRING(10.0 45.0, -10.0 45.0, -10.0 50.0, 10.0 50.0, 10.0 45.0)'::geometry, 4326));

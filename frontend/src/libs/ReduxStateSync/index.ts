8437c96df0da7b81b403e616c35577f323433436
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-09-25 07:43:56 +0200

    Comment ReduxStateSync#handleBroadcastChannelMessage() cases

--- frontend/src/libs/ReduxStateSync/index.ts
+++ frontend/src/libs/ReduxStateSync/index.ts
@@ -32,7 +32,7 @@ class ReduxStateSync {
     })
   }
 
-  createStateSyncMiddleware = (options: Partial<ReduxStateSyncOptions>) => {
+  createStateSyncMiddleware(options: Partial<ReduxStateSyncOptions>) {
     const controlledOptions = { ...DEFAULT_OPTIONS, ...options }
 
     return ({ dispatch, getState }) =>
@@ -76,34 +76,44 @@ class ReduxStateSync {
   #handleBroadcastChannelMessage(event: MessageEvent<Message>) {
     const message = event.data
 
+    // When another tab asks us to send our current Redux Store state
     switch (message.type) {
       case MessageType.GetInitialState:
+        // we check that we haven't already sent it
         if (!this.#tabs[message.tabId]) {
           this.#tabs[message.tabId] = true
 
+          // and we send it to the other tab
           this.#dispatch!(sendStateToOtherTab(message.tabId))
         }
 
         return
 
+      // When another tab sends us their current Redux Store state
+      // (because we asked for it via `MessageType.GetInitialState`)
       case MessageType.SendInitialState:
+        // we check that we haven't already received it
         if (!this.#isSynced && message.state) {
           this.#isSynced = true
 
+          // and we initialize our Redux Store state with the one we received
           this.#dispatch!(initializeStateFromOtherTab(message.tabId, message.state))
         }
 
         return
 
+      // When another tab has an action dispatched to their Redux Store state,
       case MessageType.DispatchAction:
         if (message.stampedAction) {
+          // we dispatch it to our Redux Store state to keep it synchronized
           this.#dispatch!(message.stampedAction)
         }
 
-        // eslint-disable-next-line no-useless-return
         return
 
+      // When another tab gets closed,
       case MessageType.DestroyTab:
+        // we delete it from our tabs list
         delete this.#tabs[message.tabId]
 
         // eslint-disable-next-line no-useless-return

adc1b0d9c08d7964567a8b35208bc06a1d3a4ac5
Vincent <vincent.chery@m4x.org>
2024-09-26 17:07:15 +0200

    Change columns names

--- backend/src/main/resources/db/migration/internal/V0.279__Update_prior_notification_sent_messages_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.279__Update_prior_notification_sent_messages_table.sql
@@ -1,5 +1,5 @@
 DELETE FROM public.prior_notification_sent_messages;
 
 ALTER TABLE public.prior_notification_sent_messages
-    ADD COLUMN control_unit VARCHAR NOT NULL,
-    ADD COLUMN administration VARCHAR NOT NULL;
+    ADD COLUMN recipient_name VARCHAR NOT NULL,
+    ADD COLUMN recipient_organization VARCHAR NOT NULL;

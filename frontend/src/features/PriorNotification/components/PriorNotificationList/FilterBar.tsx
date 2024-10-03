006c390a522968bf51a5cc60959e3a9c2814837d
Loup Theron <loup-rodney@hotmail.fr>
2024-10-03 11:36:15 +0200

    Prevent reportings filter to be displayed in non super-user

--- frontend/src/features/PriorNotification/components/PriorNotificationList/FilterBar.tsx
+++ frontend/src/features/PriorNotification/components/PriorNotificationList/FilterBar.tsx
@@ -32,6 +32,7 @@ import {
   ExpectedArrivalPeriod,
   FILTER_STATUSES_AS_OPTIONS
 } from './constants'
+import { useIsSuperUser } from '../../../../auth/hooks/useIsSuperUser'
 import { priorNotificationActions } from '../../slice'
 
 import type { FilterStatus } from './types'
@@ -42,9 +43,10 @@ export type FilterBarProps = {
   searchQuery: string | undefined
 }
 export function FilterBar() {
+  const dispatch = useMainAppDispatch()
+  const isSuperUser = useIsSuperUser()
   const { newWindowContainerRef } = useNewWindow()
   const listFilterValues = useMainAppSelector(store => store.priorNotification.listFilterValues)
-  const dispatch = useMainAppDispatch()
 
   const { fleetSegmentsAsOptions } = useGetFleetSegmentsAsOptions()
   const { gearsAsTreeOptions } = useGetGearsAsTreeOptions()
@@ -219,16 +221,18 @@ export function FilterBar() {
           style={{ minWidth: 224 }}
           value={listFilterValues.lastControlPeriod}
         />
-        <RichBooleanCheckbox
-          falseOptionLabel="Sans signalement"
-          isInline
-          isLabelHidden
-          label="Signalements"
-          name="hasOneOrMoreReportings"
-          onChange={updateHasOneOrMoreReportings}
-          trueOptionLabel="Avec signalements"
-          value={listFilterValues.hasOneOrMoreReportings}
-        />
+        {isSuperUser && (
+          <RichBooleanCheckbox
+            falseOptionLabel="Sans signalement"
+            isInline
+            isLabelHidden
+            label="Signalements"
+            name="hasOneOrMoreReportings"
+            onChange={updateHasOneOrMoreReportings}
+            trueOptionLabel="Avec signalements"
+            value={listFilterValues.hasOneOrMoreReportings}
+          />
+        )}
       </Row>
 
       <Row>

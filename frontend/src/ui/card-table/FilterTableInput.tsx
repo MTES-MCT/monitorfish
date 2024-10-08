42ebe3931cfb6c82d5532cb096102117df8cb750
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-10-09 00:31:24 +0200

    Prefix styled component props in ReportingList & SilencedAlerts

--- frontend/src/ui/card-table/FilterTableInput.tsx
+++ frontend/src/ui/card-table/FilterTableInput.tsx
@@ -1,26 +1,25 @@
 import styled from 'styled-components'
 
-import { COLORS } from '../../constants/constants'
 import SearchIconSVG from '../../features/icons/Loupe_dark.svg?react'
 
 export const FilterTableInput = styled.input<{
-  baseUrl: string
+  $baseUrl: string
 }>`
   background-color: white;
-  border: 1px ${COLORS.lightGray} solid;
+  border: 1px ${p => p.theme.color.lightGray} solid;
   border-radius: 0px;
-  color: ${COLORS.gunMetal};
+  color: ${p => p.theme.color.gunMetal};
   font-size: 13px;
   height: 40px;
   width: 280px;
   padding: 0 5px 0 10px;
-  background-image: url(${p => `${p.baseUrl}${SearchIconSVG}`});
+  background-image: url(${p => `${p.$baseUrl}${SearchIconSVG}`});
   background-size: 25px;
   background-position: bottom 3px right 5px;
   background-repeat: no-repeat;
 
   &:hover,
   &:focus {
-    border-bottom: 1px ${COLORS.lightGray} solid;
+    border-bottom: 1px ${p => p.theme.color.lightGray} solid;
   }
 `

202d67f463e209b1d05c2b0d8250ec8e45d81dfe
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-10-09 10:34:41 +0200

    Make CardReportingList more flexible via props

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/bff/VesselController.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/bff/VesselController.kt
@@ -223,7 +223,7 @@ class VesselController(
     fun searchVessel(
         @Parameter(
             description =
-            "Vessel internal reference number (CFR), external marker, IRCS, MMSI, name or beacon number",
+                "Vessel internal reference number (CFR), external marker, IRCS, MMSI, name or beacon number",
             required = true,
         )
         @RequestParam(name = "searched")
@@ -242,7 +242,7 @@ class VesselController(
         internalReferenceNumber: String,
         @Parameter(
             description =
-            "Voyage request (LAST, PREVIOUS or NEXT) with respect to date",
+                "Voyage request (LAST, PREVIOUS or NEXT) with respect to date",
             required = true,
         )
         @RequestParam(name = "voyageRequest")

ec82fe3b37de59be19642d5634eb78437d7f0677
Loup Theron <loup-rodney@hotmail.fr>
2024-10-17 18:51:11 +0200

    SSO - Correction des erreurs de connexion (#3763)
    - Resolve #3759
    - Suppression de ApiError pour n'utiliser que FrontendApiError
    - Utilisation d'un `<RequireAuth/>`, sur le même principe que sur
    MonitorEnv
    
    ----
    
    - [ ] Tests E2E (Cypress)

--- frontend/src/api/faoAreas.ts
+++ frontend/src/api/faoAreas.ts
@@ -1,5 +1,6 @@
+import { FrontendApiError } from '@libs/FrontendApiError'
+
 import { monitorfishApi, monitorfishApiKy } from './api'
-import { ApiError } from '../libs/ApiError'
 
 export const FAO_AREAS_ERROR_MESSAGE = "Nous n'avons pas pu récupérer les zones FAO"
 
@@ -30,13 +31,13 @@ export const { useComputeVesselFaoAreasQuery, useGetFaoAreasQuery } = faoAreasAp
 /**
  * Get FAO areas
  *
- * @throws {ApiError}
+ * @throws {FrontendApiError}
  */
 async function getFAOAreasFromAPI() {
   try {
     return await monitorfishApiKy.get(`/bff/v1/fao_areas`).json<Array<string>>()
   } catch (err) {
-    throw new ApiError(FAO_AREAS_ERROR_MESSAGE, err)
+    throw new FrontendApiError(FAO_AREAS_ERROR_MESSAGE, (err as FrontendApiError).originalError)
   }
 }
 

05605bb2b5afa6654a30ac03e4b33a51edf7df8b
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-10-18 09:50:22 +0200

    Skip custom date range api query when unfilled in pno list

--- frontend/src/features/PriorNotification/components/PriorNotificationList/index.tsx
+++ frontend/src/features/PriorNotification/components/PriorNotificationList/index.tsx
@@ -25,7 +25,7 @@ import styled, { css } from 'styled-components'
 import { useDebounce } from 'use-debounce'
 
 import { getTableColumns } from './columns'
-import { DEFAULT_PAGE_SIZE, SUB_MENUS_AS_OPTIONS } from './constants'
+import { DEFAULT_PAGE_SIZE, ExpectedArrivalPeriod, SUB_MENUS_AS_OPTIONS } from './constants'
 import { FilterBar } from './FilterBar'
 import { FilterTags } from './FilterTags'
 import { Row } from './Row'
@@ -77,11 +77,18 @@ export function PriorNotificationList({ isFromUrl }: PriorNotificationListProps)
     },
     1000
   )
+  const areRtkQueryParamsValid = !(
+    rtkQueryParams.listFilter.expectedArrivalPeriod === ExpectedArrivalPeriod.CUSTOM &&
+    !rtkQueryParams.listFilter.expectedArrivalCustomPeriod
+  )
   // `!!error` !== `isError` because `isError` is `false` when the query is fetching.
-  const { data, error, isError, isFetching } = useGetPriorNotificationsQuery(rtkQueryParams, {
-    ...RTK_ONE_MINUTE_POLLING_QUERY_OPTIONS,
-    ...RTK_FORCE_REFETCH_QUERY_OPTIONS
-  })
+  const { data, error, isError, isFetching } = useGetPriorNotificationsQuery(
+    areRtkQueryParamsValid ? rtkQueryParams : skipToken,
+    {
+      ...RTK_ONE_MINUTE_POLLING_QUERY_OPTIONS,
+      ...RTK_FORCE_REFETCH_QUERY_OPTIONS
+    }
+  )
   useHandleFrontendApiError(
     DisplayedErrorKey.SIDE_WINDOW_PRIOR_NOTIFICATION_LIST_ERROR,
     error,

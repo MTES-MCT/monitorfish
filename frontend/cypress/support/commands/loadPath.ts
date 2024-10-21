3db351a2e9f4c2ae6b3aa163c2e75fad7074ffe4
Loup Theron <loup-rodney@hotmail.fr>
2024-10-21 16:36:00 +0200

    Add cy.origin

--- frontend/cypress/support/commands/loadPath.ts
+++ frontend/cypress/support/commands/loadPath.ts
@@ -4,19 +4,25 @@ export type LoadPathOptions = {
 }
 Cypress.Commands.add('loadPath', (path: string, options?: LoadPathOptions): void => {
   cy.visit(path)
-
   cy.wait(500)
+
   cy.url().then((url: string) => {
     if (url.includes('login')) {
       cy.clickButton('Se connecter avec Cerbère')
 
-      // Login with Keycloak
-      cy.get('[name="username"]').type(options?.isSuperUser ? 'superuser' : 'user')
-      cy.get('[name="password"]').type('fish')
-      cy.get('[name="login"]').click()
+      cy.origin(
+        'http://localhost:8085/realms/monitor',
+        { args: { isSuperUser: options?.isSuperUser } },
+        ({ isSuperUser }) => {
+          // Login with Keycloak
+          cy.get('[name="username"]').type(isSuperUser ? 'superuser' : 'user')
+          cy.get('[name="password"]').type('fish')
+          cy.get('[name="login"]').click()
 
-      // There is a redirect to `/` after successful login
-      cy.wait(500)
+          // There is a redirect to `/` after successful login
+          cy.wait(500)
+        }
+      )
 
       cy.visit(path, options?.visitOptions)
     }

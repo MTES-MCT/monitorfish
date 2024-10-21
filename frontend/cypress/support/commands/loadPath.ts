d25e78c0dd0d92a6738ea9fb0c39d9eb778a2bab
Loup Theron <loup-rodney@hotmail.fr>
2024-10-21 17:20:37 +0200

    Add session

--- frontend/cypress/support/commands/loadPath.ts
+++ frontend/cypress/support/commands/loadPath.ts
@@ -3,32 +3,32 @@ export type LoadPathOptions = {
   visitOptions?: Partial<Cypress.VisitOptions>
 }
 Cypress.Commands.add('loadPath', (path: string, options?: LoadPathOptions): void => {
-  cy.visit(path)
-  cy.wait(500)
+  const username = options?.isSuperUser ? 'superuser' : 'user'
 
-  cy.url().then((url: string) => {
-    if (url.includes('login')) {
-      cy.clickButton('Se connecter avec Cerbère')
+  cy.session(username, () => {
+    cy.visit(path)
+    cy.wait(500)
 
-      cy.origin(
-        'http://0.0.0.0:8085/realms/monitor',
-        { args: { isSuperUser: options?.isSuperUser } },
-        ({ isSuperUser }) => {
+    cy.url().then((url: string) => {
+      if (url.includes('login')) {
+        cy.clickButton('Se connecter avec Cerbère')
+
+        cy.origin('http://0.0.0.0:8085', { args: {} }, () => {
           // Login with Keycloak
-          cy.get('[name="username"]').type(isSuperUser ? 'superuser' : 'user')
+          cy.get('[name="username"]').type(username)
           cy.get('[name="password"]').type('fish')
           cy.get('[name="login"]').click()
 
           // There is a redirect to `/` after successful login
           cy.wait(500)
-        }
-      )
+        })
 
-      cy.visit(path, options?.visitOptions)
-    }
+        cy.visit(path, options?.visitOptions)
+      }
+    })
+
+    cy.url().should('include', path)
+
+    cy.wait(2000)
   })
-
-  cy.url().should('include', path)
-
-  cy.wait(2000)
 })

d1d4284c0d1d63c905befd7fe528d311f4f3369f
Loup Theron <loup-rodney@hotmail.fr>
2024-10-22 18:50:54 +0200

    Fix other URLs

--- frontend/config/cypress.config.ts
+++ frontend/config/cypress.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'cypress'
+import {defineConfig} from 'cypress'
 import initCypressMousePositionPlugin from 'cypress-mouse-position/plugin'
-import { initPlugin } from 'cypress-plugin-snapshots/plugin'
+import {initPlugin} from 'cypress-plugin-snapshots/plugin'
 
 const IS_CI = Boolean(process.env.CI)
-const DEFAULT_PORT = IS_CI ? 8880 : 3000
 
 export default defineConfig({
   e2e: {
-    baseUrl: `http://localhost:${DEFAULT_PORT}`,
+    baseUrl: `http://${IS_CI ? '0.0.0.0:8880' : 'localhost:3000'}`,
     excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*'],
     setupNodeEvents(on, config) {
       initCypressMousePositionPlugin(on)

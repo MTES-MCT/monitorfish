d29ea5a0e79606c350bc191c31bfcbcd49e95090
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-10-23 09:21:54 +0200

    Rename APIControlUnitRepository to APILegacyControlUnitRepository

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/control_units/GetAllControlUnits.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/control_units/GetAllControlUnits.kt
@@ -2,17 +2,13 @@ package fr.gouv.cnsp.monitorfish.domain.use_cases.control_units
 
 import fr.gouv.cnsp.monitorfish.config.UseCase
 import fr.gouv.cnsp.monitorfish.domain.entities.mission.ControlUnit
-import fr.gouv.cnsp.monitorfish.domain.repositories.ControlUnitRepository
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import fr.gouv.cnsp.monitorfish.domain.repositories.LegacyControlUnitRepository
 
 @UseCase
 class GetAllControlUnits(
-    private val controlUnitsRepository: ControlUnitRepository,
+    private val legacyControlUnitsRepository: LegacyControlUnitRepository,
 ) {
-    private val logger: Logger = LoggerFactory.getLogger(GetAllControlUnits::class.java)
-
     fun execute(): List<ControlUnit> {
-        return controlUnitsRepository.findAll()
+        return legacyControlUnitsRepository.findAll()
     }
 }

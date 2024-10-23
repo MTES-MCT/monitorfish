3c00731582ba82baf4dde0495e9f530e423ae3c7
Loup Theron <loup-rodney@hotmail.fr>
2024-10-23 17:13:16 +0200

    Fix URL encoding

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/proxy/KeycloakProxyController.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/proxy/KeycloakProxyController.kt
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
+import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 import java.util.stream.Collectors
 
@@ -49,7 +50,7 @@ class KeycloakProxyController (
         if (params.isNotEmpty()) {
             targetUri.append("?")
             params.entries.joinToString("&") { (key, values) ->
-                "$key=${values.joinToString(",")}"
+                "${URLEncoder.encode(key, Charsets.UTF_8)}=${URLEncoder.encode(values.joinToString(","), Charsets.UTF_8)}"
             }.let { targetUri.append(it) }
         }
         logger.info("Forwarding ${request.requestURI} to $targetUri")

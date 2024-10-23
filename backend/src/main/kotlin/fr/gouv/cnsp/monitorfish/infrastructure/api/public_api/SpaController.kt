8de348135bc97db5c2378f2c5ecd56141f230a51
Loup Theron <loup-rodney@hotmail.fr>
2024-10-23 18:33:29 +0200

    filter realms

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/public_api/SpaController.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/public_api/SpaController.kt
@@ -1,7 +1,9 @@
 package fr.gouv.cnsp.monitorfish.infrastructure.api.public_api
 
+import jakarta.servlet.RequestDispatcher
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.boot.web.servlet.error.ErrorController
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping
  */
 @Controller
 class SpaController : ErrorController {
+    private val logger = LoggerFactory.getLogger(SpaController::class.java)
+
     @RequestMapping("/error")
     fun error(
         request: HttpServletRequest,
@@ -25,11 +29,12 @@ class SpaController : ErrorController {
             "/backoffice/**",
             "/ext",
             "/light",
+            "favicon",
             "/load_light"
         )
-
-        if (response.status != HttpStatus.UNAUTHORIZED.value() &&
-            appPaths.any { path -> request.requestURI.contains(path) } ) {
+        val originalUri = request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI).toString()
+        logger.info(originalUri)
+        if (response.status != HttpStatus.UNAUTHORIZED.value() && !originalUri.contains("realms") ) {
             response.status = HttpStatus.OK.value()
             return "forward:/index.html"
         }

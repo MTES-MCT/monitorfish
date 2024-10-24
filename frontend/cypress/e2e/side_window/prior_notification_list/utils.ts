0971501b4a3e8c87bb7c99d4cbc8779e103c4de2
Loup Theron <loup-rodney@hotmail.fr>
2024-10-24 12:39:34 +0200

    Fix few cypress tests

--- frontend/cypress/e2e/side_window/prior_notification_list/utils.ts
+++ frontend/cypress/e2e/side_window/prior_notification_list/utils.ts
@@ -1,4 +1,3 @@
-import { SideWindowMenuLabel } from '../../../../src/domain/entities/sideWindow/constants'
 import { openSideWindowAsSuperUser, openSideWindowAsUser } from '../utils'
 
 export const openSideWindowPriorNotificationListAsUser = openSideWindowAsUser
@@ -6,7 +5,6 @@ export const openSideWindowPriorNotificationListAsUser = openSideWindowAsUser
 export const openSideWindowPriorNotificationListAsSuperUser = () => {
   openSideWindowAsSuperUser()
 
-  cy.clickButton(SideWindowMenuLabel.PRIOR_NOTIFICATION_LIST)
   if (document.querySelector('[data-cy="first-loader"]')) {
     cy.getDataCy('first-loader').should('not.be.visible')
   }

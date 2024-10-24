2a8b10291371be6dc0eb70d7e206c579ae7a3435
Loup Theron <loup-rodney@hotmail.fr>
2024-10-24 12:15:53 +0200

    Fix backend tests

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/input/LogbookPriorNotificationFormDataInput.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/input/LogbookPriorNotificationFormDataInput.kt
@@ -1,5 +1,8 @@
 package fr.gouv.cnsp.monitorfish.infrastructure.api.input
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class LogbookPriorNotificationFormDataInput(
     val note: String?,
 )

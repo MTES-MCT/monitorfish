4b8b1161f02253d259a3019239a6c7d75872986f
Loup Theron <loup-rodney@hotmail.fr>
2024-10-28 17:19:33 +0100

    Fix cy.requests tests

---
+++ frontend/cypress/support/commands/getAuthorizationHeader.ts
@@ -0,0 +1,34 @@
+import { User } from 'oidc-client-ts'
+
+export function getAuthorizationHeader() {
+  return getOIDCUser().then(user => {
+    const token = user?.access_token
+
+    // If we have a token set in state, we pass it.
+    if (token) {
+      return Promise.resolve(`Bearer ${token}`)
+    }
+
+    return Promise.resolve(undefined)
+  })
+}
+
+function getOIDCUser() {
+  const OIDC_AUTHORITY = Cypress.env('FRONTEND_OIDC_AUTHORITY')
+  const OIDC_CLIENT_ID = Cypress.env('FRONTEND_OIDC_CLIENT_ID')
+  const LOCALSTORAGE_URL = Cypress.env('LOCALSTORAGE_URL')
+
+  return cy.getAllLocalStorage().then(localStorages => {
+    const testLocalStorage = localStorages[LOCALSTORAGE_URL]
+    if (!testLocalStorage) {
+      return Promise.resolve(undefined)
+    }
+
+    const oidcStorage = testLocalStorage[`oidc.user:${OIDC_AUTHORITY}:${OIDC_CLIENT_ID}`]
+    if (!oidcStorage) {
+      return Promise.resolve(undefined)
+    }
+
+    return Promise.resolve(User.fromStorageString(oidcStorage as string))
+  })
+}

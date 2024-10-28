9dd3c321d6924f698fc0a87bcf29e0d5bbd301f8
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-10-28 10:15:26 +0100

    Add e2e tests for pno subscriber list filters

--- frontend/cypress/e2e/constants.ts
+++ frontend/cypress/e2e/constants.ts
@@ -1,3 +1,5 @@
+import { type Administration } from '@mtes-mct/monitor-ui'
+
 export const FAKE_API_POST_RESPONSE = {
   body: {},
   statusCode: 201
@@ -17,3 +19,37 @@ export const FAKE_MISSION_WITHOUT_EXTERNAL_ACTIONS = {
   body: { canDelete: true, sources: [] },
   statusCode: 200
 }
+
+export const FAKE_ADMINISTRATIONS: Administration.Administration[] = [
+  {
+    controlUnitIds: [1],
+    controlUnits: [
+      {
+        administrationId: 101,
+        areaNote: undefined,
+        departmentAreaInseeCode: undefined,
+        id: 1,
+        isArchived: false,
+        name: 'Unité 1',
+        termsNote: undefined
+      }
+    ],
+    id: 101,
+    isArchived: false,
+    name: 'Administration 1'
+  },
+  {
+    controlUnitIds: [],
+    controlUnits: [],
+    id: 102,
+    isArchived: false,
+    name: 'Administration 2'
+  },
+  {
+    controlUnitIds: [],
+    controlUnits: [],
+    id: 103,
+    isArchived: false,
+    name: 'Administration 3'
+  }
+]

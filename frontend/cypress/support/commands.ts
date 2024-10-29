5d2f86f55d93727016b82227be836e9f778c2433
Loup Theron <loup-rodney@hotmail.fr>
2024-10-29 15:07:10 +0100

    Remove unsued functions

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -6,7 +6,6 @@ import { countRequestsByAlias, resetCountRequestsByAlias } from './commands/coun
 import { getComputedStyle } from './commands/getComputedStyle'
 import { getDownloadedFileContent } from './commands/getDownloadedFileContent'
 import { login } from './commands/login'
-import { postLoginToKeycloak } from './commands/postLoginToKeycloak'
 
 registerMonitorUiCustomCommands()
 
@@ -22,5 +21,4 @@ Cypress.Commands.add('countRequestsByAlias', countRequestsByAlias)
 Cypress.Commands.add('getComputedStyle', getComputedStyle)
 Cypress.Commands.add('getDownloadedFileContent', getDownloadedFileContent)
 Cypress.Commands.add('resetCountRequestsByAlias', resetCountRequestsByAlias)
-Cypress.Commands.add('postLoginToKeycloak', postLoginToKeycloak)
 Cypress.Commands.add('login', login)
--- frontend/puppeteer/e2e/utils.ts
+++ frontend/puppeteer/e2e/utils.ts
@@ -71,30 +71,6 @@ export async function getInputContent(page: Page, selector: string) {
   return element && element.evaluate((el: HTMLInputElement) => el.value)
 }
 
-export async function login(page: Page, path: string) {
-  await page.goto(path, { waitUntil: 'domcontentloaded' })
-  const url = page.url()
-
-  if (url.includes('login')) {
-    const loginButton = await page.waitForSelector('[title="Se connecter avec Cerbère"]')
-    await loginButton.click()
-
-    // Login with Keycloak
-    const username = await page.waitForSelector('[name="username"]')
-    await username.type('superuser', { delay: 50 })
-    const password = await page.waitForSelector('[name="password"]')
-    await password.type('fish', { delay: 50 })
-
-    const submitButton = await page.waitForSelector('[name="login"]')
-    await submitButton.click()
-
-    // There is a redirect to `/` after successful login
-    await wait(500)
-
-    await page.goto(path, { waitUntil: 'domcontentloaded' })
-  }
-}
-
 export async function getFirstTab(browser: Browser) {
   const [firstTab] = await browser.pages()
 

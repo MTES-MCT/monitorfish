3ede69c7be87862a75fabbef82fd8cbe7f6523e0
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-10-30 12:06:29 +0100

    Add missing login in pno subscriber e2e tests

--- frontend/cypress/e2e/back_office/prior_notification_subscriber_form/utils.ts
+++ frontend/cypress/e2e/back_office/prior_notification_subscriber_form/utils.ts
@@ -3,6 +3,8 @@ import dayjs from 'dayjs'
 import { openBackOfficePriorNotificationSubscriberList } from '../prior_notification_subscriber_table/utils'
 
 export const editBackOfficePriorNotificationSubscriber = (controlUnitId: number) => {
+  cy.login('superuser')
+
   cy.intercept('GET', `/bff/v1/prior_notification_subscribers/${controlUnitId}`).as('getPriorNotificationSubscriber')
   cy.intercept('GET', `/bff/v1/fleet_segments/${dayjs().year()}`).as('getFleetSegments')
   cy.intercept('GET', `/bff/v1/ports`).as('getPorts')
--- frontend/cypress/e2e/back_office/prior_notification_subscriber_table/utils.ts
+++ frontend/cypress/e2e/back_office/prior_notification_subscriber_table/utils.ts
@@ -1,4 +1,6 @@
 export const openBackOfficePriorNotificationSubscriberList = () => {
+  cy.login('superuser')
+
   cy.intercept('GET', '/bff/v1/prior_notification_subscribers?sortColumn=CONTROL_UNIT_NAME&sortDirection=ASC').as(
     'getPriorNotificationSubscribers'
   )

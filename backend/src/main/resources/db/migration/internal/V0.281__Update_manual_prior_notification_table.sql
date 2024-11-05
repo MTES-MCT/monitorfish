a8af47af9c397a2d90dcd38e1e24bf0074973f18
Loup Theron <loup-rodney@hotmail.fr>
2024-11-05 15:49:21 +0100

    Fix migration

--- backend/src/main/resources/db/migration/internal/V0.281__Update_manual_prior_notification_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.281__Update_manual_prior_notification_table.sql
@@ -1,2 +1,8 @@
+UPDATE public.manual_prior_notifications
+    SET
+        value = JSONB_SET(value, '{updatedAt}', TO_JSONB(TO_CHAR(updated_at, 'YYYY-MM-DD"T"HH24:MI:SS"Z"')), true)
+    WHERE
+        updated_at is not null;
+
 ALTER TABLE public.manual_prior_notifications
     DROP COLUMN updated_at;

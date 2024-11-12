13d73f607224a4962e211c0d7d5f5a0aecd5ebfc
Loup Theron <loup-rodney@hotmail.fr>
2024-11-12 11:08:08 +0100

    Add producer organization membership table

---
+++ backend/src/main/resources/db/testdata/V666.30__Insert_dummy_producer_organization_memberships.sql
@@ -0,0 +1,18 @@
+INSERT INTO public.producer_organization_memberships (
+    internal_reference_number,
+    start_membership_date,
+    organization_name
+) VALUES
+    ('FAK000999999',    '14/03/2015', 'SA THO AN'),
+    ('U_W0NTFINDME',    '22/07/2016', 'OP DU SUD'),
+    ('FR263418260',     '09/10/2017', 'SA THO AN'),
+    ('SOCR4T3',         '23/05/2018', 'SA THO AN'),
+    ('ABC000939217',    '14/01/2019', 'OPBN'),
+    ('ABC000717263',    '30/11/2016', 'OP DU SUD'),
+    ('ABC000480070',    '12/06/2017', 'OP DU LEVANT'),
+    ('ABC000640738',    '17/09/2020', 'OPBN'),
+    ('ABC000042310',    '03/02/2019', 'OPBN'),
+    ('ABC000597493',    '21/04/2018', 'COBRENORD'),
+    ('FR263465414',     '14/12/2015', 'PDB'),
+    ('FR263454484',     '15/07/2020', 'FROM-NORD');
+

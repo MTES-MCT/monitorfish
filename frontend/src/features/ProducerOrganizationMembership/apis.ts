26a908a3636c92aba443a563e8233a1d09d07d4c
Loup Theron <loup-rodney@hotmail.fr>
2024-11-13 12:26:23 +0100

    Remove unused api

--- frontend/src/features/ProducerOrganizationMembership/apis.ts
+++ frontend/src/features/ProducerOrganizationMembership/apis.ts
@@ -4,19 +4,12 @@ import { FrontendApiError } from '@libs/FrontendApiError'
 
 import type { ProducerOrganizationMembership } from '@features/ProducerOrganizationMembership/types'
 
-const GET_PRODUCER_ORGANIZATION_MEMBERSHIP_ERROR_MESSAGE = "Nous n'avons pas pu récupérer l'OP de ce navire."
 const GET_PRODUCER_ORGANIZATION_MEMBERSHIPS_ERROR_MESSAGE =
   "Nous n'avons pas pu récupérer la liste des adhérents aux OP."
 const SET_PRODUCER_ORGANIZATION_MEMBERSHIPS_ERROR_MESSAGE = "Nous n'avons pas pu mettre à jour les adhésions aux OPs."
 
 export const producerOrganizationMembershipApi = monitorfishApi.injectEndpoints({
   endpoints: builder => ({
-    getProducerOrganizationMembership: builder.query<ProducerOrganizationMembership, string>({
-      query: internalReferenceNumber => `/producer_organization_memberships/${internalReferenceNumber}`,
-      transformErrorResponse: response =>
-        new FrontendApiError(GET_PRODUCER_ORGANIZATION_MEMBERSHIP_ERROR_MESSAGE, response)
-    }),
-
     getProducerOrganizationMemberships: builder.query<ProducerOrganizationMembership[], void>({
       providesTags: () => [{ type: RtkCacheTagType.ProducerOrganizationMemberships }],
       query: () => `/producer_organization_memberships`,

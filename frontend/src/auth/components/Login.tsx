0cd12452ed8cd4f5169d92eb8fd11382a1ce853c
Loup Theron <loup-rodney@hotmail.fr>
2024-11-18 10:23:43 +0100

    Update Login.tsx

--- frontend/src/auth/components/Login.tsx
+++ frontend/src/auth/components/Login.tsx
@@ -47,7 +47,7 @@ export function Login() {
 
   return (
     <LoginBackground>
-      {auth?.isLoading || isLoading ? (
+      {!!auth?.isLoading || isLoading ? (
         <LoadingSpinnerWall isVesselShowed />
       ) : (
         <div>

0164430283f59a4e130b4a98b1a2d1790cad2c1f
Loup Theron <loup-rodney@hotmail.fr>
2024-11-29 13:33:03 +0100

    Add checkbox filter cypress test

--- frontend/cypress/e2e/side_window/reporting_list/table.spec.ts
+++ frontend/cypress/e2e/side_window/reporting_list/table.spec.ts
@@ -51,12 +51,26 @@ context('Side Window > Reporting List > Table', () => {
     cy.get('.Table-SimpleTable tr').should('have.length.to.be.greaterThan', 3)
 
     /**
+     * Filter by observation
+     */
+
+    cy.fill("Suspicions d'infraction", false)
+    cy.fill('Observations', true)
+
+    cy.get('.Table-SimpleTable tr').should('have.length', 3)
+    cy.get('.Table-SimpleTable tr').eq(1).contains('Observation')
+    cy.get('.Table-SimpleTable tr').eq(2).contains('Observation')
+
+    cy.fill("Suspicions d'infraction", true)
+    cy.fill('Observations', false)
+
+    /**
      * Sort reporting table by date
      */
 
     cy.get('.Table-SimpleTable tr').eq(1).contains('3 milles - Chaluts')
 
-    cy.get('th > div').filter(':contains("Il y a...")').click()
+    cy.get('th > div').filter(':contains("Il y a...")').click({ force: true })
 
     cy.get('.Table-SimpleTable tr').eq(1).contains("Suspicion d'infraction 212")
   })

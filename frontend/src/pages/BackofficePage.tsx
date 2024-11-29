686d90fd9915fa1b3a941433b74567148181a050
Ivan Gabriele <ivan.gabriele@protonmail.com>
2024-11-29 12:41:11 +0100

    Remove NamespaceContext from FrontEnd

--- frontend/src/pages/BackofficePage.tsx
+++ frontend/src/pages/BackofficePage.tsx
@@ -1,15 +1,13 @@
 import { BackofficeMode } from '@api/BackofficeMode'
 import { BackOfficeMenu } from '@features/BackOffice/components/BackofficeMenu'
-import { MainMap } from '@features/MainMap/MainMap.types'
-import { Notifier } from '@components/Notifier'
 import countries from 'i18n-iso-countries'
+import { Notifier } from '@components/Notifier'
 import COUNTRIES_FR from 'i18n-iso-countries/langs/fr.json'
 import { Provider } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 import styled from 'styled-components'
 
-import { NamespaceContext } from '../context/NamespaceContext'
 import { ErrorToastNotification } from '../features/commonComponents/ErrorToastNotification'
 import { backofficeStore, backofficeStorePersistor } from '../store'
 
@@ -20,18 +18,16 @@ export function BackofficePage() {
     <Provider store={backofficeStore}>
       {/* eslint-disable-next-line no-null/no-null */}
       <PersistGate loading={null} persistor={backofficeStorePersistor}>
-        <NamespaceContext.Provider value={MainMap.LayerSliceNamespace.backoffice}>
-          <BackofficeMode isBackoffice />
+        <BackofficeMode isBackoffice />
 
-          <Wrapper>
-            <BackOfficeMenu />
+        <Wrapper>
+          <BackOfficeMenu />
 
-            <Outlet />
-          </Wrapper>
+          <Outlet />
+        </Wrapper>
 
-          <ErrorToastNotification />
-          <Notifier />
-        </NamespaceContext.Provider>
+        <ErrorToastNotification />
+        <Notifier />
       </PersistGate>
     </Provider>
   )

9c7f5a0827aca9376d594c39e1553db4d1744db2
Vincent <vincent.chery@m4x.org>
2024-12-02 16:40:15 +0100

    Update fleet segments table

--- backend/src/main/resources/db/migration/internal/V0.286__Update_fleet_segments_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.286__Update_fleet_segments_table.sql
@@ -1,14 +1,14 @@
 UPDATE public.fleet_segments
 SET target_species = ARRAY(SELECT DISTINCT unnest(bycatch_species || target_species));
 
-CREATE TYPE public.species_type AS ENUM ('PELAGIC', 'DEMERSAL');
+CREATE TYPE public.scip_species_type AS ENUM ('PELAGIC', 'DEMERSAL', 'TUNA', 'OTHER');
 
 ALTER TABLE public.fleet_segments
     DROP COLUMN bycatch_species,
     DROP COLUMN dirm,
     DROP COLUMN flag_states,
     ADD COLUMN min_share_of_target_species DOUBLE PRECISION,
-    ADD COLUMN favored_main_species_type species_type,
+    ADD COLUMN favored_main_species_type scip_species_type,
     ADD COLUMN min_mesh DOUBLE PRECISION,
     ADD COLUMN max_mesh DOUBLE PRECISION,
     ADD COLUMN priority DOUBLE PRECISION NOT NULL DEFAULT 0,
@@ -18,3 +18,14 @@ UPDATE public.fleet_segments
 SET min_share_of_target_species = 0
 WHERE target_species != '{}';
 
+UPDATE public.fleet_segments
+SET favored_main_species_type = 'DEMERSAL'
+WHERE
+    segment LIKE 'ATL%' OR
+    segment LIKE 'SWW%' OR
+    segment LIKE 'NWW%' OR
+    segment LIKE 'NS%';
+
+UPDATE public.fleet_segments
+SET favored_main_species_type = 'PELAGIC'
+WHERE segment LIKE 'PEL%';

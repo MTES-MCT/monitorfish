9427fee51769968a9093a540b902410c83bf15a3
Loup Theron <loup-rodney@hotmail.fr>
2024-12-12 16:39:03 +0100

    Minor refactoring

--- frontend/src/features/Reporting/useCases/__tests__/archiveReporting.test.ts
+++ frontend/src/features/Reporting/useCases/__tests__/archiveReporting.test.ts
@@ -2,7 +2,7 @@ import { PendingAlertValueType } from '@features/Alert/types'
 import { fortyHeightHourAlertReporting } from '@features/Reporting/useCases/__tests__/__mocks__/dummyReporting'
 import { archiveReporting } from '@features/Reporting/useCases/archiveReporting'
 import { describe, it, expect, afterAll } from '@jest/globals'
-import { dispatchProcessor } from '@store/__tests__/utils'
+import { mockedDispatch } from '@store/__tests__/utils'
 
 import { VesselIdentifier } from '../../../../domain/entities/vessel/types'
 import { deleteReporting } from '../deleteReporting'
@@ -45,7 +45,7 @@ describe('archiveReporting()', () => {
 
   it('Should delete reporting When alert is MISSING_FAR_48_HOURS_ALERT', async () => {
     // When
-    dispatchProcessor(archiveReporting(fortyHeightHourAlertReporting), INITIAL_STATE)
+    mockedDispatch(archiveReporting(fortyHeightHourAlertReporting), INITIAL_STATE)
 
     // Then
     expect(deleteReporting).toHaveBeenCalled()
@@ -62,7 +62,7 @@ describe('archiveReporting()', () => {
     }
 
     // When
-    dispatchProcessor(archiveReporting(otherAlertReporting), INITIAL_STATE)
+    mockedDispatch(archiveReporting(otherAlertReporting), INITIAL_STATE)
 
     // Then
     expect(deleteReporting).toHaveBeenCalledTimes(0)

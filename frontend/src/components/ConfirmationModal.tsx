6fb637100f77fec7f67da8338445b6c4e5d27549
Loup Theron <loup-rodney@hotmail.fr>
2024-12-19 18:00:39 +0100

    Fix cypress tests

--- frontend/src/components/ConfirmationModal.tsx
+++ frontend/src/components/ConfirmationModal.tsx
@@ -1,4 +1,4 @@
-import {Accent, Button, Dialog, Icon} from '@mtes-mct/monitor-ui'
+import { Accent, Button, Dialog, Icon } from '@mtes-mct/monitor-ui'
 import styled from 'styled-components'
 
 import type { ReactNode } from 'react'

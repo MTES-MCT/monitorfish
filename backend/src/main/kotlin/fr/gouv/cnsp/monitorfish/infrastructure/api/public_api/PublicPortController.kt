b5937706784d0765c2300a7725a24cebbe6e9b94
lwih <louis.hache.dev@protonmail.com>
2025-01-08 14:43:11 +0100

    feat: add controllers into public_api for vessel search and port

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/public_api/PublicPortController.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/public_api/PublicPortController.kt
@@ -3,16 +3,28 @@ package fr.gouv.cnsp.monitorfish.infrastructure.api.public_api
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.cache.CacheManager
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import fr.gouv.cnsp.monitorfish.domain.use_cases.port.GetActivePorts
+import fr.gouv.cnsp.monitorfish.infrastructure.api.outputs.PortDataOutput
 
 @RestController
 @RequestMapping("/api/v1/ports")
 @Tag(name = "Public APIs for ports")
 class PublicPortController(
     private val cacheManager: CacheManager,
+    private val getActivePorts: GetActivePorts,
 ) {
+    @GetMapping("")
+    @Operation(summary = "Get all active ports")
+    fun getActivePorts(): List<PortDataOutput> {
+        return getActivePorts.execute().map { port ->
+            PortDataOutput.fromPort(port)
+        }
+    }
+
     @PutMapping(value = ["/invalidate"])
     @Operation(summary = "Invalidate ports cache")
     fun invalidatePorts() {

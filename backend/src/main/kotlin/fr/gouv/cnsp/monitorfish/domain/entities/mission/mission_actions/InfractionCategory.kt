2cbc1168c5aff4c79d6ff4671362cfa5b30fc7b8
Loup Theron <loup-rodney@hotmail.fr>
2025-01-23 14:59:40 +0100

    Add missing infraction categories

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/mission/mission_actions/InfractionCategory.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/mission/mission_actions/InfractionCategory.kt
@@ -4,5 +4,11 @@ enum class InfractionCategory(
     val value: String,
 ) {
     FISHING("Pêche"),
+    REGISTER("Registre Inter."),
+    ENVIRONMENT("Environnement"),
+    SAILOR("Gens de Mer"),
+    MARITIME_NAVIGATION("Navigation Maritime"),
+    MARITIME_SECURITY("Sécurité Maritime"),
+    MARITIME_PORTS("Ports Maritimes"),
     SECURITY("Sécurité / Rôle"),
 }

ed3a2b1d1f669dfabd01f2d88236e44c7701cb19
Loup Theron <loup-rodney@hotmail.fr>
2025-01-24 13:43:48 +0100

    Add cypress tests

--- frontend/cypress/e2e/main_window/vessel_sidebar/resume.spec.ts
+++ frontend/cypress/e2e/main_window/vessel_sidebar/resume.spec.ts
@@ -12,31 +12,39 @@ context('Vessel sidebar resume tab', () => {
   it('Resume Should be opened When clicking on a vessel', () => {
     // When
     openVesselBySearch('Pheno')
-    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-sidebar"]').should('be.visible')
 
     // Then
-    cy.get('*[data-cy^="vessel-name"]').contains('PHENOMENE (FR)', { timeout: 10000 })
-    cy.get('*[data-cy^="global-risk-factor"]').contains('2.5', { timeout: 10000 })
-    cy.get('*[data-cy^="impact-risk-factor"]').contains('2.1', { timeout: 10000 })
-    cy.get('*[data-cy^="probability-risk-factor"]').contains('2.0', { timeout: 10000 })
-    cy.get('*[data-cy^="detectability-risk-factor"]').contains('3.0', { timeout: 10000 })
-    cy.wait(200)
-    cy.get('*[data-cy^="vessel-sidebar-beacon-malfunction"]').contains('NON-ÉMISSION VMS', { timeout: 10000 })
+    cy.get('*[data-cy^="vessel-name"]').contains('PHENOMENE (FR)')
+    cy.get('*[data-cy^="global-risk-factor"]').contains('2.5')
 
+    cy.get('*[data-cy^="impact-risk-factor"]').contains('2.1')
     cy.get('*[data-cy^="impact-risk-factor"]').click({ force: true, timeout: 10000 })
+    cy.get('*[data-cy="Zones de la marée"]').contains('27.8.b, 27.8.c')
+    cy.get('*[data-cy="Engins de la marée (FAR)"]').contains('Chaluts de fond à panneaux (OTB)')
+    cy.get('*[data-cy="Majorité d\'espèces à bord"]').contains('Pélagique')
+    cy.get('*[data-cy="Espèces cibles à bord"]').contains('NEP (≥ 20% du total des captures)')
+
+    cy.get('*[data-cy^="probability-risk-factor"]').contains('2.0')
     cy.get('*[data-cy^="probability-risk-factor"]').click({ force: true, timeout: 10000 })
+
+    cy.get('*[data-cy^="detectability-risk-factor"]').contains('3.0')
     cy.get('*[data-cy^="detectability-risk-factor"]').click({ force: true, timeout: 10000 })
-    cy.get('*[data-cy^="risk-factor-priority-level"]').contains('2.6 – élevée', { timeout: 10000 })
+
+    cy.wait(200)
+    cy.get('*[data-cy^="vessel-sidebar-beacon-malfunction"]').contains('NON-ÉMISSION VMS')
+
+    cy.get('*[data-cy^="risk-factor-priority-level"]').contains('2.6 – élevée')
 
     cy.get('*[data-cy^="show-risk-factor-explanation-modal"]').click({ force: true, timeout: 10000 })
   })
 
   it('An alert should be shown on the vessel sidebar', () => {
     // When
-    cy.get('*[data-cy^="vessel-search-input"]', { timeout: 10000 }).type('tempete couleur')
-    cy.get('*[data-cy^="vessel-search-item"]', { timeout: 10000 }).eq(0).click()
+    cy.get('*[data-cy^="vessel-search-input"]').type('tempete couleur')
+    cy.get('*[data-cy^="vessel-search-item"]').eq(0).click()
     cy.wait(200)
-    cy.get('*[data-cy^="vessel-sidebar"]', { timeout: 10000 }).should('be.visible')
+    cy.get('*[data-cy^="vessel-sidebar"]').should('be.visible')
 
     // Then
     cy.get('*[data-cy^="vessel-sidebar-alert"]').contains('Pêche en ZEE française par un navire tiers', {

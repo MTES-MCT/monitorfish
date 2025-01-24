54cf8c90cb08179339d27851625ffca4c9ece4f1
Loup Theron <loup-rodney@hotmail.fr>
2025-01-24 11:38:58 +0100

    Improve segment details to display new segments computation

--- frontend/src/features/FleetSegment/constants.ts
+++ frontend/src/features/FleetSegment/constants.ts
@@ -1,5 +1,14 @@
+import { ScipSpeciesType } from '@features/FleetSegment/types'
+
 export const FLEET_SEGMENT_VESSEL_TYPES = [
   'Chalutier pêche arrière - congélateur',
   'Chalutier pêche arr. congélateur rampe port. fixe',
   'Chalutiers pêche arrière congélateurs'
 ]
+
+export const SpeciesTypeToSpeciesTypeLabel: Record<ScipSpeciesType, string> = {
+  [ScipSpeciesType.DEMERSAL]: 'Démersal',
+  [ScipSpeciesType.OTHER]: 'Autres',
+  [ScipSpeciesType.PELAGIC]: 'Pélagique',
+  [ScipSpeciesType.TUNA]: 'Thon'
+}

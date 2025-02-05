7e640b36288a6939d6a875d06c17a08ad56040df
Loup Theron <loup-rodney@hotmail.fr>
2025-02-05 09:06:56 +0100

    Fix pipeline test and remove migration

--- backend/src/main/resources/db/migration/internal/V0.293__Update_logbook_reports_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.293__Update_logbook_reports_table.sql
@@ -1,1 +1,6 @@
-ALTER TYPE public.logbook_message_transmission_format ADD VALUE 'MANUAL' AFTER 'FLUX';
+ALTER TABLE public.logbook_reports
+    ALTER COLUMN operation_type SET NOT NULL;
+ALTER TABLE public.logbook_reports
+    ALTER COLUMN integration_datetime_utc SET NOT NULL;
+ALTER TABLE public.logbook_reports
+    ALTER COLUMN transmission_format SET NOT NULL;

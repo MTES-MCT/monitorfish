5226fa584e8634d789e8e96396663d384fdd8869
Loup Theron <loup-rodney@hotmail.fr>
2025-02-05 09:06:56 +0100

    Fix pipeline test and remove migration

--- backend/src/main/resources/db/migration/internal/V0.294__Update_logbook_reports_table.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE public.logbook_reports
-    ALTER COLUMN operation_type SET NOT NULL;
-ALTER TABLE public.logbook_reports
-    ALTER COLUMN integration_datetime_utc SET NOT NULL;
-ALTER TABLE public.logbook_reports
-    ALTER COLUMN transmission_format SET NOT NULL;

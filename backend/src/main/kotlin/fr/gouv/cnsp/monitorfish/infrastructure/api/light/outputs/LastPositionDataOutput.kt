63664c0dbbadfe5a107bc8e1747e81c3178133b6
Loup Theron <loup-rodney@hotmail.fr>
2025-02-07 10:39:34 +0100

    Add missing last position data output in light api

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/light/outputs/LastPositionDataOutput.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/light/outputs/LastPositionDataOutput.kt
@@ -1,8 +1,11 @@
 package fr.gouv.cnsp.monitorfish.infrastructure.api.light.outputs
 
 import com.neovisionaries.i18n.CountryCode
+import fr.gouv.cnsp.monitorfish.domain.entities.coordinates.transformCoordinates
 import fr.gouv.cnsp.monitorfish.domain.entities.last_position.LastPosition
 import fr.gouv.cnsp.monitorfish.domain.entities.position.PositionType
+import fr.gouv.cnsp.monitorfish.domain.entities.reporting.ReportingType
+import fr.gouv.cnsp.monitorfish.domain.entities.vessel.Vessel
 import fr.gouv.cnsp.monitorfish.domain.entities.vessel.VesselIdentifier
 import fr.gouv.cnsp.monitorfish.infrastructure.api.outputs.GearLastPositionDataOutput
 import fr.gouv.cnsp.monitorfish.infrastructure.api.outputs.SpeciesLastPositionDataOutput
@@ -41,9 +44,22 @@ data class LastPositionDataOutput(
     val lastControlDateTime: ZonedDateTime? = null,
     val lastControlInfraction: Boolean? = null,
     val vesselIdentifier: VesselIdentifier,
+    val vesselFeatureId: String,
     val underCharter: Boolean? = null,
     val isAtPort: Boolean,
     val beaconMalfunctionId: Int? = null,
+    // Properties for efficient filtering in frontend
+    val gearsArray: List<String>,
+    val hasAlert: Boolean,
+    val hasInfractionSuspicion: Boolean,
+    val lastControlDateTimeTimestamp: Long?,
+    val speciesArray: List<String>,
+    // Properties for WebGL
+    val coordinates: List<Double>,
+    val filterPreview: Int, // 0 is False, 1 is True - for WebGL
+    val hasBeaconMalfunction: Boolean,
+    val isFiltered: Int, // 0 is False, 1 is True - for WebGL
+    val lastPositionSentAt: Long,
 ) {
     companion object {
         fun fromLastPosition(position: LastPosition): LastPositionDataOutput =
@@ -87,6 +103,24 @@ data class LastPositionDataOutput(
                 underCharter = position.underCharter,
                 isAtPort = position.isAtPort,
                 beaconMalfunctionId = position.beaconMalfunctionId,
+                coordinates =
+                    transformCoordinates(
+                        longitude = position.longitude,
+                        latitude = position.latitude,
+                    ).toList(),
+                filterPreview = 0,
+                gearsArray = position.gearOnboard?.mapNotNull { it.gear }?.distinct() ?: listOf(),
+                hasAlert = position.alerts?.isNotEmpty() ?: false,
+                hasBeaconMalfunction = position.beaconMalfunctionId != null,
+                hasInfractionSuspicion =
+                    position.reportings.any {
+                        listOf(ReportingType.ALERT.name, ReportingType.INFRACTION_SUSPICION.name).contains(it)
+                    },
+                isFiltered = 0,
+                lastControlDateTimeTimestamp = position.lastControlDateTime?.toInstant()?.toEpochMilli(),
+                lastPositionSentAt = position.dateTime.toInstant().toEpochMilli(),
+                speciesArray = position.speciesOnboard?.mapNotNull { it.species }?.distinct() ?: listOf(),
+                vesselFeatureId = Vessel.getVesselCompositeIdentifier(position),
             )
     }
 }

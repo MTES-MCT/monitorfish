0b2ed1aae522b6e0f2155bd18e3dd3d6dd11e623
Loup Theron <loup-rodney@hotmail.fr>
2025-03-10 10:40:14 +0100

    Update Natinf and archive outdated reporting

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/alerts/type/BliBycatchMaxWeightExceededAlert.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/entities/alerts/type/BliBycatchMaxWeightExceededAlert.kt
@@ -4,4 +4,4 @@ class BliBycatchMaxWeightExceededAlert(
     override var seaFront: String? = null,
     override var dml: String? = null,
     var riskFactor: Double? = null,
-) : AlertType(AlertTypeMapping.BLI_BYCATCH_MAX_WEIGHT_EXCEEDED_ALERT, seaFront, dml, 2610)
+) : AlertType(AlertTypeMapping.BLI_BYCATCH_MAX_WEIGHT_EXCEEDED_ALERT, seaFront, dml, 12900)

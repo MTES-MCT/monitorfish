353a1b573b16cadd8a0c7cc40b51dcabc38c005e
Loup Theron <loup-rodney@hotmail.fr>
2025-03-13 09:29:16 +0100

    Add wait in cypress test

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/public_api/SpaController.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/public_api/SpaController.kt
@@ -63,7 +63,7 @@ class SpaController : ErrorController {
         // Prevent returning index.html for failed API requests
         // "/light/v1" is used to avoid clashes with the /light frontend route
         if (path.startsWith("/bff") || path.startsWith("/api") || path.startsWith("/light/v1")) {
-            logger.error("API error: $path (status: $status)")
+            logger.error("API error: $path ($status): $errorMessage")
 
             return ResponseEntity
                 .status(status)

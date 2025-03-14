62bf421d6d3cea65ea974cb605be1efa0152c7f1
Loup Theron <loup-rodney@hotmail.fr>
2025-03-14 18:02:31 +0100

    Fix cypress tests

--- frontend/src/features/Vessel/useCases/VesselListV2/undoPreviewVessels.ts
+++ frontend/src/features/Vessel/useCases/VesselListV2/undoPreviewVessels.ts
@@ -1,7 +1,20 @@
+import { renderVesselFeatures } from '@features/Vessel/useCases/renderVesselFeatures'
+
+import { setDisplayedComponents } from '../../../../domain/shared_slices/DisplayedComponent'
 import { setPreviewFilteredVesselsMode } from '../../../../domain/shared_slices/Global'
 
 import type { MainAppThunk } from '@store'
 
 export const undoPreviewVessels = (): MainAppThunk => async dispatch => {
   dispatch(setPreviewFilteredVesselsMode(false))
+
+  // TODO Delete the code after this comment when deprecated VesselList is deleted
+  // And VesselListV2 is used
+  dispatch(
+    setDisplayedComponents({
+      isVesselListModalDisplayed: true
+    })
+  )
+
+  dispatch(renderVesselFeatures())
 }

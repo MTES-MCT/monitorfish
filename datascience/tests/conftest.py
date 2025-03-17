d57213e384aeac552ce922f6c5d54b3f34cada25
Vincent <vincent.chery@m4x.org>
2025-03-17 14:38:36 +0100

    Wait for data warehouse before launching tests

--- datascience/tests/conftest.py
+++ datascience/tests/conftest.py
@@ -119,6 +119,9 @@ def monkeysession(request):
 
 @pytest.fixture(scope="session", autouse=True)
 def set_environment_variables(monkeysession):
+    for proxy_env in ["http_proxy", "https_proxy", "HTTP_PROXY", "HTTPS_PROXY"]:
+        os.environ.pop(proxy_env, None)
+
     for k, v in dotenv_values(ROOT_DIRECTORY / ".env.test").items():
         monkeysession.setenv(k, v)
 
@@ -211,6 +214,29 @@ def reset_test_data(create_tables):
         for s in test_data_scripts:
             print(f"{s.major}.{s.minor}.{s.patch}: {s.path.name}")
             connection.execute(text(s.script))
+
+
+@pytest.fixture(scope="session", autouse=True)
+def wait_for_data_warehouse(set_environment_variables, create_docker_client):
+    client = create_docker_client
+    health = None
+    timeout = 30
+    stop_time = 1
+    elapsed_time = 0
+
+    # Wait for data warehouse to start
+    while health != "healthy" and elapsed_time < timeout:
+        print(f"Waiting for data warehouse to start ({elapsed_time}/{timeout})")
+        sleep(stop_time)
+        health = client.inspect_container("data_warehouse")["State"]["Health"]["Status"]
+        elapsed_time += stop_time
+
+    if health == "healthy":
+        print("Data warehouse started")
+    else:
+        raise RuntimeError("Could not start data warehouse.")
+
+    yield
 
 
 ############################ Share fixtures between modules ############################

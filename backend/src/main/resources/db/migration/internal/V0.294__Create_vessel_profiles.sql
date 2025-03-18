53fef2d937bc8cd9e1337139f0a9deacd0bf52c2
Vincent <vincent.chery@m4x.org>
2025-03-18 17:59:43 +0100

    Add recent profiles

--- backend/src/main/resources/db/migration/internal/V0.294__Create_vessel_profiles.sql
+++ backend/src/main/resources/db/migration/internal/V0.294__Create_vessel_profiles.sql
@@ -3,13 +3,13 @@ CREATE TABLE IF NOT EXISTS public.vessel_profiles (
     cfr VARCHAR NOT NULL,
     gears JSONB,
     species JSONB,
-    segments JSONB
-    -- fao_areas JSONB,
+    fao_areas JSONB,
+    segments JSONB,
     -- landing_ports JSONB,
-    -- recent_gears JSONB,
-    -- recent_species JSONB,
-    -- recent_fao_areas JSONB,
-    -- recent_segments JSONB,
+    recent_gears JSONB,
+    recent_species JSONB,
+    recent_fao_areas JSONB,
+    recent_segments JSONB
     -- recent_landing_ports JSONB,
     -- latest_landing_port VARCHAR,
     -- latest_landing_facade facade

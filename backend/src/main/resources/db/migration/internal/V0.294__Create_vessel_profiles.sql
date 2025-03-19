f59d626ddf94400183117366f8b488fb13c53d56
Vincent <vincent.chery@m4x.org>
2025-03-19 11:01:42 +0100

    Add port profiles

--- backend/src/main/resources/db/migration/internal/V0.294__Create_vessel_profiles.sql
+++ backend/src/main/resources/db/migration/internal/V0.294__Create_vessel_profiles.sql
@@ -5,12 +5,12 @@ CREATE TABLE IF NOT EXISTS public.vessel_profiles (
     species JSONB,
     fao_areas JSONB,
     segments JSONB,
-    -- landing_ports JSONB,
+    landing_ports JSONB,
     recent_gears JSONB,
     recent_species JSONB,
     recent_fao_areas JSONB,
-    recent_segments JSONB
-    -- recent_landing_ports JSONB,
-    -- latest_landing_port VARCHAR,
-    -- latest_landing_facade facade
+    recent_segments JSONB,
+    recent_landing_ports JSONB,
+    latest_landing_port VARCHAR,
+    latest_landing_facade facade
 );
--- datascience/src/pipeline/flows/vessel_profiles.py
+++ datascience/src/pipeline/flows/vessel_profiles.py
@@ -45,6 +45,62 @@ def extract_profiles(
 
 
 @task(checkpoint=False)
+def extract_port_profiles(
+    profile_datetime_utc: datetime, profile_type: str
+) -> pd.DataFrame:
+    assert profile_type in (
+        "full",
+        "recent",
+    )
+
+    duration_in_days = {
+        "full": 365,
+        "recent": 14,
+    }[profile_type]
+
+    logger = prefect.context.get("logger")
+    logger.info(f"Extracting port {profile_type} profiles.")
+
+    return extract(
+        db_name="data_warehouse",
+        query_filepath="data_warehouse/port_profiles.sql",
+        params={
+            "profile_datetime_utc": profile_datetime_utc,
+            "duration_in_days": duration_in_days,
+        },
+    )
+
+
+@task(checkpoint=False)
+def extract_latest_port_profiles(profile_datetime_utc: datetime) -> pd.DataFrame:
+    return extract(
+        db_name="data_warehouse",
+        query_filepath="data_warehouse/latest_port_profiles.sql",
+        params={"profile_datetime_utc": profile_datetime_utc},
+    )
+
+
+@task(checkpoint=False)
+def extract_ports_facade() -> dict:
+    ports_facade = extract(
+        db_name="monitorfish_remote",
+        query_filepath="monitorfish/ports_facade.sql",
+    )
+    return ports_facade.set_index("locode")["facade"].to_dict()
+
+
+@task(checkpoint=False)
+def add_latest_port_facade(
+    latest_port_profiles: pd.DataFrame, ports_facade: dict
+) -> pd.DataFrame:
+    latest_port_profiles = latest_port_profiles.copy(deep=True)
+    latest_port_profiles[
+        "latest_landing_facade"
+    ] = latest_port_profiles.latest_landing_port.map(ports_facade)
+    return latest_port_profiles
+
+
+@task(checkpoint=False)
 def transform_profiles(
     profiles: pd.DataFrame, profile_dimension: str, profile_type: str
 ):
@@ -58,13 +114,14 @@ def transform_profiles(
         "recent": "recent_",
     }[profile_type]
 
-    profile_dimension_plural = {
+    profile_dimension_label = {
         "fao_area": "fao_areas",
         "gear": "gears",
         "species": "species",
         "segment": "segments",
+        "port_locode": "landing_ports",
     }[profile_dimension]
-    result_column_name = f"{profile_type_prefix}{profile_dimension_plural}"
+    result_column_name = f"{profile_type_prefix}{profile_dimension_label}"
     profiles = profiles[["cfr", profile_dimension, "share"]].copy(deep=True)
     profiles["share_dict"] = profiles.apply(
         lambda row: {row[profile_dimension]: row["share"]}, axis=1
@@ -85,10 +142,13 @@ def merge_vessel_profiles(
     species_profiles: pd.DataFrame,
     fao_area_profiles: pd.DataFrame,
     segment_profiles: pd.DataFrame,
+    port_profiles: pd.DataFrame,
     recent_gear_profiles: pd.DataFrame,
     recent_species_profiles: pd.DataFrame,
     recent_fao_area_profiles: pd.DataFrame,
     recent_segment_profiles: pd.DataFrame,
+    recent_port_profiles: pd.DataFrame,
+    latest_port_profiles: pd.DataFrame,
 ) -> pd.DataFrame:
     vessel_profiles = pd.merge(
         gear_profiles, species_profiles, on="cfr", how="outer", validate="1:1"
@@ -96,14 +156,21 @@ def merge_vessel_profiles(
     for profiles in [
         fao_area_profiles,
         segment_profiles,
+        port_profiles,
         recent_gear_profiles,
         recent_species_profiles,
         recent_fao_area_profiles,
         recent_segment_profiles,
+        recent_port_profiles,
+        latest_port_profiles,
     ]:
         vessel_profiles = pd.merge(
             vessel_profiles, profiles, on="cfr", how="outer", validate="1:1"
         )
+
+    vessel_profiles["latest_landing_facade"] = vessel_profiles[
+        "latest_landing_facade"
+    ].fillna("Hors façade")
     return vessel_profiles
 
 
@@ -128,10 +195,12 @@ def load_vessel_profiles(vessel_profiles: pd.DataFrame):
             "species",
             "fao_areas",
             "segments",
+            "landing_ports",
             "recent_gears",
             "recent_species",
             "recent_fao_areas",
             "recent_segments",
+            "recent_landing_ports",
         ],
     )
 
@@ -144,6 +213,8 @@ with Flow("Vessel profiles") as flow:
         # Extract
         now = get_utcnow()
 
+        ports_facade = extract_ports_facade()
+
         gear_profiles = extract_profiles(
             profile_datetime_utc=now, profile_dimension="gear", profile_type="full"
         )
@@ -155,6 +226,9 @@ with Flow("Vessel profiles") as flow:
         )
         segment_profiles = extract_profiles(
             profile_datetime_utc=now, profile_dimension="segment", profile_type="full"
+        )
+        port_profiles = extract_port_profiles(
+            profile_datetime_utc=now, profile_type="full"
         )
 
         recent_gear_profiles = extract_profiles(
@@ -171,6 +245,11 @@ with Flow("Vessel profiles") as flow:
         recent_segment_profiles = extract_profiles(
             profile_datetime_utc=now, profile_dimension="segment", profile_type="recent"
         )
+        recent_port_profiles = extract_port_profiles(
+            profile_datetime_utc=now, profile_type="recent"
+        )
+
+        latest_port_profiles = extract_latest_port_profiles(profile_datetime_utc=now)
 
         # Transform
         gear_profiles = transform_profiles(
@@ -184,6 +263,9 @@ with Flow("Vessel profiles") as flow:
         )
         segment_profiles = transform_profiles(
             segment_profiles, profile_dimension="segment", profile_type="full"
+        )
+        port_profiles = transform_profiles(
+            port_profiles, profile_dimension="port_locode", profile_type="full"
         )
 
         recent_gear_profiles = transform_profiles(
@@ -200,16 +282,26 @@ with Flow("Vessel profiles") as flow:
         recent_segment_profiles = transform_profiles(
             recent_segment_profiles, profile_dimension="segment", profile_type="recent"
         )
+        recent_port_profiles = transform_profiles(
+            recent_port_profiles, profile_dimension="port_locode", profile_type="recent"
+        )
+
+        latest_port_profiles = add_latest_port_facade(
+            latest_port_profiles, ports_facade
+        )
 
         vessel_profiles = merge_vessel_profiles(
             gear_profiles,
             species_profiles,
             fao_area_profiles,
             segment_profiles,
+            port_profiles,
             recent_gear_profiles,
             recent_species_profiles,
             recent_fao_area_profiles,
             recent_segment_profiles,
+            recent_port_profiles,
+            latest_port_profiles,
         )
         # Load
         load_vessel_profiles(vessel_profiles)

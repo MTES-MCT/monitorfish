41f8c82847e3622ea9ef18ffa24cc7c0b06d9d45
Loup Theron <loup-rodney@hotmail.fr>
2025-03-31 17:19:30 +0200

    Fix cypress config imports

--- frontend/config/cypress.config.ts
+++ frontend/config/cypress.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'cypress'
-import initCypressMousePositionPlugin from 'cypress-mouse-position/plugin'
-import {initPlugin} from 'cypress-plugin-snapshots/plugin'
+import initCypressMousePositionPlugin from 'cypress-mouse-position/plugin.js'
+import {initPlugin} from 'cypress-plugin-snapshots/plugin.js'
 
 const IS_CI = Boolean(process.env.CI)
 

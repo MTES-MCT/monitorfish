b66523f44a31d23df4bd41892fd62ceeb75d08e6
Loup Theron <loup-rodney@hotmail.fr>
2025-04-15 12:08:34 +0200

    Fix reset of filters

--- frontend/src/hooks/useGetTopOffset.tsx
+++ frontend/src/hooks/useGetTopOffset.tsx
@@ -1,12 +1,15 @@
 import { DEFAULT_VESSEL_LIST_FILTER_VALUES } from '@features/Vessel/components/VesselList/constants'
+import { countVesselListFilter } from '@features/Vessel/components/VesselList/utils'
 import { useMainAppSelector } from '@hooks/useMainAppSelector'
 import { isEqual } from 'lodash-es'
 
 export function useGetTopOffset() {
   const healthcheckTextWarning = useMainAppSelector(state => state.global.healthcheckTextWarning)
   const listFilterValues = useMainAppSelector(state => state.vessel.listFilterValues)
+  const numberOfFilters = countVesselListFilter(listFilterValues)
 
-  const areListFilterValuesEqualToDefaultOnes = isEqual(listFilterValues, DEFAULT_VESSEL_LIST_FILTER_VALUES)
+  const areListFilterValuesEqualToDefaultOnes =
+    isEqual(listFilterValues, DEFAULT_VESSEL_LIST_FILTER_VALUES) || numberOfFilters === 0
 
   return (function () {
     if (!!healthcheckTextWarning.length && !areListFilterValuesEqualToDefaultOnes) {

b6d8dc262876e13503346ed03fbcc64022a67879
Loup Theron <loup-rodney@hotmail.fr>
2025-04-30 16:05:31 +0200

    Add migration for vessel groups in redux

--- frontend/src/store/migrations.ts
+++ frontend/src/store/migrations.ts
@@ -4,3 +4,10 @@ export const MAIN_PERSISTOR_MISSION_MIGRATIONS = {
     listSeafrontGroup: state.listSeafrontGroup === 'ALL_SEAFRONT_GROUP' ? 'ALL' : state.listSeafrontGroup
   })
 }
+
+export const MAIN_PERSISTOR_VESSEL_GROUPS_MIGRATIONS = {
+  0: state => ({
+    ...state,
+    vesselGroupsIdsDisplayed: []
+  })
+}

0bac1a8244a973d9b38811ac0cc4d2993a89d231
Loup Theron <loup-rodney@hotmail.fr>
2025-05-23 17:28:05 +0200

    Display vessel profile on vessel sidebar

---
+++ frontend/src/features/Vessel/components/VesselSidebar/components/VesselProfile/index.tsx
@@ -0,0 +1,61 @@
+import { useGetPortsQuery } from '@api/port'
+import { ProfileBarChart } from '@features/Vessel/components/VesselSidebar/components/VesselProfile/ProfileBarChart'
+import { useMainAppSelector } from '@hooks/useMainAppSelector'
+import { Icon } from '@mtes-mct/monitor-ui'
+import styled from 'styled-components'
+
+export function VesselProfile() {
+  const selectedVessel = useMainAppSelector(state => state.vessel.selectedVessel)
+  const { data: ports } = useGetPortsQuery()
+
+  const landingPorts = Object.keys(selectedVessel?.profile?.landingPorts ?? {}).reduce((acc, key) => {
+    const port = ports?.find(p => p.locode === key)?.name
+
+    acc[port ?? key] = (selectedVessel?.profile?.landingPorts ?? {})[key]
+
+    return acc
+  }, {})
+
+  return (
+    <Wrapper>
+      <Header>
+        Profil du navire{' '}
+        <StyledIcon size={16} title="Le profile de navire est calculé sur 1 an de déclarations de captures." />
+      </Header>
+      {selectedVessel?.profile?.gears && (
+        <ProfileBarChart profile={selectedVessel?.profile?.gears} title="Engins habituels" />
+      )}
+      {selectedVessel?.profile?.species && (
+        <ProfileBarChart profile={selectedVessel?.profile?.species} title="Espèces habituelles" />
+      )}
+      {selectedVessel?.profile?.faoAreas && (
+        <ProfileBarChart profile={selectedVessel?.profile?.faoAreas} title="Zones de pêche habituelles" />
+      )}
+      {selectedVessel?.profile?.landingPorts && (
+        <ProfileBarChart profile={landingPorts} title="Ports de débarque habituels" />
+      )}
+    </Wrapper>
+  )
+}
+
+const StyledIcon = styled(Icon.Info)`
+  margin-left: 4px;
+`
+
+const Wrapper = styled.div`
+  margin-top: 10px;
+  padding: 12px 24px 0 24px;
+  background: ${p => p.theme.color.white};
+  color: ${p => p.theme.color.slateGray};
+  display: flex;
+  flex-direction: column;
+`
+
+const Header = styled.span`
+  font-size: 15px;
+  margin-bottom: 16px;
+
+  .Element-IconBox {
+    vertical-align: sub;
+  }
+`

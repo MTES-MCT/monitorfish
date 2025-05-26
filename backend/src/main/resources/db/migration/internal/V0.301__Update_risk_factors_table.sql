f85230cacac826781542baebd64e49a12179de3d
Vincent <vincent.chery@m4x.org>
2025-05-26 11:12:20 +0200

    Add usual_segments columns to tables

--- backend/src/main/resources/db/migration/internal/V0.301__Update_risk_factors_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.301__Update_risk_factors_table.sql
@@ -9,4 +9,12 @@ ALTER TABLE risk_factors
     ADD COLUMN recent_segments_risk_factor DOUBLE PRECISION NOT NULL,
     ADD COLUMN recent_segments_control_priority_level DOUBLE PRECISION NOT NULL,
     ADD COLUMN recent_segment_highest_impact VARCHAR(100),
-    ADD COLUMN recent_segment_highest_priority VARCHAR(100);
+    ADD COLUMN recent_segment_highest_priority VARCHAR(100),
+    ADD COLUMN usual_gear_onboard JSONB,
+    ADD COLUMN usual_segments varchar(100)[],
+    ADD COLUMN usual_segments_impact_risk_factor DOUBLE PRECISION NOT NULL,
+    ADD COLUMN usual_segments_detectability_risk_factor DOUBLE PRECISION NOT NULL,
+    ADD COLUMN usual_segments_risk_factor DOUBLE PRECISION NOT NULL,
+    ADD COLUMN usual_segments_control_priority_level DOUBLE PRECISION NOT NULL,
+    ADD COLUMN usual_segment_highest_impact VARCHAR(100),
+    ADD COLUMN usual_segment_highest_priority VARCHAR(100);
--- backend/src/main/resources/db/migration/internal/V0.302__Update_vessel_profiles_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.302__Update_vessel_profiles_table.sql
@@ -1,2 +1,3 @@
 ALTER TABLE public.vessel_profiles
-ADD COLUMN recent_gear_onboard JSONB;
+ADD COLUMN recent_gear_onboard JSONB,
+ADD COLUMN gear_onboard JSONB;

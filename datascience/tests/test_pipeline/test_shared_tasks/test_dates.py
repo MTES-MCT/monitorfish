a3a1fa0bb496e716c8784c795ace6b115feb9e28
Vincent <vincent.chery@m4x.org>
2025-06-03 15:36:00 +0200

    Refactor tests as pytest

--- datascience/tests/test_pipeline/test_shared_tasks/test_dates.py
+++ datascience/tests/test_pipeline/test_shared_tasks/test_dates.py
@@ -1,4 +1,3 @@
-import unittest
 from datetime import datetime
 from unittest.mock import patch
 
@@ -6,23 +5,21 @@ from src.pipeline.helpers.dates import Period
 from src.pipeline.shared_tasks.dates import make_periods
 
 
-class TestSharedTasksDates(unittest.TestCase):
-    @patch("src.pipeline.shared_tasks.dates.datetime")
-    def test_make_periods(self, mock_datetime):
+@patch("src.pipeline.shared_tasks.dates.datetime")
+def test_make_periods(mock_datetime):
+    mock_datetime.utcnow.return_value = datetime(2021, 10, 5)
 
-        mock_datetime.utcnow.return_value = datetime(2021, 10, 5)
+    periods = make_periods.run(
+        start_hours_ago=72,
+        end_hours_ago=0,
+        minutes_per_chunk=1560,
+        chunk_overlap_minutes=120,
+    )
 
-        periods = make_periods.run(
-            start_hours_ago=72,
-            end_hours_ago=0,
-            minutes_per_chunk=1560,
-            chunk_overlap_minutes=120,
-        )
+    expected_periods = [
+        Period(start=datetime(2021, 10, 2, 0, 0), end=datetime(2021, 10, 3, 2, 0)),
+        Period(start=datetime(2021, 10, 3, 0, 0), end=datetime(2021, 10, 4, 2, 0)),
+        Period(start=datetime(2021, 10, 4, 0, 0), end=datetime(2021, 10, 5, 0, 0)),
+    ]
 
-        expected_periods = [
-            Period(start=datetime(2021, 10, 2, 0, 0), end=datetime(2021, 10, 3, 2, 0)),
-            Period(start=datetime(2021, 10, 3, 0, 0), end=datetime(2021, 10, 4, 2, 0)),
-            Period(start=datetime(2021, 10, 4, 0, 0), end=datetime(2021, 10, 5, 0, 0)),
-        ]
-
-        self.assertEqual(periods, expected_periods)
+    assert periods == expected_periods

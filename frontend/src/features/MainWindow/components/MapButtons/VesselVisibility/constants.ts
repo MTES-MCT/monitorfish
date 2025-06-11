474d59c6f00daec780c6ec025f11f9c0d98bf5ba
Loup Theron <loup-rodney@hotmail.fr>
2025-06-11 17:53:07 +0200

    Add fleet segment vessel labels to external users

--- frontend/src/features/MainWindow/components/MapButtons/VesselVisibility/constants.ts
+++ frontend/src/features/MainWindow/components/MapButtons/VesselVisibility/constants.ts
@@ -3,7 +3,6 @@ import { VesselLabel } from '@features/Vessel/label.types'
 export const VESSEL_LABEL_OPTIONS = [
   { label: 'Nationalité', value: VesselLabel.VESSEL_NATIONALITY },
   { label: 'Nom du navire', value: VesselLabel.VESSEL_NAME },
-  { label: 'CFR', value: VesselLabel.VESSEL_INTERNAL_REFERENCE_NUMBER }
+  { label: 'CFR', value: VesselLabel.VESSEL_INTERNAL_REFERENCE_NUMBER },
+  { label: 'Segment de flotte', value: VesselLabel.VESSEL_FLEET_SEGMENT }
 ]
-
-export const SUPER_USER_VESSEL_LABEL_OPTION = { label: 'Segment de flotte', value: VesselLabel.VESSEL_FLEET_SEGMENT }

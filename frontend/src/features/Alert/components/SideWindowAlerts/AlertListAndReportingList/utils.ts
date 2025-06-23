ac7b06c4a77ffd8544a2d5531bb6c62db769494f
Loup Theron <loup-rodney@hotmail.fr>
2025-06-23 16:38:52 +0200

    Add bottom trawling alerts in backend and frontend

--- frontend/src/features/Alert/components/SideWindowAlerts/AlertListAndReportingList/utils.ts
+++ frontend/src/features/Alert/components/SideWindowAlerts/AlertListAndReportingList/utils.ts
@@ -1,4 +1,19 @@
 import { COMMON_ALERT_TYPE_OPTION, PendingAlertValueType } from '../../../constants'
 
-export const getAlertNameFromType = (type: PendingAlertValueType | 'PNO_LAN_WEIGHT_TOLERANCE_ALERT') =>
-  COMMON_ALERT_TYPE_OPTION[type] ? COMMON_ALERT_TYPE_OPTION[type].name : 'Alerte inconnue'
+import type { PendingAlertValue } from '@features/Alert/types'
+
+export const getAlertNameFromType = (
+  type: PendingAlertValueType | 'PNO_LAN_WEIGHT_TOLERANCE_ALERT',
+  value?: PendingAlertValue | undefined
+) => {
+  const name = COMMON_ALERT_TYPE_OPTION[type] ? COMMON_ALERT_TYPE_OPTION[type].name : 'Alerte inconnue'
+
+  if (
+    (!!value && type === PendingAlertValueType.BOTTOM_TRAWL_800_METERS_FISHING_ALERT) ||
+    type === PendingAlertValueType.BOTTOM_GEAR_VME_FISHING_ALERT
+  ) {
+    return `${name} (${value?.depth}m)`
+  }
+
+  return name
+}

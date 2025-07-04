06c9a30da62cf8fd4d7592a23607082ea61b52f5
Vincent <vincent.chery@m4x.org>
2025-07-04 16:20:18 +0200

    Add deployment script and command

---
+++ infra/remote/data-pipeline-prefect3/deploy-flows.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#        -v /opt2/monitorfish-data/ers:/opt2/monitorfish-data/ers \
+# docker run --rm --network=host --name monitorfish-pipeline-deploy-flows \
+docker run -t --rm --network=host --name monitorfish-pipeline-deploy-flows \
+    -v "$(pwd)"/pipeline/.env:/home/monitorfish-pipeline/pipeline/.env \
+    -e HOST_ENV_FILE_LOCATION="$(pwd)"/pipeline/.env \
+    -e MONITORFISH_VERSION \
+    -e PREFECT_API_URL \
+    -e LOGBOOK_FILES_GID \
+    monitorfish-pipeline-prefect3:$MONITORFISH_VERSION \
+    python main.py

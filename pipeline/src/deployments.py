367f52ca0e06e0e96128a5ce8e5c63f9e915e0f5
Vincent <vincent.chery@m4x.org>
2025-07-11 10:04:50 +0200

    Migrate init_species_groups flow

--- pipeline/src/deployments.py
+++ pipeline/src/deployments.py
@@ -19,6 +19,7 @@ from src.flows.facade_areas import facade_areas_flow
 from src.flows.fao_areas import fao_areas_flow
 from src.flows.fishing_gear_codes import fishing_gear_codes_flow
 from src.flows.init_pno_types import init_pno_types_flow
+from src.flows.init_species_groups import init_species_groups_flow
 from src.flows.species import species_flow
 
 
@@ -38,6 +39,7 @@ flows_to_deploy = [
     FlowAndSchedules(flow=fao_areas_flow),
     FlowAndSchedules(flow=fishing_gear_codes_flow),
     FlowAndSchedules(flow=init_pno_types_flow),
+    FlowAndSchedules(flow=init_species_groups_flow),
 ]
 
 
--- pipeline/src/flows/init_species_groups.py
+++ pipeline/src/flows/init_species_groups.py
@@ -1,34 +1,32 @@
-from pathlib import Path
-
 import pandas as pd
-import prefect
-from prefect import Flow, task
-from prefect.executors import LocalDaskExecutor
+from prefect import flow, get_run_logger, task
 
 from config import LIBRARY_LOCATION
 from src.generic_tasks import load
 
 
-@task(checkpoint=False)
+@task
 def extract_species_groups():
     return pd.read_csv(LIBRARY_LOCATION / "data/species_groups.csv", encoding="utf8")
 
 
-@task(checkpoint=False)
+@task
 def extract_species_codes_groups():
     return pd.read_csv(
         LIBRARY_LOCATION / "data/species_codes_groups.csv", encoding="utf8"
     )
 
 
-@task(checkpoint=False)
+@task
 def load_species_groups(species_groups, species_codes_groups):
+    logger = get_run_logger()
+
     load(
         species_groups,
         table_name="species_groups",
         schema="public",
         db_name="monitorfish_remote",
-        logger=prefect.context.get("logger"),
+        logger=logger,
         how="replace",
     )
 
@@ -37,14 +35,13 @@ def load_species_groups(species_groups, species_codes_groups):
         table_name="species_codes_groups",
         schema="public",
         db_name="monitorfish_remote",
-        logger=prefect.context.get("logger"),
+        logger=logger,
         how="replace",
     )
 
 
-with Flow("Species groups", executor=LocalDaskExecutor()) as flow:
+@flow(name="Species groups")
+def init_species_groups_flow():
     species_groups = extract_species_groups()
     species_codes_groups = extract_species_codes_groups()
     load_species_groups(species_groups, species_codes_groups)
-
-flow.file_name = Path(__file__).name

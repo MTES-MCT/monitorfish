d3f9a1ef252df820f79f554a753f7b3417b4d550
Loup Theron <loup-rodney@hotmail.fr>
2025-07-14 17:36:09 +0200

    Fix unauthorized api test

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/config/RestTemplateConfig.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/config/RestTemplateConfig.kt
@@ -30,7 +30,9 @@ class RestTemplateConfig {
                     } else {
                         "Request body too large to display"
                     }
-                logger.info("OUTGOING PROXIED REQUEST ${request.method} ${request.uri} ${request.headers} $bodyString")
+                logger.info(
+                    "[DEV-PROXY] OUTGOING PROXIED REQUEST ${request.method} ${request.uri} ${request.headers} $bodyString",
+                )
 
                 // Proceed with the request
                 execution.execute(request, body)

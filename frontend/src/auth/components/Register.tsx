63470c1d1a38ef5b83118ce18663160ff7dd1804
Loup Theron <loup-rodney@hotmail.fr>
2025-07-14 11:10:43 +0200

    Add missinf MonitorEnv URL

--- frontend/src/auth/components/Register.tsx
+++ frontend/src/auth/components/Register.tsx
@@ -1,16 +1,23 @@
 import { ToastContainer } from 'react-toastify'
+import styled from 'styled-components'
 
 import { Wrapper } from './Login'
 
 export function Register() {
   return (
     <Wrapper>
-      Merci de contacter{' '}
-      <a href="mailto:cnsp-france@developpement-durable.gouv.fr?subject=Création de compte MonitorFish">
-        cnsp-france@developpement-durable.gouv.fr
-      </a>{' '}
-      pour accéder à cette page.
+      <Text>
+        Merci de contacter{' '}
+        <a href="mailto:cnsp-france@developpement-durable.gouv.fr?subject=Création de compte MonitorFish">
+          cnsp-france@developpement-durable.gouv.fr
+        </a>{' '}
+        pour accéder à cette page.
+      </Text>
       <ToastContainer />
     </Wrapper>
   )
 }
+
+const Text = styled.span`
+  margin-top: 40vh;
+`

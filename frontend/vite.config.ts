b9d70a60420a09dc0dd42197bc7556cc406c04db
Loup Theron <loup-rodney@hotmail.fr>
2025-07-14 10:32:26 +0200

    Update cypress docker configuration to be runned with spring OIDC

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
     svgr(),
     importMetaEnv.vite({
       env: './.env',
-      example: '../.env.example'
+      example: './.env.example'
     })
   ],
   worker: {

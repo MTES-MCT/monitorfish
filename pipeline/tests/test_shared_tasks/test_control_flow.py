a19420da1cad9fc1dc60538fb5a3d90d3252b359
Vincent <vincent.chery@m4x.org>
2025-07-15 14:09:51 +0200

    Update control_flow shared tasks

--- pipeline/tests/test_shared_tasks/test_control_flow.py
+++ pipeline/tests/test_shared_tasks/test_control_flow.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from prefect import allow_failure, flow, task
 
-from src.shared_tasks.control_flow import filter_results, str_to_path
+from src.shared_tasks.control_flow import filter_results, flatten, str_to_path
 
 
 def test_str_to_path():
@@ -12,6 +12,11 @@ def test_str_to_path():
 @task
 def some_list() -> list:
     return [0, 1, 2, 3, 4]
+
+
+@task
+def another_list(i: int) -> list:
+    return [i + j for j in [0, 1, 2]]
 
 
 @task
@@ -52,16 +57,36 @@ def filter_flow():
     funky_li = unstable_task.map(li)
     filtered_li = filter_results(allow_failure(funky_li))
     added_li = add_one.map(filtered_li)
-    reduced_li = reduce(added_li)
-    return (li, funky_li, filtered_li, added_li, reduced_li)
+    list_of_lists = another_list.map(added_li)
+    flattened_li = flatten(list_of_lists)
+    reduced_li = reduce(flattened_li)
+    return (
+        li,
+        funky_li,
+        filtered_li,
+        added_li,
+        list_of_lists,
+        flattened_li,
+        reduced_li,
+    )
 
 
 def test_filter_failed_tasks():
     state = filter_flow(return_state=True)
     assert state.is_completed()
-    li, funky_li, filtered_li, added_li, reduced_li = state.result()
+    (
+        li,
+        funky_li,
+        filtered_li,
+        added_li,
+        list_of_lists,
+        flattened_li,
+        reduced_li,
+    ) = state.result()
     assert li == [0, 1, 2, 3, 4]
     assert len(funky_li) == 5
     assert filtered_li == [0, 4]
     assert added_li.result() == [1, 5]
-    assert reduced_li == 6
+    assert list_of_lists.result() == [[1, 2, 3], [5, 6, 7]]
+    assert flattened_li == [1, 2, 3, 5, 6, 7]
+    assert reduced_li == 24

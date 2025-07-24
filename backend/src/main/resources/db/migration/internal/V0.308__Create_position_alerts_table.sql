b93cbedfda29a7e0a776578d45c01043ee32d14e
Vincent <vincent.chery@m4x.org>
2025-07-24 09:48:59 +0200

    Rename user_defind_position_alerts table

--- backend/src/main/resources/db/migration/internal/V0.308__Create_position_alerts_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.308__Create_position_alerts_table.sql
@@ -1,7 +1,8 @@
-CREATE TABLE public.user_defined_position_alerts (
+CREATE TABLE public.position_alerts (
     id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
     description TEXT NOT NULL,
+    natinf_code INTEGER NOT NULL,
     is_activated BOOLEAN NOT NULL DEFAULT true,
     is_in_error BOOLEAN NOT NULL DEFAULT false,
     error_reason TEXT,
@@ -9,22 +10,25 @@ CREATE TABLE public.user_defined_position_alerts (
     validity_start_datetime_utc TIMESTAMP WITHOUT TIME ZONE,
     validity_end_datetime_utc TIMESTAMP WITHOUT TIME ZONE,
     repeat_each_year BOOLEAN NOT NULL DEFAULT false,
+    only_fishing_positions BOOLEAN NOT NULL DEFAULT true,
     gears JSONB,
+    include_vessels_with_unknown_gear BOOLEAN NOT NULL DEFAULT false,
     species JSONB,
     administrative_areas JSONB,
     regulatory_areas JSONB,
+    min_depth DOUBLE PRECISION,
     flag_states_iso2 VARCHAR[],
     vessel_ids INTEGER[],
     district_codes VARCHAR[],
     producer_organizations VARCHAR[],
-    CONSTRAINT is_deactivated_if_in_error CHECK (is_in_error + is_activated < 2),
+    CONSTRAINT is_deactivated_if_in_error CHECK (NOT (is_in_error AND is_activated)),
     CONSTRAINT has_start_and_end_date_if_repeat_each_year CHECK (
         NOT (
             repeat_each_year AND (
-                validity_start_date IS NULL OR
-                validity_end_date IS NULL OR
-                validity_start_date > validity_end_date            
+                validity_start_datetime_utc IS NULL OR
+                validity_end_datetime_utc IS NULL OR
+                validity_start_datetime_utc > validity_end_datetime_utc            
             )
-        ),
+        )
     )
 )

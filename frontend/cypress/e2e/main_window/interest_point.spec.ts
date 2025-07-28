4c3695a7e0ade55fad992ab3c0da30139d519567
Loup Theron <loup-rodney@hotmail.fr>
2025-07-28 17:11:59 +0200

    Delete manual PNO in cypress test

--- frontend/cypress/e2e/main_window/interest_point.spec.ts
+++ frontend/cypress/e2e/main_window/interest_point.spec.ts
@@ -64,7 +64,8 @@ context('InterestPoint', () => {
   it('An interest Should be created from input When DD coordinates are selected', () => {
     // When
     cy.get('*[data-cy="coordinates-selection"]').click({ force: true, timeout: 10000 })
-    cy.get('*[value="DD"]').click({ timeout: 10000 })
+    cy.wait(500)
+    cy.get('*[value="DD"]').click({ force: true, timeout: 10000 })
     cy.get('#root').click(159, 1000, { timeout: 10000 })
     cy.clickButton('Créer un point d\'intérêt', { withoutScroll: true })
 
@@ -84,7 +85,8 @@ context('InterestPoint', () => {
   it('An interest Should be edited When DMD coordinates are selected', () => {
     // When
     cy.get('*[data-cy="coordinates-selection"]').click({ force: true, timeout: 10000 })
-    cy.get('*[value="DMD"]').click({ timeout: 10000 })
+    cy.wait(500)
+    cy.get('*[value="DMD"]').click({ force: true, timeout: 10000 })
     cy.get('#root').click(159, 1000, { timeout: 10000 })
     cy.clickButton('Créer un point d\'intérêt', { withoutScroll: true })
     cy.get('#root').click(490, 580, { timeout: 10000 })

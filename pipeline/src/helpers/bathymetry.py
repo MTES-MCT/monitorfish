57fbc9cc85b648aaba9c79484c5cb34b869474d6
Vincent <vincent.chery@m4x.org>
2025-09-02 09:17:18 +0200

    Add proxies to get_depth

--- pipeline/src/helpers/bathymetry.py
+++ pipeline/src/helpers/bathymetry.py
@@ -1,12 +1,14 @@
 import requests
 from shapely import Point
 
-from config import BATHYMETRY_DEPTH_SAMPLE_ENDPOINT
+from config import BATHYMETRY_DEPTH_SAMPLE_ENDPOINT, PROXIES
 
 
 def get_depth(lon: float, lat: float) -> float:
     wkt = Point(lon, lat).wkt
-    response = requests.get(BATHYMETRY_DEPTH_SAMPLE_ENDPOINT, params={"geom": wkt})
+    response = requests.get(
+        BATHYMETRY_DEPTH_SAMPLE_ENDPOINT, params={"geom": wkt}, proxies=PROXIES
+    )
     response.raise_for_status()
     data = response.json()
     return data.get("avg")

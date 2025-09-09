c56aafc5529a34c2711a88afdde7051761b8889d
Claire Dagan <clairedagan@gmail.com>
2025-09-09 11:08:59 +0200

    [RegulationForm] use Select component from monitor-ui

--- frontend/src/features/Regulation/components/RegulationForm/custom_form/MenuItem.tsx
+++ frontend/src/features/Regulation/components/RegulationForm/custom_form/MenuItem.tsx
@@ -1,4 +1,4 @@
-import { Radio, Checkbox } from 'rsuite'
+import { Checkbox, Radio } from '@mtes-mct/monitor-ui'
 import styled from 'styled-components'
 
 type MenuItemProps = Readonly<{
@@ -6,23 +6,24 @@ type MenuItemProps = Readonly<{
   item: any
   tag?: string
 }>
+
 export function MenuItem({ checked = false, item, tag }: MenuItemProps) {
   const { label } = item
   const labelToDisplay = label
 
   if (tag === 'Radio') {
     return (
-      <CustomRadio checked={checked} title={labelToDisplay}>
+      <Radio checked={checked} labelPosition="right" name={labelToDisplay} title={labelToDisplay}>
         {labelToDisplay}
-      </CustomRadio>
+      </Radio>
     )
   }
 
   if (tag === 'Checkbox') {
     return (
-      <CustomCheckbox checked={checked} title={labelToDisplay}>
+      <Checkbox checked={checked} label={labelToDisplay} name={labelToDisplay} title={labelToDisplay}>
         {labelToDisplay}
-      </CustomCheckbox>
+      </Checkbox>
     )
   }
 
@@ -33,54 +34,4 @@ const Text = styled.span`
   overflow: hidden;
   text-overflow: ellipsis;
   padding: 8px 0px 4px 10px;
-`
-
-const CustomRadio = styled(Radio)`
-  .rs-radio-checker {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    padding: 4px 0px 8px 36px;
-    &:before {
-      box-sizing: border-box;
-    }
-    &:after {
-      box-sizing: border-box;
-    }
-  }
-
-  .rs-radio-checker > label {
-    font-size: 13px;
-    color: ${p => p.theme.color.slateGray};
-  }
-`
-
-const CustomCheckbox = styled(Checkbox)`
-  .rs-checkbox-checker {
-    overflow: hidden;
-    text-overflow: text-ellipsis;
-    padding: 8px 0px 4px 36px;
-    &:before {
-      box-sizing: border-box;
-    }
-    &:after {
-      box-sizing: border-box;
-    }
-  }
-
-  .rs-checkbox-checker > label {
-    font-size: 13px;
-    color: ${p => p.theme.color.slateGray};
-  }
-
-  .rs-checkbox-checker .rs-checkbox-inner {
-    &:before {
-      border: 1px solid ${p => p.theme.color.lightGray} !important;
-      box-sizing: border-box;
-    }
-
-    &:after {
-      margin-top: 0px !important;
-      margin-left: 4px !important;
-    }
-  }
 `

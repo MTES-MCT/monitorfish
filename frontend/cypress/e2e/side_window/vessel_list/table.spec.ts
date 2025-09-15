97a899fe95ae269ccf00a5a0a3e1ad0561b36a4a
Loup Theron <loup-rodney@hotmail.fr>
2025-09-15 13:39:16 +0200

    Fix vessel list cypress test number of vessels

--- frontend/cypress/e2e/side_window/vessel_list/table.spec.ts
+++ frontend/cypress/e2e/side_window/vessel_list/table.spec.ts
@@ -1,6 +1,6 @@
 context('Side Window > Vessel List > Table', () => {
 
-  it.only('Should filter vessels, open a row and download the CSV When logged as super user', () => {
+  it('Should filter vessels, open a row and download the CSV When logged as super user', () => {
     cy.cleanDownloadedFiles()
     cy.login('superuser')
 
@@ -49,7 +49,7 @@ context('Side Window > Vessel List > Table', () => {
     cy.get('.Table-SimpleTable tr').should('have.length', 2)
 
     cy.fill('Dernier contrôle quai', 'Contrôlé il y a plus de 3 mois')
-    cy.getDataCy('vessel-list-length').contains('0 navire')
+    cy.getDataCy('vessel-list-length').contains('1 navire')
     cy.fill('Dernier contrôle quai', undefined)
 
     cy.get('.Component-SingleTag').should('have.length', 10)
@@ -116,7 +116,7 @@ context('Side Window > Vessel List > Table', () => {
     cy.getDataCy('vessel-list-length').contains('1 navire')
     cy.get('.Table-SimpleTable tr').should('have.length', 2)
 
-    cy.fill('Dernier contrôle', 'Contrôlé il y a plus de 3 mois')
+    cy.fill('Dernier contrôle mer', 'Contrôlé il y a plus de 3 mois')
     cy.getDataCy('vessel-list-length').contains('1 navire')
     cy.get('.Table-SimpleTable tr').should('have.length', 2)
 

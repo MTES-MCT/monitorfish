431075f6e74f0321aa6148ecaab5db571fe194a3
Vincent <vincent.chery@m4x.org>
2025-09-16 09:24:30 +0200

    Update truncate-prefect-logs.sh

--- infra/prefect_3_server/truncate-prefect-logs.sh
+++ infra/prefect_3_server/truncate-prefect-logs.sh
@@ -1,1 +1,4 @@
-docker exec prefect-db sh -c "psql -U prefect -d prefect -c \"DELETE FROM flow_run WHERE expected_start_time < NOW() - INTERVAL '7 days'\";"
+docker exec prefect-db sh -c "psql -U prefect -d prefect -c \"DELETE FROM flow_run WHERE expected_start_time < NOW() - INTERVAL '1 day'\";"
+docker exec prefect-db sh -c "psql -U prefect -d prefect -c \"DELETE FROM log WHERE timestamp < NOW() - INTERVAL '1 day'\";"
+docker exec prefect-db sh -c "psql -U prefect -d prefect -c \"DELETE FROM event_resources WHERE occurred < NOW() - INTERVAL '1 day'\";"
+docker exec prefect-db sh -c "psql -U prefect -d prefect -c \"DELETE FROM events WHERE occurred < NOW() - INTERVAL '1 day'\";"

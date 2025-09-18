ad194a4ca1e36cc2b53d87472159affe16785b9d
Loup Theron <loup-rodney@hotmail.fr>
2025-09-18 10:07:12 +0200

    Authentification - Passage à ProConnect (UPDATE ENV VAR) (#4390)
    ## Linked issues
    
    - Resolve #3701
    - Resolve #3032
    - [x] Ajouter un test backend pour documenter le flow de connexion OIDC
    
    ## à faire en production
    
    - Récupérer le nouveau docker-compose.yml
    - Ajouter les envs :
    ```
    # Sentry
    export MONITORFISH_SENTRY_ENABLED=true
    
    # OIDC
    export MONITORFISH_OIDC_ENABLED=true
    export MONITORFISH_OIDC_LOGIN_URL=/oauth2/authorization/proconnect
    export MONITORFISH_OIDC_SUCCESS_URL=https://monitorfish.din.developpement-durable.gouv.fr/
    export MONITORFISH_OIDC_ERROR_URL=https://monitorfish.din.developpement-durable.gouv.fr/register
    export MONITORFISH_OIDC_AUTHORIZED_SIRETS=11006801200050,12002301500031
    export MONITORFISH_OIDC_CLIENT_ID=XXX
    export MONITORFISH_OIDC_CLIENT_SECRET=XXX
    export MONITORFISH_OIDC_REDIRECT_URI=https://monitorfish.din.developpement-durable.gouv.fr/login/oauth2/code/proconnect
    export MONITORFISH_OIDC_ISSUER_URI=https://auth.agentconnect.rie.gouv.fr/api/v2/
    export MONITORFISH_OIDC_PROVIDER=proconnect
    export MONITORFISH_OIDC_AUTHORIZATION_URI=https://auth.agentconnect.rie.gouv.fr/api/v2/authorize
    export MONITORFISH_OIDC_TOKEN_URI=https://auth.agentconnect.rie.gouv.fr/api/v2/token
    export MONITORFISH_OIDC_USER_INFO_URI=https://auth.agentconnect.rie.gouv.fr/api/v2/userinfo
    export MONITORFISH_OIDC_JWK_SET_URI=https://auth.agentconnect.rie.gouv.fr/api/v2/jwks
    
    # Proxy (UNUSED)
    export MONITORFISH_OIDC_PROXY_URL=
    export MONITORFISH_KEYCLOAK_PROXY_ENABLED=false
    
    # Database
    export MONITORFISH_DATABASE_MISSIONS_ACTIONS_CHUNK_SIZE=100
    export MONITORFISH_MIGRATION_PATH=classpath:/db/migration
    
    # APIs
    export MONITORFISH_API_PROTECTED_SUPER_USER_PATHS=/bff/v1/beacon_malfunctions,/bff/v1/missions,/bff/v1/operational_alerts,/bff/v1/reportings,/bff/v1/vessels/risk_factors,/bff/v1/admin/control_objectives,/bff/v1/admin/fleet_segments,/bff/v1/fleet_segments/compute
    ```
    - Retirer les envs :
    ```
    export MONITORFISH_OIDC_AUTHORITY=
    export MONITORFISH_OIDC_CLIENT=
    export MONITORFISH_OIDC_REDIRECT_URI=
    export MONITORFISH_OIDC_LOGOUT_REDIRECT_URI=
    ```
    
    ----
    
    - [ ] Tests E2E (Cypress)

--- frontend/src/auth/apis.ts
+++ frontend/src/auth/apis.ts
@@ -1,64 +1,14 @@
 import { monitorfishApi } from '@api/api'
-import { BackendApi, type Meta } from '@api/BackendApi.types'
-import { HttpStatusCode } from '@api/constants'
-import { FrontendApiError } from '@libs/FrontendApiError'
 
-import type { UserAuthorization } from './types'
-
-import ErrorCode = BackendApi.ErrorCode
-
-const ERROR_AUTHENTICATION_REQUIRED = 'Authentification requise'
-const ERROR_TOKEN_EXPIRED = "Jeton d'authentification expiré"
-const ERROR_AUTHENTICATION_FAILED = "Erreur d'authentification"
+import type { AuthorizedUser } from './types'
 
 export const authorizationAPI = monitorfishApi.injectEndpoints({
   endpoints: builder => ({
-    getCurrentUserAuthorization: builder.query<UserAuthorization, void>({
+    getCurrentUserAuthorization: builder.query<AuthorizedUser, void>({
       keepUnusedDataFor: 0,
-      query: () => '/authorization/current',
-      transformErrorResponse: (response, meta: Meta) => {
-        const authenticateResponse = meta?.response?.headers.get('WWW-Authenticate')
-        if (authenticateResponse?.includes('authentication is required')) {
-          throw new FrontendApiError(ERROR_AUTHENTICATION_REQUIRED, {
-            path: response.path,
-            requestData: response.requestData,
-            responseData: {
-              code: ErrorCode.AUTHENTICATION_REQUIRED,
-              data: { isSuperUser: false },
-              type: ErrorCode.AUTHENTICATION_REQUIRED
-            },
-            status: HttpStatusCode.FORBIDDEN
-          })
-        }
-        if (authenticateResponse?.includes('expired')) {
-          throw new FrontendApiError(ERROR_TOKEN_EXPIRED, {
-            path: response.path,
-            requestData: response.requestData,
-            responseData: {
-              code: ErrorCode.AUTHENTICATION_REQUIRED,
-              data: { isSuperUser: false },
-              type: ErrorCode.AUTHENTICATION_REQUIRED
-            },
-            status: HttpStatusCode.FORBIDDEN
-          })
-        }
-
-        throw new FrontendApiError(ERROR_AUTHENTICATION_FAILED, {
-          path: response.path,
-          requestData: response.requestData,
-          responseData: {
-            code: ErrorCode.AUTHENTICATION_REQUIRED,
-            data: { isSuperUser: false },
-            type: ErrorCode.AUTHENTICATION_REQUIRED
-          },
-          status: HttpStatusCode.FORBIDDEN
-        })
-      }
+      query: () => '/authorization/current'
     })
   })
 })
 
-export const {
-  endpoints: { getCurrentUserAuthorization },
-  useGetCurrentUserAuthorizationQuery
-} = authorizationAPI
+export const { useGetCurrentUserAuthorizationQuery } = authorizationAPI
--- frontend/src/features/PriorNotification/components/shared/UploadFiles.tsx
+++ frontend/src/features/PriorNotification/components/shared/UploadFiles.tsx
@@ -8,7 +8,6 @@ import { useMainAppDispatch } from '@hooks/useMainAppDispatch'
 import { useKey } from '@mtes-mct/monitor-ui'
 import { assertNotNullish } from '@utils/assertNotNullish'
 import { downloadFile } from '@utils/downloadFile'
-import { useAuthRequestHeaders } from 'auth/hooks/useAuthRequestHeaders'
 import { useCallback, useMemo } from 'react'
 import { Uploader } from 'rsuite'
 import styled from 'styled-components'
@@ -23,7 +22,6 @@ type UploadFilesProps = Readonly<{
 }>
 export function UploadFiles({ isManualPriorNotification, isReadOnly, operationDate, reportId }: UploadFilesProps) {
   const dispatch = useMainAppDispatch()
-  const headers = useAuthRequestHeaders()
 
   const action = `/bff/v1/prior_notifications/${reportId}/uploads?isManualPriorNotification=${isManualPriorNotification}&operationDate=${operationDate}`
   const { data: uploads } = useGetPriorNotificationUploadsQuery(reportId)
@@ -75,7 +73,7 @@ export function UploadFiles({ isManualPriorNotification, isReadOnly, operationDa
     [dispatch, isManualPriorNotification, operationDate, reportId]
   )
 
-  if (!headers || !uploadAsFileTypes) {
+  if (!uploadAsFileTypes) {
     return <></>
   }
 
@@ -86,11 +84,11 @@ export function UploadFiles({ isManualPriorNotification, isReadOnly, operationDa
         action={action}
         defaultFileList={uploadAsFileTypes}
         draggable
-        headers={headers}
         onPreview={download}
         onRemove={remove}
         onSuccess={refetch}
         readOnly={isReadOnly}
+        withCredentials
       >
         <File>
           {isReadOnly

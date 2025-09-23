9aa8166d7d0299c4b4b0f09a663451afe8711ca4
Loup Theron <loup-rodney@hotmail.fr>
2025-09-23 16:56:09 +0200

    Add activate/deactivate/delete alerts

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/bff/PositionAlertSpecificationController.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/bff/PositionAlertSpecificationController.kt
@@ -1,18 +1,21 @@
 package fr.gouv.cnsp.monitorfish.infrastructure.api.bff
 
+import fr.gouv.cnsp.monitorfish.domain.use_cases.alert.ActivateOrDeactivateAlertSpecification
+import fr.gouv.cnsp.monitorfish.domain.use_cases.alert.DeleteAlertSpecification
 import fr.gouv.cnsp.monitorfish.domain.use_cases.alert.GetPositionAlertSpecifications
 import fr.gouv.cnsp.monitorfish.infrastructure.api.outputs.PositionAlertSpecificationDataOutput
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import jakarta.websocket.server.PathParam
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/bff/v1/position_alerts_specs")
 @Tag(name = "APIs for position alerts specification")
 class PositionAlertSpecificationController(
     private val getPositionAlertSpecifications: GetPositionAlertSpecifications,
+    private val activateOrDeactivateAlertSpecification: ActivateOrDeactivateAlertSpecification,
+    private val deleteAlertSpecification: DeleteAlertSpecification,
 ) {
     @GetMapping("")
     @Operation(summary = "Get all position alerts specifications")
@@ -20,4 +23,28 @@ class PositionAlertSpecificationController(
         getPositionAlertSpecifications.execute().map {
             PositionAlertSpecificationDataOutput.fromPositionAlertSpecification(it)
         }
+
+    @PutMapping("/{id}/activate")
+    @Operation(summary = "Activate an alert")
+    fun activateAlert(
+        @PathParam("Alert id")
+        @PathVariable(name = "id")
+        id: Int,
+    ) = activateOrDeactivateAlertSpecification.execute(id = id, isActivated = true)
+
+    @PutMapping("/{id}/deactivate")
+    @Operation(summary = "Activate an alert")
+    fun deactivateAlert(
+        @PathParam("Alert id")
+        @PathVariable(name = "id")
+        id: Int,
+    ) = activateOrDeactivateAlertSpecification.execute(id = id, isActivated = false)
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete an alert")
+    fun delete(
+        @PathParam("Alert id")
+        @PathVariable(name = "id")
+        id: Int,
+    ) = deleteAlertSpecification.execute(id)
 }
--- backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/bff/PositionAlertSpecificationControllerITests.kt
+++ backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/infrastructure/api/bff/PositionAlertSpecificationControllerITests.kt
@@ -14,7 +14,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Import
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -27,6 +29,12 @@ class PositionAlertSpecificationControllerITests {
 
     @MockBean
     private lateinit var getPositionAlertSpecifications: GetPositionAlertSpecifications
+
+    @MockBean
+    private lateinit var activateOrDeactivateAlertSpecification: ActivateOrDeactivateAlertSpecification
+
+    @MockBean
+    private lateinit var deleteAlertSpecification: DeleteAlertSpecification
 
     @Autowired
     private lateinit var objectMapper: ObjectMapper
@@ -46,5 +54,41 @@ class PositionAlertSpecificationControllerITests {
             .andExpect(jsonPath("$.length()", equalTo(1)))
             .andExpect(jsonPath("$[0].name", equalTo("Chalutage dans les 3 milles")))
             .andExpect(jsonPath("$[0].type", equalTo("POSITION_ALERT")))
+    }
+
+    @Test
+    fun `Should activate an alert specification`() {
+        // When
+        api
+            .perform(put("/bff/v1/position_alerts_specs/123/activate"))
+            // Then
+            .andExpect(status().isOk)
+
+        // Verify
+        verify(activateOrDeactivateAlertSpecification).execute(123, true)
+    }
+
+    @Test
+    fun `Should deactivate an alert specification`() {
+        // When
+        api
+            .perform(put("/bff/v1/position_alerts_specs/456/deactivate"))
+            // Then
+            .andExpect(status().isOk)
+
+        // Verify
+        verify(activateOrDeactivateAlertSpecification).execute(456, false)
+    }
+
+    @Test
+    fun `Should delete an alert specification`() {
+        // When
+        api
+            .perform(delete("/bff/v1/position_alerts_specs/789"))
+            // Then
+            .andExpect(status().isOk)
+
+        // Verify
+        verify(deleteAlertSpecification).execute(789)
     }
 }

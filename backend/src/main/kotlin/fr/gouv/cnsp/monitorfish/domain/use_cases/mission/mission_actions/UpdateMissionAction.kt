43be3d8a602e98d521324c0e402e9a64ddb2a0c8
Loup Theron <loup-rodney@hotmail.fr>
2025-09-24 10:48:32 +0200

    Fix species name when updating mission action

--- backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/mission/mission_actions/UpdateMissionAction.kt
+++ backend/src/main/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/mission/mission_actions/UpdateMissionAction.kt
@@ -2,13 +2,17 @@ package fr.gouv.cnsp.monitorfish.domain.use_cases.mission.mission_actions
 
 import fr.gouv.cnsp.monitorfish.config.UseCase
 import fr.gouv.cnsp.monitorfish.domain.entities.mission.mission_actions.MissionAction
+import fr.gouv.cnsp.monitorfish.domain.entities.mission.mission_actions.SpeciesControl
+import fr.gouv.cnsp.monitorfish.domain.entities.species.Species
 import fr.gouv.cnsp.monitorfish.domain.repositories.MissionActionsRepository
+import fr.gouv.cnsp.monitorfish.domain.use_cases.species.GetSpeciesFromCode
 import org.slf4j.LoggerFactory
 
 @UseCase
 class UpdateMissionAction(
     private val missionActionsRepository: MissionActionsRepository,
     private val getMissionActionFacade: GetMissionActionFacade,
+    private val getSpeciesFromCode: GetSpeciesFromCode,
 ) {
     private val logger = LoggerFactory.getLogger(UpdateMissionAction::class.java)
 
@@ -24,14 +28,21 @@ class UpdateMissionAction(
         // We store the `storedValue` of the enum and not the enum uppercase value
         val facade = getMissionActionFacade.execute(action)?.toString()
 
-        val actionWithFixedFieldsAndId =
+        val speciesOnboard: List<SpeciesControl> =
+            action.speciesOnboard.map {
+                val singleSpecies: Species? = getSpeciesFromCode.execute(code = it.speciesCode)
+                it.apply { this.speciesName = singleSpecies?.name }
+            }
+
+        val enrichedAction =
             action.copy(
+                speciesOnboard = speciesOnboard,
                 actionEndDatetimeUtc = previousMissionAction.actionEndDatetimeUtc,
                 observationsByUnit = previousMissionAction.observationsByUnit,
                 id = actionId,
                 facade = facade,
             )
 
-        return missionActionsRepository.save(actionWithFixedFieldsAndId)
+        return missionActionsRepository.save(enrichedAction)
     }
 }
--- backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/mission/mission_actions/UpdateMissionActionUTests.kt
+++ backend/src/test/kotlin/fr/gouv/cnsp/monitorfish/domain/use_cases/mission/mission_actions/UpdateMissionActionUTests.kt
@@ -2,12 +2,19 @@ package fr.gouv.cnsp.monitorfish.domain.use_cases.mission.mission_actions
 
 import com.neovisionaries.i18n.CountryCode
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.anyOrNull
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.given
 import com.nhaarman.mockitokotlin2.verify
+import fr.gouv.cnsp.monitorfish.domain.entities.facade.Seafront
 import fr.gouv.cnsp.monitorfish.domain.entities.mission.mission_actions.Completion
 import fr.gouv.cnsp.monitorfish.domain.entities.mission.mission_actions.MissionAction
 import fr.gouv.cnsp.monitorfish.domain.entities.mission.mission_actions.MissionActionType
+import fr.gouv.cnsp.monitorfish.domain.entities.mission.mission_actions.SpeciesControl
 import fr.gouv.cnsp.monitorfish.domain.repositories.MissionActionsRepository
+import fr.gouv.cnsp.monitorfish.domain.use_cases.species.GetSpeciesFromCode
+import fr.gouv.cnsp.monitorfish.fakers.SpeciesFaker
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.catchThrowable
 import org.junit.jupiter.api.Test
@@ -23,6 +30,9 @@ class UpdateMissionActionUTests {
 
     @MockBean
     private lateinit var getMissionActionFacade: GetMissionActionFacade
+
+    @MockBean
+    private lateinit var getSpeciesFromCode: GetSpeciesFromCode
 
     @Test
     fun `execute Should throw an exception When the vesselId is missing in a control`() {
@@ -49,7 +59,11 @@ class UpdateMissionActionUTests {
         // When
         val throwable =
             catchThrowable {
-                UpdateMissionAction(missionActionsRepository, getMissionActionFacade).execute(123, action)
+                UpdateMissionAction(
+                    missionActionsRepository,
+                    getMissionActionFacade,
+                    getSpeciesFromCode,
+                ).execute(123, action)
             }
 
         // Then
@@ -82,9 +96,85 @@ class UpdateMissionActionUTests {
         given(missionActionsRepository.findById(any())).willReturn(action.copy(actionEndDatetimeUtc = endDate))
 
         // When
-        UpdateMissionAction(missionActionsRepository, getMissionActionFacade).execute(123, action)
+        UpdateMissionAction(missionActionsRepository, getMissionActionFacade, getSpeciesFromCode).execute(123, action)
 
         // Then
         verify(missionActionsRepository).save(action.copy(actionEndDatetimeUtc = endDate))
+    }
+
+    @Test
+    fun `execute Should enrich speciesOnboard with speciesName When speciesOnboard is provided`() {
+        // Given
+        val speciesCode = "COD"
+        val expectedSpeciesName = "Atlantic cod"
+        val species = SpeciesFaker.fakeSpecies(code = speciesCode, name = expectedSpeciesName)
+
+        val speciesControl = SpeciesControl()
+        speciesControl.speciesCode = speciesCode
+        speciesControl.speciesName = null // Initially null
+
+        val expectedSpeciesControl = SpeciesControl()
+        expectedSpeciesControl.speciesCode = speciesCode
+        expectedSpeciesControl.speciesName = expectedSpeciesName
+
+        val action =
+            MissionAction(
+                id = 123,
+                vesselId = 1,
+                missionId = 1,
+                longitude = 45.7,
+                latitude = 13.5,
+                actionDatetimeUtc = ZonedDateTime.now(),
+                portLocode = "AEFAT",
+                portName = "Port name",
+                actionType = MissionActionType.LAND_CONTROL,
+                gearOnboard = listOf(),
+                speciesOnboard = listOf(speciesControl),
+                seizureAndDiversion = true,
+                isDeleted = false,
+                userTrigram = "LTH",
+                hasSomeGearsSeized = false,
+                hasSomeSpeciesSeized = false,
+                completedBy = "XYZ",
+                isFromPoseidon = false,
+                flagState = CountryCode.FR,
+                completion = Completion.TO_COMPLETE,
+            )
+
+        val expectedActionWithSpeciesName =
+            action.copy(
+                facade = "NAMO",
+                speciesOnboard = listOf(expectedSpeciesControl),
+            )
+        given(missionActionsRepository.findById(anyOrNull())).willReturn(action)
+        given(missionActionsRepository.save(anyOrNull())).willReturn(expectedActionWithSpeciesName)
+        given(getMissionActionFacade.execute(anyOrNull())).willReturn(Seafront.NAMO)
+        given(getSpeciesFromCode.execute(eq(speciesCode))).willReturn(species)
+
+        // When
+        val returnedAction =
+            UpdateMissionAction(
+                missionActionsRepository,
+                getMissionActionFacade,
+                getSpeciesFromCode,
+            ).execute(actionId = 123, action = action)
+
+        // Then
+        assertThat(returnedAction).isNotNull
+        assertThat(returnedAction.speciesOnboard).hasSize(1)
+        assertThat(returnedAction.speciesOnboard.first().speciesName).isEqualTo(expectedSpeciesName)
+
+        // Verify that getSpeciesFromCode was called with the correct species code
+        verify(getSpeciesFromCode).execute(speciesCode)
+
+        // Verify that the repository save was called with the enriched action
+        argumentCaptor<MissionAction>().apply {
+            verify(missionActionsRepository).save(capture())
+
+            val savedAction = allValues.first()
+            assertThat(savedAction.facade).isEqualTo("NAMO")
+            assertThat(savedAction.speciesOnboard).hasSize(1)
+            assertThat(savedAction.speciesOnboard.first().speciesName).isEqualTo(expectedSpeciesName)
+        }
     }
 }

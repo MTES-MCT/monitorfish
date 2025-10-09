0791ea69f625d58f086e18540a6765035675ba38
Loup Theron <loup-rodney@hotmail.fr>
2025-10-09 17:57:48 +0200

    Fix other cypress tests

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -6,6 +6,7 @@ import { getComputedStyle } from './commands/getComputedStyle'
 import { getDownloadedFileContent } from './commands/getDownloadedFileContent'
 import { getFeaturesFromLayer } from './commands/getFeaturesFromLayer'
 import { getViewCenter } from './commands/getViewCenter'
+import { hoverVesselByName } from './commands/hoverVesselByName'
 import { injectAxe } from './commands/injectAxe'
 import { login } from './commands/login'
 
@@ -23,6 +24,7 @@ Cypress.Commands.add('cleanDownloadedFiles', cleanDownloadedFiles)
 Cypress.Commands.add('countRequestsByAlias', countRequestsByAlias)
 Cypress.Commands.add('getComputedStyle', getComputedStyle)
 Cypress.Commands.add('getDownloadedFileContent', getDownloadedFileContent)
+Cypress.Commands.add('hoverVesselByName', hoverVesselByName)
 Cypress.Commands.add('resetCountRequestsByAlias', resetCountRequestsByAlias)
 Cypress.Commands.add('login', login)
 Cypress.Commands.add('injectAxe', injectAxe)

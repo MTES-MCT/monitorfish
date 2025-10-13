025908db15259cd7b7e410fff4371dda097af23c
Loup Theron <loup-rodney@hotmail.fr>
2025-10-13 10:36:21 +0200

    Fix producer organization filter

--- frontend/src/features/Alert/components/SideWindowAlerts/AlertManagementForm/index.tsx
+++ frontend/src/features/Alert/components/SideWindowAlerts/AlertManagementForm/index.tsx
@@ -287,7 +287,7 @@ export function AlertManagementForm() {
                   {hasProducerOrganizationCriteria && (
                     <ProducerOrganizationCriteria
                       onDelete={() => {
-                        setSelectedCriterias(previous => previous.filter(criteria => criteria !== Criteria.NATIONALITY))
+                        setSelectedCriterias(previous => previous.filter(criteria => criteria !== Criteria.PRODUCER_ORGANIZATION))
                       }}
                     />
                   )}

0b12f3df13e64425db3cfc97542ef421c3966f81
Claire Dagan <clairedagan@gmail.com>
2025-11-07 09:18:14 +0100

    [VesselGroup] persist vesselGroup filters in store

--- frontend/src/features/VesselGroup/components/VesselGroupList/slice.ts
+++ frontend/src/features/VesselGroup/components/VesselGroupList/slice.ts
@@ -1,17 +1,42 @@
+import { GroupType, Sharing } from '@features/VesselGroup/types'
 import { createSlice } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import type { PayloadAction } from '@reduxjs/toolkit'
 
 export type VesselGroupListState = {
+  filteredExpired: boolean
+  filteredGroupTypes: GroupType[]
+  filteredSharing: Sharing[]
   searchQuery: string | undefined
 }
+
 export const INITIAL_STATE: VesselGroupListState = {
+  filteredExpired: false,
+  filteredGroupTypes: [GroupType.DYNAMIC, GroupType.FIXED],
+  filteredSharing: [Sharing.SHARED, Sharing.PRIVATE],
   searchQuery: undefined
 }
+
+const persistConfig = {
+  key: 'vesselGroupList',
+  storage
+}
+
 const vesselGroupListSlice = createSlice({
   initialState: INITIAL_STATE,
   name: 'vesselGroupList',
   reducers: {
+    setFilteredExpired(state, action: PayloadAction<boolean>) {
+      state.filteredExpired = action.payload
+    },
+    setFilteredGroupTypes(state, action: PayloadAction<GroupType[]>) {
+      state.filteredGroupTypes = action.payload
+    },
+    setFilteredSharing(state, action: PayloadAction<Sharing[]>) {
+      state.filteredSharing = action.payload
+    },
     setSearchQuery(state, action: PayloadAction<string | undefined>) {
       state.searchQuery = action.payload
     }
@@ -20,4 +45,4 @@ const vesselGroupListSlice = createSlice({
 
 export const vesselGroupListActions = vesselGroupListSlice.actions
 
-export const vesselGroupListReducer = vesselGroupListSlice.reducer
+export const vesselGroupListReducer = persistReducer(persistConfig, vesselGroupListSlice.reducer)

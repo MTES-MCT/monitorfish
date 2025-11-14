c6db07ce597bfac388aa69b14441e59556dfa907
Loup Theron <loup-rodney@hotmail.fr>
2025-11-14 16:13:58 +0100

    Migrate tags to jsonb type

--- frontend/src/features/Regulation/utils.ts
+++ frontend/src/features/Regulation/utils.ts
@@ -37,7 +37,7 @@ export const mapToRegulatoryZone = (
     region: feature.properties.region,
     regulatoryReferences: parseRegulatoryReferences(feature.properties.regulatory_references),
     speciesRegulation: parseSpeciesRegulation(feature.properties.species, speciesByCode),
-    tags: feature.properties.tags ?? [],
+    tags: feature.properties.tags ? parseJSON(feature.properties.tags) : [],
     topic: feature.properties.topic,
     zone: decodeURI(feature.properties.zone)
   }

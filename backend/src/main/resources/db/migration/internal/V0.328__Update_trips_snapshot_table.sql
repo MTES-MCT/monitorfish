5787e2c368ca82733d2a10cf09fe0ddc6744c892
Vincent <vincent.chery@m4x.org>
2025-11-24 17:23:35 +0100

    Update table and flow

---
+++ backend/src/main/resources/db/migration/internal/V0.328__Update_trips_snapshot_table.sql
@@ -0,0 +1,6 @@
+ALTER TABLE public.trips_snapshot
+RENAME COLUMN sort_order_datetime_utc TO start_datetime_utc;
+
+ALTER TABLE public.trips_snapshot
+ADD COLUMN end_datetime_utc TIMESTAMP WITHOUT TIME ZONE;
+ 

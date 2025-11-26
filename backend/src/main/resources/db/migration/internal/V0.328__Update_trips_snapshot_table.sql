a801fa9cf5d4d4f3abfa19b4b54f61f986c37c4d
Vincent <vincent.chery@m4x.org>
2025-11-26 09:23:21 +0100

    Add not null constraints on start and end trip dates

--- backend/src/main/resources/db/migration/internal/V0.328__Update_trips_snapshot_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.328__Update_trips_snapshot_table.sql
@@ -3,4 +3,10 @@ RENAME COLUMN sort_order_datetime_utc TO start_activity_datetime_utc;
 
 ALTER TABLE public.trips_snapshot
 ADD COLUMN end_activity_datetime_utc TIMESTAMP WITHOUT TIME ZONE;
+
+UPDATE public.trips_snapshot
+SET end_activity_datetime_utc = start_activity_datetime_utc;
  
+ ALTER TABLE public.trips_snapshot
+    ALTER COLUMN start_activity_datetime_utc SET NOT NULL,
+    ALTER COLUMN end_activity_datetime_utc SET NOT NULL;

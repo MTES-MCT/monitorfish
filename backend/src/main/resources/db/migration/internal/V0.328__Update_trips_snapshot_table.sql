75350789dc8ae6b8ff2d4e5197a0da8a041c609f
Vincent <vincent.chery@m4x.org>
2025-11-26 10:48:31 +0100

    Rename columns

--- backend/src/main/resources/db/migration/internal/V0.328__Update_trips_snapshot_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.328__Update_trips_snapshot_table.sql
@@ -1,12 +1,12 @@
 ALTER TABLE public.trips_snapshot
-RENAME COLUMN sort_order_datetime_utc TO start_activity_datetime_utc;
+RENAME COLUMN sort_order_datetime_utc TO start_datetime_utc;
 
 ALTER TABLE public.trips_snapshot
-ADD COLUMN end_activity_datetime_utc TIMESTAMP WITHOUT TIME ZONE;
+ADD COLUMN end_datetime_utc TIMESTAMP WITHOUT TIME ZONE;
 
 UPDATE public.trips_snapshot
-SET end_activity_datetime_utc = start_activity_datetime_utc;
+SET end_datetime_utc = start_datetime_utc;
  
  ALTER TABLE public.trips_snapshot
-    ALTER COLUMN start_activity_datetime_utc SET NOT NULL,
-    ALTER COLUMN end_activity_datetime_utc SET NOT NULL;
+    ALTER COLUMN start_datetime_utc SET NOT NULL,
+    ALTER COLUMN end_datetime_utc SET NOT NULL;
--- pipeline/src/queries/data_warehouse/trips_snapshot.sql
+++ pipeline/src/queries/data_warehouse/trips_snapshot.sql
@@ -4,12 +4,12 @@ SELECT
     COALESCE(
         MIN(CASE WHEN ABS(activity_datetime_utc - operation_datetime_utc) / 3600 / 24 / 365 < 5 THEN activity_datetime_utc END),
         MIN(operation_datetime_utc)
-    )::DateTime64 AS start_activity_datetime_utc,
+    )::DateTime64 AS start_datetime_utc,
     COALESCE(
         MAX(CASE WHEN log_type != 'LAN' AND ABS(activity_datetime_utc - operation_datetime_utc) / 3600 / 24 / 365 < 5 THEN activity_datetime_utc END),
         MAX(CASE WHEN ABS(activity_datetime_utc - operation_datetime_utc) / 3600 / 24 / 365 < 5 THEN activity_datetime_utc END),
         MAX(operation_datetime_utc)
-    )::DateTime64 AS end_activity_datetime_utc,
+    )::DateTime64 AS end_datetime_utc,
     MIN(operation_datetime_utc)::DateTime64 AS first_operation_datetime_utc,
     MAX(operation_datetime_utc)::DateTime64 AS last_operation_datetime_utc
 FROM monitorfish.activities

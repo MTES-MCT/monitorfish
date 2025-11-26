5d9295bb28c31d2fee42aea2177ba4c82d08d40a
Vincent Chéry <vincent.chery@m4x.org>
2025-11-26 08:58:31 +0100

    Update backend/src/main/resources/db/migration/internal/V0.328__Update_trips_snapshot_table.sql
    Co-authored-by: Loup Theron <loup-rodney@hotmail.fr>

--- backend/src/main/resources/db/migration/internal/V0.328__Update_trips_snapshot_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.328__Update_trips_snapshot_table.sql
@@ -1,5 +1,5 @@
 ALTER TABLE public.trips_snapshot
-RENAME COLUMN sort_order_datetime_utc TO start_datetime_utc;
+RENAME COLUMN sort_order_datetime_utc TO start_activity_datetime_utc;
 
 ALTER TABLE public.trips_snapshot
 ADD COLUMN end_datetime_utc TIMESTAMP WITHOUT TIME ZONE;

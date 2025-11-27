b3c373d5ee843221823d40d1bc7ff848c11ad105
Loup Theron <loup-rodney@hotmail.fr>
2025-11-27 14:35:10 +0100

    Restore segments    column to analytics_controls view for controls_open_data query

--- backend/src/main/resources/db/migration/internal/V0.332__Update_analytics_views_to_use_consolidated_infractions.sql
+++ backend/src/main/resources/db/migration/internal/V0.332__Update_analytics_views_to_use_consolidated_infractions.sql
@@ -3,95 +3,59 @@ DROP VIEW IF EXISTS analytics_controls;
 
 CREATE VIEW public.analytics_controls AS
 
-WITH controls_infractions AS (
+WITH controls_infraction_natinf AS (
     SELECT
         id,
         jsonb_array_elements(
             CASE WHEN jsonb_typeof(infractions) = 'array' THEN infractions ELSE '[]' END
-        ) AS mission_infraction
+        )->>'natinf' AS infraction_natinf
     FROM mission_actions
     WHERE
-        action_type IN ('SEA_CONTROL', 'LAND_CONTROL', 'AIR_CONTROL') AND
-        jsonb_array_length(
-            CASE WHEN jsonb_typeof(infractions) = 'array' THEN infractions ELSE '[]' END
-        ) > 0
-),
+    action_type IN ('SEA_CONTROL', 'LAND_CONTROL', 'AIR_CONTROL') AND
+    jsonb_array_length(CASE WHEN jsonb_typeof(infractions) = 'array' THEN infractions ELSE '[]' END) > 0
+    ),
 
-controls_infractions_details AS (
+    controls_infraction_natinfs_array AS (
     SELECT
-        id,
-        mission_infraction->>'natinf' AS infraction_natinf,
-        mission_infraction->>'infractionType' AS infraction_type,
-        mission_infraction->>'comments' AS infraction_comments
-    FROM controls_infractions
-),
-
-controls_infraction_natinf_category AS (
-    SELECT
-        controls_infractions_details.*,
-        infractions.infraction_category
-    FROM controls_infractions_details
-    LEFT JOIN infractions
-    ON infractions.natinf_code::VARCHAR = controls_infractions_details.infraction_natinf
-),
-
-controls_infraction_natinfs_array AS (
-    SELECT
-        id,
-        true AS infraction,
-        'Pêche' = ANY(ARRAY_AGG(infraction_category)) AS fishing_infraction,
-        ARRAY_AGG(DISTINCT infraction_category) FILTER (WHERE infraction_category IS NOT NULL) AS infraction_categories,
-        ARRAY_AGG(DISTINCT infraction_natinf) FILTER (WHERE infraction_natinf IS NOT NULL) AS infraction_natinfs,
-        ARRAY_AGG(DISTINCT infraction_type) FILTER (WHERE infraction_type IS NOT NULL) AS infraction_types,
-        STRING_AGG(NULLIF(infraction_comments, ''), ' - ') AS infraction_comments
-    FROM controls_infraction_natinf_category
+    id,
+    true AS infraction,
+    ARRAY_AGG(infraction_natinf) AS infraction_natinfs
+    FROM controls_infraction_natinf
     GROUP BY id
-)
+    )
 
 SELECT
     a.id,
-    a.vessel_id,
-    a.mission_id,
-    cu.id AS control_unit_id,
-    action_type AS control_type,
+    vessel_id,
+    mission_id,
+    action_type AS control_type, -- changement 'Contrôle à la mer' en 'SEA_CONTROL'
     action_datetime_utc AS control_datetime_utc,
-    EXTRACT(year FROM action_datetime_utc) AS control_year,
-    cu.name AS control_unit,
-    adm.name AS administration,
-    a.cfr,
-    a.ircs,
-    a.external_immatriculation,
-    a.vessel_name,
-    a.flag_state,
-    a.district_code,
     COALESCE(a.facade, 'Hors façade') AS facade,
-    COALESCE(a.longitude, ports.longitude) AS longitude,
-    COALESCE(a.latitude, ports.latitude) AS latitude,
+    longitude,
+    latitude,
     port_locode,
-    ports.region AS port_department,
-    ports.port_name,
     vessel_targeted,
-    COALESCE(inf.infraction, false) AS infraction,
-    COALESCE(inf.fishing_infraction, false) AS fishing_infraction,
-    COALESCE('WITH_RECORD' = ANY(inf.infraction_types), false) AS infraction_report,
-    COALESCE(infraction_categories, '{Aucune infraction}'::VARCHAR[]) AS infraction_categories,
-    COALESCE(infraction_natinfs, '{Aucune infraction}'::VARCHAR[]) AS infraction_natinfs,
-    COALESCE(seizure_and_diversion, false) AS seizure_and_diversion,
-    COALESCE(inf.infraction_comments, '') AS infraction_comments,
-    COALESCE(segment->>'segment', 'Hors segment') AS segment
+    inf.infraction,
+    inf.infraction_natinfs,
+    seizure_and_diversion,
+    seizure_and_diversion_comments,
+    other_comments,
+    gear_onboard,
+    species_onboard,
+    CASE
+        WHEN (
+            segments = '[]' OR
+            segments IS NULL OR
+            segments = 'null'
+            ) THEN '[{"segment": "Hors segment", "segmentName": "Hors segment"}]'
+        ELSE segments
+        END AS segments,
+    fao_areas
 FROM mission_actions a
-LEFT JOIN LATERAL jsonb_array_elements(CASE WHEN jsonb_typeof(segments) = 'array' THEN segments ELSE '[]' END) AS segment on true
-LEFT JOIN controls_infraction_natinfs_array inf ON inf.id = a.id
-LEFT JOIN ports ON ports.locode = a.port_locode
-JOIN analytics_missions m ON a.mission_id = m.id
-LEFT JOIN analytics_missions_control_units mcu ON m.id = mcu.mission_id
-LEFT JOIN analytics_control_units cu ON mcu.control_unit_id = cu.id
-LEFT JOIN analytics_administrations adm ON cu.administration_id = adm.id
-WHERE
-    NOT a.is_deleted AND
-    NOT m.deleted AND
-    action_type IN ('SEA_CONTROL', 'LAND_CONTROL', 'AIR_CONTROL')
-ORDER BY action_datetime_utc;
+         LEFT JOIN controls_infraction_natinfs_array inf
+                   ON inf.id = a.id
+         JOIN analytics_missions m ON a.mission_id = m.id
+WHERE action_type IN ('SEA_CONTROL', 'LAND_CONTROL', 'AIR_CONTROL') AND NOT a.is_deleted AND NOT m.deleted;
 
 CREATE MATERIALIZED VIEW public.analytics_controls_full_data AS
 

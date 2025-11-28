9b36d6d7adfcb7422d6cd53b5783e0b96d8ca5c5
Vincent <vincent.chery@m4x.org>
2025-11-28 15:50:09 +0100

    Fix make_infractions test

--- pipeline/tests/test_helpers/test_controls.py
+++ pipeline/tests/test_helpers/test_controls.py
@@ -1,5 +1,3 @@
-import pytest
-
 from src.entities.missions import InfractionType
 from src.helpers.controls import make_infractions
 
@@ -10,29 +8,9 @@ def test_make_infractions():
         {"natinf": 2, "infractionType": "WITH_RECORD"},
         {"natinf": 3, "infractionType": "WITH_RECORD"},
     ]
-    assert make_infractions(
-        {1, 2, 3}, InfractionType.WITHOUT_RECORD, only_natinfs={2, 5, 6, 1}
-    ) == [
+    assert make_infractions({1, 2}, InfractionType.WITHOUT_RECORD) == [
         {"natinf": 1, "infractionType": "WITHOUT_RECORD"},
         {"natinf": 2, "infractionType": "WITHOUT_RECORD"},
     ]
-    assert make_infractions(
-        {1, 2, 3}, InfractionType.WITH_RECORD, exclude_natinfs={2, 5, 6, 1}
-    ) == [{"natinf": 3, "infractionType": "WITH_RECORD"}]
-    assert (
-        make_infractions({}, InfractionType.WITH_RECORD, exclude_natinfs={2, 5, 6, 1})
-        == []
-    )
-    assert (
-        make_infractions({}, InfractionType.WITH_RECORD, only_natinfs={2, 5, 6, 1})
-        == []
-    )
     assert make_infractions({}, InfractionType.WITH_RECORD) == []
     assert make_infractions(None, InfractionType.WITH_RECORD) == []
-    with pytest.raises(ValueError):
-        make_infractions(
-            {1, 2, 3},
-            InfractionType.WITH_RECORD,
-            only_natinfs={2, 1},
-            exclude_natinfs={2, 5},
-        )

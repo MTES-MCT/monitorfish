b634a3eedc956a774eed16cf642aaa292aee29c7
Vincent <vincent.chery@m4x.org>
2025-12-03 17:20:43 +0100

    Fix migration

--- backend/src/main/resources/db/migration/internal/V0.340__Update_beacon_malfunctions_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.340__Update_beacon_malfunctions_table.sql
@@ -1,6 +1,6 @@
 CREATE INDEX IF NOT EXISTS beacon_malfunctions_unarchived_stage_idx
 ON beacon_malfunctions (stage)
-WHERE stage != 'archived';
+WHERE stage != 'ARCHIVED';
 
 CREATE INDEX IF NOT EXISTS beacon_malfunctions_vessel_id_idx
 ON beacon_malfunctions (vessel_id);
@@ -10,4 +10,4 @@ ON beacon_malfunctions (malfunction_start_date_utc);
 
 CREATE INDEX IF NOT EXISTS beacon_malfunctions_archived_vessel_status_last_modification_date_utc_idx
 ON beacon_malfunctions (vessel_status_last_modification_date_utc)
-WHERE stage = 'archived';
+WHERE stage = 'ARCHIVED';

b822c6e76f9555f388ac80376df62f5a4418f5a9
Vincent <vincent.chery@m4x.org>
2025-12-15 16:53:29 +0100

    Update threat_characterization_data_structure

--- backend/src/main/resources/db/migration/internal/V0.337__Create_infraction_threat_characterization_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.337__Create_infraction_threat_characterization_table.sql
@@ -1,14 +1,9 @@
 CREATE TABLE IF NOT EXISTS public.infraction_threat_characterization (
     id SERIAL PRIMARY KEY,
-    natinf_code INTEGER NOT NULL,
-    threat_id INTEGER NOT NULL,
-    threat_characterization_id INTEGER NOT NULL,
-    FOREIGN KEY (natinf_code) REFERENCES public.infractions(natinf_code),
-    FOREIGN KEY (threat_id) REFERENCES public.threats(id),
-    FOREIGN KEY (threat_characterization_id) REFERENCES public.threat_characterizations(id),
-    UNIQUE (natinf_code, threat_id, threat_characterization_id)
+    natinf_code INTEGER NOT NULL REFERENCES public.infractions(natinf_code),
+    threat_characterization_id INTEGER NOT NULL REFERENCES public.threat_characterizations(id),
+    UNIQUE (natinf_code, threat_characterization_id)
 );
 
 CREATE INDEX ON public.infraction_threat_characterization (natinf_code);
-CREATE INDEX ON public.infraction_threat_characterization (threat_id);
 CREATE INDEX ON public.infraction_threat_characterization (threat_characterization_id);

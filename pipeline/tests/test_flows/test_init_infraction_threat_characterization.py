6d172c456b69dff4b258c9b45985b4252b964dd5
Vincent <vincent.chery@m4x.org>
2025-12-16 10:46:12 +0100

    Update init_infraction_threat_characterization_flow

--- pipeline/tests/test_flows/test_init_infraction_threat_characterization.py
+++ pipeline/tests/test_flows/test_init_infraction_threat_characterization.py
@@ -1,17 +1,27 @@
 import pandas as pd
 
-from src.flows.init_infraction_threat_characterization import init_infraction_threat_characterization_flow
+from src.flows.init_infraction_threat_characterization import (
+    init_infraction_threat_characterization_flow,
+)
 from src.read_query import read_query
 
 
 def test_flow(reset_test_data):
     threats_query = "SELECT * FROM threats ORDER BY id"
-    threat_characterizations_query = "SELECT * FROM threat_characterizations ORDER BY id"
-    infraction_threat_characterization_query = "SELECT * FROM infraction_threat_characterization ORDER BY id"
+    threat_characterizations_query = (
+        "SELECT * FROM threat_characterizations ORDER BY id"
+    )
+    infraction_threat_characterization_query = (
+        "SELECT * FROM infraction_threat_characterization ORDER BY id"
+    )
 
     initial_threats = read_query(threats_query, db="monitorfish_remote")
-    initial_threat_characterizations = read_query(threat_characterizations_query, db="monitorfish_remote")
-    initial_infraction_threat_characterization = read_query(infraction_threat_characterization_query, db="monitorfish_remote")
+    initial_threat_characterizations = read_query(
+        threat_characterizations_query, db="monitorfish_remote"
+    )
+    initial_infraction_threat_characterization = read_query(
+        infraction_threat_characterization_query, db="monitorfish_remote"
+    )
 
     state = init_infraction_threat_characterization_flow(return_state=True)
     assert state.is_completed()
@@ -29,7 +39,7 @@ def test_flow(reset_test_data):
     assert len(threats_after_first_run) == 7
     assert len(initial_threat_characterizations) == 2
     assert len(threat_characterizations_after_first_run) == 76
-    assert len(initial_infraction_threat_characterization) == 3
+    assert len(initial_infraction_threat_characterization) == 2
     assert len(infraction_threat_characterization_after_first_run) == 120
 
     # Re-running should succeed and lead to the same data
@@ -46,8 +56,10 @@ def test_flow(reset_test_data):
 
     pd.testing.assert_frame_equal(threats_after_first_run, threats_after_second_run)
     pd.testing.assert_frame_equal(
-        threat_characterizations_after_first_run, threat_characterizations_after_second_run
+        threat_characterizations_after_first_run,
+        threat_characterizations_after_second_run,
     )
     pd.testing.assert_frame_equal(
-        infraction_threat_characterization_after_first_run, infraction_threat_characterization_after_second_run
+        infraction_threat_characterization_after_first_run,
+        infraction_threat_characterization_after_second_run,
     )

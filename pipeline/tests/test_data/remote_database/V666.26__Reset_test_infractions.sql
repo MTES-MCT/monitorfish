03c8b0afd41bccb2d22671e6f8e4270360b58b5d
Vincent <vincent.chery@m4x.org>
2025-12-16 11:08:03 +0100

    Fix test data

--- pipeline/tests/test_data/remote_database/V666.26__Reset_test_infractions.sql
+++ pipeline/tests/test_data/remote_database/V666.26__Reset_test_infractions.sql
@@ -1,3 +1,6 @@
+DELETE FROM public.infraction_threat_characterization;
+DELETE FROM public.threat_characterizations;
+DELETE FROM public.threats;
 DELETE FROM infractions;
 
 INSERT INTO infractions (
@@ -101,3 +104,23 @@ INSERT INTO infractions (
     (     32327, 'Reg test',   'Cat test', 'Infraction test'),
     (     33914, 'Reg test',   'Cat test', 'Infraction test'),
     (     34314, 'Reg test',   'Cat test', 'Infraction test');
+
+
+INSERT INTO public.threats (
+     id,            name) VALUES
+    ( 1, 'Test Threat 1'),
+    ( 2, 'Test Threat 2');
+
+ALTER SEQUENCE threats_id_seq RESTART WITH 3;
+
+INSERT INTO public.threat_characterizations (
+     id,                      name, threat_id) VALUES
+    ( 1, 'Test Characterization 1',         1),
+    ( 2, 'Test Characterization 2',         2);
+
+ALTER SEQUENCE threat_characterizations_id_seq RESTART WITH 3;
+
+INSERT INTO public.infraction_threat_characterization (
+     natinf_code, threat_characterization_id) VALUES
+    (      22206,                          1),
+    (      27724,                          2);

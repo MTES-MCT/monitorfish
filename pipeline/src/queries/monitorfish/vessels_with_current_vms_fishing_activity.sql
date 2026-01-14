6996a8a4b254cec6182ea242f29947e1ff2e036b
Vincent <vincent.chery@m4x.org>
2026-01-14 17:14:19 +0100

    Fix vessels with VMS activity query

--- pipeline/src/queries/monitorfish/vessels_with_current_vms_fishing_activity.sql
+++ pipeline/src/queries/monitorfish/vessels_with_current_vms_fishing_activity.sql
@@ -1,8 +1,21 @@
-SELECT DISTINCT internal_reference_number AS cfr
-FROM positions
+WITH latest_departures AS (
+    SELECT
+        internal_reference_number,
+        MAX(date_time -  make_interval(mins => (time_emitting_at_sea * 60)::INTEGER)) AS departure_datetime_utc
+    FROM  positions
+    WHERE
+        date_time > CURRENT_TIMESTAMP AT TIME ZONE 'UTC' - INTERVAL '2 days'
+        AND date_time < CURRENT_TIMESTAMP AT TIME ZONE 'UTC' + INTERVAL '1 hour'
+    GROUP BY 1
+)
+
+SELECT DISTINCT p.internal_reference_number AS cfr
+FROM positions p
+JOIN latest_departures d
+ON
+    p.internal_reference_number = d.internal_reference_number
+    AND p.date_time >= d.departure_datetime_utc
 WHERE
-    date_time >= NOW() AT TIME ZONE 'UTC' - INTERVAL '48 hours' AND
-    date_time <= NOW() AT TIME ZONE 'UTC' AND
-    is_fishing AND
-    EXTRACT(epoch FROM NOW() AT TIME ZONE 'UTC' - date_time) / 3600 < time_emitting_at_sea
-    AND internal_reference_number IS NOT NULL
+    date_time > CURRENT_TIMESTAMP AT TIME ZONE 'UTC' - INTERVAL '2 days'
+    AND date_time < CURRENT_TIMESTAMP AT TIME ZONE 'UTC' + INTERVAL '1 hour'
+    AND is_fishing

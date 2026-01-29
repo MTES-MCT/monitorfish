94c1c6d14ff721baad0d40c7d91f72aa675e22f7
Loup Theron <loup-rodney@hotmail.fr>
2026-01-29 10:49:18 +0100

    Update displayed vessel groups when filtered

--- frontend/src/features/VesselGroup/components/VesselGroupList/slice.ts
+++ frontend/src/features/VesselGroup/components/VesselGroupList/slice.ts
@@ -1,19 +1,14 @@
-import { GroupType, Sharing } from '@features/VesselGroup/types'
 import { createSlice } from '@reduxjs/toolkit'
 
 import type { PayloadAction } from '@reduxjs/toolkit'
 
 export type VesselGroupListState = {
   filteredExpired: boolean
-  filteredGroupType: GroupType | undefined
-  filteredSharing: Sharing | undefined
   searchQuery: string | undefined
 }
 
 export const INITIAL_STATE: VesselGroupListState = {
   filteredExpired: false,
-  filteredGroupType: undefined,
-  filteredSharing: undefined,
   searchQuery: undefined
 }
 
@@ -23,12 +18,6 @@ const vesselGroupListSlice = createSlice({
   reducers: {
     setFilteredExpired(state, action: PayloadAction<boolean>) {
       state.filteredExpired = action.payload
-    },
-    setFilteredGroupType(state, action: PayloadAction<GroupType | undefined>) {
-      state.filteredGroupType = action.payload
-    },
-    setFilteredSharing(state, action: PayloadAction<Sharing | undefined>) {
-      state.filteredSharing = action.payload
     },
     setSearchQuery(state, action: PayloadAction<string | undefined>) {
       state.searchQuery = action.payload
--- frontend/src/features/VesselGroup/components/VesselGroupMenuDialog/hooks/useGetVesselGroups.ts
+++ frontend/src/features/VesselGroup/components/VesselGroupMenuDialog/hooks/useGetVesselGroups.ts
@@ -1,39 +1,11 @@
 import { useGetAllVesselGroupsQuery } from '@features/VesselGroup/apis'
 import { GroupType, Sharing } from '@features/VesselGroup/types'
+import { getFilteredVesselGroups } from '@features/VesselGroup/utils/getFilteredVesselGroups'
 import { useMainAppSelector } from '@hooks/useMainAppSelector'
-import { customDayjs } from '@mtes-mct/monitor-ui'
 
 export function useGetVesselGroups(filteredGroupType: GroupType | undefined, filteredSharing: Sharing | undefined) {
   const vesselGroupsIdsPinned = useMainAppSelector(state => state.vesselGroup.vesselGroupsIdsPinned)
-
   const { data: vesselGroups } = useGetAllVesselGroupsQuery()
 
-  return (() => {
-    if (!vesselGroups?.length) {
-      return {
-        pinnedVesselGroups: [],
-        unpinnedVesselGroups: []
-      }
-    }
-
-    const filteredVesselGroups =
-      vesselGroups
-        ?.filter(vesselGroup => !filteredGroupType || vesselGroup.type === filteredGroupType)
-        ?.filter(vesselGroup =>
-          vesselGroup.endOfValidityUtc ? customDayjs(vesselGroup.endOfValidityUtc).isAfter(customDayjs(), 'day') : true
-        )
-        ?.filter(vesselGroup => !filteredSharing || vesselGroup.sharing === filteredSharing) ?? []
-
-    const pinnedVesselGroups = filteredVesselGroups.filter(vesselGroup =>
-      vesselGroupsIdsPinned.includes(vesselGroup.id)
-    )
-    const unpinnedVesselGroups = filteredVesselGroups.filter(
-      vesselGroup => !vesselGroupsIdsPinned.includes(vesselGroup.id)
-    )
-
-    return {
-      pinnedVesselGroups,
-      unpinnedVesselGroups
-    }
-  })()
+  return getFilteredVesselGroups(vesselGroups, filteredGroupType, filteredSharing, vesselGroupsIdsPinned)
 }

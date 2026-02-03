452e587dfba6a68c98d7ed1c28c48d0cb33616b6
Loup Theron <loup-rodney@hotmail.fr>
2026-02-03 17:32:07 +0100

    Fix infraction zod type

--- frontend/src/features/Infraction/schemas/InfractionSchema.ts
+++ frontend/src/features/Infraction/schemas/InfractionSchema.ts
@@ -1,8 +1,10 @@
 import { z } from 'zod'
 
+import { stringOrUndefined } from '../../../types'
+
 export const InfractionSchema = z.strictObject({
   infraction: z.string(),
   infractionCategory: z.string(),
   natinfCode: z.number(),
-  regulation: z.string()
+  regulation: stringOrUndefined
 })

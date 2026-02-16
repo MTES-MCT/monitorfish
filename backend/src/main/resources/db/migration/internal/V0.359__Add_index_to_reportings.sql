4021856379a106741b76ec009685d174005e403d
Loup Theron <loup-rodney@hotmail.fr>
2026-02-16 18:04:21 +0100

    Update indexes

--- backend/src/main/resources/db/migration/internal/V0.359__Add_index_to_reportings.sql
+++ backend/src/main/resources/db/migration/internal/V0.359__Add_index_to_reportings.sql
@@ -1,3 +1,8 @@
-CREATE INDEX ON public.reportings (archived, deleted);
-CREATE INDEX ON public.reportings (archived, deleted, internal_reference_number);
-CREATE INDEX ON public.reportings (archived, deleted, vessel_id);
+CREATE INDEX reportings_all_active_idx
+    ON reportings (id)
+    INCLUDE (type, vessel_id, internal_reference_number)
+WHERE archived = false AND deleted = false;
+
+CREATE INDEX ON public.reportings (internal_reference_number);
+
+CREATE INDEX ON public.reportings (vessel_id);

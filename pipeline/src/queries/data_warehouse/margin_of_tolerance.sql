84949d34e131cc782e18b3d1f9cd2b8bc7ee2cb0
Vincent <vincent.chery@m4x.org>
2026-03-04 17:59:47 +0100

    Add margin of tolerance query

---
+++ pipeline/src/queries/data_warehouse/margin_of_tolerance.sql
@@ -0,0 +1,84 @@
+WITH estimated_live_weights AS (
+    SELECT cfr, trip_number, species, SUM(weight) AS elw_weight
+    FROM monitorfish.catches
+    WHERE far_datetime_utc >= NOW() - Interval '1 year' AND flag_state = 'FRA'
+    GROUP BY cfr, trip_number, species
+),
+
+live_weight_equivalents AS (
+    SELECT
+        cfr,
+        trip_number,
+        species,
+        SUM(weight * COALESCE(conversion_factor, 1)) AS lwe_weight
+    FROM monitorfish.landings
+    WHERE landing_datetime_utc >= NOW() - Interval '1 year' AND flag_state = 'FRA'
+    GROUP BY cfr, trip_number, species
+),
+
+live_weight_equivalents_top_5 AS (
+    SELECT
+        *,
+        CASE
+            WHEN lwe_weight <= 100 THEN 0.8 * lwe_weight
+            ELSE 0.9 * lwe_weight
+        END AS min_allowed_elw,
+        CASE
+            WHEN lwe_weight <= 100 THEN 1.2 * lwe_weight
+            ELSE 1.1 * lwe_weight
+        END AS max_allowed_elw
+    FROM live_weight_equivalents
+    WINDOW win AS (PARTITION BY cfr, trip_number ORDER BY lwe_weight DESC)
+    QUALIFY
+        ROW_NUMBER() OVER win <= 5
+        AND lwe_weight / (SUM(lwe_weight) OVER (PARTITION BY cfr, trip_number)) >= 0.1
+),
+
+ok_trips AS (
+    SELECT
+        lwe.cfr AS cfr,
+        lwe.trip_number AS trip_number
+    FROM live_weight_equivalents_top_5 lwe
+    LEFT JOIN estimated_live_weights elw
+    ON 
+        elw.cfr = lwe.cfr AND
+        elw.trip_number = lwe.trip_number AND
+        elw.species = lwe.species
+    GROUP BY lwe.cfr, lwe.trip_number
+    HAVING Min(elw_weight BETWEEN min_allowed_elw AND max_allowed_elw) = 1
+),
+
+nb_ok_trips AS (
+    SELECT
+        cfr,
+        COUNT(*) AS nb_trips
+    FROM ok_trips
+    GROUP BY cfr
+),
+
+all_trips AS (
+    SELECT DISTINCT cfr, trip_number
+    FROM monitorfish.catches
+    WHERE far_datetime_utc >= NOW() - Interval '1 year' AND flag_state = 'FRA'
+    UNION ALL
+    SELECT DISTINCT cfr, trip_number
+    FROM monitorfish.landings
+    WHERE landing_datetime_utc >= NOW() - Interval '1 year' AND flag_state = 'FRA'
+),
+
+nb_total_trips AS (
+    SELECT
+        cfr,
+        COUNT(DISTINCT trip_number) AS nb_trips
+    FROM all_trips
+    GROUP BY cfr
+)
+
+SELECT
+    tt.cfr,
+    tt.nb_trips AS nb_total_trips,
+    tt.nb_trips - okt.nb_trips AS nb_nok_trips,
+    (tt.nb_trips - okt.nb_trips) / tt.nb_trips AS nct_over_tt
+FROM nb_total_trips tt
+LEFT JOIN nb_ok_trips okt
+ON tt.cfr = okt.cfr

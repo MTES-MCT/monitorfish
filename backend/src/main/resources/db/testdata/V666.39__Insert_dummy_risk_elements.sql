0cb41e6620bc8a5c3581efb994546577e4f60d59
Vincent <vincent.chery@m4x.org>
2026-03-04 17:59:19 +0100

    Add risk_elements tables

---
+++ backend/src/main/resources/db/testdata/V666.39__Insert_dummy_risk_elements.sql
@@ -0,0 +1,7 @@
+INSERT INTO risk_elements (
+    code, name, threat_characterization_id
+) VALUES 
+    ('PNO_MR', 'Prior notification misrecording', 60),
+    ('MOT_MR', 'Exceeding the margin of tolerance', 56),
+    ('VMS_MR', 'Unjustified VMS failure', 26),
+    ('CLA_CM', 'Fishing in closed area', 28);

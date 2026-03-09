86160dd22177ccb8dae3aea7ad9ebec96a63269b
Vincent <vincent.chery@m4x.org>
2026-03-09 09:41:55 +0100

    Fix migration

--- backend/src/main/resources/db/migration/internal/V0.366__Create_vessels_risk_elements_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.366__Create_vessels_risk_elements_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE vessels_risk_elements (
     risk_element_code VARCHAR NOT NULL REFERENCES risk_elements (code),
     metrics JSONB NOT NULL,
     risk_level INTEGER NOT NULL,
-    CONSTRAINT vessels_risk_elements_pkey PRIMARY KEY AS (cfr, risk_element_code)
+    CONSTRAINT vessels_risk_elements_pkey PRIMARY KEY (cfr, risk_element_code)
 );
 
 CREATE INDEX vessels_risk_elements_cfr_key ON vessels_risk_elements(cfr);

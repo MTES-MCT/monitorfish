a8df3ab095cfeec81dacea3b21717c116d79660f
Vincent <vincent.chery@m4x.org>
2026-03-09 16:17:05 +0100

    Reorder columns

--- backend/src/main/resources/db/migration/internal/V0.366__Create_vessels_risk_elements_table.sql
+++ backend/src/main/resources/db/migration/internal/V0.366__Create_vessels_risk_elements_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE vessels_risk_elements (
-    cfr VARCHAR(12) NOT NULL,
     risk_element_code VARCHAR NOT NULL REFERENCES risk_elements (code),
+    cfr VARCHAR(12) NOT NULL,
     metrics JSONB NOT NULL,
     risk_level INTEGER NOT NULL,
     CONSTRAINT vessels_risk_elements_pkey PRIMARY KEY (cfr, risk_element_code)

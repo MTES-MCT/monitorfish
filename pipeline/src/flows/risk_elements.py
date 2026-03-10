6420a0a71a0e01e3ac198846125f0aea4c0316e0
Vincent <vincent.chery@m4x.org>
2026-03-10 15:33:36 +0100

    Add fishing in closed areas risk element

--- pipeline/src/flows/risk_elements.py
+++ pipeline/src/flows/risk_elements.py
@@ -22,6 +22,39 @@ def extract_total_trips(from_datetime_utc: datetime) -> pd.DataFrame:
 
 
 @task
+def extract_vms_vessels(from_datetime_utc: datetime) -> pd.DataFrame:
+    return extract(
+        db_name="data_warehouse",
+        query_filepath="data_warehouse/vms_vessels.sql",
+        params={
+            "from_datetime_utc": from_datetime_utc,
+        },
+    )
+
+
+@task
+def extract_reportings(
+    risk_element: RiskElement, from_datetime_utc: datetime
+) -> pd.DataFrame:
+    queries = {
+        RiskElement.CLA_CM: (
+            "monitorfish_remote",
+            "monitorfish/fishing_in_closed_areas.sql",
+        )
+    }
+
+    db_name, query_filepath = queries[risk_element]
+
+    return extract(
+        db_name=db_name,
+        query_filepath=query_filepath,
+        params={
+            "from_datetime_utc": from_datetime_utc,
+        },
+    )
+
+
+@task
 def extract_compliant_trips(
     risk_element: RiskElement, from_datetime_utc: datetime
 ) -> pd.DataFrame:
@@ -38,6 +71,35 @@ def extract_compliant_trips(
             "from_datetime_utc": from_datetime_utc,
         },
     )
+
+
+@task
+def compute_reportings_vessels_risk_elements(
+    vessels: pd.DataFrame, reportings: pd.DataFrame, risk_element: RiskElement
+):
+    res = duckdb.sql(
+        """
+        SELECT
+            v.cfr,
+            COALESCE(r.nb_reportings, 0) AS nb_reportings
+        FROM vessels v
+        LEFT JOIN reportings r
+        ON v.cfr = r.cfr
+    """
+    ).to_df()
+
+    res["metrics"] = df_to_dict_series(
+        res[["nb_reportings"]],
+        result_colname="metrics",
+    )
+
+    # 0 reportings = 1, 1 reporting = 2, 2 or 3 reportings = 3, 4+ reportings = 4
+    risk_thresholds = [-0.1, 0.5, 1.5, 3.5, 1000000]
+    res["risk_level"] = pd.cut(res.nb_reportings, risk_thresholds, labels=False) + 1
+
+    res["risk_element_code"] = risk_element.value
+
+    return res[["risk_element_code", "cfr", "metrics", "risk_level"]]
 
 
 @task
@@ -97,13 +159,23 @@ def risk_elements_flow():
     from_datetime_utc = today - make_relativedelta(years=1)
 
     total_trips = extract_total_trips(from_datetime_utc=from_datetime_utc)
+    vms_vessels = extract_vms_vessels(from_datetime_utc=from_datetime_utc)
+
     mot_mr_compliant_trips = extract_compliant_trips(
         RiskElement.MOT_MR, from_datetime_utc=from_datetime_utc
     )
+    cla_cm_reportings = extract_reportings(
+        RiskElement.CLA_CM, from_datetime_utc=from_datetime_utc
+    )
 
+    # Transform
     mot_mr_vessels_risk_elements = compute_vessels_risk_elements(
         total_trips, mot_mr_compliant_trips, RiskElement.MOT_MR
+    )
+    cla_cm_vessels_risk_elements = compute_reportings_vessels_risk_elements(
+        vms_vessels, cla_cm_reportings, RiskElement.CLA_CM
     )
 
     # Load
     load_vessels_risk_elements(mot_mr_vessels_risk_elements)
+    load_vessels_risk_elements(cla_cm_vessels_risk_elements)

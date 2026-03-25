91beddcd8024657280e47180158f9ee461a34d27
Loup Theron <loup-rodney@hotmail.fr>
2026-03-25 07:49:09 +0100

    Rollback close group

--- frontend/cypress/e2e/side_window/vessel_group/list.spec.ts
+++ frontend/cypress/e2e/side_window/vessel_group/list.spec.ts
@@ -99,7 +99,6 @@ context('Side Window > Vessel Group List', () => {
     cy.get('input[placeholder="Rechercher un navire pour l\'ajouter au groupe"]').scrollIntoView().type('POINT')
     cy.contains('mark', 'POINT').click()
     cy.wait(500)
-    cy.contains('Mission Thémis – semaine 04').click()
     cy.get('[title="Mission Thémis – semaine 04"]').within(() => {
       cy.get('.Table-SimpleTable tr').should('have.length', 7)
     })

b6875c74cc5c36b409124b285e10f9949c112601
Loup Theron <loup-rodney@hotmail.fr>
2026-05-18 09:02:29 +0200

    Fix last position ais search

--- frontend/cypress/e2e/vessels/vessel_search.spec.ts
+++ frontend/cypress/e2e/vessels/vessel_search.spec.ts
@@ -38,12 +38,12 @@ context('VesselSearch', () => {
     cy.intercept('GET', '/bff/v1/vessels/ais*').as('aisVessels')
     cy.clickButton('AIS')
     cy.wait('@aisVessels')
-    cy.get('*[data-cy^="VesselSearch-input"]', { timeout: 10000 }).clear().type('VAN TILLMAN')
+    cy.get('*[data-cy^="VesselSearch-input"]', { timeout: 10000 }).clear().type('JANET TILLMAN')
     cy.wait(500)
-    cy.get('*[data-cy^="VesselSearch-item"]', { timeout: 10000 }).contains('VAN TILLMAN')
+    cy.get('*[data-cy^="VesselSearch-item"]', { timeout: 10000 }).contains('JANET TILLMAN')
 
     // Clicking an AIS result zooms the map but does not open the sidebar
-    cy.get('*[data-cy^="VesselSearch-item"]', { timeout: 10000 }).contains('VAN TILLMAN').click()
+    cy.get('*[data-cy^="VesselSearch-item"]', { timeout: 10000 }).contains('JANET TILLMAN').click()
     cy.get('*[data-cy^="vessel-sidebar"]').should('not.exist')
     cy.get('*[data-cy^="VesselSearch-input"]').should('have.value', '')
   })

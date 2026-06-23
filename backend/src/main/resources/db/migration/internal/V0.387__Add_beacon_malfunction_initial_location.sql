9cca6df954105efec45e0320b3c5bca32747a604
Vincent <vincent.chery@m4x.org>
2026-06-23 17:24:01 +0200

    Change the approach to filter which malfunctions are followed

--- backend/src/main/resources/db/migration/internal/V0.387__Add_beacon_malfunction_initial_location.sql
+++ backend/src/main/resources/db/migration/internal/V0.387__Add_beacon_malfunction_initial_location.sql
@@ -1,4 +1,2 @@
-CREATE TYPE beacon_malfunction_initial_location AS ENUM ('AT_SEA', 'AT_PORT');
-
 ALTER TABLE beacon_malfunctions
-    ADD COLUMN beacon_malfunction_initial_location beacon_malfunction_initial_location NOT NULL DEFAULT 'AT_SEA';
+    ADD COLUMN is_followed BOOLEAN NOT NULL DEFAULT true;

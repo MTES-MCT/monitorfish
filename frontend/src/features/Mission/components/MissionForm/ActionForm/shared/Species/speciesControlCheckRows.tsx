5e75f567e2cc65c5656b464b0918ddfc95c87a15
Loup Theron <loup-rodney@hotmail.fr>
2026-08-24 17:10:05 +0200

    Remove species weight controlled on land controls

--- frontend/src/features/Mission/components/MissionForm/ActionForm/shared/Species/speciesControlCheckRows.tsx
+++ frontend/src/features/Mission/components/MissionForm/ActionForm/shared/Species/speciesControlCheckRows.tsx
@@ -6,7 +6,6 @@ import type { SpeciesEISRApplicability } from './getSpeciesEISRApplicability'
 import type { ControlCheckRow } from '../ControlCheckTable'
 
 const BASE_SPECIES_CHECK_ROWS: ControlCheckRow[] = [
-  { isRequired: true, label: 'Poids des espèces vérifiés', name: 'speciesWeightControlled' },
   { isRequired: true, label: 'Taille des espèces vérifiées', name: 'speciesSizeControlled' },
   {
     isRequired: true,
@@ -58,6 +57,7 @@ const LAND_CONTROL_NOT_LANDED_CHECK_ROWS: ControlCheckRow[] = [
 ]
 
 const SEA_CONTROL_EISR_CHECK_ROWS: ControlCheckRow[] = [
+  { isRequired: true, label: 'Poids des espèces vérifiés', name: 'speciesWeightControlled' },
   {
     isRequired: true,
     label: "Arrimage séparé des poissons n'ayant pas la taille requise",
